e0aca307a9b2aeea32c4b5bfd9ff3bd2a865727a
Peter W. Draper <p.w.draper@durham.ac.uk>
2002-07-24 17:56:08 +0000

    Added Starlink tasks and types.

---
+++ ant/src/main/uk/ac/starlink/ant/types/ExtClasspath.java
@@ -0,0 +1,152 @@
+package uk.ac.starlink.ant.types;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.jar.Attributes;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
+
+import org.apache.tools.ant.AntClassLoader;
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.DirectoryScanner;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.ProjectHelper;
+import org.apache.tools.ant.types.Path;
+import org.apache.tools.ant.types.Reference;
+
+/**
+ * Create a path-like structure to contain all the given jar files and
+ * those that are specified as bundled optional packages in their
+ * manifests. This generates a path that is equivalent to the path
+ * used by the extension mechanism classloader when using the given
+ * list of jar files.
+ * <p>
+ * The list of jar files can be given as either a reference to a
+ * path-like structure, or by using embedded path elements.
+ * <p>
+ * Note that the java compiler (javac) does not perform this tracking 
+ * down of bundled jar files (it's just done by the JVM), so this type
+ * is of particular use when compiling.
+ *
+ * @author Peter W. Draper
+ * @since $Id$
+ */
+public class ExtClasspath extends Path
+{
+    protected String name;
+    protected StringBuffer value;
+    protected Path path;
+    protected Path resultPath;
+    protected Reference ref;
+
+    public ExtClasspath( Project p )
+    {
+        super( p );
+    }
+
+    public ExtClasspath( Project p, String path )
+    {
+        super( p, path );
+    }
+
+    /**
+     * Returns all path elements defined by this and nested path objects.
+     *
+     * @return list of path elements.
+     */
+    public String[] list()
+    {
+        //  Extend any jar files to include their bundled optional
+        //  references and in turn any references in these new jars
+        //  files until path is complete and all bundled optional jars
+        //  have been located and then proceed as normal.
+        String[] jars = super.list();
+        for ( int i = 0; i < jars.length; i++ ) {
+            File jarFile = project.resolveFile( jars[i] );
+            if ( jarFile.exists() ) {
+                if ( "jar".equals( getExtension( jarFile ) ) ) {
+                    addDownloads( jarFile );
+                }
+            }
+        }
+        return super.list();
+    }
+
+    /**
+     * Add the downloads from a jar file to the path. The downloads
+     * are also checked for and downloads they reference.
+     */
+    protected void addDownloads( File jarFile )
+    {
+        File jarBase = jarFile.getParentFile();
+        File newJarFile = null;
+        String s[] = getDownloads( jarFile );
+        if ( s != null && s.length > 0 ) {
+            for ( int j = 0; j < s.length; j++ ) {
+                newJarFile = new File( jarBase, s[j] );
+
+                if ( newJarFile.exists() ) {
+                    if ( "jar".equals( getExtension( newJarFile ) ) ) {
+                        setLocation( newJarFile );
+                        addDownloads( newJarFile );
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Extract any bundled optional packages from a jar fils's
+     * manifest and return them as an array of Strings.
+     */
+    protected String[] getDownloads( File jarFile )
+    {
+        ArrayList results = new ArrayList();
+
+        JarFile jarFileFile = null;
+        Manifest manifest = null;
+        try {
+            jarFileFile = new JarFile( jarFile );
+            manifest = jarFileFile.getManifest();
+            if ( manifest == null ) {
+                return null;
+            }
+        }
+        catch (Exception e) {
+            return null;
+        }
+        Attributes attributes = manifest.getMainAttributes();
+
+        Attributes.Name pathKey = Attributes.Name.CLASS_PATH;
+        String path = attributes.getValue( pathKey );
+        if ( path != null ) {
+
+            //  Needs splitting into parts (space separated ).
+            StringTokenizer tokenizer = new StringTokenizer( path, " " );
+            String[] result = new String[ tokenizer.countTokens() ];
+            for( int i = 0; i < result.length; i++ ) {
+                results.add( tokenizer.nextToken() );
+            }
+        }
+        return (String []) results.toArray( new String[0] );
+    }
+
+    /**
+     * Get a file extension.
+     */
+    public String getExtension( File file )
+    {
+        if ( file != null ) {
+            String filename = file.getName();
+            int i = filename.lastIndexOf( '.' );
+            if( i > 0 && i < filename.length() - 1 ) {
+                return filename.substring( i + 1 ).toLowerCase();
+            }
+        }
+        return null;
+    }
+}
