d6894a7f0f6c029c893696e6b303b6a78a86a020
Mark Taylor <m.b.taylor@bristol.ac.uk>
2002-09-04 14:07:46 +0000

    Modified to write data (as an MEF) when called upon to store a virtual NDX as XML, rather than writing a data-less NDX.

--- ndx/src/main/uk/ac/starlink/ndx/XMLNdxHandler.java
+++ ndx/src/main/uk/ac/starlink/ndx/XMLNdxHandler.java
@@ -9,6 +9,7 @@ import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Iterator;
 import java.util.logging.Logger;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Source;
@@ -20,10 +21,15 @@ import org.w3c.dom.CharacterData;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.w3c.dom.Text;
 import uk.ac.starlink.array.AccessMode;
+import uk.ac.starlink.array.ArrayBuilder;
 import uk.ac.starlink.array.NDArray;
+import uk.ac.starlink.array.NDArrays;
 import uk.ac.starlink.array.NDArrayFactory;
+import uk.ac.starlink.array.NDShape;
+import uk.ac.starlink.array.Type;
 import uk.ac.starlink.ast.FrameSet;
 import uk.ac.starlink.ast.xml.XAstReader;
 import uk.ac.starlink.util.SourceReader;
@@ -131,7 +137,7 @@ public class XMLNdxHandler implements NdxHandler {
             if ( child instanceof Element ) {
                 Element cel = (Element) child;
                 String tagname = cel.getTagName();
-                if ( tagname.equals( "image" ) || tagname.equals( "data" ) ) {
+                if ( tagname.equals( "image" ) ) {
                     image = makeNDArray( cel, AccessMode.READ );
                 }
                 else if ( tagname.equals( "variance" ) ) {
@@ -185,12 +191,14 @@ public class XMLNdxHandler implements NdxHandler {
     private NDArray makeNDArray( Element el, AccessMode mode )
             throws IOException {
         URL url;
+        String loc = getTextContent( el );
         try {
-            url = new URL( getTextContent( el ) );
+            url = new URL( loc );
         }
         catch ( MalformedURLException e ) {
             throw (IOException) 
-                  new IOException( "Bad location for " + el.getTagName() )
+                  new IOException( "Bad location for " + el.getTagName() 
+                                 + ": " + loc )
                  .initCause( e );
         }
         return arrayfact.makeNDArray( url, mode );
@@ -246,71 +254,181 @@ public class XMLNdxHandler implements NdxHandler {
         return new BridgeNdx( impl );
     }
     
-
-    /**
-     * Provides an XML Source representation of an existing Ndx object.
-     * <p>
-     * This method, which just invokes {@link Ndx#toXML}, is only really
-     * provided for symmetry with {@link
-     * #makeNdx(javax.xml.transform.Source)}.
-     *
-     * @param  ndx  the Ndx object to turn into XML
-     */
-    public Source outputNdx( Ndx ndx ) {
-        return ndx.toXML();
-    }
-
     /**
      * Writes an XML representation of an existing Ndx object to the given 
-     * (writable) URL.
+     * (writable) URL.  
+     * <p>
+     * If any of the array components is virtual, then the array data
+     * needs to be output too.  Currently, for a URL 'blah.xml'
+     * these are written into successive components of a new FITS file
+     * 'blah-data.fits'.  If the FITS handlers are not installed,
+     * or <tt>url</tt> does not end in '.xml' an IOException will be thrown.
+     * This behaviour may be subject to change in future releases.
      *
-     * @param  url  the URL to which the Ndx is to be serialised
+     * @param  xurl the URL to which the Ndx is to be serialised in XML
      * @param  ndx  the Ndx object to serialise
      * @throws java.net.UnknownServiceException if the URL does not support
      *         output (most protocols, apart from <tt>file</tt>, don't)
      * @throws IOException  if some other I/O error occurs, 
      */
-    public boolean outputNdx( URL url, Ndx ndx ) throws IOException {
-        OutputStream ostrm;
-        if ( url.toString().equals( "file:-" ) ) {
-            ostrm = System.out;
-        }
-        else if ( isXmlUrl( url ) ) {
-            if ( url.getProtocol().equals( "file" ) ) {
-                String filename = url.getPath();
-                ostrm = new FileOutputStream( filename );
+    public boolean outputNdx( URL xurl, Ndx ndx ) throws IOException {
+
+        /* If we need one, get a URL for writing array data. */
+        URL aurl = null;
+        ArrayBuilder fab = null;
+        if ( ! ndx.isPersistent() ) {
+            if ( xurl.toString().endsWith( ".xml" ) ) {
+                String aloc = xurl.toString()
+                             .replaceFirst( ".xml$", "-data.fits" );
+                try {
+                    aurl = new URL( aloc );
+                }
+                catch ( MalformedURLException e ) {
+                    throw new AssertionError();
+                }
             }
             else {
-                URLConnection conn = url.openConnection();
-                conn.setDoInput( false );
-                conn.setDoOutput( true );
+                throw new IOException( "Cannot write non-persistent NDX to " +
+                                       "URL not ending in '.xml': " + xurl );
+            }
+
+            /* Get hold of a FitsArrayBuilder, or throw an exception if
+             * we don't have one. */
+            for ( Iterator it = arrayfact.getBuilders().iterator();
+                  it.hasNext(); ) {
+                ArrayBuilder builder = (ArrayBuilder) it.next();
+                if ( builder.getClass().getName()
+                    .equals( "uk.ac.starlink.fits.FitsArrayBuilder" ) ) {
+                    fab = builder;
+                    break;
+                }
+            }
+            if ( fab == null ) {
+                throw new IOException( "Cannot write non-persistent NDX; " +
+                                       "FITS package not installed" );
+            }
+        }
+        boolean writeArrays = ( aurl != null );
+
+        /* Get XML output stream. */
+        OutputStream xstrm;
+        if ( xurl.toString().equals( "file:-" ) ) {
+            if ( writeArrays ) {
+                throw new IOException( "Cannot serialise non-persistent NDX " 
+                                     + "to a stream" );
+            }
+            xstrm = System.out;
+        }
+        else if ( isXmlUrl( xurl ) ) {
+            if ( xurl.getProtocol().equals( "file" ) ) {
+                xstrm = new FileOutputStream( xurl.getPath() );
+            }
+            else {
+                URLConnection xconn = xurl.openConnection();
+                xconn.setDoInput( false );
+                xconn.setDoOutput( true );
     
                 /* The following may throw a java.net.UnknownServiceException
                  * (which is-a IOException) - in fact it almost certiainly will,
                  * since I don't know of any URL protocols (including file)
                  * which support output streams. */
-                conn.connect();
-                ostrm = conn.getOutputStream();
+                xconn.connect();
+                xstrm = xconn.getOutputStream();
             }
-            ostrm = new BufferedOutputStream( ostrm );
+            xstrm = new BufferedOutputStream( xstrm );
         }
         else {
             return false;
         }
-        SourceReader sr = new SourceReader();
 
+        /* Get an XML source representing the XML to be written. */
+        Source xsrc;
+        if ( ! writeArrays ) {
+            xsrc = ndx.toXML();
+        }
+        else {
+            int hdu = 0;
+            URL iurl;
+            URL vurl;
+            URL qurl;
+
+            Node ndxel;
+            try {
+                ndxel = new SourceReader().getDOM( ndx.toXML() );
+            }
+            catch ( TransformerException e ) {
+                throw (IOException) new IOException( e.getMessage() )
+                                   .initCause( e );
+            }
+ 
+            iurl = new URL( aurl, "#" + ++hdu );
+            NDArray inda1= ndx.getImage();
+            NDArray inda2 = fab.makeNewNDArray( iurl, inda1.getShape(),
+                                                inda1.getType() );
+            NDArrays.copy( inda1, inda2 );
+
+            if ( ndx.hasVariance() ) {
+                vurl = new URL( aurl, "#" + ++hdu );
+                NDArray vnda1 = ndx.getVariance();
+                NDArray vnda2 = fab.makeNewNDArray( vurl, vnda1.getShape(),
+                                                    vnda1.getType() );
+                NDArrays.copy( vnda1, vnda2 );
+            }
+            else {
+                vurl = null;
+            }
+
+            if ( ndx.hasQuality() ) {
+                qurl = new URL( aurl, "#" + ++hdu );
+                NDArray qnda1 = ndx.getQuality();
+                NDArray qnda2 = fab.makeNewNDArray( qurl, qnda1.getShape(),
+                                                    qnda1.getType() );
+                NDArrays.copy( qnda1, qnda2 );
+            }
+            else {
+                qurl = null;
+            }
+
+            Document doc = ndxel.getOwnerDocument();
+            NodeList children = ndxel.getChildNodes();
+            for ( int i = 0; i < children.getLength(); i++ ) {
+                Node node = children.item( i );
+                if ( node instanceof Element &&
+                     node.getNodeName().equals( "image" ) ) {
+                    Node inode = doc.createElement( node.getNodeName() );
+                    inode.appendChild( doc.createTextNode( iurl.toString() ) );
+                    ndxel.replaceChild( inode, node );
+                }
+                if ( node instanceof Element &&
+                     node.getNodeName().equals( "variance" ) ) {
+                    Node vnode = doc.createElement( node.getNodeName() );
+                    vnode.appendChild( doc.createTextNode( vurl.toString() ) );
+                    ndxel.replaceChild( vnode, node );
+                }
+                if ( node instanceof Element &&
+                     node.getNodeName().equals( "quality" ) ) {
+                    Node qnode = doc.createElement( node.getNodeName() );
+                    qnode.appendChild( doc.createTextNode( qurl.toString() ) );
+                    ndxel.replaceChild( qnode, node );
+                }
+            }
+            xsrc = new DOMSource( ndxel );
+        }
+
+        /* Write the XML to the XML stream. */
+        SourceReader sr = new SourceReader();
         // this bit isn't safe - can't guarantee the transformer will be
         // invoked, so we may get no declaration after all.
         sr.setIncludeDeclaration( true );
         sr.setIndent( 2 );
         try { 
-            sr.writeSource( outputNdx( ndx ), ostrm );
+            sr.writeSource( xsrc, xstrm );
         }
         catch ( TransformerException e ) {
             throw (IOException) new IOException( "Trouble writing XML" )
                                .initCause( e );
         }
-        ostrm.close();
+        xstrm.close();
         return true;
     }
 
