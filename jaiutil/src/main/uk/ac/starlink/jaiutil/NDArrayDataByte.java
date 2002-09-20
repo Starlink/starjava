20c1dbf7fd1bc6e24501e06dc4c2aec97f87e220
Peter W. Draper <p.w.draper@durham.ac.uk>
2002-09-20 12:11:40 +0000

    Initial JAIUtil source code checkin

---
+++ jaiutil/src/main/uk/ac/starlink/jaiutil/NDArrayDataByte.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2002 Central Laboratory of the Research Councils
+ */
+package uk.ac.starlink.jaiutil;
+
+import java.awt.image.DataBufferByte;
+import java.awt.image.Raster;
+import java.io.IOException;
+
+import uk.ac.starlink.hdx.array.NDArray;
+import uk.ac.starlink.hdx.HdxException;
+
+/**
+ * Used for short NDArray data.
+ *
+ * @version $Id$
+ * @author Allan Brighton
+ * @author Peter W. Draper
+ */
+public class NDArrayDataByte extends NDArrayData
+{
+    /** constructor */
+    public NDArrayDataByte( NDArray tiler ) 
+    {
+	super( tiler );
+    }
+
+   /** constructor */
+    public NDArrayDataByte ( NDArray tiler, int[] axes ) 
+    {
+        super( tiler, axes );
+    }
+
+    /** 
+     * Fill in the given tile with the appropriate image data.
+     */
+    public Raster getTile( Raster tile ) 
+        throws IOException, HdxException
+    {
+        //System.out.println( "NDArrayDataByte: getTile = " + tile );
+	DataBufferByte dataBuffer = (DataBufferByte) tile.getDataBuffer();
+	byte[] destArray = dataBuffer.getData();
+	fillTile( destArray, tile.getMinX(), tile.getMinY(), 
+                  tile.getWidth(), tile.getHeight() );
+	return tile;
+    }
+
+    /** 
+     * Return a prescaled preview image at "1/factor" of the normal
+     * size in the given raster tile.
+     */
+    public Raster getPreviewImage( Raster tile, int factor ) 
+        throws IOException
+    {
+        DataBufferByte dataBuffer = (DataBufferByte) tile.getDataBuffer();
+        byte[] destArray = dataBuffer.getData();
+        byte[] line = new byte[width];
+
+        int tw = tile.getWidth(),
+            th = tile.getHeight(),
+            w = tw*factor,
+            h = th*factor,
+            n = 0, 
+            m = 0;
+        try {
+            for ( int j = 0; j < h; j += factor ) {
+                n =  m++ * tw;
+                fillTile( line, 0, j, width, 1 );
+                for ( int i = 0; i < w; i += factor ) {
+                    destArray[n++] = line[i];
+                }
+            }
+            return tile;
+        }
+        catch (Exception e) {
+            throw new RuntimeException( e.getMessage() );
+        }
+    }
+}
