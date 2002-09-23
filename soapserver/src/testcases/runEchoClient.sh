8785b6c89593ed28717a89740dd3e5cb9a415f80
Peter W. Draper <p.w.draper@durham.ac.uk>
2002-09-23 15:43:59 +0000

    *** empty log message ***

--- soapserver/src/testcases/runEchoClient.sh
+++ soapserver/src/testcases/runEchoClient.sh
@@ -2,9 +2,4 @@
 #  Send a message to the EchoServer application. Should respond with 
 #  "tomato".
 
-export CLASSPATH=".:../../build/classes:`../../../../lib/jetty/echo_classpath`"
-export CLASSPATH="`../../../../lib/axis/echo_classpath`:${CLASSPATH}"
-
-javac EchoClient.java
-
 java EchoClient "tomato"
