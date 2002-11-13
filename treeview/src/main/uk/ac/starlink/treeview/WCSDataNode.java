c841ee09ac1a29c0f24e7476bc4634aecc67dc15
Mark Taylor <m.b.taylor@bristol.ac.uk>
2002-11-13 17:48:38 +0000

    Added XML Source constructor for WCSDataNode.

--- treeview/src/main/uk/ac/starlink/treeview/WCSDataNode.java
+++ treeview/src/main/uk/ac/starlink/treeview/WCSDataNode.java
@@ -4,12 +4,17 @@ import java.util.*;
 import java.io.*;
 import java.awt.*;
 import javax.swing.*;
+import javax.xml.transform.Source;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.dom.DOMSource;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import uk.ac.starlink.ast.*;
+import uk.ac.starlink.ast.xml.XAstReader;
 import uk.ac.starlink.ast.xml.XAstWriter;
 import uk.ac.starlink.hds.*;
+import uk.ac.starlink.util.DOMUtils;
+import uk.ac.starlink.util.SourceReader;
 
 /**
  * A {@link DataNode} representing the WCS component of 
@@ -49,6 +54,13 @@ public class WCSDataNode extends DefaultDataNode {
      */
     public WCSDataNode( String path ) throws NoSuchDataException {
         this( HDSDataNode.getHDSFromPath( path ) );
+    }
+
+    /**
+     * Constructs a WCSDataNode from an XML Source.
+     */
+    public WCSDataNode( Source xsrc ) throws NoSuchDataException {
+        this( getWcsFromSource( xsrc ) );
     }
 
     public boolean allowsChildren() {
@@ -205,5 +217,32 @@ public class WCSDataNode extends DefaultDataNode {
         catch ( HDSException e ) {
             throw new NoSuchDataException( e.getMessage() );
         }
+    }
+
+    private static FrameSet getWcsFromSource( Source xsrc ) 
+            throws NoSuchDataException {
+        try {
+            Node node = new SourceReader().getDOM( xsrc );
+            if ( node instanceof Element ) {
+                Element el = (Element) node;
+                if ( el.getTagName().equals( "wcs" ) ) {
+                    if ( el.getAttribute( "encoding" ).equals( "AST-XML" ) ) {
+                        Element fsel = 
+                            DOMUtils.getChildElementByName( el, "FrameSet" );
+                        if ( fsel != null ) {
+                            return (FrameSet) new XAstReader()
+                                             .makeAst( fsel, null );
+                        }
+                    }
+                }
+            }
+        }
+        catch ( IOException e ) {
+            throw new NoSuchDataException( e );
+        }
+        catch ( TransformerException e ) {
+            throw new NoSuchDataException( e );
+        }
+        throw new NoSuchDataException( "Wrong sort of Source for AST" );
     }
 }
