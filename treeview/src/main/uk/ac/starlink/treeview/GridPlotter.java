4aa17eefd22c4f03db43886dc224095893161c0a
Mark Taylor <m.b.taylor@bristol.ac.uk>
2002-12-05 12:35:22 +0000

    Improved scaled WCS grid plotting.

--- treeview/src/main/uk/ac/starlink/treeview/GridPlotter.java
+++ treeview/src/main/uk/ac/starlink/treeview/GridPlotter.java
@@ -1,6 +1,7 @@
 package uk.ac.starlink.treeview;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Rectangle;
@@ -10,6 +11,7 @@ import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
+import javax.swing.JComponent;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.border.BevelBorder;
@@ -25,6 +27,8 @@ import uk.ac.starlink.ast.Plot;
  * @version  $Id$
  */
 public class GridPlotter extends JPanel {
+
+    private int currentFrame;
 
     /**
      * Constructs a GridPlotter.
@@ -50,15 +54,21 @@ public class GridPlotter extends JPanel {
         }
 
         /* Construct a panel to hold the plot display itself. */
-        final ScalingPlot plotPan = new ScalingPlot( wcs, lower, upper );
+        final ScalingPlot plotPan = new ScalingPlot( wcs, lower, upper ) {
+            protected void configurePlot( Plot plot ) {
+                plot.setCurrent( currentFrame );
+                super.configurePlot( plot );
+            }
+        };
+        plotPan.setBorder( BorderFactory.createLineBorder( Color.BLACK ) );
         add( plotPan, BorderLayout.CENTER );
 
         /* Construct a current-frame selector. */
         ButtonGroup bgrp = new ButtonGroup();
         ActionListener buttwatch = new ActionListener() {
             public void actionPerformed( ActionEvent evt ) {
-               int ifrm = Integer.parseInt( evt.getActionCommand() );
-               plotPan.setCurrent( ifrm );
+               currentFrame = Integer.parseInt( evt.getActionCommand() );
+               plotPan.refreshPlot();
             }
         };
         int nfrm = wcs.getNframe();
@@ -81,91 +91,4 @@ public class GridPlotter extends JPanel {
         setVisible( true );
     }
 
-    private static class ScalingPlot extends JPanel {
-
-        private static int lgap = 70;
-        private static int rgap = 30;
-        private static int tgap = 40;
-        private static int bgap = 60;
-
-        private final FrameSet wcs;
-        private final double[] lower;
-        private final double[] upper;
-        private final double[] bbox;
-        private final double gridAspect;
-        private Plot plot;
-        private Dimension lastSize;
-        private int currentFrame;
-
-        public ScalingPlot( FrameSet wcs, double[] lower, double[] upper ) {
-            if ( lower.length != 2 || upper.length != 2 ||
-                 wcs.getFrame( FrameSet.AST__BASE ).getNaxes() != 2 ) {
-                throw new IllegalArgumentException( "Not a 2d WCS" );
-            }
-            this.wcs = wcs;
-            this.lower = lower;
-            this.upper = upper;
-            bbox = new double[] { lower[ 0 ], lower[ 1 ], 
-                                  upper[ 0 ], upper[ 1 ] };
-            gridAspect = ( upper[ 1 ] - lower[ 1 ] )
-                       / ( upper[ 0 ] - lower[ 0 ] );
-            currentFrame = wcs.getCurrent();
-            setBorder( BorderFactory.createBevelBorder( BevelBorder.RAISED ) );
-        }
-
-        public void setCurrent( int ifrm ) {
-            currentFrame = ifrm;
-            if ( plot != null ) {
-                plot.setCurrent( ifrm );
-                plot.clear();
-                plot.grid();
-                repaint();
-            }
-        }
-
-        protected void paintComponent( Graphics g ) {
-            super.paintComponent( g );
-            Dimension size = getSize();
-            if ( size.width < lgap + rgap + 150 ) {
-                size.width = lgap + rgap + 150;
-            }
-            if ( size.height < tgap + bgap + 150 ) {
-                size.height = tgap + bgap + 150;
-            }
-            if ( ! size.equals( lastSize ) ) {
-                lastSize = size;
-                reconfigurePlot( size );
-            }
-            plot.paint( g );
-        }
-
-        private void reconfigurePlot( Dimension size ) {
-            Dimension plotWindow = new Dimension( size );
-            plotWindow.width -= ( lgap + rgap );
-            plotWindow.height -= ( tgap + bgap );
-            double windowAspect = 
-                (double) plotWindow.height / (double) plotWindow.width;
-            Dimension plotSize = new Dimension();
-            if ( gridAspect < windowAspect ) {
-                plotSize.width = plotWindow.width;
-                plotSize.height = 
-                    (int) Math.round( plotSize.width * gridAspect );
-            }
-            else {
-                plotSize.height = plotWindow.height;
-                plotSize.width =
-                    (int) Math.round( plotSize.height / gridAspect );
-            }
-            Rectangle position = new Rectangle( plotSize );
-            position.x = lgap + 
-                ( size.width - ( lgap + plotSize.width + rgap ) ) / 2;
-            position.y = tgap +
-                ( size.height - ( tgap + plotSize.height + bgap ) ) / 2;
-            plot = new Plot( wcs, position, bbox );
-            plot.setGrid( true );
-            plot.setCurrent( currentFrame );
-            plot.grid();
-            setPreferredSize( plotSize );
-        }
-    }
 }
---
+++ treeview/src/main/uk/ac/starlink/treeview/ScalingPlot.java
@@ -0,0 +1,159 @@
+package uk.ac.starlink.treeview;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import javax.swing.JPanel;
+import uk.ac.starlink.ast.FrameSet;
+import uk.ac.starlink.ast.Plot;
+
+/**
+ * Draws an AST Plot which will paint itself to fill the actual size
+ * of the component.  The aspect ratio will be retained, and the
+ * plot will not shrink in either dimension to below a given threshold
+ * (otherwise it gets too small and possibly gives an AstException),
+ * but otherwise it will check its component size any time it is
+ * drawn and reconfigure itself to fit.
+ * 
+ * @author   Mark Taylor (Starlink)
+ * @see    uk.ac.starlink.ast.Plot
+ */
+public class ScalingPlot extends JPanel {
+
+    private final FrameSet wcs;
+    private final double[] lower;
+    private final double[] upper;
+    private final double[] bbox;
+    private final double gridAspect;
+    private final int lgap;
+    private final int rgap;
+    private final int tgap;
+    private final int bgap;
+    private Plot plot;
+    private Dimension lastSize;
+
+    /**
+     * Constructs a new plot window given a FrameSet and the bounds of
+     * of the base frame corners.
+     *
+     * @param   wcs  the WCS FrameSet on which to base the AST Plot
+     * @param   a 2-d array giving the x,y coordinates of the bottom 
+     *          left hand corner in the base frame of <tt>wcs</tt>
+     * @param   a 2-d array giving the x,y coordinates of the top right
+     *          hand corner in the base frame of <tt>wcs</tt>
+     * @throws  IllegalArgumentException  if <tt>lower</tt> or <tt>upper</tt>
+     *          or the base frame of <tt>wcs</tt> do not represent
+     *          a 2-d frame
+     */
+    public ScalingPlot( FrameSet wcs, double[] lower, double[] upper ) {
+        if ( lower.length != 2 || upper.length != 2 ||
+             wcs.getFrame( FrameSet.AST__BASE ).getNaxes() != 2 ) {
+            throw new IllegalArgumentException( "Not a 2d WCS" );
+        }
+        this.wcs = (FrameSet) wcs.copy();
+        this.lower = (double[]) lower.clone();
+        this.upper = (double[]) upper.clone();
+        bbox = new double[] { lower[ 0 ], lower[ 1 ],
+                              upper[ 0 ], upper[ 1 ] };
+        gridAspect = ( upper[ 1 ] - lower[ 1 ] )
+                   / ( upper[ 0 ] - lower[ 0 ] );
+        int[] pixelBounds = getPixelBounds();
+        lgap = pixelBounds[ 0 ];
+        rgap = pixelBounds[ 1 ];
+        tgap = pixelBounds[ 2 ];
+        bgap = pixelBounds[ 3 ];
+    }
+
+    /**
+     * Configures the plot prior to painting it.  This implementation
+     * does the following:
+     * <pre>
+     *     plot.setGrid(true);
+     *     plot.clear();
+     *     plot.grid();
+     * </pre>
+     * Subclasses may override this method to change any required
+     * plot attributes prior to plotting.
+     *
+     * @param  the Plot object to configure
+     */
+    protected void configurePlot( Plot plot ) {
+        plot.setGrid( true );
+        plot.clear();
+        plot.grid();
+    }
+
+    /**
+     * Returns the minimum gaps in pixels which are left in the window 
+     * round the outside of the plot.
+     *
+     * @return  a 4-element array of pixel gaps in the order left, right,
+     *          top, bottom
+     */
+    protected int[] getPixelBounds() {
+        return new int[] {
+            70, // lgap
+            30, // rgap
+            40, // tgap
+            60, // bgap
+        };
+    }
+
+    protected void paintComponent( Graphics g ) {
+        super.paintComponent( g );
+        Dimension size = getSize();
+        if ( size.width < lgap + rgap + 150 ) {
+            size.width = lgap + rgap + 150;
+        }
+        if ( size.height < tgap + bgap + 150 ) {
+            size.height = tgap + bgap + 150;
+        }
+        if ( ! size.equals( lastSize ) ) {
+            lastSize = size;
+            makeNewPlot();
+        }
+        plot.paint( g );
+    }
+
+    /**
+     * Causes the {@link #configurePlot} method to be called and the 
+     * plot repainted (unless it hasn't been painted for the first time yet).
+     */
+    public void refreshPlot() {
+
+        /* Only need to do anything if we have
+        if ( plot != null ) {
+            lastSize = null;
+            repaint();
+        }
+    }
+
+    private void makeNewPlot() {
+        Dimension size = getSize();
+        Dimension plotWindow = new Dimension( size );
+        plotWindow.width -= ( lgap + rgap );
+        plotWindow.height -= ( tgap + bgap );
+        double windowAspect =
+            (double) plotWindow.height / (double) plotWindow.width;
+        Dimension plotSize = new Dimension();
+        if ( gridAspect < windowAspect ) {
+            plotSize.width = plotWindow.width;
+            plotSize.height =
+                (int) Math.round( plotSize.width * gridAspect );
+        }
+        else {
+            plotSize.height = plotWindow.height;
+            plotSize.width =
+                (int) Math.round( plotSize.height / gridAspect );
+        }
+        Rectangle position = new Rectangle( plotSize );
+        position.x = lgap +
+            ( size.width - ( lgap + plotSize.width + rgap ) ) / 2;
+        position.y = tgap +
+            ( size.height - ( tgap + plotSize.height + bgap ) ) / 2;
+        plot = new Plot( wcs, position, bbox );
+        configurePlot( plot );
+        setPreferredSize( plotSize );
+    }
+
+}
