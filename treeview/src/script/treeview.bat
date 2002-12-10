0846a43fda39a7f00dd5db3ea6baca79902e482d
Mark Taylor <m.b.taylor@bristol.ac.uk>
2002-12-10 16:49:24 +0000

    Changes to the www documentation.

--- treeview/src/script/treeview.bat
+++ treeview/src/script/treeview.bat
@@ -1,5 +1,7 @@
+echo off
 REM Batch file for invoking Treeview in Windows.
-REM Not sure exactly what operating systems (if any?) this will work for.
+REM Not sure exactly what operating systems (if any?) this will work for...
+REM ...looks like NT may be OK, but not Win9X (the %~dp0 business is no good)
 
 rem Get the path of the directory in which this file is sitting.
 rem ------------------------------------------------------------
@@ -7,12 +9,12 @@ set BINDIR=%~dp0
 
 rem Set the name of the script which invokes java.
 rem ----------------------------------------------
-set JAVACMD="%BINDIR%\starjava"
+set JAVACMD="%BINDIR%starjava"
 
 rem Set the name of some resources.
 rem -------------------------------
-set APPJAR="%BINDIR%\..\lib\treeview\treeview.jar"
-set DEMODIR="%BINDIR%\..\etc\treeview\demo"
+set APPJAR="%BINDIR%..\lib\treeview\treeview.jar"
+set DEMODIR="%BINDIR%..\etc\treeview\demo"
 
 
 rem Get the command line arguments.
@@ -31,5 +33,5 @@ set CMD_LINE_ARGS=%$
 
 rem Invoke Treeview with the requested args.
 rem ----------------------------------------
-"%JAVACMD%" -jar "%APPJAR%" -Duk.ac.starlink.treeview.demodir="%DEMODIR" %CMD_LINE_ARGS%
+"%JAVACMD%" -Duk.ac.starlink.treeview.demodir="%DEMODIR%" -jar "%APPJAR%" %CMD_LINE_ARGS%
 
