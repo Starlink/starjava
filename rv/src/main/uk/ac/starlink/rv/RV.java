5b8917b75c7e636669d38d3938467f5d1e6768c7
Starlink Software Collection Librarian cvs2svn <ussc@star.rl.ac.uk>
2003-01-30 13:55:08 +0000

    This commit was manufactured by cvs2svn to create tag 'start'.

---
+++ rv/src/main/uk/ac/starlink/rv/RV.java
@@ -0,0 +1,61 @@
+/** RV.java
+ *
+ * @author Roy Platon
+ * @version 1.00 26 MArch 2002
+ * Provides front end to run RV either as an Applet or Application
+ **/
+
+package uk.ac.starlink.rv;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import uk.ac.starlink.rv.*;
+    
+/**
+ * The main application for running RV as an Application or an Applet.
+ */
+public class RV extends JApplet {
+    private static RVPanel pane;
+    private static String task = null;
+    private static Container contentPane;
+
+/**
+ * Initialise the GUI and create the Panel
+ */
+    public void init( ) {
+        contentPane = getContentPane();
+        pane = new RVPanel( contentPane );
+        contentPane.add( pane );
+        pane.setup( task );
+    }
+    
+/**
+ * Run RV as an Application
+ */
+    static public void main( String[] args ) {
+        if ( args.length > 0 ) task = args[0];
+        if ( task == null ) task = "Radial Velocities";
+        new RVFrame( new RV( ), 800, 600 );
+    }
+}
+
+/**
+ * Run RV as an Application
+ */
+class RVFrame extends JFrame {
+    RVFrame( RV a, int x, int y ) {
+        setTitle( a.getClass().getName() );
+        setSize( x, y );
+        addWindowListener( new WindowAdapter()  {
+            public void windowClosing( WindowEvent e ) {
+                System.exit(0);
+            }
+        } );
+        Container contentPane = getContentPane();
+        contentPane.add( a );
+        a.init();
+        show();
+        a.start();       
+    }
+}
