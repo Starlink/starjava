242e4f3536df8b0f2e87e138e43141200f5293f3
Mark Taylor <m.b.taylor@bristol.ac.uk>
2003-03-04 13:31:20 +0000

    Oops.

--- table/src/main/uk/ac/starlink/table/SequentialStarTable.java
+++ table/src/main/uk/ac/starlink/table/SequentialStarTable.java
@@ -70,6 +70,7 @@ public abstract class SequentialStarTable extends AbstractStarTable {
     public void next() throws IOException {
         if ( hasNextRow() ) {
             currentRowData = getNextRow();
+            current++;
         }
         else {
             currentRowData = null;
