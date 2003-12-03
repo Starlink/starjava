3e2f3b6b4f6b3e448b4095ce5fa98b06fce5e734
Mark Taylor <m.b.taylor@bristol.ac.uk>
2003-12-03 14:26:06 +0000

    Put some of the chooser manipulation in the event dispatch thread - it was causing intermittent thread trouble in the main thread.

--- table/src/testcases/uk/ac/starlink/table/gui/StarTableNodeChooserTest.java
+++ table/src/testcases/uk/ac/starlink/table/gui/StarTableNodeChooserTest.java
@@ -1,8 +1,12 @@
 package uk.ac.starlink.table.gui;
 
+import javax.swing.SwingUtilities;
+import junit.framework.AssertionFailedError;
 import junit.framework.TestCase;
 
 public class StarTableNodeChooserTest extends TestCase {
+
+    Throwable storedThrowable;
 
     public StarTableNodeChooserTest( String name ) {
         super( name );
@@ -13,15 +17,30 @@ public class StarTableNodeChooserTest extends TestCase {
      */
     public void testChooserAvailability() throws Throwable {
 
-       /* This works using reflection on the class 
-        * StarTableNodeChooser.CHOOSER_CLASS (somewhere in Treeview, probably), 
-        * which may not be available when this package is built.  Hence this 
-        * test may fail if the whole starjava set has not yet been installed. */
-        StarTableNodeChooser.reflect();
+        SwingUtilities.invokeAndWait( new Runnable() {
+            public void run() {
+                try {
+                    /* This works using reflection on the class 
+                     * StarTableNodeChooser.CHOOSER_CLASS 
+                     * (somewhere in Treeview, probably), 
+                     * which may not be available when this package is built.
+                     * Hence this test may fail if the whole starjava 
+                     * set has not yet been installed. */
+                    StarTableNodeChooser.reflect();
 
-        String msg = "Wrong signatures on " 
-                   + StarTableNodeChooser.CHOOSER_CLASS;
-        assertTrue( msg, StarTableNodeChooser.isAvailable() );
-        assertNotNull( msg, StarTableNodeChooser.newInstance() );
+                    String msg = "Wrong signatures on " 
+                               + StarTableNodeChooser.CHOOSER_CLASS;
+                    assertTrue( msg, StarTableNodeChooser.isAvailable() );
+                    assertNotNull( msg, StarTableNodeChooser.newInstance() );
+                }
+                catch ( Throwable e ) {
+                    storedThrowable = e;
+                }
+            }
+        } );
+
+        if ( storedThrowable != null ) {
+            throw storedThrowable;
+        }
     }
 }
