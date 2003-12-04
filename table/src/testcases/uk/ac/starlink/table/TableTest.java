a071afa341fb52d2c0ef0db3872827fbb784dc20
Mark Taylor <m.b.taylor@bristol.ac.uk>
2003-12-04 18:56:19 +0000

    Tracked some changes to ArrayColumn.

--- table/src/testcases/uk/ac/starlink/table/TableTest.java
+++ table/src/testcases/uk/ac/starlink/table/TableTest.java
@@ -19,13 +19,19 @@ public class TableTest extends TestCase {
         fillCycle( xData, 0, 10 );
         fillRandom( yData, 0., 1. );
 
-        ColumnInfo iColInfo = new ColumnInfo( "Index", Integer.class, "row index" );
-        ColumnInfo xColInfo = new ColumnInfo( "X", Double.class, "X coordinate" );
-        ColumnInfo yColInfo = new ColumnInfo( "Y", Double.class, "Y coordinate" );
+        ColumnInfo iColInfo =
+            new ColumnInfo( "Index", Integer.class, "row index" );
+        ColumnInfo xColInfo =
+            new ColumnInfo( "X", Double.class, "X coordinate" );
+        ColumnInfo yColInfo =
+            new ColumnInfo( "Y", Double.class, "Y coordinate" );
 
-        ArrayColumn iCol = ArrayColumn.makeColumn( iColInfo, iData );
-        ArrayColumn xCol = ArrayColumn.makeColumn( xColInfo, xData );
-        ArrayColumn yCol = ArrayColumn.makeColumn( yColInfo, yData );
+        ArrayColumn iCol = PrimitiveArrayColumn
+                          .makePrimitiveColumn( iColInfo, iData );
+        ArrayColumn xCol = PrimitiveArrayColumn
+                          .makePrimitiveColumn( xColInfo, xData );
+        ArrayColumn yCol = PrimitiveArrayColumn
+                          .makePrimitiveColumn( yColInfo, yData );
 
         ColumnStarTable st = ColumnStarTable.makeTableWithRows( (long) nrow );
         st.setName( "Position data table" );
