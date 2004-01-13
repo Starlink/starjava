95a646c62151521c7aeba4eeb89d23931f41ae05
Mark Taylor <m.b.taylor@bristol.ac.uk>
2004-01-13 16:41:39 +0000

    Added a couple of new wrapper star tables.

---
+++ table/src/main/uk/ac/starlink/table/RowPermutedStarTable.java
@@ -0,0 +1,68 @@
+package uk.ac.starlink.table;
+
+import java.io.IOException;
+
+/**
+ * Wrapper table which provides a view of a base table in which the 
+ * rows are permuted.  Each row in the wrapper table is a view
+ * of one in the base table.  It is permitted for the wrapper to contain
+ * multiple views of the same column in the base.
+ *
+ * @author   Mark Taylor (Starlink)
+ */
+public class RowPermutedStarTable extends WrapperStarTable {
+
+    private long[] rowMap;
+
+    /**
+     * Constructs a new <tt>RowPermutedStarTable</tt> from a base table
+     * and an array of <tt>long</tt>s describing which rows in the new
+     * wrapper table correspond to which rows in the base one.
+     * The new table will have <tt>rowMap.length</tt> rows, and 
+     * row <tt>i</tt> in the new table will correspond to row 
+     * <tt>rowMap[i]</tt> in the base one.  The <tt>rowMap</tt> array
+     * may contain duplicate entries; any negative entry is treated as
+     * a special case resulting in a 'blank' row of all null objects.
+     * <p>
+     * <tt>baseTable</tt> must provide random access.
+     *
+     * @param  baseTable  base table
+     * @param  rowMap     array mapping rows in <tt>baseTable</tt> to rows in
+     *                    the new permuted table
+     * @throws IllegalArgumentException  if <tt>baseTable.isRandom</tt> returns
+     *         <tt>false</tt>
+     */
+    public RowPermutedStarTable( StarTable baseTable, long[] rowMap ) {
+        super( baseTable );
+        if ( ! baseTable.isRandom() ) {
+            throw new IllegalArgumentException( "No random access in base " +
+                                                "table " + baseTable );
+        }
+        this.rowMap = rowMap;
+    }
+
+    public long getRowCount() {
+        return (long) rowMap.length;
+    }
+
+    /**
+     * Returns true.
+     */
+    public boolean isRandom() {
+        return true;
+    }
+
+    public RowSequence getRowSequence() {
+        return new RandomRowSequence( this );
+    }
+
+    public Object getCell( long irow, int icol ) throws IOException {
+        return baseTable.getCell( rowMap[ checkedLongToInt( irow ) ], icol );
+    }
+
+    public Object[] getRow( long irow ) throws IOException {
+        long baseRow = rowMap[ checkedLongToInt( irow ) ];
+        return baseRow > 0 ? baseTable.getRow( baseRow )
+                           : new Object[ baseTable.getColumnCount() ];
+    }
+}
---
+++ table/src/main/uk/ac/starlink/table/RowSubsetStarTable.java
@@ -0,0 +1,124 @@
+package uk.ac.starlink.table;
+
+import java.io.IOException;
+import java.util.BitSet;
+
+/**
+ * Wraps a StarTable to present only a subset of its rows.
+ * A {@link java.util.BitSet} is used to keep track of which rows in the
+ * base table should be visible from this one; a set (true) bit in the
+ * mask indicates a row in the base table which will be visible in this one.
+ * It is the responsibility of the user to ensure that no bits in the
+ * mask are set beyond the end of the underlying table - behaviour is
+ * undefined in the case that this condition is violated (but probably
+ * procured RowSequence objects will misbehave).
+ *
+ * @author   Mark Taylor (Starlink)
+ */
+public class RowSubsetStarTable extends WrapperStarTable {
+
+    private BitSet mask;
+
+    /**
+     * Constructs a new RowSubsetStarTable with no rows showing, 
+     * in which a set bit in the mask indicates a visible row.
+     *
+     * @param  baseTable  base table which provides the underlying data
+     */
+    public RowSubsetStarTable( StarTable baseTable ) {
+        this( baseTable, new BitSet() );
+    }
+
+    /**
+     * Constructs a new RowSubsetStarTable with a given mask.
+     *
+     * @param  baseTable  base table which provides the underlying data
+     * @param  mask    bitmask determining which rows in <tt>baseTable</tt>
+     *                 are seen
+     */
+    public RowSubsetStarTable( StarTable baseTable, BitSet mask ) {
+        super( baseTable );
+        this.mask = mask;
+    }
+
+    /**
+     * Returns the bit mask which defines which rows are seen.
+     *
+     * @return   row mask
+     */
+    public BitSet getMask() {
+        return mask;
+    }
+
+    /**
+     * Sets the mask which determines which rows are seen.
+     * 
+     * @param   row mask
+     */
+    public void setMask( BitSet mask ) {
+        this.mask = mask;
+    }
+
+    /**
+     * Returns false.
+     */
+    public boolean isRandom() {
+        return false;
+    }
+
+    public long getRowCount() {
+        return (long) mask.cardinality();
+    }
+
+    public RowSequence getRowSequence() throws IOException {
+        return new RowSequence() {
+            RowSequence baseSeq = baseTable.getRowSequence();
+            int iBase = -1;
+            int iSubset = -1;
+
+            public void next() throws IOException {
+                int nextBase = mask.nextSetBit( iBase + 1 );
+                if ( nextBase == -1 ) {
+                    assert ! hasNext();
+                    throw new IllegalStateException( "No more rows" );
+                }
+                int nskip = nextBase - iBase;
+                assert nskip > 0;
+                baseSeq.advance( nskip );
+                iBase += nskip;
+                iSubset++;
+            }
+
+            public boolean hasNext() {
+                return mask.nextSetBit( iBase + 1 ) >= 0;
+            }
+
+            public void advance( long nrows ) throws IOException {
+                int nrow = checkedLongToInt( nrows );
+                int pos = iBase;
+                for ( int i = 0; i < nrow; i++ ) {
+                    pos = mask.nextSetBit( pos + 1 );
+                    if ( pos < 0 ) {
+                        throw new IOException( "Table too short" );
+                    }
+                }
+                int baseSkip = pos - iBase;
+                baseSeq.advance( baseSkip );
+                iBase += baseSkip;
+                iSubset += nrow;
+            }
+
+            public Object getCell( int icol ) throws IOException {
+                return baseSeq.getCell( icol );
+            }
+
+            public Object[] getRow() throws IOException {
+                return baseSeq.getRow();
+            }
+
+            public long getRowIndex() {
+                return iSubset;
+            }
+        };
+    }
+}
