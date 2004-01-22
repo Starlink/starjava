0bf97d68abc3656a2c0befccc7205ecc12ea2861
Mark Taylor <m.b.taylor@bristol.ac.uk>
2004-01-22 18:11:35 +0000

    Fixed javadoc niggle.

--- table/src/main/uk/ac/starlink/table/IteratorRowSequence.java
+++ table/src/main/uk/ac/starlink/table/IteratorRowSequence.java
@@ -7,7 +7,7 @@ import java.util.Iterator;
  * Implementation of <tt>RowSequence</tt> based on an Iterator which 
  * returns table rows in sequence.  The iterator's next method may 
  * throw an exception of the (unchecked) type 
- * {@link IteratorRowSequence.PackagedIOException}
+ * {@link uk.ac.starlink.table.IteratorRowSequence.PackagedIOException}
  * if it needs to throw something (<tt>Iterator.next</tt> is not declared
  * to throw any checked exceptions).
  */
@@ -22,7 +22,8 @@ public class IteratorRowSequence implements RowSequence {
      * Each object returned by the iterator's <tt>next</tt> method must
      * be an <tt>Object[]</tt> array representing the data in the next
      * table row (it must have one element for each column).
-     * The <tt>next</tt> method may throw {@link #PackagedIOException}s.
+     * The <tt>next</tt> method may throw 
+     * {@link uk.ac.starlink.table.IteratorRowSequence.PackagedIOException}s.
      *
      * @param   rowIt  iterator over the rows
      */
