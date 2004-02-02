13d13edf96bc785866f5a00498852853799a7d74
Peter W. Draper <p.w.draper@durham.ac.uk>
2004-02-02 12:12:43 +0000

    Changed to support save and restore of Figures using AST coordinates.

--- splat/src/main/uk/ac/starlink/splat/plot/DivaPlotGraphicsPane.java
+++ splat/src/main/uk/ac/starlink/splat/plot/DivaPlotGraphicsPane.java
@@ -27,7 +27,9 @@ import java.util.Iterator;
 
 import uk.ac.starlink.splat.ast.ASTJ;
 import uk.ac.starlink.ast.Mapping;
+import uk.ac.starlink.ast.CmpMap;
 
+import uk.ac.starlink.diva.DrawActions;    // for javadocs
 import uk.ac.starlink.diva.DrawGraphicsPane;
 import uk.ac.starlink.diva.DrawFigure;
 import uk.ac.starlink.diva.DrawBasicFigure;
@@ -50,7 +52,7 @@ public class DivaPlotGraphicsPane
     /**
      *  Constructor accepts a FigureDecorator. Normally this will be tuned
      *  to interact with all the types of figures that will be offered
-     *  (see {@link DivaPlotCanvasDraw} and the TypedDecorator it creates).
+     *  (see {@link DrawActions} and the TypedDecorator it creates).
      */
     public DivaPlotGraphicsPane( FigureDecorator decorator )
     {
@@ -77,45 +79,47 @@ public class DivaPlotGraphicsPane
         // Switch off figure resizing constraints
         new DrawBasicFigure().setTransformFreely( true );
 
+        // Create a simplified series map of the two mappings. Note
+        // newMapping is inverted temporarily.
+        boolean invert = newMapping.getInvert();
+        newMapping.setInvert( true );
+        CmpMap cmpmap = new CmpMap( oldMapping, newMapping, true );
+        Mapping smap = cmpmap.simplify();
+
+        AffineTransform at;
+        Figure figure;
+        Rectangle2D rect;
+        double xscale;
+        double yscale;
         double[] oldCoords = new double[4];
-        double[] tmpCoords = new double[4];
-        double[][] neutralCoords = null;
-        double[][] newCoords = null;
+        double[][] newCoords;
 
         Iterator it = getFigureLayer().figures();
         while ( it.hasNext() ) {
 
-            Figure figure = (Figure) it.next();
-            Rectangle2D rect = figure.getBounds();
+            figure = (Figure) it.next();
+            rect = figure.getBounds();
 
             oldCoords[0] = rect.getX();
             oldCoords[1] = rect.getY();
-            oldCoords[2] = rect.getX() + rect.getWidth();
-            oldCoords[3] = rect.getY() + rect.getHeight();
+            oldCoords[2] = oldCoords[0] + rect.getWidth();
+            oldCoords[3] = oldCoords[1] + rect.getHeight();
 
-            neutralCoords = ASTJ.astTran2( oldMapping, oldCoords, true );
+            newCoords = ASTJ.astTran2( smap, oldCoords, true );
 
-            tmpCoords[0] = neutralCoords[0][0];
-            tmpCoords[1] = neutralCoords[1][0];
-            tmpCoords[2] = neutralCoords[0][1];
-            tmpCoords[3] = neutralCoords[1][1];
+            xscale = ( newCoords[0][1] - newCoords[0][0] ) /
+                        ( oldCoords[2] - oldCoords[0] );
+            yscale = ( newCoords[1][1] - newCoords[1][0] ) /
+                        ( oldCoords[3] - oldCoords[1] );
 
-            newCoords = ASTJ.astTran2( newMapping, tmpCoords, false );
-
-            double xscale = ( newCoords[0][1] - newCoords[0][0] ) /
-                            ( oldCoords[2] - oldCoords[0] );
-            double yscale = ( newCoords[1][1] - newCoords[1][0] ) /
-                            ( oldCoords[3] - oldCoords[1] );
-
-            AffineTransform at = new AffineTransform();
-
+            at = new AffineTransform();
             at.translate( newCoords[0][0], newCoords[1][0] );
             at.scale( xscale, yscale );
             at.translate( -oldCoords[0], -oldCoords[1] );
 
             figure.transform( at );
-
         }
         new DrawBasicFigure().setTransformFreely( false );
+        newMapping.setInvert( invert );
     }
 }
