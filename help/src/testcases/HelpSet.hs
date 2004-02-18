bc30fa5b0e4f2f3dc0b3ee5ead5d56b858cf4832
Peter W. Draper <p.w.draper@durham.ac.uk>
2004-02-18 15:46:18 +0000

    Updated to Javahelp version 2.

--- help/src/testcases/HelpSet.hs
+++ help/src/testcases/HelpSet.hs
@@ -1,28 +1,29 @@
 <?xml version='1.0' encoding='ISO-8859-1' ?>
 <!DOCTYPE helpset
-  PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 1.0//EN"
-  "http://java.sun.com/products/javahelp/helpset_1_0.dtd">
+  PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN"
+  "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
 
-<helpset version="1.0">
+<helpset version="2.0">
 
      <title>HELP a Javahelp based help package</title>
 
      <!-- The Map file. This documents IDs to URLs -->
      <maps>
+          <homeID>main-help</homeID>
           <mapref location="Map.jhm"/>
      </maps>
 
      <!-- Navigation views of various kinds (tabbed panes)-->
-     <view>
+     <view xml:lang="en">
          <name>TOC</name>
-         <label>TOC</label>
+         <label>Table of contents</label>
          <type>javax.help.TOCView</type>
          <data>TOC.xml</data>
      </view>
 
-     <!--<view>
+     <!--<view xml:lang="en">
          <name>Search</name>
-         <label>Word Search</label>
+         <label>Search</label>
          <type>javax.help.SearchView</type>
          <data engine="com.sun.java.help.search.DefaultSearchEngine">
                JavaHelpSearch
