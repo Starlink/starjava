c50749d37ab98692a785ca6546142d41f8dad358
Mark Taylor <m.b.taylor@bristol.ac.uk>
2004-03-24 19:06:54 +0000

    Added the possibility to invert the sense of the base BitSet.

--- topcat/src/main/uk/ac/starlink/topcat/BitsRowSubset.java
+++ topcat/src/main/uk/ac/starlink/topcat/BitsRowSubset.java
@@ -11,18 +11,35 @@ import java.util.BitSet;
 public class BitsRowSubset implements RowSubset {
 
      private BitSet bits;
+     private boolean invert;
      private String name;
 
      /**
+      * Constructs a new row subset with a given BitSet, name and sense.
+      * The <tt>invert</tt> argument indicates whether the sense of the
+      * bit set is to be reversed prior to interpretation.
+      *
+      * @param   name  subset name
+      * @param   bits  flag vector
+      * @param   inver  whether to invert the bits from the BitSet
+      */
+     public BitsRowSubset( String name, BitSet bits, boolean invert ) {
+         this.name = name;
+         this.bits = bits;
+         this.invert = invert;
+     }
+
+     /**
       * Constructs a new row subset with a given BitSet and name.
+      * Same as <tt>BitsRowSubset(name,bits,false)</tt>
       *
       * @param   name  subset name
       * @param   bits  flag vector
       */
      public BitsRowSubset( String name, BitSet bits ) {
-         this.name = name;
-         this.bits = bits;
+         this( name, bits, false );
      }
+
 
      /**
       * Returns the <tt>BitSet</tt> object used to store the inclusion
@@ -34,12 +51,22 @@ public class BitsRowSubset implements RowSubset {
          return bits;
      }
 
+     /**
+      * Returns the inversion sense of the inclusion flags represented by
+      * this subset relative to the bit set.
+      *
+      * @return  true iff bitset bits are inverted to give inclusion flag
+      */
+     public boolean getInvert() {
+         return invert;
+     }
+
      public String getName() {
          return name;
      }
 
      public boolean isIncluded( long lrow ) {
-         return bits.get( (int) lrow );
+         return bits.get( (int) lrow ) ^ invert;
      }
 
      public String toString() {
