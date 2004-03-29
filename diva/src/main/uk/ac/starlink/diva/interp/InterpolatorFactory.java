d4b161ce76bab8f9cdb6ba467c0aa8e1a9a39580
Peter W. Draper <p.w.draper@durham.ac.uk>
2004-03-29 15:01:13 +0000

    Modified to an interface so that InterpolatorFactory can be plugable.

--- diva/src/main/uk/ac/starlink/diva/interp/InterpolatorFactory.java
+++ diva/src/main/uk/ac/starlink/diva/interp/InterpolatorFactory.java
@@ -8,13 +8,14 @@
 package uk.ac.starlink.diva.interp;
 
 /**
- * This class creates and enumerates the types of {@link Interpolator}
- * that are available.
- *
+ * Interface for a class that defines how to create {@link Interpolator}
+ * instances for an application. Usually there will be just one class
+ * that implements this interface.
+ * 
  * @author Peter W. Draper
  * @version $Id$
  */
-public class InterpolatorFactory
+public interface InterpolatorFactory
 {
     //
     // Enumeration of the "types" of Interpolator that can be created.
@@ -36,108 +37,41 @@ public class InterpolatorFactory
      *  X coordinates are always monotonic) */
     public static final int LINEAR = 4;
 
-    /** Display names for Curves */
-    public static final String[] shortNames =
-        {
-            "hermite",
-            "akima",
-            "cubic",
-            "polynomial",
-            "linear",
-        };
-
-    /** The number of interpolator types */
-    public static final int NUM_INTERPOLATORS = shortNames.length;
-
-    /**
-     *  Create the single class instance.
-     */
-    private static final InterpolatorFactory instance = new InterpolatorFactory();
-
-    /**
-     *  Hide the constructor from use.
-     */
-    private InterpolatorFactory() {}
-
-    /**
-     *  Return reference to the only allowed instance of this class.
-     *
-     *  @return reference to only instance of this class.
-     */
-    public static InterpolatorFactory getReference()
+    /** Display names for Curves defined in the interface. Implementors should
+     *  base any extensions on this list */
+    public static final String[] defaultShortNames =
     {
-        return instance;
-    }
+        "hermite",
+        "akima",
+        "cubic",
+        "polynomial",
+        "linear",
+    };
+
+    /**
+     * Get the number of interpolators supported.
+     */
+    public int getInterpolatorCount();
 
     /**
      *  Create an Interpolator of the given type.
      */
-    public Interpolator makeInterpolator( int interpolator )
-    {
-        switch( interpolator )
-        {
-           case HERMITE: {
-               return new HermiteSplineInterp();
-           }
-           case AKIMA: {
-               return new AkimaSplineInterp();
-           }
-           case CUBIC: {
-               return new CubicSplineInterp();
-           }
-           case POLYNOMIAL: {
-               return new PolynomialInterp();
-           }
-           case LINEAR: {
-               return new LinearInterp();
-           }
-        }
-        return null;
-    }
+    public Interpolator makeInterpolator( int interpolator );
 
     /**
      *  Get the short name of an interpolator type.
      */
-    public String getShortName( int interpolator )
-    {
-        return shortNames[interpolator];
-    }
+    public String getShortName( int interpolator );
 
     /**
      *  Get the interpolator type, given a short name. Returns -1 if
      *  the name isn't known.
      */
-    public int getTypeFromName( String name )
-    {
-        for ( int i = 0; i < NUM_INTERPOLATORS; i++ ) {
-            if ( shortNames[i].equalsIgnoreCase( name ) ) {
-                return i;
-            }
-        }
-        return -1;
-    }
+    public int getTypeFromName( String name );
 
     /**
      * Return the numeric type of a given Interpolator. Returns -1 if
      * the type is unknown.
      */
-    public int getInterpolatorType( Interpolator interpolator )
-    {
-        if ( interpolator instanceof HermiteSplineInterp ) {
-            return HERMITE;
-        }
-        if ( interpolator instanceof AkimaSplineInterp ) {
-            return AKIMA;
-        }
-        if ( interpolator instanceof CubicSplineInterp ) {
-            return CUBIC;
-        }
-        if ( interpolator instanceof PolynomialInterp ) {
-            return POLYNOMIAL;
-        }
-        if ( interpolator instanceof LinearInterp ) {
-            return LINEAR;
-        }
-        return -1;
-    }
+    public int getInterpolatorType( Interpolator interpolator );
 }
