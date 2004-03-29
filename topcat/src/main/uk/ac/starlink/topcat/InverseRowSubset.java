e06502c967dbccdbd7953f97e7ef36b11df748e8
Mark Taylor <m.b.taylor@bristol.ac.uk>
2004-03-29 13:42:18 +0000

    Added Invert Subset action.

---
+++ topcat/src/main/uk/ac/starlink/topcat/InverseRowSubset.java
@@ -0,0 +1,27 @@
+package uk.ac.starlink.topcat;
+
+/**
+ * RowSubset implementation which provides the complement of a base set.
+ *
+ * @author   Mark Taylor (Starlink)
+ * @since    26 Mar 2004
+ */
+public class InverseRowSubset implements RowSubset {
+    private final RowSubset base;
+    private static final String INVERT_PREFIX = "not_";
+
+    public InverseRowSubset( RowSubset base ) {
+        this.base = base;
+    }
+
+    public boolean isIncluded( long lrow ) {
+        return ! base.isIncluded( lrow );
+    }
+
+    public String getName() {
+        String baseName = base.getName();
+        return baseName.startsWith( INVERT_PREFIX ) 
+             ? baseName.substring( INVERT_PREFIX.length() )
+             : INVERT_PREFIX + baseName;
+    }
+}
