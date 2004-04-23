07bc30c699b88a57ce296eefe0b495e81900f974
Starlink Software Collection Librarian cvs2svn <ussc@star.rl.ac.uk>
2004-04-23 14:55:13 +0000

    This commit was manufactured by cvs2svn to create tag 'topcat-USSC219-fixes1'.

--- soapserver/src/script/runEchoClient.sh
+++
@@ -1,5 +0,0 @@
-
-#  Send a message to the EchoServer application. Should respond with 
-#  "tomato".
-
-java uk.ac.starlink.soap.EchoClient "tomato"
--- treeview/src/main/uk/ac/starlink/treeview/Draggable.java
+++
@@ -1,8 +0,0 @@
-package uk.ac.starlink.treeview;
-
-import java.io.IOException;
-
-public interface Draggable {
-    void customiseTransferable( DataNodeTransferable trans ) throws IOException;
-}
-
