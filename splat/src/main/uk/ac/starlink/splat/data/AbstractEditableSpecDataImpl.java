74a6383f58f84ddf53d752e400623cd9167bcf41
Peter W. Draper <p.w.draper@durham.ac.uk>
2004-04-28 16:27:43 +0000

    Added raft of new methods that provide for the transfer of spectral coordinate systems. Renamed old methods to more meaningful versions (also needed as overloading was compromised by the expansion).

--- splat/src/main/uk/ac/starlink/splat/data/AbstractEditableSpecDataImpl.java
+++ splat/src/main/uk/ac/starlink/splat/data/AbstractEditableSpecDataImpl.java
@@ -11,9 +11,8 @@ import uk.ac.starlink.splat.util.SplatException;
 import uk.ac.starlink.ast.FrameSet;
 
 /**
- * Extends the {@link SpecDataImpl} class to include methods for
- * modifying the spectrum values by implementing the 
- * {@link EditableSpecDataImpl} interface.
+ * Extends the {@link SpecDataImpl} class to include methods for modifying the
+ * spectrum values by implementing the {@link EditableSpecDataImpl} interface.
  *
  * @author Peter W. Draper
  * @version $Id$
@@ -46,114 +45,59 @@ public abstract class AbstractEditableSpecDataImpl
         super( name, spectrum );
     }
 
-    /**
-     * Change the complete spectrum data. Takes a copy of all data.
-     *
-     * @param coords the spectrum coordinates, one per data value.
-     * @param data the spectrum data values.
-     */
-    abstract public void setData( double[] coords, double[] data )
+    abstract public void setSimpleData( double[] coords, double[] data )
         throws SplatException;
 
-    /**
-     * Change the spectrum data and WCS. Takes a copy of all data.
-     *
-     * @param frameSet the FrameSet to be used for generating
-     *                 coordinates.
-     * @param data the spectrum data values.
-     */
-    abstract public void setData( FrameSet frameSet, double[] data )
+    abstract public void setSimpleUnitData( FrameSet frameSet, double[] coords,
+                                            double[] data )
         throws SplatException;
 
-    /**
-     * Change the complete spectrum data. Original data is not copied.
-     *
-     * @param coords the spectrum coordinates, one per data value.
-     * @param data the spectrum data values.
-     */
-    abstract public void setDataQuick( double[] coords, double[] data )
+    abstract public void setFullData( FrameSet frameSet, double[] data )
         throws SplatException;
 
-    /**
-     * Change the spectrum data and WCS. Original data is not copied.
-     *
-     * @param frameSet the FrameSet to be used for generating
-     *                 coordinates.
-     * @param data the spectrum data values.
-     */
-    abstract public void setDataQuick( FrameSet frameSet, double[] data )
+    abstract public void setSimpleDataQuick( double[] coords, double[] data )
         throws SplatException;
 
-    /**
-     * Change the complete spectrum data. Takes a copy of all data.
-     *
-     * @param coords the spectrum coordinates, one per data value.
-     * @param data the spectrum data values.
-     * @param errors the errors of the spectrum data values.
-     */
-    abstract public void setData( double[] coords, double[] data, 
-                                  double[] errors )
+    abstract public void setSimpleUnitDataQuick( FrameSet frameSet, double[] coords,
+                                                 double[] data )
         throws SplatException;
 
-    /**
-     * Change the spectrum data and WCS. Takes a copy of all data.
-     *
-     * @param frameSet the FrameSet to be used for generating
-     *                 coordinates.
-     * @param data the spectrum data values.
-     * @param errors the errors of the spectrum data values.
-     */
-    abstract public void setData( FrameSet frameSet, double[] data, 
-                                  double[] errors )
+    abstract public void setFullDataQuick( FrameSet frameSet, double[] data )
         throws SplatException;
 
-    /**
-     * Change the complete spectrum data. Original data is not copied.
-     *
-     * @param coords the spectrum coordinates, one per data value.
-     * @param data the spectrum data values.
-     * @param errors the errors of the spectrum data values.
-     */
-    abstract public void setDataQuick( double[] coords, double[] data, 
-                                       double[] errors )
+    abstract public void setSimpleData( double[] coords,  double[] data,
+                                        double[] errors )
         throws SplatException;
 
-    /**
-     * Change the complete spectrum data. Original data is not copied.
-     *
-     * @param frameSet the FrameSet to be used for generating
-     *                 coordinates.
-     * @param data the spectrum data values.
-     * @param errors the errors of the spectrum data values.
-     */
-    abstract public void setDataQuick( FrameSet frameSet, double[] data, 
-                                       double[] errors )
+    abstract public void setSimpleUnitData( FrameSet frameSet, double[] coords,
+                                            double[] data, double[] errors )
         throws SplatException;
 
-    /**
-     * Change a coordinate value.
-     */
+    abstract public void setFullData( FrameSet frameSet, double[] data,
+                                      double[] errors )
+        throws SplatException;
+
+    abstract public void setSimpleDataQuick( double[] coords, double[] data, 
+                                             double[] errors )
+        throws SplatException;
+
+    abstract public void setSimpleUnitDataQuick( FrameSet frameSet, double[] coords,
+                                                 double[] data, double[] errors )
+        throws SplatException;
+
+    abstract public void setFullDataQuick( FrameSet frameSet, double[] data,
+                                           double[] errors )
+        throws SplatException;
+
     abstract public void setXDataValue( int index, double value )
         throws SplatException;
 
-    /**
-     * Change a data value.
-     */
     abstract public void setYDataValue( int index, double value )
         throws SplatException;
 
-    /**
-     * Change a data error value.
-     */
     abstract public void setYDataErrorValue( int index, double value )
         throws SplatException;
 
-    /**
-     * Set the FrameSet used for the coordinate system. This should
-     * be one-dimensional (at the base and current frames at least)
-     * and map the base coordinates to some wavelength-related
-     * coordinate.
-     */
     abstract public void setAst( FrameSet frameSet )
         throws SplatException;
 }
