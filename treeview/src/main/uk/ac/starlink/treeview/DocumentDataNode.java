ebf7899cd0882de5bd590f437958c0a6319ee687
Mark Taylor <m.b.taylor@bristol.ac.uk>
2004-06-16 10:01:00 +0000

    Added constructor for javax.xml.transform.Source.

--- treeview/src/main/uk/ac/starlink/treeview/DocumentDataNode.java
+++ treeview/src/main/uk/ac/starlink/treeview/DocumentDataNode.java
@@ -10,13 +10,18 @@ import javax.swing.JComponent;
 import javax.swing.JTextArea;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Source;
+import javax.xml.transform.TransformerException;
 import javax.xml.transform.dom.DOMSource;
 import org.xml.sax.Attributes;
 import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
+import org.xml.sax.helpers.AttributesImpl;
+import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
+import uk.ac.starlink.util.SourceReader;
 
 /**
  * DataNode implementation which describes a top-level XML document.
@@ -26,17 +31,68 @@ import org.w3c.dom.Node;
  */
 public class DocumentDataNode extends DefaultDataNode {
 
-    public final XMLDocument xdoc;
-    private List parseLog;
-    private DOMSource docsrc;
-    private Exception parseError;
-    private boolean doingParse;
-    private final Object parseLock = new Object();
+    private final XMLDocument xdoc_;
+    private final String systemId_;
+    private final String publicId_;
+    private final String topLocalName_;
+    private final String topNamespaceURI_;
+    private final Attributes topAttributes_;
+    private List parseLog_;
+    private DOMSource docsrc_;
+    private Exception parseError_;
+    private boolean doingParse_;
+    private final Object parseLock_ = new Object();
 
     public DocumentDataNode( XMLDocument xdoc ) {
-        this.xdoc = xdoc;
-        setName( "<" + xdoc.getTopLocalName() + ">" );
+        xdoc_ = xdoc;
+        systemId_ = xdoc.getSystemId();
+        publicId_ = xdoc.getPublicId();
+        topLocalName_ = xdoc.getTopLocalName();
+        topNamespaceURI_ = xdoc.getTopNamespaceURI();
+        topAttributes_ = xdoc.getTopAttributes();
+        setName( "<" + topAttributes_ + ">" );
         setIconID( IconFactory.XML_DOCUMENT );
+    }
+
+    public DocumentDataNode( Source xsrc ) throws NoSuchDataException {
+        xdoc_ = null;
+        if ( xsrc instanceof DOMSource ) {
+            docsrc_ = (DOMSource) xsrc;
+        }
+        else {
+            try {
+                docsrc_ = new DOMSource( new SourceReader().getDOM( xsrc ) );
+                docsrc_.setSystemId( xsrc.getSystemId() );
+            }
+            catch ( TransformerException e ) {
+                throw new NoSuchDataException( "Parse failed", e );
+            }
+        }
+        systemId_ = docsrc_.getSystemId();
+        publicId_ = null;
+        Node topNode = docsrc_.getNode();
+        Element topEl;
+        if ( topNode instanceof Document ) {
+            topEl = ((Document) topNode).getDocumentElement();
+        }
+        else if ( topNode instanceof Element ) {
+            topEl = (Element) topNode;
+        }
+        else {
+            throw new NoSuchDataException( "Strange node " + topNode );
+        }
+        topLocalName_ = topEl.getLocalName();
+        topNamespaceURI_ = topEl.getNamespaceURI();
+        topAttributes_ = new AttributesImpl();
+        for ( Node node = topEl.getFirstChild(); node != null;
+              node = node.getNextSibling() ) {
+            if ( node instanceof Attr ) {
+                Attr att = (Attr) node;
+                ((AttributesImpl) topAttributes_)
+                    .addAttribute( att.getNamespaceURI(), att.getLocalName(),
+                                   att.getName(), "CDATA", att.getValue() );
+            }
+        }
     }
 
     public boolean allowsChildren() {
@@ -55,11 +111,33 @@ public class DocumentDataNode extends DefaultDataNode {
         return "#";
     }
 
+    protected void checkTopElement( String localName, String namespaceURI ) 
+            throws NoSuchDataException {
+        checkTopLocalName( localName );
+        boolean reqBlank = namespaceURI == null 
+                         || namespaceURI.trim().length() == 0;
+        boolean gotBlank = topNamespaceURI_ == null
+                         || topNamespaceURI_.trim().length() == 0;
+        if ( ! ( reqBlank && gotBlank ||
+                 namespaceURI.equals( topNamespaceURI_ ) ) ) {
+            throw new NoSuchDataException( "Namespace is " + topNamespaceURI_ +
+                                           " not " + namespaceURI );
+        }
+    }
+
+    protected void checkTopLocalName( String localName ) 
+            throws NoSuchDataException {
+        if ( ! localName.equals( topLocalName_ ) ) {
+            throw new NoSuchDataException( "Document type is " + topLocalName_ +
+                                           " not " + localName );
+        }
+    }
+
     protected Iterator getChildIterator( final Node parentNode ) {
         return new Iterator() {
             private Node next =
                 XMLDataNode.firstUsefulSibling( parentNode.getFirstChild() );
-            private String systemId = xdoc.getSystemId();
+            private String systemId = xdoc_.getSystemId();
             public boolean hasNext() {
                 return next != null;
             }
@@ -98,30 +176,30 @@ public class DocumentDataNode extends DefaultDataNode {
     }
 
     public void configureDetail( DetailViewer dv ) {
-        String systemId = xdoc.getSystemId();
-        String publicId = xdoc.getPublicId();
-        String localName = xdoc.getTopLocalName();
-        String namespaceURI = xdoc.getTopNamespaceURI();
-        Attributes atts = xdoc.getTopAttributes();
-        if ( systemId != null && systemId.trim().length() > 0 ) {
-            dv.addKeyedItem( "System ID", systemId );
+        if ( systemId_ != null && systemId_.trim().length() > 0 ) {
+            dv.addKeyedItem( "System ID", systemId_ );
         }
-        if ( publicId != null && publicId.trim().length() > 0 ) {
-            dv.addKeyedItem( "Public ID", publicId );
+        if ( publicId_ != null && publicId_.trim().length() > 0 ) {
+            dv.addKeyedItem( "Public ID", publicId_ );
         }
         dv.addSeparator();
-        if ( namespaceURI == null || namespaceURI.trim().length() == 0 ) {
-            dv.addKeyedItem( "Document Type", localName );
+        if ( topNamespaceURI_ == null || 
+             topNamespaceURI_.trim().length() == 0 ) {
+            dv.addKeyedItem( "Document Type", topLocalName_ );
         }
         else {
-            dv.addKeyedItem( "Document Type", localName );
-            dv.addKeyedItem( "Root Namespace", namespaceURI );
+            dv.addKeyedItem( "Document Type", topLocalName_ );
+            dv.addKeyedItem( "Root Namespace", topNamespaceURI_ );
         }
-        if ( atts != null && atts.getLength() > 0 ) {
+        if ( topAttributes_ != null && topAttributes_.getLength() > 0 ) {
             dv.addSubHead( "Top element attributes" );
-            int natt = atts.getLength();
+            int natt = topAttributes_.getLength();
             for ( int i = 0; i < natt; i++ ) {
-                dv.addKeyedItem( atts.getQName( i ), atts.getValue( i ) );  
+                String attName = topAttributes_.getQName( i );
+                if ( attName == null ) {
+                    attName = topAttributes_.getLocalName( i );
+                }
+                dv.addKeyedItem( attName, topAttributes_.getValue( i ) );  
             }
         }
 
@@ -141,32 +219,34 @@ public class DocumentDataNode extends DefaultDataNode {
         } );
 
         /* Add a pane with parse results. */
-        dv.addPane( "Parse results", new ComponentMaker() {
-            public JComponent getComponent() {
-                final JTextArea parseDoc = new JTextArea();
-                new Thread( "Document parser" ) {
-                    public void run() {
-                        try {
-                            doParse();
-                        }
-                        catch ( SAXException e ) {
-                        }
-                        catch ( IOException e ) {
-                        }
-                        Iterator it = parseLog.iterator();
-                        if ( it.hasNext() ) {
-                            while ( it.hasNext() ) {
-                                parseDoc.append( it.next().toString() + '\n' );
+        if ( xdoc_ != null ) {
+            dv.addPane( "Parse results", new ComponentMaker() {
+                public JComponent getComponent() {
+                    final JTextArea parseDoc = new JTextArea();
+                    new Thread( "Document parser" ) {
+                        public void run() {
+                            try {
+                                doParse();
+                            }
+                            catch ( SAXException e ) {
+                            }
+                            catch ( IOException e ) {
+                            }
+                            Iterator it = parseLog_.iterator();
+                            if ( it.hasNext() ) {
+                                while ( it.hasNext() ) {
+                                    parseDoc.append( it.next() + "\n" );
+                                }
+                            }
+                            else {
+                                parseDoc.append( "Parse successful" );
                             }
                         }
-                        else {
-                            parseDoc.append( "Parse successful" );
-                        }
-                    }
-                }.start();
-                return parseDoc;
-            }
-        } );
+                    }.start();
+                    return parseDoc;
+                }
+            } );
+        }
     }
 
     /**
@@ -175,66 +255,67 @@ public class DocumentDataNode extends DefaultDataNode {
      * already available a parse will be performed to construct it.
      */
     protected DOMSource getDocument() throws IOException, SAXException {
-        synchronized ( parseLock ) {
-            while ( doingParse ) {
+        synchronized ( parseLock_ ) {
+            while ( doingParse_ ) {
                 try {
-                    parseLock.wait();
+                    parseLock_.wait();
                 }
                 catch ( InterruptedException e ) {
                     throw (IOException) new IOException( "Interrupted?" )
                                        .initCause( e );
                 }
             }
-            if ( parseError != null ) {
-                if ( parseError instanceof IOException ) {
-                    throw (IOException) parseError;
+            if ( parseError_ != null ) {
+                if ( parseError_ instanceof IOException ) {
+                    throw (IOException) parseError_;
                 }
-                else if ( parseError instanceof SAXException ) {
-                    throw (SAXException) parseError;
+                else if ( parseError_ instanceof SAXException ) {
+                    throw (SAXException) parseError_;
                 }
                 else {
-                    throw new SAXException( parseError );
+                    throw new SAXException( parseError_ );
                 }
             }
-            if ( docsrc == null ) {
+            if ( docsrc_ == null ) {
+                assert xdoc_ != null;
                 doParse();
             }
-            assert docsrc != null;
-            return docsrc;
+            assert docsrc_ != null;
+            return docsrc_;
         }
     }
 
     private void doParse() throws SAXException, IOException {
-        synchronized ( parseLock ) {
-            doingParse = true;
-            parseLog = new ArrayList();
+        synchronized ( parseLock_ ) {
+            doingParse_ = true;
+            parseLog_ = new ArrayList();
             ErrorHandler ehandler = new ErrorHandler() {
                 public void error( SAXParseException e ) {
-                    parseLog.add( e.toString() );
+                    parseLog_.add( e.toString() );
                 }
                 public void warning( SAXParseException e ) {
-                    parseLog.add( e.toString() );
+                    parseLog_.add( e.toString() );
                 }
                 public void fatalError( SAXParseException e )
                         throws SAXException {
-                    parseError = e;
-                    parseLog.add( e.toString() );
+                    parseError_ = e;
+                    parseLog_.add( e.toString() );
                     throw e;
                 }
             };
             try {
-                docsrc = xdoc.parseToDOM( true, ehandler );
+                docsrc_ = xdoc_.parseToDOM( true, ehandler );
             }
             catch ( IOException e ) {
-                parseError = e;
+                parseError_ = e;
                 throw e;
             }
             catch ( SAXException e ) {
-                parseError = e;
+                parseError_ = e;
                 throw e;
             }
-            doingParse = false;
-            parseLock.notifyAll();
+            doingParse_ = false;
+            parseLock_.notifyAll();
         }
     }
  
