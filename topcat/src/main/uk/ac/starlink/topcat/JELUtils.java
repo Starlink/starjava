c102f718e380b0e0979766832706ad63ffc6276b
Mark Taylor <m.b.taylor@bristol.ac.uk>
2004-08-09 18:28:58 +0000

    Removed the one method which was used as a static method available from JEL expressions.  This has been replaced by a 'special' method in JELRowReader.

--- topcat/src/main/uk/ac/starlink/topcat/JELUtils.java
+++ topcat/src/main/uk/ac/starlink/topcat/JELUtils.java
@@ -13,8 +13,7 @@ import uk.ac.starlink.topcat.func.Miscellaneous;
 
 /**
  * This class provides some utility methods for use with the JEL
- * expression compiler.  The methods provided here are static ones which
- * complement the per-row ones provided by the {@link JELRowReader} class.
+ * expression compiler.
  *
  * @author   Mark Taylor (Starlink)
  */
@@ -23,27 +22,6 @@ public class JELUtils {
     private static List staticClasses;
     public static final String AUX_CLASSES_PROPERTY = "gnu.jel.static.classes";
     private static Logger logger = Logger.getLogger( "uk.ac.starlink.topcat" );
-
-    /**
-     * This method is designed to be installed in a JEL library and
-     * referenced by JEL expressions compiled at runtime.  It returns
-     * an object which can be viewed as a numeric primitive by the
-     * compiler but which generates a NullPointerException when 
-     * evaluated.  It works using JEL's 'Automatic unwrapping of objects
-     * to primitive types' functionality - JEL can view it as a
-     * <tt>byte</tt> value which can be promoted to any other 
-     * numeric primitive type since its declared type implements 
-     * the {@link gnu.jel.reflect.Byte} interface.  It would be better
-     * if it could implement all the <tt>gnu.jel.reflect</tt> 
-     * reflection interfaces, but this is impossible(?) since each
-     * declares a method with the same name and args (<tt>getValue()</tt>)
-     * but a different return type.  Thus this will not work 
-     * (it will generate a compilation error) if used in an expression
-     * representing a <tt>char</tt> or <tt>boolean</tt> primitive.
-     */
-    public static gnu.jel.reflect.Byte NULL() {
-        return null;
-    }
 
     /**
      * Returns a JEL Library suitable for expression evaluation on this
@@ -77,7 +55,6 @@ public class JELUtils {
 
             /* Basic classes always present. */
             List classList = new ArrayList( Arrays.asList( new Class[] {
-                JELUtils.class,
                 Math.class, Integer.class, Float.class, Double.class,
                 Angles.class, Miscellaneous.class,
             } ) );
