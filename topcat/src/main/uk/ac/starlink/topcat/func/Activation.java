ccdb9975d228d245955d5f366026f94c1785567c
Mark Taylor <m.b.taylor@bristol.ac.uk>
2004-08-25 13:58:39 +0000

    Added SPLAT invocation.

--- topcat/src/main/uk/ac/starlink/topcat/func/Activation.java
+++ topcat/src/main/uk/ac/starlink/topcat/func/Activation.java
@@ -3,10 +3,20 @@ package uk.ac.starlink.topcat.func;
 import java.awt.Component;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import uk.ac.starlink.topcat.TopcatUtils;
 import uk.ac.starlink.sog.SOG;
 import uk.ac.starlink.sog.SOGNavigatorImageDisplay;
+import uk.ac.starlink.splat.data.SpecData;
+import uk.ac.starlink.splat.data.SpecDataComp;
+import uk.ac.starlink.splat.data.SpecDataFactory;
+import uk.ac.starlink.splat.iface.PlotControlFrame;
+import uk.ac.starlink.splat.iface.SplatBrowser;
+import uk.ac.starlink.splat.iface.SplatBrowserMain;
+import uk.ac.starlink.splat.plot.PlotControl;
+import uk.ac.starlink.splat.util.SplatException;
 
 /**
  * Class containing static methods suitable for use during point activation.
@@ -18,6 +28,10 @@ public class Activation {
 
     private static SOG sog_;
     private static List soggers_ = new ArrayList();
+    private static SplatBrowser splat_;
+    private static Map splatSpectra_ = new HashMap();
+    private static PlotControlFrame splatPlotFrame_;
+    private static PlotControl splatPlot_;
     
     /**
      * Outputs a string value to the user log.
@@ -54,7 +68,7 @@ public class Activation {
     /**
      * Displays the file at a given location as an image 
      * in a graphical (SoG) viewer.
-     * <tt>val</tt> should be a filename or URL, pointing to an image in 
+     * <tt>loc</tt> should be a filename or URL, pointing to an image in 
      * a format that SOG understands (this includes FITS, compressed FITS,
      * and NDFs).
      * 
@@ -84,10 +98,10 @@ public class Activation {
      * Generic routine for displaying multiple images simultaneously in
      * SoG viewers.
      * 
-     * @param  locs  array of image file locations
+     * @param  locs  array of image file locations (file/URL)
      * @return  short report message
      */
-    private static String multiSog( String[] locs ) {
+    public static String multiSog( String[] locs ) {
         if ( ! TopcatUtils.canSog() ) {
             return "Error: SOG classes not available";
         }
@@ -95,12 +109,13 @@ public class Activation {
         SOGNavigatorImageDisplay[] sogs = getSoggers( nsog );
         String[] msgs = new String[ nsog ];
         for ( int i = 0; i < nsog; i++ ) {
-            String loc = locs[ i ].trim();
+            String loc = locs[ i ];
             String msg;
-            if ( loc == null || loc.length() == 0 ) {
+            if ( loc == null || loc.trim().length() == 0 ) {
                 msg = null;
             }
             else {
+                loc = loc.trim();
                 try {
                     sogs[ i ].setFilename( loc, false );
                     msg = "sog(\"" + loc + "\")";
@@ -149,17 +164,159 @@ public class Activation {
         SOGNavigatorImageDisplay[] sogs = new SOGNavigatorImageDisplay[ nsog ];
         for ( int i = 0; i < nsog; i++ ) {
             sogs[ i ] = (SOGNavigatorImageDisplay) soggers_.get( i );
+            if ( ! sogs[ i ].isShowing() ) {
+                sogs[ i ].setVisible( true );
+            }
         }
         return sogs;
     }
 
-    public static String splat( Object val ) {
-        if ( val == null ) {
-            return "No SPLAT target";
+    /**
+     * Displays the resource at a given location as a spectrum in a 
+     * spectrum viewer program (SPLAT).
+     * <tt>loc</tt> should be a filename pointing to a spectrum
+     * in a format that SPLAT understands (includes FITS, NDF).
+     * In some cases, a URL can be used too.
+     *
+     * @param   loc  spectrum location
+     * @return  short log message
+     * @see     <http://www.starlink.ac.uk/splat/>
+     */
+    public static String splat( String loc ) {
+        return multiSplat( new String[] { loc } );
+    }
+
+    /**
+     * Displays two spectra in the same (SPLAT) viewer.  This may be useful
+     * to compare two spectra which correspond to the same table row.
+     *
+     * @param  loc1   location of the first spectrum
+     * @param  loc2   location of the second spectrum
+     */
+    public static String splat2( String loc1, String loc2 ) {
+        return multiSplat( new String[] { loc1, loc2 } );
+    }
+
+    /**
+     * Generic routine for displaying multiple spectra simultaneously in the
+     * same SPLAT plot.
+     * 
+     * @param  locs  array of spectrum locations (file, or in some cases URL)
+     * @return short report message
+     */
+    public static String multiSplat( String[] locs ) {
+
+        /* Check that classes are available. */
+        if ( ! TopcatUtils.canSplat() ) {
+            return "Error: SPLAT classes not available";
         }
-        else {
-            return "splat " + val;
+        SplatBrowser splat = getSplat();
+
+        /* Get the SpecData objects corresponding to the locations we have. */
+        int nsplat = locs.length;
+        String[] msgs = new String[ nsplat ];
+        SpecDataFactory splatFac = SpecDataFactory.getInstance();
+        SpecDataComp specGroup = new SpecDataComp();
+        for ( int i = 0; i < nsplat; i++ ) {
+            String loc = locs[ i ];
+            if ( loc == null || loc.trim().length() == 0 ) {
+                msgs[ i ] = null;
+            }
+            else {
+                loc = loc.trim();
+                if ( ! splatSpectra_.containsKey( loc ) ) {
+                    try {
+                        SpecData spec = splatFac.get( loc );
+                        splat_.addSpectrum( spec );
+                        splatSpectra_.put( loc, spec );
+                        specGroup.add( spec );
+                        msgs[ i ] = loc;
+                    }
+                    catch ( Exception e ) {
+                        splatSpectra_.put( loc, null );
+                        msgs[ i ] = "<Error: " + e.getMessage() + ">";
+                    }
+                }
+                else {
+                    if ( loc != null ) {
+                        SpecData spec = (SpecData) splatSpectra_.get( loc );
+                        if ( spec != null ) {
+                            msgs[ i ] =  loc;
+                            specGroup.add( spec );
+                        }
+                        else {
+                            msgs[ i ] = "<No data>";
+                        }
+                    }
+                    else {
+                        msgs[ i ] = "<No data>";
+                    }
+                }
+            }
         }
+
+        /* Work out the successful plot output message. */
+        StringBuffer sbuf = new StringBuffer()
+                           .append( "splat(" );
+        for ( int i = 0; i < nsplat; i++ ) {
+            if ( i > 0 ) {
+                sbuf.append( ',' );
+            }
+            sbuf.append( msgs[ i ] );
+        }
+        sbuf.append( ")" );
+        String msg = sbuf.toString();
+
+        /* If we don't already have a plot, get one now.  We initialize it
+         * with the first spectrum for display, but this is just a dummy. */
+        if ( splatPlot_ == null && specGroup.count() > 0 ) {
+            synchronized ( Activation.class ) {
+                splatPlotFrame_ = splat.displaySpectrum( specGroup.get( 0 ) );
+                splatPlot_ = splatPlotFrame_.getPlot();
+            }
+        }
+
+        /* Configure the plot to contain the list of spectra for display. */
+        if ( splatPlot_ != null ) {
+            try {
+                splatPlot_.setSpecDataComp( specGroup );
+                splatPlot_.updateThePlot( null );
+            }
+            catch ( SplatException e ) {
+                return "<SPLAT Error: " + e.getMessage() + ">";
+            }
+        }
+
+        /* Ensure the plot window is visible. */
+        if ( ! splatPlotFrame_.isShowing() ) {
+            splatPlotFrame_.show();
+        }
+
+        /* Return the log message. */
+        return msg;
+    }
+
+    /**
+     * Returns a SplatBrowser window, creating one lazily if necessary.
+     *
+     * @return  splat top-level window
+     */
+    private static SplatBrowser getSplat() {
+        if ( splat_ == null ) {
+            synchronized ( Activation.class ) {
+
+                /* Application-level initialisations for SPLAT. */
+                SplatBrowserMain.guessProperties();
+
+                /* Create the browser. */
+                splat_ = new SplatBrowser( true );
+
+                /* But iconize this so that we see only the plots. */
+                splat_.setExtendedState( splat_.ICONIFIED );
+            }
+        }
+        splat_.setVisible( true );
+        return splat_;
     }
 
 }
