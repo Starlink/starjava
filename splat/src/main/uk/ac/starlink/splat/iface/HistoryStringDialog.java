a3194e973fd3f3ecb224359f8e9b13ed6d42d3be
Peter W. Draper <p.w.draper@durham.ac.uk>
2004-08-31 18:05:35 +0000

    Mostly working now.

--- splat/src/main/uk/ac/starlink/splat/iface/HistoryStringDialog.java
+++ splat/src/main/uk/ac/starlink/splat/iface/HistoryStringDialog.java
@@ -8,22 +8,30 @@
 
 package uk.ac.starlink.splat.iface;
 
-import java.awt.Component;
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
+import java.util.Vector;
+
+import javax.swing.AbstractListModel;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.MutableComboBoxModel;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JComboBox;
+
+import uk.ac.starlink.util.gui.GridBagLayouter;
 
 /**
  * Create a dialog for obtaining a single {@link String} value.
@@ -38,12 +46,17 @@ import javax.swing.JComboBox;
 
 public class HistoryStringDialog
     extends JDialog
+    implements ActionListener, KeyListener
 {
     /** The JComboBox used to get value */
-    protected JComboBox comboBox = new JComboBox();
+    protected JComboBox comboBox = new JComboBox( new UniqueModel() );
 
     /** The accepted value, null if not accepted */
     protected String value = null;
+
+    /** Buttons */
+    protected JButton okButton = new JButton( "OK" );
+    protected JButton cancelButton = new JButton( "Cancel" );
 
     /**
      * Show a dialog window. Returning the user value.
@@ -67,44 +80,31 @@ public class HistoryStringDialog
      * @param title the title for the dialog window
      * @param labelText description of the value that is to be obtained
      */
-    public HistoryStringDialog( Component component, String title, 
+    public HistoryStringDialog( Component component, String title,
                                 String labelText )
     {
         super( JOptionPane.getFrameForComponent( component ), title, true );
 
         //  Buttons.
-        JButton okButton = new JButton( "OK" );
-        okButton.addActionListener( new ActionListener()
-            {
-                public void actionPerformed( ActionEvent e )
-                {
-                    acceptValue();
-                    closeWindow();
-                }
-            });
-
-        JButton cancelButton = new JButton( "Cancel" );
-        cancelButton.addActionListener( new ActionListener()
-            {
-                public void actionPerformed( ActionEvent e )
-                {
-                    closeWindow();
-                }
-            });
+        okButton.addActionListener( this );
+        cancelButton.addActionListener( this );
         getRootPane().setDefaultButton( okButton );
 
-        //  Need to edit the JComboBox.
+        //  JComboBox editable and <return> accepts.
         comboBox.setEditable( true );
+
+        comboBox.getEditor().getEditorComponent().addKeyListener( this );
+
+        //  Make typical size longer.
+        comboBox.setPrototypeDisplayValue
+            ("                                                ");
 
         //  Label and JComboBox
         JPanel actionPane = new JPanel();
-        JLabel label = new JLabel( labelText + ": " );
-        actionPane.setLayout( new BoxLayout( actionPane, BoxLayout.X_AXIS ) );
-        actionPane.setBorder( BorderFactory.createEmptyBorder( 0, 10, 10, 10 ) );
-        //actionPane.add( Box.createHorizontalGlue() );
-        actionPane.add( label );
-        actionPane.add( Box.createRigidArea( new Dimension( 10, 0 ) ) );
-        actionPane.add( comboBox );
+        GridBagLayouter layouter =
+            new GridBagLayouter( actionPane, GridBagLayouter.SCHEME3 );
+        layouter.add( labelText + ": " );
+        layouter.add( comboBox, true );
 
         //  Lay out the buttons from left to right.
         JPanel buttonPane = new JPanel();
@@ -134,7 +134,19 @@ public class HistoryStringDialog
      */
     protected void acceptValue()
     {
-        value = (String) comboBox.getEditor().getItem();
+        String newValue = (String) comboBox.getEditor().getItem();
+        if ( newValue != value ) {
+            value = newValue;
+            comboBox.addItem( value );
+        }
+    }
+
+    /**
+     * Clear the value.
+     */
+    protected void clearValue()
+    {
+        value = null;
     }
 
     /**
@@ -143,5 +155,118 @@ public class HistoryStringDialog
     protected void closeWindow()
     {
         setVisible( false );
+    }
+
+    // ActionListener interface.
+    public void actionPerformed( ActionEvent e )
+    {
+        Object source = e.getSource();
+        if ( source.equals( okButton ) ) {
+            acceptValue();
+        }
+        else if ( source.equals( cancelButton ) ) {
+            clearValue();
+        }
+        closeWindow();
+        return;
+    }
+
+    // KeyListener interface.
+
+    public void keyPressed( KeyEvent e )
+    {
+        if ( e.getKeyCode() == KeyEvent.VK_ENTER ) {
+            acceptValue();
+            closeWindow();
+        }
+    }
+    public void keyReleased( KeyEvent e )
+    {
+        //  Do nothing.
+    }
+    public void keyTyped( KeyEvent e )
+    {
+        // Do nothing.
+    }
+
+    /**
+     * ComboBoxModel that maintains a unique set of history values.
+     */
+    protected class UniqueModel
+        extends AbstractListModel
+        implements MutableComboBoxModel
+    {
+        private Vector vector = new Vector();
+        private int selected = -1;
+
+        public UniqueModel()
+        {
+            // Do nothing.
+        }
+
+        public Object getSelectedItem()
+        {
+            if ( selected != -1 ) {
+                return vector.get( selected );
+            }
+            return null;
+        }
+
+        public void setSelectedItem( Object anItem )
+        {
+            selected = vector.indexOf( anItem );
+        }
+
+        public Object getElementAt( int index )
+        {
+            return vector.get( index );
+        }
+
+        public int getSize()
+        {
+            return vector.size();
+        }
+
+        //  Do not add Objects that are already stored (repeated Strings)
+        //  or blank Strings.
+        public void addElement( Object anItem )
+        {
+            if ( anItem instanceof String ) {
+                if ( ( (String) anItem).equals( "" ) ) {
+                    return;
+                }
+            }
+
+            int i = vector.indexOf( anItem );
+            if ( i == -1 ) {
+                vector.add( anItem );
+                int pos = vector.size() - 1;
+                fireIntervalAdded( anItem, pos, pos );
+            }
+        }
+
+        public void insertElementAt( Object anItem, int index )
+        {
+            int i = vector.indexOf( anItem );
+            if ( i == -1 ) {
+                vector.add( index, anItem );
+                fireIntervalAdded( anItem, index, index );
+            }
+        }
+
+        public void removeElement( Object anItem )
+        {
+            int pos = vector.indexOf( anItem );
+            vector.remove( anItem );
+            fireIntervalRemoved( anItem, pos, pos );
+        }
+
+        public void removeElementAt( int index )
+        {
+            Object anItem = vector.get( index );
+            vector.remove( index );
+            fireIntervalRemoved( anItem, index, index );
+        }
+
     }
 }
