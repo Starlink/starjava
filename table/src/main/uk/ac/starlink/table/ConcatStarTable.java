13c9b3cc730173f5a15f4c540028bd1039f4fab9
Mark Taylor <m.b.taylor@bristol.ac.uk>
2004-09-24 13:35:29 +0000

    Simplified ConcatStarTable implementation

--- table/src/main/uk/ac/starlink/table/ConcatStarTable.java
+++ table/src/main/uk/ac/starlink/table/ConcatStarTable.java
@@ -1,85 +1,90 @@
 package uk.ac.starlink.table;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 
 /**
- * Table representing the concatenation of rows from two tables.
+ * Table representing the concatenation of rows from a list of tables.
  * The first (base) table provides all the metadata plus the first
- * lot of rows, and the second (secondary) one provides only additional rows.
- * Of course it must be specified which columns in the secondary table
- * correspond to the columns in the base.
+ * lot of rows, and subsequent ones provide only additional rows.
+ * All the constituent tables have to have similar columns in the same
+ * order (compatible column class types) or there will be trouble when
+ * they come to be read.
  * 
  * @author   Mark Taylor (Starlink)
  * @since    26 Mar 2004
  */
 public class ConcatStarTable extends WrapperStarTable {
 
-    private int[] colMap;
-    private final StarTable t1;
-    private final StarTable t2;
+    private final StarTable[] tables_;
+
+    private static final RowSequence EMPTY_SEQUENCE =
+        new IteratorRowSequence( new ArrayList().iterator() );
  
     /**
      * Constructs a new concatenated table.
-     * The supplied <tt>colMap</tt> array defines which columns in the
-     * secondary correspond to which columns in the base;
-     * <code>colMap[iBase]=iSecondary</code> means that 
-     * the data in column <tt>iBase</tt> of this table are supplied 
-     * from column <tt>iSecondary</tt> of <tt>secondary</tt>.
-     * If <tt>iSecondary&lt;0</tt> or <tt>colMap.length&lt;=iBase</tt> 
-     * then the cells in column <tt>iBase</tt> are all <tt>null</tt>
-     * for the secondary part of the table.
      *
-     * @param   base  base table
-     * @param   secondary  secondary table
-     * @param   colMap  mapping of columns in the base to columns in the
-     *          secondary
+     * @param   tables   array of constituent tables
      */
-    public ConcatStarTable( StarTable base, StarTable secondary,
-                            int[] colMap ) {
-        super( base );
-        this.colMap = colMap;
-        this.t1 = base;
-        this.t2 = secondary;
+    public ConcatStarTable( StarTable[] tables ) {
+        super( tables[ 0 ] );
+        tables_ = tables;
     }
 
     public long getRowCount() {
-        long n1 = t1.getRowCount();
-        long n2 = t2.getRowCount();
-        return ( n1 >= 0L && n2 >= 0L ) ? n1 + n2 : -1L;
+        long nrow = 0;
+        for ( int i = 0; i < tables_.length; i++ ) {
+            long nr = tables_[ i ].getRowCount();
+            if ( nr < 0 ) {
+                return -1L;
+            }
+            else {
+                nrow += nr;
+            }
+        }
+        return nrow;
     }
 
     public boolean isRandom() {
-        return t1.isRandom() && t2.isRandom();
+        boolean isRandom = true;
+        for ( int i = 0; isRandom && i < tables_.length; i++ ) {
+            isRandom = isRandom && tables_[ i ].isRandom();
+        }
+        return isRandom;
     }
 
     public Object getCell( long irow, int icol ) throws IOException {
         if ( ! isRandom() ) {
             throw new UnsupportedOperationException( "No random access" );
         }
-        long nt1 = t1.getRowCount();
-        if ( irow < nt1 ) {
-            return t1.getCell( irow, icol );
+        for ( int itab = 0; itab < tables_.length; itab++ ) {
+            StarTable t = tables_[ itab ];
+            long nr = t.getRowCount();
+            if ( irow < nr ) {
+                return t.getCell( irow, icol );
+            }
+            irow -= nr;
         }
-        else {
-            long irow2 = irow - nt1;
-            int icol2 = mapColumnIndex( icol );
-            return icol2 < 0 ? null
-                             : t2.getCell( irow2, icol2 );
-        }
+        throw new ArrayIndexOutOfBoundsException( 
+            "Attempt to access row beyond end of table" );
     }
 
     public Object[] getRow( long irow ) throws IOException {
         if ( ! isRandom() ) {
             throw new UnsupportedOperationException( "No random access" );
         }
-        long nt1 = t1.getRowCount();
-        if ( irow < nt1 ) {
-            return t1.getRow( irow );
+        for ( int itab = 0; itab < tables_.length; itab++ ) {
+            StarTable t = tables_[ itab ];
+            long nr = t.getRowCount();
+            if ( irow < nr ) {
+                return t.getRow( irow );
+            }
+            irow -= nr;
         }
-        else {
-            long irow2 = irow - nt1;
-            return mapRow( t2.getRow( irow2 ) );
-        }
+        throw new ArrayIndexOutOfBoundsException( 
+            "Attempt to access row beyond end of table" );
     }
 
     public RowSequence getRowSequence() throws IOException {
@@ -87,125 +92,111 @@ public class ConcatStarTable extends WrapperStarTable {
     }
     
     /**
-     * Returns the column in the secondary table corresponding to a
-     * given column in the base table, or -1 if there isn't a corresponding 
-     * one.
-     *
-     * @param  icol1  base column index
-     * @return   secondary column index, or -1
-     */
-    private int mapColumnIndex( int icol1 ) {
-        return icol1 < colMap.length ? colMap[ icol1 ] : -1;
-    }
-
-    /**
-     * Returns the row of this table which corresponds to a row taken
-     * from the secondary.
-     *
-     * @param  row2  row from the secondary
-     * @return   row in the format of this table
-     */
-    private Object[] mapRow( Object[] row2 ) {
-        int ncol = getColumnCount();
-        Object[] row = new Object[ ncol ];
-        for ( int icol = 0; icol < ncol; icol++ ) {
-            int icol2 = mapColumnIndex( icol );
-            if ( icol2 >= 0 ) {
-                row[ icol ] = row2[ icol2 ];
-            }
-        }
-        return row;
-    }
-
-    /**
      * Implements RowSequence for a concatenated table.
      */
     private class ConcatRowSequence implements RowSequence {
-        RowSequence rseq;
-        RowSequence t2seq;
-        boolean onExtras = false;
-        long extraRows = 0L;
-
-        ConcatRowSequence() throws IOException {
-            rseq = t1.getRowSequence();
-            t2seq = t2.getRowSequence();
-        }
+        final Iterator tabIt_ = Arrays.asList( tables_ ).iterator();
+        RowSequence rseq_ = EMPTY_SEQUENCE;
+        long extraRows_;
+        boolean finished_;
 
         public void next() throws IOException {
-            if ( rseq.hasNext() ) {
-                rseq.next();
-            }
-            else if ( ! onExtras ) {
-                rseq = getSecondaryRowSequence();
-                rseq.next();
+            if ( hasNext() ) {
+                assert rseq_.hasNext();
+                rseq_.next();
             }
             else {
-                assert ! hasNext();
                 throw new IllegalStateException( "No more rows" );
             }
         }
 
         public boolean hasNext() {
-            if ( rseq.hasNext() ) {
-                return true;
-            }
-            else if ( ! onExtras ) {
-                rseq = getSecondaryRowSequence();
-                return rseq.hasNext();
+            if ( finished_ ) {
+                return false;
             }
             else {
-                return false;
+                while ( ! rseq_.hasNext() ) {
+                    extraRows_ += rseq_.getRowIndex() + 1;
+                    try {
+                        rseq_.close();
+                    }
+                    catch ( IOException e ) {
+                        // surely this doesn't matter
+                    }
+                    if ( tabIt_.hasNext() ) {
+                        try {
+                            rseq_ = ((StarTable) tabIt_.next())
+                                   .getRowSequence();
+                        }
+                        catch ( IOException e ) {
+                            rseq_ = new ErrorRowSequence( e );
+                        }
+                    } 
+                    else {
+                        rseq_ = EMPTY_SEQUENCE;
+                        finished_ = true;
+                        return false;
+                    }
+                }
+                return true;
             }
         }
 
+        public void close() throws IOException {
+            rseq_.close();
+        }
+
         public void advance( long nrow ) throws IOException {
-            if ( onExtras ) {
-                rseq.advance( nrow );
-            }
-            else {
-                while ( nrow > 0 ) {
-                    if ( rseq.hasNext() ) {
-                        rseq.next();
-                        nrow--;
-                    }
-                    else {
-                        advance( nrow );
-                        return;
-                    }
+            while ( nrow-- > 0 ) {
+                if ( hasNext() ) {
+                    next();
+                }
+                else {
+                    throw new IOException( "Off end of table" );
                 }
             }
         }
 
         public Object getCell( int icol ) throws IOException {
-            if ( onExtras ) {
-                int icol2 = mapColumnIndex( icol );
-                return icol2 < 0 ? null
-                                 : rseq.getCell( icol2 );
-            }
-            else {
-                return rseq.getCell( icol );
-            }
+            return rseq_.getCell( icol );
         }
 
         public Object[] getRow() throws IOException {
-            if ( onExtras ) {
-                return mapRow( rseq.getRow() );
-            }
-            else {
-                return rseq.getRow();
-            }
+            return rseq_.getRow();
         }
 
         public long getRowIndex() {
-            return rseq.getRowIndex() + extraRows;
+            return rseq_.getRowIndex() + extraRows_;
         }
+    }
 
-        private RowSequence getSecondaryRowSequence() {
-            if ( rseq != t2seq ) {
-                extraRows = rseq.getRowIndex() + 1L;
-                rseq = t2seq;
-            }
-            return t2seq;
+    /**
+     * RowSequence throws an IOException whenever possible.
+     */
+    private static class ErrorRowSequence implements RowSequence {
+        final IOException ex_;
+        ErrorRowSequence( IOException ex ) {
+            ex_ = ex;
+        }
+        public void advance( long nrows ) throws IOException {
+            throw ex_;
+        }
+        public Object getCell( int icol ) throws IOException {
+            throw ex_;
+        }
+        public Object[] getRow() throws IOException {
+            throw ex_;
+        }
+        public void next() throws IOException {
+            throw ex_;
+        }
+        public long getRowIndex() {
+            return 0L;
+        }
+        public boolean hasNext() {
+            return true;  // otherwise the exception will never get seen
+        }
+        public void close() {
         }
     }
 }
