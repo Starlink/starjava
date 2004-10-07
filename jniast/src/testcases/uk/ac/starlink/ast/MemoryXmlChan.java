7ae420dd421579cd42ce18fa68204b8d993d3ba1
Mark Taylor <m.b.taylor@bristol.ac.uk>
2004-10-07 09:05:26 +0000

    New method for new test.

--- jniast/src/testcases/uk/ac/starlink/ast/MemoryXmlChan.java
+++ jniast/src/testcases/uk/ac/starlink/ast/MemoryXmlChan.java
@@ -1,7 +1,10 @@
 package uk.ac.starlink.ast;
 
+import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import javax.xml.transform.stream.StreamSource;
 
 class MemoryXmlChan extends XmlChan {
     List buf = new ArrayList();
@@ -11,5 +14,12 @@ class MemoryXmlChan extends XmlChan {
     }
     protected String source() {
         return buf.isEmpty() ? null : (String) buf.remove( 0 );
+    }
+    public StreamSource getSource() {
+        StringBuffer sbuf = new StringBuffer();
+        for ( Iterator it = buf.iterator(); it.hasNext(); ) {
+            sbuf.append( (String) it.next() );
+        }
+        return new StreamSource( new StringReader( sbuf.toString() ) );
     }
 }
