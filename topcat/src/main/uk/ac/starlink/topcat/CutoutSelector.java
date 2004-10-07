e27865cbb67298009e7bcbf55da6abc589872aa8
Mark Taylor <m.b.taylor@bristol.ac.uk>
2004-10-07 14:29:09 +0000

    Added activation option to display a cutout image.

---
+++ topcat/src/main/uk/ac/starlink/topcat/CutoutSelector.java
@@ -0,0 +1,249 @@
+package uk.ac.starlink.topcat;
+
+import java.awt.Component;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.io.IOException;
+import javax.swing.Box;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import uk.ac.starlink.table.ColumnData;
+import uk.ac.starlink.table.Tables;
+import uk.ac.starlink.topcat.func.Sdss;
+import uk.ac.starlink.topcat.func.SuperCosmos;
+
+/**
+ * Component for selecting how image cutout requests will be submitted
+ * to remote cutout services.  This component can return an Activator
+ * {@link #makeActivator} based on its current settings.
+ *
+ * @author   Mark Taylor (Starlink)
+ * @since    7 Oct 2004
+ */
+public class CutoutSelector extends JPanel implements ItemListener {
+
+    private final TopcatModel tcModel_;
+    private final JComboBox serviceSelector_;
+    private final ColumnSelector raSelector_;
+    private final ColumnSelector decSelector_;
+    private final IntSelector npixSelector_;
+    private final JLabel pixsizeLabel_;
+    private final Component[] enablables_;
+
+    /** List of available cutout services. */
+    private static final CutoutService[] SERVICES = new CutoutService[] {
+        new CutoutService( "SuperCOSMOS All-Sky Blue", 0.67 ) {
+            String displayCutout( double ra, double dec, int npix ) {
+                return SuperCosmos.sssCutout( ra, dec, npix );
+            }
+        },
+        new CutoutService( "SuperCOSMOS All-Sky Red", 0.67 ) {
+            String displayCutout( double ra, double dec, int npix ) {
+                return SuperCosmos.sssCutout( ra, dec, npix );
+            }
+        },
+        new CutoutService( "SDSS Colour Images", 0.4 ) {
+            String displayCutout( double ra, double dec, int npix ) {
+                return Sdss.sdssCutout( ra, dec, npix );
+            }
+        },
+    };
+
+    /**
+     * Constructs a new cutout selector.
+     *
+     * @param  tcModel  table this selector is to work for
+     */
+    public CutoutSelector( TopcatModel tcModel ) {
+        tcModel_ = tcModel;
+
+        /* Set up components for user interaction. */
+        JLabel serviceLabel = new JLabel( "Cutout Service: " );
+        serviceSelector_ = new JComboBox( SERVICES );
+        serviceSelector_.addItemListener( this );
+        raSelector_ = new ColumnSelector( 
+                          tcModel_.getColumnSelectorModel( Tables.RA_INFO ) );
+        decSelector_ = new ColumnSelector(
+                           tcModel_.getColumnSelectorModel( Tables.DEC_INFO ) );
+        int[] sizes = new int[] { 25, 50, 75, 100, 150, 200, 300, 400, 500 };
+        JLabel npixLabel = new JLabel( "Width/Height in Pixels: " );
+        npixSelector_ = new IntSelector( sizes );
+        npixSelector_.setValue( 100 );
+        pixsizeLabel_ = new JLabel( "" );
+
+        /* Maintain list for {dis,en}ablement. */
+        enablables_ = new Component[] {
+            serviceLabel, serviceSelector_,
+            raSelector_,
+            decSelector_,
+            npixLabel, npixSelector_, pixsizeLabel_,
+        };
+
+        /* Place components. */
+        Box box = Box.createVerticalBox();
+        add( box );
+
+        /* Cutout service selector. */
+        Box cutoutLine = Box.createHorizontalBox();
+        cutoutLine.add( serviceLabel );
+        cutoutLine.add( serviceSelector_ );
+        box.add( cutoutLine );
+
+        /* RA column selector. */
+        Box raLine = Box.createHorizontalBox();
+        raLine.add( raSelector_ );
+        box.add( raLine );
+
+        /* Declination column selector. */
+        Box decLine = box.createHorizontalBox();
+        decLine.add( decSelector_ );
+        box.add( decLine );
+
+        /* Cutout image size selector. */
+        Box npixLine = Box.createHorizontalBox();
+        npixLine.add( npixLabel );
+        npixLine.add( npixSelector_ );
+        npixLine.add( pixsizeLabel_ );
+        box.add( npixLine );
+    }
+
+    /**
+     * Returns an Activator based on the current settings of this widget.
+     * The consequence of the activation will be an image popping up 
+     * around the given row's RA,Dec position.
+     * If not enough state has been specified to create a suitable activator,
+     * <tt>null</tt> will be returned.
+     *
+     * @return  new activator
+     */
+    public Activator makeActivator() {
+        CutoutService serv = (CutoutService) serviceSelector_.getSelectedItem();
+        String trouble;
+        if ( serv != null ) {
+            ColumnData raData = raSelector_.getColumnData();
+            ColumnData decData = decSelector_.getColumnData();
+            int npix = npixSelector_.getValue();
+            if ( raData == null ) {
+                trouble = "No RA column defined";
+            }
+            else if ( decData == null ) {
+                trouble = "No Declination column defined";
+            }
+            else if ( npix <= 0 ) {
+                trouble = "Non-positive number of pixels";
+            }
+            else {
+                return serv.makeActivator( raData, decData, npix );
+            }
+        }
+        else {
+            trouble = "No Cutout Service selected";
+        }
+
+        /* If we've got this far, we can't come up with an activator -
+         * message the user and return null. */
+        JOptionPane.showMessageDialog( this, trouble, 
+                                       "Underspecified Action Error",
+                                       JOptionPane.ERROR_MESSAGE );
+        return null;
+    }
+
+    public void setEnabled( boolean enabled ) {
+        for ( int i = 0; i < enablables_.length; i++ ) {
+            enablables_[ i ].setEnabled( enabled );
+        }
+    }
+
+    public void itemStateChanged( ItemEvent evt ) {
+        if ( evt.getSource() == serviceSelector_ ) {
+            CutoutService serv = 
+                (CutoutService) serviceSelector_.getSelectedItem();
+            if ( serv != null ) {
+                pixsizeLabel_.setText( " (" + serv.getPixelSize() + 
+                                       ") arcsec" );
+            }
+            else {
+                pixsizeLabel_.setText( "     " );
+            }
+        }
+    }
+
+    /**
+     * Defines a service which can display cutout images for RA/Dec pairs.
+     */
+    private static abstract class CutoutService {
+        final String name_;
+        final float pixelSize_;
+
+        /**
+         * Constructor for cutout set.
+         *
+         * @param  name  cutout service name
+         * @param  pixelSize  linear dimension of pixels in arcsec
+         */
+        protected CutoutService( String name, double pixelSize ) {
+            name_ = name;
+            pixelSize_ = (float) pixelSize;
+        }
+
+        /**
+         * Returns pixel size in arcseconds.
+         *
+         * @return pixel size
+         */
+        public float getPixelSize() {
+            return pixelSize_;
+        }
+
+        /**
+         * Displays an image centred around a given position.
+         *
+         * @param  ra   right ascension in radians
+         * @param  dec  declination in radians
+         * @param  npix linear dimension of image in pixels
+         * @return log message for display operation
+         */
+        abstract String displayCutout( double ra, double dec, int npix );
+
+        /**
+         * Returns an activator for this service in accordance with the 
+         * current settings of the containing CutoutSelector.
+         */
+        public Activator makeActivator( final ColumnData raData,
+                                        final ColumnData decData,
+                                        final int npix ) {
+            final String activatorName = name_ + "($ra, $dec, " + npix + ")";
+            return new Activator() {
+                public String activateRow( long lrow ) {
+                    Object raObj;
+                    Object decObj;
+                    try {
+                        raObj = raData.readValue( lrow );
+                        decObj = decData.readValue( lrow );
+                    }
+                    catch ( IOException e ) {
+                        return "Error reading position " + e;
+                    }
+                    if ( raObj instanceof Number &&
+                         decObj instanceof Number ) {
+                        double ra = ((Number) raObj).doubleValue();
+                        double dec = ((Number) decObj).doubleValue();
+                        if ( ! Double.isNaN( ra ) && ! Double.isNaN( dec ) ) {
+                            return displayCutout( ra, dec, npix );
+                        }
+                    }
+                    return "No position at (" + raObj + ", " + decObj + ")";
+                }
+                public String toString() {
+                    return activatorName;
+                }
+            };
+        }
+
+        public String toString() {
+            return name_;
+        }
+    }
+}
---
+++ topcat/src/main/uk/ac/starlink/topcat/IntSelector.java
@@ -0,0 +1,97 @@
+package uk.ac.starlink.topcat;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import javax.swing.JComboBox;
+import javax.swing.JPanel;
+
+/**
+ * Some sort of component which allows the user to select an integer.
+ * At time of writing this is only used in one context, but could be
+ * generalised for other uses, in which case it might need some less
+ * specific methods/constructors.
+ *
+ * @author   Mark Taylor (Starlink)
+ * @since    7 Oct 2004
+ */
+public class IntSelector extends JPanel implements ItemListener {
+
+    private final JComboBox cbox_;
+    private int selected_;
+
+    /**
+     * Constructs a new selector with a set of preselected options.
+     * Other options can be chosen by the user.
+     *
+     * @param  initial selections
+     */
+    public IntSelector( int[] options ) {
+        if ( options != null ) {
+            int nopt = options.length;
+            Object[] items = new Object[ nopt ];
+            for ( int i = 0; i < nopt; i++ ) {
+                items[ i ] = new Integer( options[ i ] );
+            }
+            cbox_ = new JComboBox( items );
+        }
+        else {
+            cbox_ = new JComboBox();
+        }
+        cbox_.setEditable( true );
+        cbox_.addItemListener( this );
+        cbox_.setPreferredSize(
+                  new Dimension( 64, cbox_.getPreferredSize().height ) );
+        add( cbox_ );
+    }
+
+    /**
+     * Returns the currently selected value.
+     *
+     * @return  selected integer
+     */
+    public int getValue() {
+        return selected_;
+    }
+
+    /**
+     * Sets the seleced value.
+     *
+     * @param  value  selected integer
+     */
+    public void setValue( int value ) {
+        cbox_.setSelectedItem( new Integer( value ) );
+    }
+
+    public void setEnabled( boolean enabled ) {
+        cbox_.setEnabled( enabled );
+    }
+
+    /**
+     * Implements ItemListener interface, called when the selection is
+     * changed.  This maintains the value of the currently selected value
+     * and validates it.
+     */
+    public void itemStateChanged( ItemEvent evt ) {
+        if ( evt.getStateChange() == ItemEvent.SELECTED ) {
+            Object item = evt.getItem();
+            if ( item instanceof Integer ) {
+                selected_ = ((Integer) item).intValue();
+            }
+            else if ( item instanceof String ) {
+                try {
+                    selected_ = Integer.parseInt( (String) item );
+                }
+                catch ( NumberFormatException e ) {
+                    Toolkit.getDefaultToolkit().beep();
+                    cbox_.setSelectedItem( new Integer( selected_ ) );
+                }
+            }
+            else {  // can't happen?
+                Toolkit.getDefaultToolkit().beep();
+                cbox_.setSelectedItem( new Integer( selected_ ) );
+            }
+        }
+    }
+}
