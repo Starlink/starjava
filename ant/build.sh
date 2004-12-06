d14e9bcdb04c101d7aa7879e08c8111b631a1288
Peter W. Draper <p.w.draper@durham.ac.uk>
2004-12-06 10:55:02 +0000

    This commit was generated by cvs2svn to compensate for changes in r2988, which included commits to RCS files with non-trunk default branches.

--- ant/build.sh
+++ ant/build.sh
@@ -3,54 +3,43 @@
 #   Copyright (c) 2000-2003 The Apache Software Foundation.  All rights
 #   reserved.
 
+# OS specific support.  $var _must_ be set to either true or false.
 cygwin=false;
+darwin=false;
 case "`uname`" in
   CYGWIN*) cygwin=true ;;
+  Darwin*) darwin=true
+           if [ -z "$JAVA_HOME" ] ; then
+             JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Home
+           fi
+           ;;
 esac
 
 REALANTHOME=$ANT_HOME
-ANT_HOME=bootstrap
+if [ -z "$PWD" ]; then
+    ANT_HOME=./bootstrap
+else
+    ANT_HOME="$PWD"/bootstrap
+fi
 export ANT_HOME
 
 if test ! -f bootstrap/lib/ant.jar -o  ! -x bootstrap/bin/ant -o ! -x bootstrap/bin/antRun ; then
   /bin/sh ./bootstrap.sh
-fi    
+fi
 
 if test ! -f bootstrap/lib/ant.jar -o  ! -x bootstrap/bin/ant -o ! -x bootstrap/bin/antRun ; then
   echo Bootstrap FAILED
   exit
 fi
 
-LOCALCLASSPATH=lib/xercesImpl.jar:lib/xml-apis.jar:bootstrap/lib/ant.jar
-# add in the dependency .jar files
-DIRLIBS=lib/optional/*.jar
-for i in ${DIRLIBS}
-do
-    if [ "$i" != "${DIRLIBS}" ] ; then
-        LOCALCLASSPATH=$LOCALCLASSPATH:"$i"
-    fi
-done
-
-# make sure the classpath is in unix format
-if $cygwin ; then
-    CLASSPATH=`cygpath --path --unix "$CLASSPATH"`    
-fi
-
-CLASSPATH=$LOCALCLASSPATH:$CLASSPATH
-
-# switch back to Windows format
-if $cygwin ; then
-    CLASSPATH=`cygpath --path --windows "$CLASSPATH"`    
-fi
-
-export CLASSPATH
-
-
 if [ "$REALANTHOME" != "" ] ; then
+  if $cygwin; then
+     REALANTHOME=`cygpath --windows "$REALANTHOME"`
+  fi
   ANT_INSTALL="-Dant.install=$REALANTHOME"
 else
   ANT_INSTALL="-emacs"
 fi
 
-bootstrap/bin/ant -emacs "$ANT_INSTALL" $*
+bootstrap/bin/ant -lib lib/optional "$ANT_INSTALL" $*
 
