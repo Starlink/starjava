b9e5d420babb43fa7486e90c0f9b9d74f425f5f6
Peter W. Draper <p.w.draper@durham.ac.uk>
2004-12-15 15:25:31 +0000

    Updated for Jetty 5.1.1. Note that the log output is now controlled using the Jakarta commons controls and is no longer performed here (in fact we need to only use the Java 1.4 logging system and I plan to remove all log4j instances from StarJava).

--- soapserver/src/main/uk/ac/starlink/soap/AppHttpSOAPServer.java
+++ soapserver/src/main/uk/ac/starlink/soap/AppHttpSOAPServer.java
@@ -1,74 +1,67 @@
 /*
- *Copyright (C) 2002 Central Laboratory of the Research Councils
+ * Copyright (C) 2002-2004 Central Laboratory of the Research Councils
  */
 
 package uk.ac.starlink.soap;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.Socket;
 import java.util.Vector;
-
-import javax.servlet.Servlet;
-import javax.xml.rpc.ServiceException;
 
 import org.apache.axis.client.Call;
 import org.apache.axis.AxisEngine;
 import org.apache.axis.client.Service;
 import org.apache.axis.configuration.SimpleProvider;
 import org.apache.axis.message.SOAPBodyElement;
-import org.apache.axis.transport.http.AxisServlet;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import org.mortbay.http.HttpContext;
 import org.mortbay.http.HttpServer;
-import org.mortbay.jetty.servlet.ServletHandler;
-import org.mortbay.jetty.servlet.ServletHolder;
 import org.mortbay.jetty.servlet.ServletHttpContext;
 import org.mortbay.jetty.servlet.WebApplicationContext;
-import org.mortbay.util.Code;
-import org.mortbay.util.Log;
 import org.mortbay.xml.XmlConfiguration;
 
 /**
- * A builtin in HTTP and SOAP server for applications.
+ * A HTTP and SOAP server for embedding in applications.
  * <p>
- *
- * This should be used in applications that want to offer web services
- * for clients to use. The web services offered are SOAP RPC requests
- * for the activation of registered methods in classes that are part
- * of the application.
- *
+ * This should be used in applications that want to offer web services for
+ * clients to use. The web services offered are SOAP RPC requests for the
+ * activation of registered methods in classes that are part of the
+ * application.
  * <p>
- * A special property of this server over the standard Jetty one is
- * that it requires on resources that are available locally and are
- * located using the classloader (so the resource files can be
- * packaged in jar files).
+ * A special property of this server over the standard Jetty one is that it
+ * requires only resources that are available locally and are located using
+ * the classloader (so the resource files can be packaged in jar files).
  * <p>
- * The configuration of this server is builtin to provide Axis SOAP
- * services.
+ * If the given port number is in use, then a search for a free port will be
+ * undertaken by incrementing the given port number.
+ * <p>
+ * The configuration of this server is builtin to provide Axis SOAP services.
  * <p>
  * Start up sequence:
  * <pre>
  *    AppHttpSOAPServer server = new AppHttpSOAPServer( port_number );
  *    server.start();
- *    server.addSOAPServices( <Url of "deploy.wsdd" file> );
+ *    server.addSOAPServices( <URL of "deploy.wsdd" file> );
  * </pre>
- *
- * Automatically incrememnets the port number from the one given until
- * it finds a free port to put the server onto.
+ * <p>
  *
  * @author Peter W. Draper, Alasdair Allan
  * @version $Id$
  * @since 22-MAY-2002
  */
-public class AppHttpSOAPServer extends HttpServer
+public class AppHttpSOAPServer 
+    extends HttpServer
 {
     /**
      * Resource name of the configuration file. This is local (to this
      * class) and cannot be overridden.
      */
-    private URL configFile =
+    private URL configFile = 
         AppHttpSOAPServer.class.getResource( "jetty.xml" );
 
     /**
@@ -77,20 +70,28 @@ public class AppHttpSOAPServer extends HttpServer
     private int portNum = 8080;
 
     /**
-     * Constructor loads the default configuration and starts a
-     * HTTP/SOAP server on a given port. To complete this sequence you
-     * should "start()" the server and then load any application SOAP
-     * services.
-     *
-     * @param portNum the port on which to establish the HTTP services
+     * Logger. Output control of this is performed using the logging.properties
+     * file of the Starlink util package (when applications are started from
+     * the command-line using the "starjava" command).
      */
-    public AppHttpSOAPServer( int portNum ) throws IOException
+    private static Log log = LogFactory.getLog( AppHttpSOAPServer.class );
+
+    /**
+     * Constructor loads the default configuration and starts a HTTP/SOAP
+     * server on a given port. To complete this sequence you should "start()"
+     * the server and then load any application SOAP services.
+     *
+     * @param portNum the requested port on which to establish the HTTP
+     *                services, this may not be the port actually used.
+     */
+    public AppHttpSOAPServer( int portNum ) 
+        throws IOException
     {
         //  Define the port number.
         setDefaultPort( portNum );
 
-        //  Set the Log system to use our logger.
-        Log.instance().add( new AppLogSink() );
+        //  Never validate the configuration files, avoids warnings.
+        System.setProperty("org.mortbay.xml.XmlParser.NotValidating", "true");
 
         try {
             XmlConfiguration config = new XmlConfiguration( configFile );
@@ -100,7 +101,6 @@ public class AppHttpSOAPServer extends HttpServer
             throw e;
         }
         catch( Exception e ) {
-            Code.warning( e );
             e.printStackTrace();
             throw new IOException( "Jetty configuration problem: " + e );
         }
@@ -109,47 +109,55 @@ public class AppHttpSOAPServer extends HttpServer
     /**
      * Create a new ServletHttpContext.
      *
-     * This method is called by HttpServer to create new contexts.
-     * Thus calls to addContext or getContext that result in a new
-     * Context being created will return an
-     * org.mortbay.jetty.servlet.ServletHttpContext instance (rather
-     * than a non-servlet type).
+     * This method is called by HttpServer to create new contexts. Thus calls
+     * to addContext or getContext that result in a new Context being created
+     * will return an {@link ServletHttpContext} instance (rather than a
+     * non-servlet type).
      *
      * @param contextPathSpec
      *
-     * @return ServletHttpContext
+     * @return {@link ServletHttpContext}
      */
-    protected HttpContext newHttpContext( String contextPathSpec )
+    protected HttpContext newHttpContext()
     {
-        return new ServletHttpContext( this, contextPathSpec );
+        return new ServletHttpContext();
     }
 
     /**
-     * Add the Axis SOAP services (this is loaded when parsing the
-     * "jetty.xml" file).
+     * Add the Axis SOAP services (this is loaded when parsing the "jetty.xml"
+     * file). The WAR file may be stored in a jar file.
      *
      * @param direct name of the Axis war file.
      *
-     * @return The WebApplicationContext
+     * @return {@link WebApplicationContext}
      * @exception IOException
      */
     public WebApplicationContext addAxisSOAPServices( String direct )
         throws IOException
     {
         URL axisURL = AxisEngine.class.getResource( direct );
-        WebApplicationContext soapContext = 
-            new WebApplicationContext( this, "/", axisURL.toString() );
-        addContext( null, soapContext );
-        Log.event( "Web Application "+ soapContext + " added" );
+        WebApplicationContext soapContext = new WebApplicationContext();
+
+        //  Set the configuration classes. If these are null then we cannot
+        //  use a HttpServer, but use a Server instead (and move over to the
+        //  full Jetty servlet-based system).
+        soapContext.setConfigurationClassNames( new String[] {
+            "org.mortbay.jetty.servlet.XMLConfiguration", 
+            "org.mortbay.jetty.servlet.JettyWebConfiguration" } );
+
+        //  Set the Axis war file.
+        soapContext.setWAR( axisURL.toString() );
+        soapContext.setContextPath( "/" );
+        addContext( soapContext );
+        log.info( "Web Application "+ soapContext + " added" );
         return soapContext;
     }
 
     /**
-     * Add new services to the SOAP server. These are defined in a
-     * "Web Service Deployment Descriptor" (wsdd) file that is located
-     * using a URL (which can consequently be in a jar file, use the
-     * "getResource" method of "Class" to locate a file stored with
-     * the application classes).
+     * Add new services to the SOAP server. These are defined in a "Web
+     * Service Deployment Descriptor" (wsdd) file that is located using a URL
+     * (which can consequently be in a jar file, use the "getResource" method
+     * of "Class" to locate a file stored with the application classes).
      *
      * @param deployURL URL of a wsdd file containing deployment
      *                  descriptions for the SOAP services to be
@@ -157,13 +165,11 @@ public class AppHttpSOAPServer extends HttpServer
      */
     public void addSOAPService( URL deployURL )
     {
-        
-        // Deploy any SOAP services using the client mechanism, this
-        // makes sure that the connection is working and makes sure
-        // that the correct initialisations are performed. A problem
-        // with this is that Axis wants to use any proxy server
-        // information, so we need to disable it and then make certain
-        // that we re-enable it.
+        // Deploy any SOAP services using the client mechanism, this makes
+        // sure that the connection is working and makes sure that the correct
+        // initialisations are performed. A problem with this is that Axis
+        // wants to use any proxy server information, so we need to disable it
+        // and then make certain that we re-enable it.
         String proxyHost = System.getProperty( "http.proxyHost" );
         if ( proxyHost != null ) {
             System.setProperty( "http.proxyHost", "" );
@@ -186,7 +192,7 @@ public class AppHttpSOAPServer extends HttpServer
             input.close();
             
             SOAPBodyElement body = (SOAPBodyElement) result.elementAt(0);
-            System.out.println( body.toString() );
+            log.info( body.toString() );
         }
         catch (Exception e) {
             e.printStackTrace();
@@ -197,16 +203,15 @@ public class AppHttpSOAPServer extends HttpServer
     }
 
     /**
-     * Set the default port number to use, will try the suggested port
-     * number, if free will set the port number to that. However if that
-     * port is in use it will in incremement the number until it finds
-     * a free port. It will then put the server on that port.
+     * Set the default port number to use, will try the suggested port number
+     * and if free will set the port number to that. However if that port is
+     * in use it will in incremement the number until it finds a free port
+     * (within the next 1000 ports). It will then put the server on that port.
      *
      * @param portNum the port number.
      */
     protected void setDefaultPort( int portNum )
     {
-    
         int startPort = portNum;
         int usingPort = startPort;           
         for( int i = startPort; i < startPort+1000; i++ ) {
@@ -216,25 +221,28 @@ public class AppHttpSOAPServer extends HttpServer
            try {
               Socket tempSocket = new Socket( "localhost", i );
               tempSocket.close();
-           } catch (Exception any) {
+           } 
+           catch (Exception any) {
               // Fails if not already in use, which is good.
               open = false;
            }
            
            if( !open) {
               usingPort = i;
-              //System.out.println( "Using port " + usingPort );
               break;
            }
         }   
-    
         System.setProperty( "jetty.port", Integer.toString( usingPort ) );
-        this.portNum = usingPort;
+        portNum = usingPort;
     }
     
+    /**
+     * Return the port number being used. This may differ from the port number
+     * asked for.
+     */
     public int getPort( ) 
     {
-       return this.portNum;
+       return portNum;
     }   
 
     /* ------------------------------------------------------------ */
@@ -258,12 +266,12 @@ public class AppHttpSOAPServer extends HttpServer
                 public void run()
                 {
                     setName( "Shutdown" );
-                    Log.event( "Shutdown hook executing" );
+                    log.info( "Shutdown hook executing" );
                     try {
                         server.stop();
                     }
                     catch(Exception e) {
-                        Code.warning( e );
+                        log.warn( "Error shutting down", e );
                     }
 
                     // Try to avoid JVM crash
@@ -271,7 +279,7 @@ public class AppHttpSOAPServer extends HttpServer
                         Thread.sleep( 1000 );
                     }
                     catch( Exception e ) {
-                        Code.warning( e );
+                        log.warn( e.getMessage(), e );
                     }
                 }
             };
