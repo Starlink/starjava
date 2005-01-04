3e2ecb1838a6924fe36e8602be19648495958af4
Mark Taylor <m.b.taylor@bristol.ac.uk>
2005-01-04 17:46:12 +0000

    Rewrote ConeSearchDialog.  It now inherits from BasicTableLoadDialog, which makes it more generic and less complicated.

--- vo/src/main/uk/ac/starlink/vo/ConeSearchDialog.java
+++ vo/src/main/uk/ac/starlink/vo/ConeSearchDialog.java
@@ -1,45 +1,34 @@
 package uk.ac.starlink.vo;
 
-import java.awt.Component;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.Frame;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
-import java.awt.event.ActionEvent;
 import java.io.IOException;
-import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
-import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
-import javax.swing.ComboBoxModel;
-import javax.swing.DefaultComboBoxModel;
-import javax.swing.ListSelectionModel;
-import javax.swing.Box;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
-import javax.swing.JMenu;
 import javax.swing.JMenuBar;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JProgressBar;
-import javax.swing.SwingUtilities;
+import javax.swing.ListSelectionModel;
 import javax.swing.border.Border;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import org.us_vo.www.SimpleResource;
+import uk.ac.starlink.table.DescribedValue;
 import uk.ac.starlink.table.StarTable;
 import uk.ac.starlink.table.StarTableFactory;
-import uk.ac.starlink.table.gui.LoadWorker;
-import uk.ac.starlink.table.gui.TableConsumer;
-import uk.ac.starlink.table.gui.TableLoadDialog;
+import uk.ac.starlink.table.gui.BasicTableLoadDialog;
 
 /**
  * Table load dialogue which allows cone searches.  Cone search services
@@ -48,37 +37,24 @@ import uk.ac.starlink.table.gui.TableLoadDialog;
  * @author   Mark Taylor (Starlink)
  * @since    21 Dec 2004
  */
-public class ConeSearchDialog extends JPanel implements TableLoadDialog {
+public class ConeSearchDialog extends BasicTableLoadDialog {
 
-    private final Action okAction_;
-    private final Action cancelAction_;
     private final RegistryPanel regPanel_;
-    private final ValueField raField_;
-    private final ValueField decField_;
-    private final ValueField srField_;
-    private JDialog dialog_;
-    private SearchSpec selectedSearch_;
-    private static Boolean available_;
-    private static final Logger logger_ = Logger.getLogger( "uk.ac.starlink" );
+    private final DoubleValueField raField_;
+    private final DoubleValueField decField_;
+    private final DoubleValueField srField_;
+    private Boolean available_;
+
+    private static Logger logger_ = Logger.getLogger( "uk.ac.starlink.vo" );
 
     /**
      * Constructor.
      */
     public ConeSearchDialog() {
-        super( new BorderLayout() );
-
-        /* Define actions for OK and Cancel buttons. */
-        okAction_ = new AbstractAction( "Submit Search" ) {
-            public void actionPerformed( ActionEvent evt ) {
-                submitSearch();
-            }
-        };
-        cancelAction_ = new AbstractAction( "Cancel Search" ) {
-            public void actionPerformed( ActionEvent evt ) {
-                cancelSearch();
-            }
-        };
-        okAction_.setEnabled( false );
+        super( "Cone Search",
+               "Obtain source catalogues using cone search web services" );
+        final Action okAction = getOkAction();
+        setLayout( new BorderLayout() );
 
         /* Construct and configure a panel which knows how to query the
          * registry and display the result. */
@@ -88,60 +64,55 @@ public class ConeSearchDialog extends JPanel implements TableLoadDialog {
         regPanel_.getResourceSelectionModel()
                  .addListSelectionListener( new ListSelectionListener() {
             public void valueChanged( ListSelectionEvent evt ) {
-                okAction_.setEnabled( regPanel_.getSelectedResources()
-                                               .length == 1 );
+                okAction.setEnabled( regPanel_.getSelectedResources()
+                                              .length == 1 );
             }
         } );
         regPanel_.getQueryPanel().setPresetQueries( new String[] {
             "serviceType like 'CONE'",
         } );
         add( regPanel_, BorderLayout.CENTER );
-
+ 
         /* Add controls for performing the actual cone search. */
         JComponent controlBox = Box.createVerticalBox();
         add( controlBox, BorderLayout.SOUTH );
 
         /* Add fields for entering query parameters. */
-        raField_ = ValueField.makeRADegreesField();
-        decField_ = ValueField.makeDecDegreesField();
-        srField_ = ValueField.makeRadiusDegreesField();
-        raField_.getEntryField().addActionListener( okAction_ );
-        decField_.getEntryField().addActionListener( okAction_ );
-        srField_.getEntryField().addActionListener( okAction_ );
+        raField_ = DoubleValueField.makeRADegreesField();
+        raField_.getEntryField().addActionListener( okAction );
+        raField_.getValueInfo()
+                .setDescription( "Right Ascension of cone centre (J2000)" );
+
+        decField_ = DoubleValueField.makeDecDegreesField();
+        decField_.getEntryField().addActionListener( okAction );
+        decField_.getValueInfo()
+                 .setDescription( "Declination of cone centre (J2000)" );
+
+        srField_ = DoubleValueField.makeRadiusDegreesField();
+        srField_.getEntryField().addActionListener( okAction );
+        srField_.getValueInfo()
+                .setDescription( "Radius of cone search" );
+
         ControlPanel qPanel = new ControlPanel();
         qPanel.addField( raField_, new JLabel( "(J2000)" ) );
         qPanel.addField( decField_, new JLabel( "(J2000)" ) );
         qPanel.addField( srField_ );
         controlBox.add( qPanel );
-        controlBox.add( Box.createVerticalStrut( 5 ) );
-
-        /* Add OK and Cancel buttons. */
-        JComponent actLine = Box.createHorizontalBox();
-        actLine.add( Box.createHorizontalGlue() );
-        actLine.add( new JButton( cancelAction_ ) );
-        actLine.add( Box.createHorizontalStrut( 5 ) );
-        actLine.add( new JButton( okAction_ ) );
-        controlBox.add( actLine );
 
         /* Cosmetics. */
         Border lineBorder = BorderFactory.createLineBorder( Color.BLACK );
         Border gapBorder = BorderFactory.createEmptyBorder( 5, 5, 5, 5 );
         Border etchedBorder = BorderFactory.createEtchedBorder();
-        regPanel_.setBorder( BorderFactory.createTitledBorder(
-            lineBorder, "Available Cone Search Services" ) );
+        regPanel_.setBorder(
+            BorderFactory.createTitledBorder(
+                BorderFactory.createCompoundBorder( lineBorder, gapBorder ),
+                "Available Cone Search Services" ) );
         controlBox.setBorder(
             BorderFactory.createTitledBorder(
-                lineBorder, "Cone Search Parameters" ) );
+                BorderFactory.createCompoundBorder( lineBorder, gapBorder ),
+                "Cone Search Parameters" ) );
         setBorder( gapBorder );
-        setPreferredSize( new Dimension( 600, 500 ) );
-    }
-
-    public String getName() {
-        return "Cone Search";
-    }
-
-    public String getDescription() {
-        return "Obtain tables using cone search web services";
+        setPreferredSize( new Dimension( 600, 450 ) );
     }
 
     public boolean isAvailable() {
@@ -196,147 +167,65 @@ public class ConeSearchDialog extends JPanel implements TableLoadDialog {
         return 0;
     }
 
-    public boolean showLoadDialog( Component parent,
-                                   final StarTableFactory factory,
-                                   ComboBoxModel formatModel,
-                                   TableConsumer consumer ) {
-
-        /* Check state. */
-        if ( dialog_ != null ) {
-            throw new IllegalStateException( "Dialogue already active" );
+    protected TableSupplier getTableSupplier() {
+        SimpleResource[] resources = regPanel_.getSelectedResources();
+        if ( resources.length != 1 ) {
+            throw new IllegalStateException( "No cone search service " +
+                                             "selected" );
         }
-
-        /* Create a dialogue containing this component. */
-        Frame frame = null;
-        if ( parent != null ) {
-            frame = parent instanceof Frame
-                  ? (Frame) parent
-                  : (Frame) SwingUtilities.getAncestorOfClass( Frame.class,
-                                                               parent );
+        final ConeSearch coner;
+        try {
+            coner = new ConeSearch( resources[ 0 ] );
         }
-
-        /* Obtain and pop up a modal dialogue based on this component. */
-        dialog_ = createDialog( parent );
-        selectedSearch_ = null;
-        dialog_.show();
-        final SearchSpec search = selectedSearch_;
-
-        /* If the dialog is no longer active it's been cancelled for some
-         * reason - don't try to proceed. */
-        if ( dialog_ == null ) {
-            return false;
+        catch ( IllegalArgumentException e ) {
+            throw new IllegalStateException( e.getMessage() );
         }
-        dialog_ = null;
-
-        /* Recover the table asynchronously and feed it to the consumer. */
-        String id = search.toString();
-        new LoadWorker( consumer, id ) {
-            protected StarTable attemptLoad() throws IOException {
-                return search.coner_.performSearch( search.ra_, search.dec_,
-                                                    search.sr_, search.verb_,
+        final double ra = getRA();
+        final double dec = getDec();
+        final double sr = getSR();
+        final int verb = getVerb();
+        final List metadata = new ArrayList();
+        metadata.addAll( Arrays.asList( new DescribedValue[] {
+            raField_.getDescribedValue(),
+            decField_.getDescribedValue(),
+            srField_.getDescribedValue(),
+        } ) );
+        metadata.addAll( Arrays.asList( coner.getMetadata() ) );
+        return new TableSupplier() {
+            public StarTable getTable( StarTableFactory factory, 
+                                       String format ) throws IOException {
+                StarTable st = coner.performSearch( ra, dec, sr, verb,
                                                     factory );
+                st.getParameters().addAll( metadata );
+                return st;
             }
-        }.invoke();
-        return true;
-    }
-
-    /**
-     * Constructs a modal dialogue based on this component.
-     *
-     * @param   parent  parent component
-     * @return  dialog
-     */
-    public JDialog createDialog( Component parent ) {
-
-        /* Work out the new dialogue's master. */
-        Frame frame = null;
-        if ( parent != null ) {
-            frame = parent instanceof Frame
-                  ? (Frame) parent
-                  : (Frame) SwingUtilities.getAncestorOfClass( Frame.class,
-                                                               parent );
-        }
-
-        /* Create the dialogue containing this component. */
-        JDialog dialog = new JDialog( frame, "Cone Search", true );
-        dialog.getContentPane().setLayout( new BorderLayout() );
-        dialog.getContentPane().add( this, BorderLayout.CENTER );
-
-        /* Add a menu which allows selective display of resource attributes. */
-        JMenu colMenu = regPanel_.makeColumnVisibilityMenu( "Display" );
-        dialog.setJMenuBar( new JMenuBar() );
-        dialog.getJMenuBar().add( colMenu );
-
-        /* Prepare and return dialogue. */
-        dialog.setLocationRelativeTo( parent );
-        dialog.pack();
-        return dialog;
-    }
-
-    /**
-     * Invoked when the submit button is hit.
-     * Attempts to prepare and store a SearchSpec based on the 
-     * current state of this component, and deactivate and dispose 
-     * the dialogue on success.
-     */
-    protected void submitSearch() {
-        if ( dialog_ == null ) {
-            return;
-        }
-        final SimpleResource[] ress = regPanel_.getSelectedResources();
-        String errmsg;
-        if ( ress.length == 1 ) {
-            try {
-                selectedSearch_ = new SearchSpec() {{
-                    ra_ = getRA(); 
-                    dec_ = getDec();
-                    sr_ = getSR();
-                    verb_ = getVerb();
-                    coner_ = new ConeSearch( ress[ 0 ] );
-                }};
-                dialog_.dispose();
-                return;
+            public String getTableID() {
+                return coner.toString();
             }
-            catch ( IllegalArgumentException e ) {
-                errmsg = e.getMessage();
-            }
-        }
-        else {
-            errmsg = "No cone search service selected";
-        }
-        JOptionPane.showMessageDialog( dialog_, errmsg,
-                                       "Search Specification Error",
-                                       JOptionPane.ERROR_MESSAGE );
+        };
     }
 
-    /**
-     * Invoked when the cancel button is hit.
-     * Deactivates and disposes the dialogue.
-     */
-    protected void cancelSearch() {
-        if ( dialog_ == null ) {
-            return;
+    protected JDialog createDialog( Component parent ) {
+
+        /* Embellish the dialogue with a menu allowing selection of which
+         * columns are visible in the displayed registry table. */
+        JDialog dia = super.createDialog( parent );
+        if ( dia.getJMenuBar() == null ) {
+            dia.setJMenuBar( new JMenuBar() );
         }
-        dialog_.dispose();
-        dialog_ = null;
+        dia.getJMenuBar()
+           .add( regPanel_.makeColumnVisibilityMenu( "Display" ) );
+        return dia;
     }
 
-    /**
-     * Struct-like class which specifies the details of a cone search.
-     */
-    private static class SearchSpec {
-        ConeSearch coner_;
-        double ra_;
-        double dec_;
-        double sr_;
-        int verb_;
-        URL getURL() {
-            return coner_.getSearchURL( ra_, dec_, sr_, verb_ );
-        }
-        public String toString() {
-            return coner_.toString();
-        }
+    public void setEnabled( boolean enabled ) {
+        super.setEnabled( enabled );
+        regPanel_.setEnabled( enabled );
+        raField_.setEnabled( enabled );
+        decField_.setEnabled( enabled );
+        srField_.setEnabled( enabled );
     }
+
 
     /**
      * Helper class to deal with GridBagLayout.
@@ -370,7 +259,7 @@ public class ConeSearchDialog extends JPanel implements TableLoadDialog {
             setLayout( layer );
         }
 
-        void addField( ValueField vf, JComponent trailer ) {
+        void addField( DoubleValueField vf, JComponent trailer ) {
             JComponent[] comps = new JComponent[] {
                 vf.getLabel(),
                 vf.getEntryField(),
@@ -387,7 +276,7 @@ public class ConeSearchDialog extends JPanel implements TableLoadDialog {
             }
         }
 
-        void addField( ValueField vf ) {
+        void addField( DoubleValueField vf ) {
             addField( vf, null );
         }
     }
