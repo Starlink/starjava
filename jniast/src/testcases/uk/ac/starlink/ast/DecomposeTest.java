86ae1534341237c7dd32533f5474b02727623656
Mark Taylor <m.b.taylor@bristol.ac.uk>
2005-01-21 16:40:21 +0000

    Add an explicit test with Mappings

--- jniast/src/testcases/uk/ac/starlink/ast/DecomposeTest.java
+++ jniast/src/testcases/uk/ac/starlink/ast/DecomposeTest.java
@@ -15,10 +15,26 @@ public class DecomposeTest extends TestCase {
         f2.setID( "F2" );
         CmpFrame cfrm = new CmpFrame( f1, f2 );
 
-        Mapping[] maps = cfrm.decompose( null, null );
+        Mapping[] maps = ((Frame) cfrm).decompose( null, null );
         assertEquals( Frame.class, f1.getClass() );
         assertEquals( Frame.class, f2.getClass() );
         assertEquals( f1, maps[ 0 ] );
         assertEquals( f2, maps[ 1 ] );
+
+        Frame[] frms = ((CmpFrame) cfrm).decompose();
+        assertEquals( f1, frms[ 0 ] );
+        assertEquals( f2, frms[ 1 ] );
+    }
+
+    public void testMapping() {
+        Mapping m1 = new UnitMap( 1 );
+        m1.setID( "M1" );
+        Mapping m2 = new UnitMap( 1 );
+        m2.setID( "M2" );
+        CmpMap cmap = new CmpMap( m1, m2, true );
+
+        Mapping[] maps = cmap.decompose( null, null );
+        assertEquals( m1, maps[ 0 ] );
+        assertEquals( m2, maps[ 1 ] );
     }
 }
