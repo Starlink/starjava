70ae115d5f8e74207d85374f4bebf325f95175e9
Peter W. Draper <p.w.draper@durham.ac.uk>
2005-02-16 11:26:48 +0000

    Final SPLAT-VO tweaks

--- splat/src/docs/sun243.htx/labels.pl
+++ splat/src/docs/sun243.htx/labels.pl
@@ -2,8 +2,16 @@
 # Associate labels original text with physical files.
 
 
+$key = q/xref_animation_window/;
+$external_labels{$key} = "$URL/" . q|node12.html|; 
+$noresave{$key} = "$nosave";
+
 $key = q/xref_abstract/;
 $external_labels{$key} = "$URL/" . q|node1.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/xref_getting_started/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/stardoccontents/;
@@ -14,35 +22,15 @@ $key = q/Displaying:/;
 $external_labels{$key} = "$URL/" . q|node9.html|; 
 $noresave{$key} = "$nosave";
 
+$key = q/xref_displaying_more_than_one_spectrum/;
+$external_labels{$key} = "$URL/" . q|node5.html|; 
+$noresave{$key} = "$nosave";
+
 $key = q/xref_help_window_individual/;
 $external_labels{$key} = "$URL/" . q|node7.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/selecting_spectra/;
-$external_labels{$key} = "$URL/" . q|node5.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/xref_overview/;
-$external_labels{$key} = "$URL/" . q|node2.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/xref_plot_window/;
-$external_labels{$key} = "$URL/" . q|node9.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/xref_basic_control/;
-$external_labels{$key} = "$URL/" . q|node6.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/xref_animation_window/;
-$external_labels{$key} = "$URL/" . q|node12.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/xref_getting_started/;
-$external_labels{$key} = "$URL/" . q|node3.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/xref_displaying_more_than_one_spectrum/;
 $external_labels{$key} = "$URL/" . q|node5.html|; 
 $noresave{$key} = "$nosave";
 
@@ -54,8 +42,20 @@ $key = q/Fitstatus/;
 $external_labels{$key} = "$URL/" . q|node34.html|; 
 $noresave{$key} = "$nosave";
 
+$key = q/xref_plot_window/;
+$external_labels{$key} = "$URL/" . q|node9.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/xref_overview/;
+$external_labels{$key} = "$URL/" . q|node2.html|; 
+$noresave{$key} = "$nosave";
+
 $key = q/xref_/;
 $external_labels{$key} = "$URL/" . q|sun243.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/xref_basic_control/;
+$external_labels{$key} = "$URL/" . q|node6.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/xref_browser_window/;
