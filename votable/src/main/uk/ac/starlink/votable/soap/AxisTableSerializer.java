073e2978f7510230a624d242ac0d0860d44201bc
Mark Taylor <m.b.taylor@bristol.ac.uk>
2005-03-24 09:49:07 +0000

    Added output of VOTable schema, for all the good it does me.

--- votable/src/main/uk/ac/starlink/votable/soap/AxisTableSerializer.java
+++ votable/src/main/uk/ac/starlink/votable/soap/AxisTableSerializer.java
@@ -3,7 +3,11 @@ package uk.ac.starlink.votable.soap;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.net.URL;
+import java.util.logging.Logger;
 import javax.xml.namespace.QName;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.stream.StreamSource;
 import org.apache.axis.Constants;
 import org.apache.axis.encoding.SerializationContext;
 import org.apache.axis.encoding.Serializer;
@@ -11,6 +15,7 @@ import org.apache.axis.wsdl.fromJava.Types;
 import org.w3c.dom.Element;
 import org.xml.sax.Attributes;
 import uk.ac.starlink.table.StarTable;
+import uk.ac.starlink.util.SourceReader;
 import uk.ac.starlink.votable.DataFormat;
 import uk.ac.starlink.votable.VOTableWriter;
 
@@ -21,6 +26,10 @@ import uk.ac.starlink.votable.VOTableWriter;
  * @since    23 Mar 2005
  */
 public class AxisTableSerializer implements Serializer {
+
+    private static Element schema_;
+    private static Logger logger_ = 
+        Logger.getLogger( "uk.ac.starlink.votable.soap" );
 
     /**
      * Writes <tt>table</tt> out to the serialization context as a
@@ -48,10 +57,23 @@ public class AxisTableSerializer implements Serializer {
     }
 
     /**
-     * Currently returns null.  Could return the VOTable schema I suppose.
+     * Returns an element containing the VOTable 1.1 schema.
+     *
+     * <p>When this method is called by AXIS in the course of creating
+     * WSDL (using the service?wsdl URL) it results in a WRONG_DOCUMENT_ERR
+     * DOM exception.  I can only imagine this is an AXIS bug, arising
+     * from AXIS not calling importNode on the returned Element.
      */
     public Element writeSchema( Class javaType, Types types ) {
-        return null;
+        if ( schema_ == null ) {
+            try {
+                schema_ = getVOTableSchema();
+            }
+            catch ( TransformerException e ) {
+                logger_.warning( "VOTable schema production failed: " + e );
+            }
+        }
+        return schema_;
     }
 
     public String getMechanismType() {
@@ -85,5 +107,17 @@ public class AxisTableSerializer implements Serializer {
         public void close() {
             flush();
         }
+    }
+
+    /**
+     * Reads the VOTable 1.1 schema and returns an element containing it.
+     *
+     * @return   VOTable schema element
+     */
+    private static Element getVOTableSchema() throws TransformerException {
+        URL url = AxisTableSerializer.class
+                 .getResource( "/uk/ac/starlink/util/text/VOTable1.1.xsd" );
+        return new SourceReader()
+              .getElement( new StreamSource( url.toString() ) );
     }
 }
