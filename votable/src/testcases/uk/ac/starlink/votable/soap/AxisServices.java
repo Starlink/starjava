7e6542fc72b75252f6d9e2a2eb06f29dae375c28
Mark Taylor <m.b.taylor@bristol.ac.uk>
2005-03-29 11:28:32 +0000

    Add tests for AXIS table custom serialization/deserialization.

---
+++ votable/src/testcases/uk/ac/starlink/votable/soap/AxisServices.java
@@ -0,0 +1,11 @@
+package uk.ac.starlink.votable.soap;
+
+import uk.ac.starlink.table.StarTable;
+
+public class AxisServices {
+
+    public static StarTable copy( StarTable table ) {
+        return table;
+    }
+
+}
