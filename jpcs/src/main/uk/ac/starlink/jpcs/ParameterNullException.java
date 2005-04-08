9eced89659cfa9cb46525e39e441bc2c4d2426d4
Starlink Software Collection Librarian cvs2svn <ussc@star.rl.ac.uk>
2005-04-08 16:48:20 +0000

    This commit was manufactured by cvs2svn to create tag 'pre-CONNECT-CHOOSER'.

--- jpcs/src/main/uk/ac/starlink/jpcs/ParameterNullException.java
+++
@@ -1,9 +0,0 @@
-package uk.ac.starlink.jpcs;
-
-public class ParameterNullException extends ParameterException {
-
-ParameterNullException( String keyword ) {
-   super( "Parameter " + keyword + " is Null" );
-}
-
-}
