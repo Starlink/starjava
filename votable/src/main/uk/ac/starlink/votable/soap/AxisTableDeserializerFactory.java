bbc1e53d8375e0f19ebd197eb1289a38c9bbdde8
Mark Taylor <m.b.taylor@bristol.ac.uk>
2005-04-15 15:48:23 +0000

    Made use of fixed storage policy explicit.

--- votable/src/main/uk/ac/starlink/votable/soap/AxisTableDeserializerFactory.java
+++ votable/src/main/uk/ac/starlink/votable/soap/AxisTableDeserializerFactory.java
@@ -5,6 +5,7 @@ import java.util.Iterator;
 import javax.xml.rpc.encoding.Deserializer;
 import org.apache.axis.Constants;
 import org.apache.axis.encoding.DeserializerFactory;
+import uk.ac.starlink.table.StoragePolicy;
 
 /**
  * Custom deserializer factory for streaming StarTables using AXIS.
@@ -15,7 +16,7 @@ import org.apache.axis.encoding.DeserializerFactory;
 public class AxisTableDeserializerFactory implements DeserializerFactory {
 
     public Deserializer getDeserializerAs( String mechanismType ) {
-        return new AxisTableDeserializer();
+        return new AxisTableDeserializer( StoragePolicy.getDefaultPolicy() );
     }
 
     public Iterator getSupportedMechanismTypes() {
