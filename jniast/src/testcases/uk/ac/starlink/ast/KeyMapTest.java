7c03a179464a261ab2508fecbbfdc97e3e342051
Mark Taylor <m.b.taylor@bristol.ac.uk>
2005-08-02 14:31:27 +0000

    Added mapGet1<X> methods

--- jniast/src/testcases/uk/ac/starlink/ast/KeyMapTest.java
+++ jniast/src/testcases/uk/ac/starlink/ast/KeyMapTest.java
@@ -40,11 +40,19 @@ public class KeyMapTest extends TestCase {
         assertNull( map.mapGet0I( unkey ) );
         assertNull( map.mapGet0C( unkey ) );
         assertNull( map.mapGet0A( unkey ) );
+        assertNull( map.mapGet1D( unkey ) );
+        assertNull( map.mapGet1I( unkey ) );
+        assertNull( map.mapGet1C( unkey, 1 ) );
+        assertNull( map.mapGet1A( unkey ) );
         map.mapRemove( unkey );
         assertNull( map.mapGet0D( unkey ) );
         assertNull( map.mapGet0I( unkey ) );
         assertNull( map.mapGet0C( unkey ) );
         assertNull( map.mapGet0A( unkey ) );
+        assertNull( map.mapGet1D( unkey ) );
+        assertNull( map.mapGet1I( unkey ) );
+        assertNull( map.mapGet1C( unkey, 10 ) );
+        assertNull( map.mapGet1A( unkey ) );
         assertEquals( 0, map.mapLength( unkey ) );
     }
 
@@ -72,21 +80,31 @@ public class KeyMapTest extends TestCase {
         AstObject o2 = new Frame( 2 );
         map.mapPut1A( "A", new AstObject[] { o1, o2 },
                       null );
+        assertEquals( 2, map.mapLength( "A" ) );
+        assertArrayEquals( new AstObject[] { o1, o2 }, map.mapGet1A( "A" ) );
+        assertEquals( o1, map.mapGet0A( "A" ) );
+
         map.mapPut1C( "C", new String[] { "one", "two", "three" },
                       "strings" );
+        assertEquals( 3, map.mapLength( "C" ) );
+        assertArrayEquals( new String[] { "one", "two", "three" },
+                           map.mapGet1C( "C", 10 ) );
+        assertArrayEquals( new String[] { "one", "two", "thr" },
+                           map.mapGet1C( "C", 3 ) );
+        assertEquals( "one", map.mapGet0C( "C" ) );
+
         map.mapPut1D( "D", new double[] { 1., 2., 3., 4. },
                       "" );
+        assertEquals( 4, map.mapLength( "D" ) );
+        assertArrayEquals( new double[] { 1., 2., 3., 4. },
+                           map.mapGet1D( "D" ) );
+        assertEquals( new Double( 1.0 ), map.mapGet0D( "D" ) );
+
         map.mapPut1I( "I", new int[] { 1, 2, 3, 4, 5 },
                       " n-n-numbers " );
-
-        assertEquals( 2, map.mapLength( "A" ) );
-        assertEquals( 3, map.mapLength( "C" ) );
-        assertEquals( 4, map.mapLength( "D" ) );
         assertEquals( 5, map.mapLength( "I" ) );
-
-        assertEquals( o1, map.mapGet0A( "A" ) );
-        assertEquals( "one", map.mapGet0C( "C" ) );
-        assertEquals( new Double( 1.0 ), map.mapGet0D( "D" ) );
+        assertArrayEquals( new int[] { 1, 2, 3, 4, 5 },
+                           map.mapGet1I( "I" ) );
         assertEquals( new Integer( 1 ), map.mapGet0I( "I" ) );
     }
 
