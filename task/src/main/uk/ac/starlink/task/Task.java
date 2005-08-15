60b5ec0224e7589642cb18f2b98a295ce1fe3859
Mark Taylor <m.b.taylor@bristol.ac.uk>
2005-08-15 15:31:03 +0000

    Reinstate usage for now

--- task/src/main/uk/ac/starlink/task/Task.java
+++ task/src/main/uk/ac/starlink/task/Task.java
@@ -17,9 +17,25 @@ public interface Task {
     Parameter[] getParameters();
 
     /**
+     * Returns a usage message for this task. 
+     * It should just contain the argument list (not the task's name)
+     * The argument list should be consistent with the parameter list
+     * returned by {@link #getParameters}; in particular it should use
+     * the same parameter names.
+     *
+     * @return   usage message
+     */
+    String getUsage();
+
+    /**
      * Invokes the task based on the given <tt>Environment</tt> object.
      *
      * @param  env  the environment in which the task will operate
+     * @throws  ExcecutionException  if a simple error message will suffice -
+     *          the object invoking this Task will typically not print
+     *          a stack trace in this case
+     * @throws  Exception  any other exception may be thrown -
+     *          a normal stacktrace will typically be displayed to the user
      */
     void invoke( Environment env ) throws TaskException;
 }
