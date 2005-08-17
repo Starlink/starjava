d81e99476f0fb955470dfd4798bf78d9eebde820
Mark Taylor <m.b.taylor@bristol.ac.uk>
2005-08-17 17:57:01 +0000

    Recoded TableCopy as a task on its own (not subclass of MapperTask) - it's just simpler that way, and I get more control over the parameters.

--- ttools/src/main/uk/ac/starlink/ttools/task/TableCopy.java
+++ ttools/src/main/uk/ac/starlink/ttools/task/TableCopy.java
@@ -1,13 +1,65 @@
 package uk.ac.starlink.ttools.task;
 
+import java.io.IOException;
+import uk.ac.starlink.table.StarTable;
+import uk.ac.starlink.task.Environment;
+import uk.ac.starlink.task.ExecutionException;
+import uk.ac.starlink.task.Parameter;
+import uk.ac.starlink.task.Task;
+import uk.ac.starlink.task.TaskException;
+import uk.ac.starlink.ttools.TableConsumer;
+
 /**
  * Task which copies a single table from input to output.
  *
  * @author   Mark Taylor 
  * @since    15 Aug 2005
  */
-public class TableCopy extends MapperTask {
+public class TableCopy implements Task {
+
+    private final InputTableParameter inParam_;
+    private final InputFormatParameter ifmtParam_;
+    private final OutputTableParameter outParam_;
+    private final OutputFormatParameter ofmtParam_;
+
     public TableCopy() {
-        super( new CopyMapper(), false );
+        inParam_ = new InputTableParameter( "in" );
+        inParam_.setPrompt( "Location of input table" );
+        inParam_.setPosition( 1 );
+
+        outParam_ = new OutputTableParameter( "out" );
+        outParam_.setPosition( 2 );
+
+        ifmtParam_ = inParam_.getFormatParameter();
+        ifmtParam_.setPrompt( "Format of input table" );
+        ifmtParam_.setName( "ifmt" );
+
+        ofmtParam_ = outParam_.getFormatParameter();
+        ofmtParam_.setPrompt( "Format of output table" );
+        ofmtParam_.setName( "ofmt" );
+    }
+
+    public Parameter[] getParameters() {
+        return new Parameter[] {
+            inParam_,
+            outParam_,
+            ifmtParam_,
+            ofmtParam_,
+        };
+    }
+
+    public String getUsage() {
+        return null;
+    }
+
+    public void invoke( Environment env ) throws TaskException {
+        StarTable inTable = inParam_.tableValue( env );
+        TableConsumer consumer = outParam_.consumerValue( env );
+        try {
+            consumer.consume( inTable );
+        }
+        catch ( IOException e ) {
+            throw new ExecutionException( e.getMessage(), e );
+        }
     }
 }
