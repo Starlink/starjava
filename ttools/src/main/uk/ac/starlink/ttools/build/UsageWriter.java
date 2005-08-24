7991fd0550a5101cde5d98afb9517c4dc1a0681e
Mark Taylor <m.b.taylor@bristol.ac.uk>
2005-08-24 09:58:37 +0000

    Increased the extent to which SUN/256 is written automatically, by querying the classes which perform the work.

--- ttools/src/main/uk/ac/starlink/ttools/build/UsageWriter.java
+++ ttools/src/main/uk/ac/starlink/ttools/build/UsageWriter.java
@@ -6,7 +6,9 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Iterator;
+import uk.ac.starlink.task.Parameter;
 import uk.ac.starlink.task.Task;
 import uk.ac.starlink.ttools.LoadException;
 import uk.ac.starlink.ttools.Stilts;
@@ -35,6 +37,8 @@ public class UsageWriter {
 
     private void writeXml() throws IOException {
         String prefix = "   stilts <stilts-flags> " + taskName_;
+        outln( "<subsubsect>" );
+        outln( "<subhead><title>Usage</title></subhead>" );
         outln( "<p>The usage of <code>" + taskName_ + "</code> is" );
         outln( "<verbatim><![CDATA[" );
         outln( LineInvoker.getPrefixedTaskUsage( task_, prefix ) );
@@ -50,6 +54,48 @@ public class UsageWriter {
         outln( "as explained in <ref id=\"task-args\"/>." );
         outln( "They are as follows:" );
         outln( "</p>" );
+        outln( "<p>" );
+        outln( "<dl>" );
+
+        Parameter[] params = task_.getParameters();
+        Arrays.sort( params, new Comparator() {
+            public int compare( Object o1, Object o2 ) {
+                Parameter p1 = (Parameter) o1;
+                Parameter p2 = (Parameter) o2;
+                return ((Parameter) o1).getName()
+                      .compareTo( ((Parameter) o2).getName() );
+            }
+        } );
+        for ( int i = 0; i < params.length; i++ ) {
+            outln( xmlItem( params[ i ] ) );
+        }
+        outln( "</dl>" );
+        outln( "</p>" );
+        outln( "</subsubsect>" );
+    }
+
+    /**
+     * Returns a list item (dt/dd pair) for a parameter giving its usage
+     * and description.
+     * 
+     * @param  param  parameter
+     * @return   XML snippet for <code>param</code>
+     */
+    public static String xmlItem( Parameter param ) {
+        String usage = ( "-" + param.getName() + " = " + param.getUsage() )
+                      .replaceAll( "<", "&lt;" )
+                      .replaceAll( ">", "&gt;" );
+        String descrip = param.getDescription().toString();
+        return new StringBuffer()
+            .append( "<dt><code>" )
+            .append( ( "-" + param.getName() + " = " + param.getUsage() )
+                    .replaceAll( "<", "&lt;" )
+                    .replaceAll( ">", "&gt;" ) )
+            .append( "</code></dt>\n" )
+            .append( "<dd><p>" )
+            .append( param.getDescription().toString() )
+            .append( "</p></dd>" )
+            .toString();
     }
 
     private void out( String text ) throws IOException {
