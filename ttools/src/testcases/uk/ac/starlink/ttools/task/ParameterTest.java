89b9764a0138a324dbe8c7e69dba73d0432a643d
Mark Taylor <m.b.taylor@bristol.ac.uk>
2005-08-31 13:01:09 +0000

    Added check for formatted XML

--- ttools/src/testcases/uk/ac/starlink/ttools/task/ParameterTest.java
+++ ttools/src/testcases/uk/ac/starlink/ttools/task/ParameterTest.java
@@ -7,14 +7,18 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import junit.framework.TestCase;
+import org.xml.sax.SAXException;
 import uk.ac.starlink.task.Parameter;
 import uk.ac.starlink.task.Task;
+import uk.ac.starlink.ttools.Formatter;
 import uk.ac.starlink.ttools.LoadException;
 import uk.ac.starlink.ttools.ObjectFactory;
 import uk.ac.starlink.ttools.Stilts;
 import uk.ac.starlink.ttools.mode.ProcessingMode;
 
 public class ParameterTest extends TestCase {
+
+    private Formatter formatter_ = new Formatter();
 
     private Collection noDefaults = new HashSet( Arrays.asList( new String[] {
         "protocol", "database", "newtable",
@@ -24,7 +28,7 @@ public class ParameterTest extends TestCase {
         super( name );
     }
 
-    public void testParams() throws LoadException {
+    public void testParams() throws LoadException, SAXException {
         Task[] tasks = getTasks();
         for ( int i = 0; i < tasks.length; i++ ) {
             checkParams( tasks[ i ].getParameters() );
@@ -36,17 +40,18 @@ public class ParameterTest extends TestCase {
         }
     }
 
-    private void checkParams( Parameter[] params ) {
+    private void checkParams( Parameter[] params ) throws SAXException {
         for ( int i = 0; i < params.length; i++ ) {
             checkParam( params[ i ] );
         }
     }
 
-    private void checkParam( Parameter param ) {
+    private void checkParam( Parameter param ) throws SAXException {
         assertTrue( param.getName(),
                     param.getDefault() != null ||
                     param.isNullPermitted() ||
                     noDefaults.contains( param.getName() ) );
+        formatter_.formatXML( param.getDescription() );
     }
 
     private ProcessingMode[] getModes() throws LoadException {
