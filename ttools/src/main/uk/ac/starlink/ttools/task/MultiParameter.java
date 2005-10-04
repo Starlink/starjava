a2d95204422e949176fff38c1b22dfe46007ad6f
Starlink Software Collection Librarian cvs2svn <ussc@star.rl.ac.uk>
2005-10-04 11:56:19 +0000

    This commit was manufactured by cvs2svn to create tag 'topcat-1-7-1'.

--- ttools/src/main/uk/ac/starlink/ttools/task/MultiParameter.java
+++
@@ -1,8 +0,0 @@
-package uk.ac.starlink.ttools.task;
-
-/**
- * Marker interface which marks a parameter as one which can have multiple
- * appearances on the command line.
- */
-public interface MultiParameter {
-}
