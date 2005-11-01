85f701b2eda92e90cfca35e8bf890c6d67b7f470
Mark Taylor <m.b.taylor@bristol.ac.uk>
2005-11-01 12:37:51 +0000

    Component shape massager.

---
+++ topcat/src/main/uk/ac/starlink/topcat/plot/SizeWrapper.java
@@ -0,0 +1,34 @@
+package uk.ac.starlink.topcat.plot;
+
+import java.awt.Component;
+import javax.swing.JPanel;
+import javax.swing.SpringLayout;
+
+/**
+ * Wraps a component so that it assumes the same size as its container.
+ * Another installment in my gruelling battle against layout managers.
+ *
+ * @author   Mark Taylor
+ * @since    1 Nov 2005
+ */
+public class SizeWrapper extends JPanel {
+
+    /**
+     * Constructor.
+     *
+     * @param  comp  component to wrap
+     */
+    public SizeWrapper( Component comp ) {
+        SpringLayout layer = new SpringLayout();
+        setLayout( layer );
+        add( comp );
+        layer.putConstraint( SpringLayout.WEST, this, 0,
+                             SpringLayout.WEST, comp );
+        layer.putConstraint( SpringLayout.EAST, this, 0,
+                             SpringLayout.EAST, comp );
+        layer.putConstraint( SpringLayout.NORTH, this, 0,
+                             SpringLayout.NORTH, comp );
+        layer.putConstraint( SpringLayout.SOUTH, this, 0,
+                             SpringLayout.SOUTH, comp );
+    }
+}
