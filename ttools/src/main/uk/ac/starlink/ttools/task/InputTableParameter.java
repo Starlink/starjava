c0c026ecd3dd486b3f290570bbe17fecc6303250
Mark Taylor <m.b.taylor@bristol.ac.uk>
2006-01-13 12:18:34 +0000

    When streaming is set for a non-stream data source, the data source input stream will now be re-read if more than one RowSequence is required rather than assuming that it can be read only once.  This means that using istream=true is less likely to lead to failure.
    Also improved the error message when no input format is specified for
    streamed tables.

--- ttools/src/main/uk/ac/starlink/ttools/task/InputTableParameter.java
+++ ttools/src/main/uk/ac/starlink/ttools/task/InputTableParameter.java
@@ -3,14 +3,18 @@ package uk.ac.starlink.ttools.task;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import uk.ac.starlink.table.RowSequence;
 import uk.ac.starlink.table.StarTable;
 import uk.ac.starlink.table.StarTableFactory;
 import uk.ac.starlink.table.TableBuilder;
+import uk.ac.starlink.table.WrapperStarTable;
 import uk.ac.starlink.task.BooleanParameter;
 import uk.ac.starlink.task.Environment;
 import uk.ac.starlink.task.ExecutionException;
 import uk.ac.starlink.task.Parameter;
+import uk.ac.starlink.task.ParameterValueException;
 import uk.ac.starlink.task.TaskException;
+import uk.ac.starlink.ttools.StreamRereadException;
 import uk.ac.starlink.ttools.StreamRowStore;
 import uk.ac.starlink.ttools.TableConsumer;
 import uk.ac.starlink.util.DataSource;
@@ -51,9 +55,11 @@ public class InputTableParameter extends Parameter {
             "Depending on the required operations and processing mode,",
             "this may cause the read to fail (sometimes it is necessary",
             "to read the input table more than once).",
-            "It is not normally useful or necessary to set this flag -",
+            "It is not normally necessary to set this flag;",
             "in most cases the data will be streamed automatically",
             "if that is the best thing to do.",
+            "However it can sometimes result in less resource usage when",
+            "processing large files in certain formats (such as VOTable).",
         } );
     }
 
@@ -88,11 +94,15 @@ public class InputTableParameter extends Parameter {
                 String fmt = formatParam_.stringValue( env );
                 boolean stream = streamParam_.booleanValue( env );
                 StarTableFactory tfact = getTableFactory( env );
-                if ( stream || loc.equals( "-" ) ) {
+                if ( loc.equals( "-" ) ) {
                     InputStream in =
                         new BufferedInputStream( DataSource
                                                 .getInputStream( loc ) );
                     table_ = getStreamedTable( tfact, in, fmt );
+                }
+                else if ( stream ) {
+                    table_ = getStreamedTable( tfact,
+                                               DataSource.makeDataSource( loc ),                                               fmt );
                 }
                 else {
                     table_ = tfact.makeStarTable( loc, fmt );
@@ -121,14 +131,17 @@ public class InputTableParameter extends Parameter {
      * It may fail with a TableFormatException for input formats
      * which can't be made from a stream.
      *
+     * @param   tfact   table factory
      * @param   in  input stream
      * @return  one-shot startable
      */
     StarTable getStreamedTable( StarTableFactory tfact, final InputStream in,
-                                String inFmt ) throws IOException {
-        if ( inFmt == null ) {
-            throw new IOException(
-                "Must specify input format for streamed table" );
+                                String inFmt )
+            throws IOException, TaskException {
+        if ( inFmt == null ||
+             inFmt.equals( StarTableFactory.AUTO_HANDLER ) ) {
+            String msg = "Must specify input format for streamed table";
+            throw new ParameterValueException( formatParam_, msg );
         }
         final TableBuilder tbuilder = tfact.getTableBuilder( inFmt );
         assert tbuilder != null;
@@ -154,6 +167,47 @@ public class InputTableParameter extends Parameter {
         streamer.setDaemon( true );
         streamer.start();
         return streamStore.waitForStarTable();
+    }
+
+    /**
+     * Returns a StarTable which re-reads the input stream every time
+     * the data are required.  This has similar efficiency characteristics to
+     * {@link #getStreamedTable(uk.ac.starlink.table.StarTableFactory,java,io.InputStream,java.lang.String)}
+     * but doesn't fall over if more than one RowSequence is taken out on it.
+     * It needs a DataSource not an InputStream of course, since the
+     * stream of bytes has to be re-readable.
+     *
+     * @param   tfact   table factory
+     * @param   datsrc  data source
+     * @return  re-readable streaming startable
+     */
+    StarTable getStreamedTable( final StarTableFactory tfact,
+                                final DataSource datsrc,
+                                final String inFmt )
+            throws IOException, TaskException {
+        InputStream in1 = new BufferedInputStream( datsrc.getInputStream() );
+        StarTable t1 = getStreamedTable( tfact, in1, inFmt );
+        return new WrapperStarTable( t1 ) {
+            public RowSequence getRowSequence() throws IOException {
+                try {
+                    return super.getRowSequence();
+                }
+                catch ( StreamRereadException e ) {
+                    InputStream in =
+                        new BufferedInputStream( datsrc.getInputStream() );
+                    StarTable t2;
+                    try {
+                        t2 = getStreamedTable( tfact, in, inFmt );
+                    }
+                    catch ( TaskException e1 ) {
+                        /* We've already got this once before, so it shouldn't
+                         * be able to fail with a TaskException this time. */
+                        throw new AssertionError( e1 );
+                    }
+                    return t2.getRowSequence();
+                }
+            }
+        };
     }
 
     /**
