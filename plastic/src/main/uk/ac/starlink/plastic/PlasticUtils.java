b58bcdaf028823ad73edd6e1a630ce5cf22d1c6f
Mark Taylor <m.b.taylor@bristol.ac.uk>
2006-02-17 17:12:05 +0000

    Added some methods, most notably one-stop RMI and XML-RPC based registration methods.

--- plastic/src/main/uk/ac/starlink/plastic/PlasticUtils.java
+++ plastic/src/main/uk/ac/starlink/plastic/PlasticUtils.java
@@ -5,11 +5,20 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.SocketException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.rmi.NotBoundException;
+import java.util.List;
 import java.util.Properties;
+import java.util.Vector;
 import net.ladypleaser.rmilite.Client;
+import org.apache.xmlrpc.WebServer;
+import org.apache.xmlrpc.XmlRpcClient;
+import org.apache.xmlrpc.XmlRpcException;
+import org.apache.xmlrpc.XmlRpcHandler;
 import org.votech.plastic.PlasticListener;
 import org.votech.plastic.PlasticHubListener;
 
@@ -152,6 +161,156 @@ public class PlasticUtils {
             throw (IOException)
                   new IOException( "Bad .plastic file: " 
                                  + "plastic.rmi.port=" + sport )
+                 .initCause( e );
+        }
+    }
+
+    /**
+     * Registers a PlasticApplication with the current hub using RMI-type
+     * communication.
+     *
+     * @param   app  application to register
+     * @return  registered ID
+     */
+    public static URI registerRMI( final PlasticApplication app )
+            throws IOException {
+        HubManager hubber = new HubManager( app.getName(),
+                                            app.getSupportedMessages() ) {
+            public Object doPerform( URI sender, URI message, List args ) {
+                return app.perform( sender, message, args );
+            }
+        };
+        hubber.register();
+        return hubber.getRegisteredId();
+    }
+
+    /**
+     * Registers a PlasticApplication with the current hub using XML-RPC type
+     * communication.
+     *
+     * @param  app  application to register
+     * @return  registered ID
+     */
+    public static URI registerXMLRPC( final PlasticApplication app )
+            throws IOException {
+        final XmlRpcClient client = new XmlRpcClient( getXmlRpcUrl() );
+        int port = 3112 - 1;
+        WebServer server = null;
+        IOException error = null;
+        for ( int i = 0; i < 20 && server == null; i++ ) {
+            port++;
+            try {
+                server = new WebServer( port );
+                server.start();
+            }
+            catch ( RuntimeException e ) {  // probably from a BindException
+                server = null;
+                error = (IOException) new IOException( e.getMessage() )
+                                     .initCause( e );
+            }
+        }
+        if ( server == null ) {
+            throw error;
+        }
+
+        URL serverUrl = new URL( "http://"
+                               + InetAddress.getLocalHost().getHostName()
+                               + ":" + port + "/" );
+        XmlRpcHandler handler = new XmlRpcHandler() {
+            public Object execute( String method, Vector params ) 
+                    throws URISyntaxException {
+                URI sender = new URI( (String) params.get( 0 ) );
+                URI message = new URI( (String) params.get( 1 ) );
+                List args = (List) params.get( 2 );
+                return app.perform( sender, message, args );
+            } 
+        };
+        server.addHandler( "plastic.client", handler );
+
+        Vector argv = new Vector();
+        argv.add( app.getName() );
+        argv.add( XmlRpcAgent.doctorArgs( app.getSupportedMessages() ) );
+        argv.add( XmlRpcAgent.doctorArgs( serverUrl.toString() ) );
+        Object result;
+        try {
+            result = client.execute( "plastic.hub.registerXMLRPC", argv );
+            if ( result instanceof XmlRpcException ) {
+                throw (XmlRpcException) result;
+            }
+            else if ( result instanceof String ) {
+                final URI id = new URI( (String) result );
+                Runtime.getRuntime().addShutdownHook( new Thread() {
+                    public void run() {
+                        Vector argv = new Vector();
+                        argv.add( id.toString() );
+                        try {
+                            client.execute( "plastic.hub.unregister", argv );
+                        }
+                        catch ( Throwable e ) {
+                        }
+                    }
+                } );
+                return id;
+            }
+            else {
+                return null; // only happens if hub returns a bad ID
+            }
+        }
+        catch ( XmlRpcException e ) {
+            throw (IOException) new IOException( e.getMessage() )
+                               .initCause( e );
+        }
+        catch ( URISyntaxException e ) {
+            return null;  // only happens if hub returns a bad ID
+        }
+    }
+
+    /**
+     * Sends a single asynchronous message to the hub without registering
+     * for callback.
+     *
+     * @param   name  application name
+     * @param   message  message ID
+     * @param   args   message arguments
+     */
+    public static void singleRequestAsynch( String name, URI message,
+                                            List args ) 
+            throws IOException {
+        PlasticHubListener hub = getLocalHub();
+        URI id = null;
+        try {
+            id = hub.registerNoCallBack( name );
+        }
+        catch ( Throwable e ) {
+            throw (IOException) new IOException( e.getMessage() )
+                               .initCause( e );
+        }
+        if ( id == null ) {
+            throw new IOException( "Can't connect to hub" );
+        }
+        try {
+            hub.requestAsynch( id, message, args );
+        }
+        finally {
+            hub.unregister( id );
+        }
+    }
+
+    /**
+     * Convenience method to turn a String into a URI without throwing
+     * any pesky checked exceptions.
+     * 
+     * @param  uri  URI text
+     * @return  URI
+     * @throws  IllegalArgumentException   if uri doesn't look like a URI
+     */
+    public static URI createURI( String uri ) {
+        try {
+            return new URI( uri );
+        }
+        catch ( URISyntaxException e ) {
+            throw (IllegalArgumentException)
+                  new IllegalArgumentException( "Bad URI: " + uri )
                  .initCause( e );
         }
     }
