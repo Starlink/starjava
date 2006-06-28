a66ce04e4abfb0593c6f1f427897f94dba28fb1e
Mark Taylor <m.b.taylor@bristol.ac.uk>
2006-06-28 18:56:25 +0000

    Added tests for StoragePolicies and tests for SidewaysRowStore

--- table/src/testcases/uk/ac/starlink/table/storage/StorageTest.java
+++ table/src/testcases/uk/ac/starlink/table/storage/StorageTest.java
@@ -9,6 +9,7 @@ import uk.ac.starlink.table.ColumnStarTable;
 import uk.ac.starlink.table.StarTable;
 import uk.ac.starlink.table.FormatsTest;
 import uk.ac.starlink.table.RowStore;
+import uk.ac.starlink.table.StoragePolicy;
 import uk.ac.starlink.table.TableFormatException;
 import uk.ac.starlink.table.Tables;
 import uk.ac.starlink.util.TestCase;
@@ -27,6 +28,38 @@ public class StorageTest extends TestCase {
 
     public void setUp() {
         fTest_ = new FormatsTest( "tame test" );
+    }
+
+    public void testPolicies() {
+        assertEquals( StoragePolicy.PREFER_MEMORY, getPolicy( "memory" ) );
+        assertEquals( StoragePolicy.PREFER_DISK, getPolicy( "disk" ) );
+        assertEquals( StoragePolicy.SIDEWAYS, getPolicy( "sideways" ) );
+        assertEquals( StoragePolicy.DISCARD, getPolicy( "discard" ) );
+
+        assertEquals( "StoragePolicy.PREFER_MEMORY",
+                       StoragePolicy.PREFER_MEMORY.toString() );
+        assertEquals( "StoragePolicy.PREFER_DISK",
+                       StoragePolicy.PREFER_DISK.toString() );
+        assertEquals( "StoragePolicy.SIDEWAYS",
+                       StoragePolicy.SIDEWAYS.toString() );
+        assertEquals( "StoragePolicy.DISCARD",
+                       StoragePolicy.DISCARD.toString() );
+
+        assertTrue( StoragePolicy.PREFER_MEMORY.makeRowStore()
+                    instanceof ListRowStore );
+        assertTrue( StoragePolicy.PREFER_DISK.makeRowStore()
+                    instanceof DiskRowStore );
+        assertTrue( StoragePolicy.SIDEWAYS.makeRowStore()
+                    instanceof SidewaysRowStore );
+        assertTrue( StoragePolicy.DISCARD.makeRowStore()
+                    instanceof DiscardRowStore );
+    }
+
+    public StoragePolicy getPolicy( String policyName ) {
+        String prop = "startable.storage";
+        StoragePolicy.setDefaultPolicy( null );
+        System.setProperty( prop, policyName );
+        return StoragePolicy.getDefaultPolicy();
     }
 
     public void testStorage() throws IOException {
@@ -70,6 +103,12 @@ public class StorageTest extends TestCase {
         ListRowStore mst1 = (ListRowStore) fillStore( new ListRowStore(), t1 );
         ListRowStore mst2 = (ListRowStore) fillStore( new ListRowStore(), t2 );
         ListRowStore mst3 = (ListRowStore) fillStore( new ListRowStore(), t3 );
+        SidewaysRowStore sst1 = (SidewaysRowStore)
+                                fillStore( new SidewaysRowStore(), t1 );
+        SidewaysRowStore sst2 = (SidewaysRowStore)
+                                fillStore( new SidewaysRowStore(), t2 );
+        SidewaysRowStore sst3 = (SidewaysRowStore)
+                                fillStore( new SidewaysRowStore(), t3 );
 
         assertTrue( fixedRows( dst1 ) );
         assertTrue( fixedRows( dst2 ) );
@@ -81,19 +120,24 @@ public class StorageTest extends TestCase {
         StarTable mt1 = mst1.getStarTable();
         StarTable mt2 = mst2.getStarTable();
         StarTable mt3 = mst3.getStarTable();
-        checkTables( t1, dt1, mt1 );
-        checkTables( t2, dt2, mt2 );
-        checkTables( t3, dt3, mt3 );
+        StarTable st1 = sst1.getStarTable();
+        StarTable st2 = sst2.getStarTable();
+        StarTable st3 = sst3.getStarTable();
+        checkTables( t1, dt1, mt1, st1 );
+        checkTables( t2, dt2, mt2, st2 );
+        checkTables( t3, dt3, mt3, st3 );
 
         fTest_.assertTableEquals( t1, dt1 );
         fTest_.assertTableEquals( t1, mt1 );
+        fTest_.assertTableEquals( t1, st1 );
         fTest_.assertTableEquals( t3, dt3 );
         fTest_.assertTableEquals( t3, mt3 );
+        fTest_.assertTableEquals( t3, st3 );
 
         String err;
         try {
-            fTest_.assertTableEquals( t2, dt2 );
             fTest_.assertTableEquals( t2, mt2 );
+            fTest_.assertTableEquals( t2, dt2 );
             err = "";
         }
         catch ( AssertionFailedError e ) {
@@ -111,16 +155,25 @@ public class StorageTest extends TestCase {
         }
         catch ( TableFormatException e ) {
         }
+        try {
+            new SidewaysRowStore().acceptMetadata( table );
+            fail();
+        }
+        catch ( TableFormatException e ) {
+        }
     }
 
-    private void checkTables( StarTable tab1, StarTable tab2, StarTable tab3 )
+    private void checkTables( StarTable tab1, StarTable tab2, StarTable tab3,
+                              StarTable tab4 )
             throws IOException {
         assertTrue( tab1.isRandom() );
         assertTrue( tab2.isRandom() );
         assertTrue( tab3.isRandom() );
+        assertTrue( tab4.isRandom() );
         fTest_.checkStarTable( tab1 );
         fTest_.checkStarTable( tab2 );
         fTest_.checkStarTable( tab3 );
+        fTest_.checkStarTable( tab4 );
     }
 
     private RowStore fillStore( RowStore store, StarTable table )
