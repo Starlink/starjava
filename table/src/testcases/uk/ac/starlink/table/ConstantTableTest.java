756131db68047c8aee5462ff3375af66cb45458f
Mark Taylor <m.b.taylor@bristol.ac.uk>
2006-07-03 16:42:11 +0000

    New utility StarTable implementation with all rows the same

---
+++ table/src/testcases/uk/ac/starlink/table/ConstantTableTest.java
@@ -0,0 +1,28 @@
+package uk.ac.starlink.table;
+
+import java.io.IOException;
+import uk.ac.starlink.util.TestCase;
+
+public class ConstantTableTest extends TestCase {
+
+    public ConstantTableTest( String name ) {
+        super( name );
+    }
+
+    public void testTable() throws IOException {
+        ColumnInfo c1 = new ColumnInfo( "integer", Integer.class, null );
+        ColumnInfo c2 = new ColumnInfo( "double_precision", Double.class,
+                                        null );
+        ColumnInfo c3 = new ColumnInfo( "char_string", String.class, null );
+        ColumnInfo[] infos = new ColumnInfo[] { c1, c2, c3 };
+        Object[] row = new Object[] { new Integer( 23 ), new Double( Math.E ),
+                                      "Knickers" };
+        StarTable table = new ConstantStarTable( infos, row, 101 );
+        Tables.checkTable( table );
+        assertEquals( 101L, table.getRowCount() );
+        assertEquals( 3, table.getColumnCount() );
+        assertEquals( "Knickers", table.getCell( 25L, 2 ) );
+        assertEquals( "double_precision", table.getColumnInfo( 1 ).getName() );
+        assertArrayEquals( row, table.getRow( 45 ) );
+    }
+}
