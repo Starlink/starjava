0be102642d528d29da2cf9285739a91b2e3806a7
Mark Taylor <m.b.taylor@bristol.ac.uk>
2006-07-05 13:45:11 +0000

    Another rewrite.  Behaviour made simpler to predict (incompatible tables are rejected) and the data can start to be read before all the tables have been added (table Iterator constructor).

--- table/src/main/uk/ac/starlink/table/ConcatStarTable.java
+++ table/src/main/uk/ac/starlink/table/ConcatStarTable.java
@@ -1,187 +1,89 @@
 package uk.ac.starlink.table;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
+import java.util.logging.Logger;
 
 /**
- * StarTable implementation which concatenates several tables together 
- * top-to-bottom, making some effort to check that the concatenated
- * columns are compatible with each other.
- *
- * <p>Column contents from each table are only added if their contents
- * are compatible with the column in question.  Some attempt is made to
- * guess which column can match up with which - this isn't very sophisticated,
- * so if the columns are not compatible it's likely you'll end up with
- * a lot of null values.  However, the result should at least be a legal
- * <code>StarTable</code> instance.
+ * StarTable implementation which concatenates several tables together
+ * top-to-bottom.  A (possibly blank) metadata table is supplied to 
+ * define the column and table metadata for the result, and other data
+ * tables are added on one way or another, depending which constructor
+ * is used.  The columns of each data table must be compatible with the
+ * columns of the metadata table, or the the data will not be included.
  *
  * @author   Mark Taylor
- * @since    4 Jul 2006
+ * @since    29 Mar 2004
  */
-public class ConcatStarTable extends AbstractStarTable {
+public class ConcatStarTable extends WrapperStarTable {
 
-    private final StarTable[] tables_;
-    private final int[][] colMaps_;
     private final ColumnInfo[] colInfos_;
-    private final boolean isRandom_;
-
-    private static final RowSequence EMPTY_SEQUENCE = new EmptyRowSequence();
+    private final List tableList_;
+    private Iterator tableIt_;
+    private Boolean isRandom_;
+    private long nrow_ = -1L;
+    private static final Logger logger_ =
+        Logger.getLogger( "uk.ac.starlink.table" );
 
     /**
-     * Constructs a new concatenated table.
+     * Constructs a concatenated table from a metadata table and an iterator
+     * over data-bearing tables.
+     * If any of the tables supplied by the iterator are incompatible with
+     * the metadata in <code>meta</code> a warning message is 
+     * issued through the logging system and its rows are ignored.
      *
-     * @param   tables   array of constituent tables
+     * @param   meta   table supplying column and table metadata for this one;
+     *                 its row count is ignored and its data is never read
+     * @param  tableIt iterator over constituent {@link StarTable}s which
+     *                 taken in sequence supply the row data for this one
      */
-    public ConcatStarTable( StarTable[] tables ) {
-
-        /* Get array of column metadata objects from the first table in the
-         * input array. */
-        tables_ = tables;
-        StarTable table0 = tables[ 0 ];
-        ColumnInfo[] colInfos = new ColumnInfo[ table0.getColumnCount() ];
-        int ncol = colInfos.length;
-        for ( int icol = 0; icol < ncol; icol++ ) {
-            colInfos[ icol ] = new ColumnInfo( table0.getColumnInfo( icol ) );
-        }
-
-        /* Permute the columns of all the following tables if necessary
-         * to match those in the first table.  The algorithm is rather
-         * unsophisticated - if subsequent tables don't have columns in
-         * an order which matches those of the first one, you'll probably
-         * get a lot of empty columns.  However, the table should at least
-         * obey the StarTable contract correctly. */
-        int nTable = tables.length;
-        colMaps_ = new int[ nTable ][];
-        for ( int itab = 0; itab < nTable; itab++ ) {
-            colMaps_[ itab ] = new int[ ncol ];
-            Arrays.fill( colMaps_[ itab ], -1 );
-            StarTable table = tables[ itab ];
-            int jcol = 0;
-            for ( int icol = 0; icol < table.getColumnCount(); icol++ ) {
-                ColumnInfo info = table.getColumnInfo( icol );
-                while ( jcol < ncol && ! matches( colInfos[ jcol ], info ) ) {
-                    jcol++;
-                }
-                if ( jcol < ncol ) {
-                    colMaps_[ itab ][ jcol++ ] = icol;
-                }
-            }
-        }
-
-        /* Adjust each column metadata object to cover all the objects which
-         * might appear in that column (may need to be generalised since
-         * it's holding items from tables with non-identical per-column
-         * metadata). */
-        for ( int icol = 0; icol < ncol; icol++ ) {
-            ColumnInfo info0 = colInfos[ icol ];
-            Class clazz0 = info0.getContentClass();
-            int[] shape0 = info0.getShape();
-            int esize0 = info0.getElementSize();
-            for ( int itab = 0; itab < nTable; itab++ ) {
-                int jcol = colMaps_[ itab ][ icol ];
-                if ( jcol >= 0 ) {
-                    ColumnInfo info1 = tables_[ itab ].getColumnInfo( jcol );
-                    Class clazz1 = info1.getContentClass();
-                    int[] shape1 = info1.getShape();
-                    int esize1 = info1.getElementSize();
-                    while ( ! clazz0.isAssignableFrom( clazz1 ) ) {
-                        clazz0 = clazz0.getSuperclass();
-                        if ( clazz0 == null ) {
-                            clazz0 = Object.class;
-                        }
-                    }
-                    if ( info0.isArray() ) {
-                        int ndim = shape0.length;
-                        if ( shape1.length != ndim ) {
-                            shape0 = new int[] { -1 };
-                        }
-                        else if ( shape1[ ndim - 1 ] != shape0[ ndim - 1 ] ) {
-                            shape0[ ndim - 1 ] = -1;
-                        }
-                    }
-                    if ( esize1 != esize0 ) {
-                        esize0 = -1;
-                    }
-                    if ( info1.isNullable() ) {
-                        info0.setNullable( true );
-                    }
-                }
-                else {
-                    info0.setNullable( true );
-                }
-            }
-            if ( clazz0 != info0.getContentClass() ) {
-                info0.setContentClass( clazz0 );
-            }
-            if ( info0.isArray() &&
-                 ! Arrays.equals( shape0, info0.getShape() ) ) {
-                info0.setShape( shape0 );
-            }
-            if ( esize0 != info0.getElementSize() ) {
-                info0.setElementSize( esize0 );
-            }
-        }
-        colInfos_ = colInfos;
-
-        /* Determine whether we have random access. */
-        boolean rand = true;
-        for ( int itab = 0; rand && itab < nTable; itab++ ) {
-            rand = rand && tables_[ itab ].isRandom();
-        }
-        isRandom_ = rand;
+    public ConcatStarTable( StarTable meta, Iterator tableIt ) {
+        super( meta );
+        colInfos_ = Tables.getColumnInfos( meta );
+        tableIt_ = tableIt;
+        tableList_ = new ArrayList();
     }
 
     /**
-     * Used to determine whether two column metadata objects are sufficiently
-     * similar to count as a match.
-     * The <code>ConcatStarTable</code> implementation of this method
-     * tests compatible content class and array-ness.  
-     * It may be overridden to for instance to check also for UCD or
-     * column name.
+     * Constructs a concatenated table from a metadata table and an array
+     * of data-bearing tables.
+     * If any of <code>tables</code> are incompatible with the metadata
+     * in <code>meta</code> an IOException is thrown.
      *
-     * @param  info0  base metadata object
-     * @param  info1  metadata object for comparison
-     * @return  true iff objects in the column described by <code>info1</code>
-     *          should be allowed in the column described by <code>info0</code>
+     * @param   meta   table supplying column and table metadata for this one;
+     *                 its row count is ignored and its data is never read
+     * @param   tables array of tables which taken in sequence supply the
+     *                 row data for this one
+     * @throws  IOException  if any of <code>tables</code> are not compatible
+     *          with <code>meta</code>
      */
-    public boolean matches( ColumnInfo info0, ColumnInfo info1 ) {
-        return info0.getContentClass()
-                    .isAssignableFrom( info1.getContentClass() )
-            && info0.isArray() == info1.isArray();
-    }
-
-    public int getColumnCount() {
-        return colInfos_.length;
-    }
-
-    public ColumnInfo getColumnInfo( int icol ) {
-        return colInfos_[ icol ];
+    public ConcatStarTable( StarTable meta, StarTable[] tables ) 
+            throws IOException {
+        super( meta );
+        colInfos_ = Tables.getColumnInfos( meta );
+        for ( int itab = 0; itab < tables.length; itab++ ) {
+            checkCompatible( tables[ itab ] );
+        }
+        tableIt_ = null;
+        tableList_ = Arrays.asList( tables );
+        gotTables();
     }
 
     public long getRowCount() {
-        long nrow = 0;
-        for ( int itab = 0; itab < tables_.length; itab++ ) {
-            long nr = tables_[ itab ].getRowCount();
-            if ( nr < 0 ) {
-                return -1L;
-            }
-            else {
-                nrow += nr;
-            }
-        }
-        return nrow;
+        return nrow_;
     }
 
     public Object getCell( long irow, int icol ) throws IOException {
-        if ( isRandom_ ) {
-            for ( int itab = 0; itab < tables_.length; itab++ ) {
-                StarTable t = tables_[ itab ];
-                long nr = t.getRowCount();
+        if ( isRandom() ) {
+            for ( Iterator it = tableList_.iterator(); it.hasNext(); ) {
+                StarTable table = (StarTable) it.next();
+                long nr = table.getRowCount();
+                assert nr >= 0;
                 if ( irow < nr ) {
-                    int jcol = colMaps_[ itab ][ icol ];
-                    return jcol >= 0 ? t.getCell( irow, jcol )
-                                     : null;
+                    return table.getCell( irow, icol );
                 }
                 irow -= nr;
             }
@@ -193,20 +95,13 @@ public class ConcatStarTable extends AbstractStarTable {
     }
 
     public Object[] getRow( long irow ) throws IOException {
-        if ( isRandom_ ) {
-            for ( int itab = 0; itab < tables_.length; itab++ ) {
-                StarTable t = tables_[ itab ];
-                long nr = t.getRowCount();
+        if ( isRandom() ) {
+            for ( Iterator it = tableList_.iterator(); it.hasNext(); ) {
+                StarTable table = (StarTable) it.next();
+                long nr = table.getRowCount();
+                assert nr >= 0;
                 if ( irow < nr ) {
-                    Object[] inRow = t.getRow( irow );
-                    int ncol = colInfos_.length;
-                    Object[] outRow = new Object[ ncol ];
-                    for ( int icol = 0; icol < ncol; icol++ ) {
-                        int jcol = colMaps_[ itab ][ icol ];
-                        outRow[ icol ] = jcol >= 0 ? inRow[ jcol ]
-                                                   : null;
-                    }
-                    return outRow;
+                    return table.getRow( irow );
                 }
                 irow -= nr;
             }
@@ -218,28 +113,155 @@ public class ConcatStarTable extends AbstractStarTable {
     }
 
     public boolean isRandom() {
-        return isRandom_;
+        return isRandom_ != null && isRandom_.booleanValue();
     }
 
     public RowSequence getRowSequence() throws IOException {
-        return new ConcatRowSequence();
+        return new ConcatRowSequence( getTableIterator() );
     }
 
+    /**
+     * Checks whether a given table is compatible with the metadata of
+     * this one.  The main thing to check is that the columns have 
+     * compatible types.
+     *
+     * @param   table  table to check against this one
+     * @throws  IOException  in case of incompatibility
+     */
+    private void checkCompatible( StarTable table ) throws IOException {
+        int ncol0 = colInfos_.length;
+        int ncol1 = table.getColumnCount();
+        if ( ncol1 != ncol0 ) {
+            throw new IOException( "Column count mismatch ("
+                                 + ncol1 + " != " + ncol0 + ")" );
+        }
+        for ( int icol = 0; icol < ncol0; icol++ ) {
+            ColumnInfo info0 = colInfos_[ icol ];
+            ColumnInfo info1 = table.getColumnInfo( icol );
+            if ( ! info0.getContentClass()
+                        .isAssignableFrom( info1.getContentClass() ) ||
+                 info0.isArray() != info1.isArray() ) {
+                throw new IOException( "Column type mismatch (" +
+                                       info1 + " not compatible with " +
+                                       info0 );
+            }
+        }
+    }
+
+    /**
+     * Called once, when all the constituent tables have been gathered 
+     * and are stored in <code>tableList_</code>.
+     * Updates certain bits of state which can't be known until then.
+     */
+    private synchronized void gotTables() {
+        assert isRandom_ == null;
+        boolean isRand = true;
+        long nrow = 0L;
+        for ( Iterator it = tableList_.iterator(); it.hasNext(); ) {
+            StarTable table = (StarTable) it.next();
+            isRand = isRand && table.isRandom();
+            if ( nrow >= 0 ) {
+                long nr = table.getRowCount();
+                nrow = nr >= 0 ? nrow + nr
+                               : -1L;
+            }
+        }
+        isRandom_ = Boolean.valueOf( isRand );
+        nrow_ = nrow;
+    }
+
+    /**
+     * Returns an iterator over all the constituent tables of this
+     * concatenation.
+     *
+     * @return  iterator over tables
+     */
+    private synchronized Iterator getTableIterator() {
+
+        /* If the list of constituent tables is complete, just return an
+         * iterator over it. */
+        if ( tableIt_ == null ) {
+            return tableList_.iterator();
+        }
+
+        /* If not, life is more interesting.  The initial iterator over tables
+         * is still live.  The returned iterator will use existing elements
+         * of the list when they're there, but when it reaches the end of
+         * that list it will have to update the list and then return the
+         * new element.  It is essential that two of these iterators don't
+         * get confused about who is picking the next table from the initial
+         * iterator, so careful use of synchronisation is made. */
+        else {
+            return new Iterator() {
+                private int index_;
+                public boolean hasNext() {
+                    synchronized ( ConcatStarTable.this ) {
+                        if ( index_ < tableList_.size() ) {
+                            return true;
+                        }
+                        else if ( tableIt_ == null ) {
+                            return false;
+                        }
+                        else if ( tableIt_.hasNext() ) {
+                            StarTable table = (StarTable) tableIt_.next();
+                            try {
+                                checkCompatible( table );
+                            }
+                            catch ( IOException e ) {
+                                logger_.warning( "Omitting incompatible table"
+                                               + " #" + tableList_.size()
+                                               + " - " + e.getMessage() );
+                                table = new EmptyStarTable( table );
+                            }
+                            tableList_.add( table );
+                            return true;
+                        }
+                        else {
+                            tableIt_ = null;
+                            gotTables();
+                            return false;
+                        }
+                    }
+                }
+                public Object next() {
+                    if ( hasNext() ) {
+                        return tableList_.get( index_++ );
+                    }
+                    else {
+                        throw new IllegalStateException();
+                    }
+                }
+                public void remove() {
+                    throw new UnsupportedOperationException();
+                }
+            };
+        }
+    }
+
+    /**
+     * RowSequence implementation which uses an iterator over tables.
+     */
     private class ConcatRowSequence implements RowSequence {
-        final Iterator tabIt_ = Arrays.asList( tables_ ).iterator();
-        final Iterator mapIt_ = Arrays.asList( colMaps_ ).iterator();
-        RowSequence rseq_ = EMPTY_SEQUENCE;
-        int[] colMap_ = new int[ colInfos_.length ];
+
+        private final Iterator tIt_;
+        private RowSequence rseq_;
+
+        /**
+         * Constructs a row sequence which iterates over all the rows in
+         * each table in an iterator in sequence.
+         *
+         * @param  tableIt  iterator over {@link StarTable} objects
+         */
+        ConcatRowSequence( Iterator tableIt ) {
+            tIt_ = tableIt;
+            rseq_ = EmptyRowSequence.getInstance();
+        }
 
         public boolean next() throws IOException {
             while ( ! rseq_.next() ) {
-                if ( rseq_ != EMPTY_SEQUENCE ) {
-                    rseq_.close();
-                }
-                if ( tabIt_.hasNext() ) {
-                    assert mapIt_.hasNext();
-                    rseq_ = ((StarTable) tabIt_.next()).getRowSequence();
-                    colMap_ = (int[]) mapIt_.next();
+                rseq_.close();
+                if ( tIt_.hasNext() ) {
+                    rseq_ = ((StarTable) tIt_.next()).getRowSequence();
                 }
                 else {
                     return false;
@@ -248,26 +270,16 @@ public class ConcatStarTable extends AbstractStarTable {
             return true;
         }
 
-        public void close() throws IOException {
-            rseq_.close();
-        }
-
         public Object getCell( int icol ) throws IOException {
-            int jcol = colMap_[ icol ];
-            return jcol >= 0 ? rseq_.getCell( jcol )
-                             : null;
+            return rseq_.getCell( icol );
         }
 
         public Object[] getRow() throws IOException {
-            Object[] inRow = rseq_.getRow();
-            int ncol = colInfos_.length;
-            Object[] outRow = new Object[ ncol ];
-            for ( int icol = 0; icol < ncol; icol++ ) {
-                int jcol = colMap_[ icol ];
-                outRow[ icol ] = jcol >= 0 ? inRow[ jcol ]
-                                           : null;
-            }
-            return outRow;
+            return rseq_.getRow();
+        }
+
+        public void close() throws IOException {
+            rseq_.close();
         }
     }
 }
