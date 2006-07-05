6cb7c54d8f49aac910faa75a4b01fba6147b4154
Mark Taylor <m.b.taylor@bristol.ac.uk>
2006-07-05 13:35:14 +0000

    Changed for another rewrite of ConcatStarTable

--- table/src/testcases/uk/ac/starlink/table/ConcatTableTest.java
+++ table/src/testcases/uk/ac/starlink/table/ConcatTableTest.java
@@ -2,12 +2,14 @@ package uk.ac.starlink.table;
 
 import java.io.IOException;
 import java.util.Arrays;
-import uk.ac.starlink.util.TestCase;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class ConcatTableTest extends TestCase {
+public class ConcatTableTest extends TableCase {
 
     public ConcatTableTest( String name ) {
         super( name );
+        Logger.getLogger( "uk.ac.starlink.table" ).setLevel( Level.SEVERE );
     }
 
     public void testDiverse() throws IOException {
@@ -26,7 +28,20 @@ public class ConcatTableTest extends TestCase {
                                               row1, 100 );
         StarTable t2 = new ConstantStarTable( new ColumnInfo[] { c0, c1f, c2 },
                                               row2, 100 );
-        StarTable ct = new ConcatStarTable( new StarTable[] { t0, t1, t2 } );
+
+        StarTable[] tables = new StarTable[] { t0, t1, t2 };
+        try {
+            StarTable ct = new ConcatStarTable( t0, tables );
+            fail();
+        }
+        catch ( IOException e ) {
+        }
+
+        StarTable ct =
+            new ConcatStarTable( t0, Arrays.asList( tables ).iterator() ) ;
+        checkStarTable( ct );
+        ct = Tables.randomTable( ct );
+
         assertEquals( "A", ct.getColumnInfo( 0 ).getName() );
         assertEquals( "B", ct.getColumnInfo( 1 ).getName() );
         assertEquals( "C", ct.getColumnInfo( 2 ).getName() );
@@ -34,14 +49,10 @@ public class ConcatTableTest extends TestCase {
         assertEquals( Number.class, ct.getColumnInfo( 1 ).getContentClass() );
         assertEquals( String.class, ct.getColumnInfo( 2 ).getContentClass() );
 
-        assertEquals( 300L, ct.getRowCount() );
-
         assertArrayEquals( row0, ct.getRow( 50 ) );
-        assertArrayEquals( new Object[] { new Integer( 24 ), null, "Bums" },
-                           ct.getRow( 150 ) );
-        assertArrayEquals( row2, ct.getRow( 250 ) );
+        assertArrayEquals( row2, ct.getRow( 150 ) );
 
-        Tables.checkTable( ct );
+        assertEquals( 200L, ct.getRowCount() );
     }
 
     public void testSame() throws IOException {
@@ -53,13 +64,14 @@ public class ConcatTableTest extends TestCase {
         StarTable t1 = new ConstantStarTable( infos, row, 10 );
         StarTable[] items = new StarTable[ 10 ];
         Arrays.fill( items, t1 );
-        StarTable ct = new ConcatStarTable( items );
-        Tables.checkTable( t1 );
-        Tables.checkTable( ct );
+        StarTable ct = new ConcatStarTable( items[ 0 ], items );
+        checkStarTable( t1 );
+        checkStarTable( ct );
         assertArrayEquals( row, t1.getRow( 5 ) );
         assertEquals( 100L, ct.getRowCount() );
         for ( int i = 0; i < ct.getRowCount(); i++ ) {
             assertArrayEquals( row, ct.getRow( i ) );
         }
     }
+
 }
