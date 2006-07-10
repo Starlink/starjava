fa60bc966fbe8366b59ce26456de073477e0fc40
Mark Taylor <m.b.taylor@bristol.ac.uk>
2006-07-10 14:13:43 +0000

    Remove redundant import

--- plastic/src/main/uk/ac/starlink/plastic/PlasticHub.java
+++ plastic/src/main/uk/ac/starlink/plastic/PlasticHub.java
@@ -14,7 +14,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
