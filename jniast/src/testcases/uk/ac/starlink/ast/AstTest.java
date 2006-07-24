66104bdbe37b58165416f79eb548f34bcdfa1003
Peter W. Draper <p.w.draper@durham.ac.uk>
2006-07-24 15:05:49 +0000

    Add new LO check to DSB specframes

--- jniast/src/testcases/uk/ac/starlink/ast/AstTest.java
+++ jniast/src/testcases/uk/ac/starlink/ast/AstTest.java
@@ -381,6 +381,8 @@ public class AstTest extends TestCase {
         assertEquals( "lsb", dsb.getSideBand().toLowerCase() );
         dsb.setSideBand( "usb" );
         assertEquals( "usb", dsb.getSideBand().toLowerCase() );
+        dsb.setSideBand( "lo" );
+        assertEquals( "lo", dsb.getSideBand().toLowerCase() );
         try {
             dsb.setSideBand( "sideways" );
             fail();
