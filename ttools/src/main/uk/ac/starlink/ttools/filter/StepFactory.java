c6a9a14d373876be3c87eb7949a92069afd0db10
Mark Taylor <m.b.taylor@bristol.ac.uk>
2006-08-03 11:31:56 +0000

    Add new setparam filter

--- ttools/src/main/uk/ac/starlink/ttools/filter/StepFactory.java
+++ ttools/src/main/uk/ac/starlink/ttools/filter/StepFactory.java
@@ -50,6 +50,7 @@ public class StepFactory {
         filterFactory_.register( "replaceval", pkg + "ReplaceValueFilter" );
         filterFactory_.register( "select", pkg + "SelectFilter" );
         filterFactory_.register( "sequential", pkg + "SequentialFilter" );
+        filterFactory_.register( "setparam", pkg + "SetParameterFilter" );
         filterFactory_.register( "sort", pkg + "SortFilter" );
         filterFactory_.register( "sorthead", pkg + "SortHeadFilter" );
         filterFactory_.register( "stats", pkg + "StatsFilter" );
