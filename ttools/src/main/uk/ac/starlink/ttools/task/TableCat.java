f2706072eec8cd503c52e81292c12503f0f46e95
Mark Taylor <m.b.taylor@bristol.ac.uk>
2006-09-15 11:40:14 +0000

    TableCat task rewrite - now concatenates multiple homogeneous tables

---
+++ ttools/src/main/uk/ac/starlink/ttools/task/TableCat.java
@@ -0,0 +1,13 @@
+package uk.ac.starlink.ttools.task;
+
+/**
+ * Concatenates multiple homogeneous tables top-to-bottom.
+ *
+ * @author   Mark Taylor
+ * @since    15 Sep 2006
+ */
+public class TableCat extends HomogeneousMapperTask {
+    public TableCat() {
+        super( new CatMapper(), new ChoiceMode(), true, true );
+    }
+}
