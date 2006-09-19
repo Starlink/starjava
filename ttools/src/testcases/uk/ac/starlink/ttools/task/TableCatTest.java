9fae47ed9957a27c6fec4fdc05e2789c56627e35
Mark Taylor <m.b.taylor@bristol.ac.uk>
2006-09-19 11:03:58 +0000

    More tests for added columns

--- ttools/src/testcases/uk/ac/starlink/ttools/task/TableCatTest.java
+++ ttools/src/testcases/uk/ac/starlink/ttools/task/TableCatTest.java
@@ -20,6 +20,8 @@ public class TableCatTest extends TableTestCase {
 
     public TableCatTest( String name ) {
         super( name );
+        t1_.setName( "table1" );
+        t2_.setName( "table2" );
     }
 
     public void test2() throws Exception {
@@ -70,5 +72,44 @@ public class TableCatTest extends TableTestCase {
         StarTable outN = envN.getOutputTable( "omode" );
         assertArrayEquals( box( new int[] { 2, 3, 2 } ),
                            getColData( outN, 1 ) );
+    }
+
+    public void testAddCols() throws Exception {
+        MapEnvironment env2 = new MapEnvironment()
+                             .setValue( "in1", t1_ )
+                             .setValue( "in2", t2_ )
+                             .setValue( "seqcol", "seq" )
+                             .setValue( "loccol", "loc" )
+                             .setValue( "uloccol", "uloc" );
+        new TableCat2().createExecutable( env2 ).execute();
+        StarTable out2 = env2.getOutputTable( "omode" );
+
+        MapEnvironment envN = new MapEnvironment()
+                             .setValue( "in", new StarTable[] { t1_, t2_, } )
+                             .setValue( "seqcol", "seq" )
+                             .setValue( "loccol", "loc" )
+                             .setValue( "uloccol", "uloc" );
+        new TableCat().createExecutable( envN ).execute();
+        StarTable outN = envN.getOutputTable( "omode" );
+
+        assertSameData( out2, outN );
+        assertArrayEquals(
+            new String[] { "index", "name", "seq", "loc", "uloc"  },
+            getColNames( out2 ) );
+        assertArrayEquals(
+            new String[] { "index", "name", "seq", "loc", "uloc" },
+            getColNames( outN ) );
+        Short s1 = new Short( (short) 1 );
+        Short s2 = new Short( (short) 2 );
+        assertArrayEquals( new Object[] { s1, s1, s2, s2, s2 },
+                           getColData( out2, 2 ) );
+        String t1 = "table1";
+        String t2 = "table2";
+        assertArrayEquals( new Object[] { t1, t1, t2, t2, t2, },
+                           getColData( outN, 3 ) );
+        String n1 = "1";
+        String n2 = "2";
+        assertArrayEquals( new Object[] { n1, n1, n2, n2, n2, },
+                           getColData( out2, 4 ) );
     }
 }
