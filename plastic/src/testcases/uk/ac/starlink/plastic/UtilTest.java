4b03ed339b8a3d3f3527181ba972917350cd2780
Mark Taylor <m.b.taylor@bristol.ac.uk>
2006-09-22 13:26:30 +0000

    Add HubManager.checkArgs tests

--- plastic/src/testcases/uk/ac/starlink/plastic/UtilTest.java
+++ plastic/src/testcases/uk/ac/starlink/plastic/UtilTest.java
@@ -1,5 +1,7 @@
 package uk.ac.starlink.plastic;
 
+import java.io.IOException;
+import java.util.Arrays;
 import junit.framework.TestCase;
 
 public class UtilTest extends TestCase {
@@ -16,5 +18,36 @@ public class UtilTest extends TestCase {
         assertEquals( PlasticUtils.getSendIcon(), PlasticUtils.getSendIcon() );
         assertTrue( ! PlasticUtils.getSendIcon()
                      .equals( PlasticUtils.getBroadcastIcon() ) );
+    }
+
+    public void testCheckArgs() throws IOException {
+        Class[] reqs =
+            new Class[] { Integer.class, Number.class, String.class };
+        assertTrue( HubManager.checkArgs(
+            Arrays.asList( new Object[] { new Integer( 1 ), new Float( 2f ), 
+                                          "3", } ),
+            reqs ) );
+        assertTrue( HubManager.checkArgs(
+            Arrays.asList( new Object[] { new Integer( 1 ),
+                                          new Short( (short) 2 ), "3",
+                                          new Object() } ),
+            reqs ) );
+        try {
+            HubManager.checkArgs(
+                Arrays.asList( new Object[] { new Integer( 1 ),
+                                              new Short( (short) 2 ), } ),
+                reqs );
+            fail();
+        }
+        catch ( IOException e ) {
+        }
+        try {
+            HubManager.checkArgs(
+                Arrays.asList( new Object[] { new Integer( 1 ), "2", "3" } ),
+                reqs );
+            fail();
+        }
+        catch ( IOException e ) {
+        }
     }
 }
