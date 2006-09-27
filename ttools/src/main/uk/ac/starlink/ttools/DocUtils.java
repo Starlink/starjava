a2aa8836b135d8a9b8f5be64d74780a1d0b42a52
Mark Taylor <m.b.taylor@bristol.ac.uk>
2006-09-27 17:38:05 +0000

    Utility class for use in automatic documentation

---
+++ ttools/src/main/uk/ac/starlink/ttools/DocUtils.java
@@ -0,0 +1,71 @@
+package uk.ac.starlink.ttools;
+
+/**
+ * Utilities used for automatically-generated documentation.
+ *
+ * @author   Mark Taylor
+ * @since    27 Sep 2006
+ */
+public class DocUtils {
+
+    /**
+     * Private sole constructor prevents instantiation.
+     */
+    private DocUtils() {
+    }
+
+
+    /**
+     * Concatenates an array of strings, appending a carriage return
+     * to each one.
+     *
+     * @param   lines  array of input strings
+     * @return  one long output string
+     */
+    public static String join( String[] lines ) {
+        StringBuffer sbuf = new StringBuffer();
+        for ( int i = 0; i < lines.length; i++ ) {
+            sbuf.append( lines[ i ] )
+                .append( '\n' );
+        }
+        return sbuf.toString();
+    }
+
+    /**
+     * Provides a snippet of XML which references a named 
+     * {@link uk.ac.starlink.ttools.filter.ProcessingFilter}.
+     *
+     * @param  name  filter  name
+     * @return  filter reference
+     */
+    public static String filterRef( String name ) {
+        return new StringBuffer()
+            .append( "<code>" )
+            .append( "<ref id=\"" )
+            .append( name )
+            .append( "\">" )
+            .append( name )
+            .append( "</ref>" )
+            .append( "</code>" )
+            .toString();
+    }
+
+    /**
+     * Provides a snippet of XML which references a named
+     * {@link uk.ac.starlink.ttools.mode.ProcessingMode}.
+     *
+     * @param name  mode name
+     * @return  mode reference
+     */
+    public static String modeRef( String name ) {
+        return new StringBuffer()
+            .append( "<code>" )
+            .append( "<ref id=\"mode-" )
+            .append( name )
+            .append( "\">" )
+            .append( name )
+            .append( "</ref>" )
+            .append( "</code>" )
+            .toString();
+    }
+}
