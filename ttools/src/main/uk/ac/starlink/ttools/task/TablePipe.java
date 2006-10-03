06bae10da97de3082650ec96e6649cd4ae28dd12
Mark Taylor <m.b.taylor@bristol.ac.uk>
2006-10-03 12:20:32 +0000

    TablePipe now doesn't use a TableMapper object but does its own (very minimal) work by extending SingleMapperTask.

--- ttools/src/main/uk/ac/starlink/ttools/task/TablePipe.java
+++ ttools/src/main/uk/ac/starlink/ttools/task/TablePipe.java
@@ -1,14 +1,36 @@
 package uk.ac.starlink.ttools.task;
 
+import java.util.Iterator;
+import uk.ac.starlink.task.Environment;
+import uk.ac.starlink.task.Parameter;
+import uk.ac.starlink.task.TaskException;
+
 /**
- * Task which implements a table pipeline.
+ * TableMapper which does the work for the tpipe command. 
+ * It can stack several processing filters to edit the table.
  *
  * @author   Mark Taylor
- * @since    15 Aug 2005
+ * @since    16 Aug 2005
  */
 public class TablePipe extends SingleMapperTask {
+
     public TablePipe() {
-        super( new PipeMapper(), "Performs pipeline processing on a table",
-               new ChoiceMode(), false, false );
+        super( "Performs pipeline processing on a table", new ChoiceMode(),
+               false, true );
+        int nfilter = 0;
+        for ( Iterator it = getParameterList().iterator(); it.hasNext(); ) {
+            Parameter param = (Parameter) it.next();
+            if ( param instanceof FilterParameter ) {
+                assert param.getName().equals( "icmd" );
+                param.setName( "cmd" );
+                nfilter++;
+            }
+        }
+        assert nfilter == 1;
+    }
+
+    public TableProducer createProducer( Environment env )
+            throws TaskException {
+        return createInputProducer( env );
     }
 }
