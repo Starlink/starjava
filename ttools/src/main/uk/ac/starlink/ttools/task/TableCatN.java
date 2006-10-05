b1f9b7a41238dcdef7aeecfb11dca35f5fdf0c04
Mark Taylor <m.b.taylor@bristol.ac.uk>
2006-10-05 12:56:24 +0000

    tcat variant based on VariableMapperTask

---
+++ ttools/src/main/uk/ac/starlink/ttools/task/TableCatN.java
@@ -0,0 +1,14 @@
+package uk.ac.starlink.ttools.task;
+
+/**
+ * Concatenates N tables top to bottom.
+ *
+ * @author   Mark Taylor
+ * @since    5 Oct 2006
+ */
+public class TableCatN extends VariableMapperTask {
+    public TableCatN() {
+        super( "Concatenates multiple tables", new ChoiceMode(), true,
+               new CatMapper( false ), true );
+    }
+}
