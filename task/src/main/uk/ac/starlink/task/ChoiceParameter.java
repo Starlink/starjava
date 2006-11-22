3ba37a53c4ae9317c113ca9802749c0aa531749c
Mark Taylor <m.b.taylor@bristol.ac.uk>
2006-11-22 14:40:30 +0000

    Permit explicit labelling of option objects, rather than relying on toString()

--- task/src/main/uk/ac/starlink/task/ChoiceParameter.java
+++ task/src/main/uk/ac/starlink/task/ChoiceParameter.java
@@ -1,5 +1,9 @@
 package uk.ac.starlink.task;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Parameter whose legal value must be one of a disjunction of given values.
  * Matching is case-insensitive against the stringified value of the option.
@@ -9,27 +13,72 @@ package uk.ac.starlink.task;
  */
 public class ChoiceParameter extends Parameter {
 
-    private final Object[] options_;
+    private final List optionList_;
+    private final List nameList_;
     private Object objectValue_;
 
     /**
-     * Constructor.
+     * Constructs a ChoiceParameter with a given list of options.
      *
      * @param   name  parameter name
      * @param   options  legal values of this parameter
      */
     public ChoiceParameter( String name, Object[] options ) {
         super( name );
-        options_ = (Object[]) options.clone();
+        optionList_ = new ArrayList();
+        nameList_ = new ArrayList();
+        if ( options != null ) {
+            for ( int iopt = 0; iopt < options.length; iopt++ ) {
+                addOption( options[ iopt ] );
+            }
+        }
+    }
+
+    /**
+     * Constructs a ChoiceParameter with no options.
+     *
+     * @param   name  parameter name
+     */
+    public ChoiceParameter( String name ) {
+        this( name, null );
+    }
+
+    /**
+     * Adds an option value to this parameter with a given name.
+     * The name is the parameter value string used to identify this option.
+     *
+     * @param   option  option object
+     * @param   name   label for option
+     */
+    public void addOption( Object option, String name ) {
+        if ( name == null ) {
+            name = option.toString();
+        }
+        if ( nameList_.contains( name ) ) {
+            throw new IllegalArgumentException( "Option " + name +
+                                                " already exists" );
+        }
+        optionList_.add( option );
+        nameList_.add( name );
+    }
+
+    /**
+     * Adds an option value to this parameter. 
+     * The object's <code>toString</code> method supplies the name.
+     *
+     * @param  option  option object
+     */
+    public void addOption( Object option ) {
+        addOption( option, null );
     }
 
     public String getUsage() {
         StringBuffer sbuf = new StringBuffer();
-        for ( int i = 0; i < options_.length; i++ ) {
-            if ( i > 0 ) {
+        for ( Iterator it = nameList_.iterator(); it.hasNext(); ) {
+            sbuf.append( (String) it.next() );
+            if ( it.hasNext() ) {
                 sbuf.append( '|' );
             }
-            sbuf.append( options_[ i ].toString() );
         }
         return sbuf.toString();
     }
@@ -40,9 +89,10 @@ public class ChoiceParameter extends Parameter {
             super.setValueFromString( env, value );
             return;
         }
-        for ( int i = 0; i < options_.length; i++ ) {
-            if ( value.equalsIgnoreCase( options_[ i ].toString() ) ) {
-                objectValue_ = options_[ i ];
+        int nopt = optionList_.size();
+        for ( int i = 0; i < nopt; i++ ) {
+            if ( value.equalsIgnoreCase( (String) nameList_.get( i ) ) ) {
+                objectValue_ = optionList_.get( i );
                 super.setValueFromString( env, value );
                 return;
             }
@@ -51,11 +101,11 @@ public class ChoiceParameter extends Parameter {
             .append( "Unknown value " )
             .append( value )
             .append( " - must be one of " );
-        for ( int i = 0; i < options_.length; i++ ) {
-            if ( i > 0 ) {
+        for ( Iterator it = nameList_.iterator(); it.hasNext(); ) {
+            sbuf.append( (String) it.next() );
+            if ( it.hasNext() ) {
                 sbuf.append( ", " );
             }
-            sbuf.append( options_[ i ] );
         }
         throw new ParameterValueException( this, sbuf.toString() );
     }
@@ -73,12 +123,12 @@ public class ChoiceParameter extends Parameter {
     }
 
     /**
-     * Returns a copy of the array of options which is accepted by this
+     * Returns an array of the string values of options accepted by this
      * parameter.
      *
-     * @return  permitted options
+     * @return  permitted options, stringified
      */
-    public Object[] getOptions() {
-        return (Object[]) options_.clone();
+    public String[] getOptionNames() {
+        return (String[]) nameList_.toArray( new String[ 0 ] );
     }
 }
