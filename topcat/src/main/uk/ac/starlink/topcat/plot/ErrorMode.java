1376d4a78dc3e9892bd966589ff9137bdf471ed3
Mark Taylor <m.b.taylor@bristol.ac.uk>
2007-03-01 15:53:20 +0000

    Add Extent enumerations to each mode

--- topcat/src/main/uk/ac/starlink/topcat/plot/ErrorMode.java
+++ topcat/src/main/uk/ac/starlink/topcat/plot/ErrorMode.java
@@ -9,6 +9,7 @@ package uk.ac.starlink.topcat.plot;
 public class ErrorMode {
 
     private final String name_;
+    private final Extent[] extents_;
     private final double loOff_;
     private final double hiOff_;
 
@@ -27,21 +28,55 @@ public class ErrorMode {
     /** Both lower and upper bounds specified independently. */
     public static final ErrorMode BOTH;
 
+    /** Extent describing errors only lower than the point value. */
+    public static final Extent LOWER_EXTENT = new Extent( "Lower", "-" );
+
+    /** Extent describing errors only higher than the point value. */
+    public static final Extent UPPER_EXTENT = new Extent( "Upper", "+" );
+
+    /** Extent describing errors symmetrically lower and higher than 
+     *  the point value. */
+    public static final Extent BOTH_EXTENT = new Extent( "Both", "+/-" );
+
+    /** All known modes. */
     private static final ErrorMode[] ALL_OPTIONS = new ErrorMode[] {
-        NONE = new ErrorMode( "None", 0, 0 ),
-        SYMMETRIC = new ErrorMode( "Symmetric", 1, 1 ),
-        LOWER = new ErrorMode( "Lower Only", 1, 0 ),
-        UPPER = new ErrorMode( "Upper Only", 0, 1 ),
-        BOTH = new ErrorMode( "Lower & Upper", 1, 0.75 ),
+        NONE =
+            new ErrorMode( "None", new Extent[ 0 ], 0, 0 ),
+        SYMMETRIC =
+            new ErrorMode( "Symmetric", new Extent[] { BOTH_EXTENT, }, 1, 1 ),
+        LOWER =
+            new ErrorMode( "Lower Only", new Extent[] { LOWER_EXTENT, }, 1, 0 ),
+        UPPER =
+            new ErrorMode( "Upper Only", new Extent[] { UPPER_EXTENT, }, 0, 1 ),
+        BOTH =
+            new ErrorMode( "Lower & Upper",
+                           new Extent[] { LOWER_EXTENT, UPPER_EXTENT, },
+                           1, 0.75 ),
     };
 
     /**
      * Constructor.
+     *
+     * @param  name   mode name
+     * @param  extents   array of extents
+     * @param  loOff    example lower offset (0..1)
+     * @param  hiOff    example upper offset (0..1)
      */
-    private ErrorMode( String name, double loOff, double hiOff ) {
+    private ErrorMode( String name, Extent[] extents,
+                       double loOff, double hiOff ) {
         name_ = name;
+        extents_ = (Extent[]) extents.clone();
         loOff_ = loOff;
         hiOff_ = hiOff;
+    }
+
+    /**
+     * Returns the extent objects which characterise this mode.
+     *
+     * @return  extent array
+     */
+    public Extent[] getExtents() {
+        return (Extent[]) extents_.clone();
     }
 
     /**
@@ -98,6 +133,46 @@ public class ErrorMode {
                 } 
             }
             return true;
+        }
+    }
+
+    /**
+     * Enumeration class which lists the distance quantities used by different
+     * error modes.  Each error mode will have zero or more of these,
+     * corresponding to scalar values constituting the described error.
+     */
+    public static class Extent {
+
+        private final String name_;
+        private final String label_;
+
+        /**
+         * Constructor.
+         *
+         * @param  name   name of this extent
+         * @param  label  label suitable for labelling a control for this extent
+         */
+        private Extent( String name, String label ) {
+            name_ = name;
+            label_ = label;
+        }
+
+        /**
+         * Returns the name of this extent.
+         *
+         * @return  extent name
+         */
+        public String getName() {
+            return name_;
+        }
+
+        /**
+         * Returns a name suitable for labelling a control for this extent.
+         *
+         * @return  extent control label
+         */
+        public String getLabel() {
+            return label_;
         }
     }
 }
