afe20cac91025a50bf9ecfd33e3fa3108383290b
Mark Taylor <m.b.taylor@bristol.ac.uk>
2007-04-03 18:04:12 +0000

    More framework inserted - ready to write actual error calculation code

--- topcat/src/main/uk/ac/starlink/topcat/plot/SphericalPolarPointStore.java
+++ topcat/src/main/uk/ac/starlink/topcat/plot/SphericalPolarPointStore.java
@@ -25,9 +25,12 @@ public class SphericalPolarPointStore implements PointStore {
     private final boolean hasTanerr_;
     private final int npoint_;
     private final int nword_;
-    private final ErrorReader radialReader_;
+    private final ErrorReader errorReader_;
     private final double[] linebuf_;
     private final double[] point_;
+    private final double[] centre_;
+    private final double[] rawerrs_;
+    private final double[][] errors_;
     private int ipoint_;
 
     /**
@@ -39,16 +42,19 @@ public class SphericalPolarPointStore implements PointStore {
      */
     public SphericalPolarPointStore( ErrorMode radialMode, boolean hasTanerr,
                                      int npoint ) {
-        radialReader_ = getErrorReader( radialMode );
         hasTanerr_ = hasTanerr;
         npoint_ = npoint;
 
-        /* Calculate the number of words required for each data point. */
-        nword_ = 3 + ( hasTanerr_ ? 1 : 0 ) + radialReader_.getWordCount();
+        /* Set up the object which will interpret raw error information. */
+        errorReader_ = getErrorReader( radialMode, hasTanerr );
+        nword_ = 3 + errorReader_.getWordCount();
 
         /* Initialise some buffers. */
         linebuf_ = new double[ nword_ ];
         point_ = new double[ 3 ];
+        centre_ = new double[ 3 ];
+        rawerrs_ = new double[ errorReader_.getWordCount() ];
+        errors_ = new double[ errorReader_.getNerror() ][];
 
         /* Initialise the value store which will hold the values.
          * Other implementations are possible. */
@@ -81,22 +87,49 @@ public class SphericalPolarPointStore implements PointStore {
     }
 
     public int getNerror() {
-        return 0;
+        return errorReader_.getNerror();
     }
 
     public double[][] getErrors( int ipoint ) {
-        return new double[ 0 ][];
+        if ( errors_.length > 0 ) {
+            long off = ipoint * (long) nword_;
+            valueStore_.get( off, centre_ );
+            valueStore_.get( off + 3, rawerrs_ );
+            errorReader_.calcErrors( centre_, rawerrs_, errors_ );
+        }
+        return errors_;
     }
 
     /**
-     * Returns an ErrorReader object suitable for a given ErrorMode.
+     * Returns an error reader suitable for given radial and tangential
+     * error configurations.
+     *
+     * @param   radialMode  type of radial error information to store
+     * @param   hasTanerr   whether to store tangential error information
+     * @return  error reader
+     */
+    private static ErrorReader getErrorReader( ErrorMode radialMode, 
+                                               boolean hasTanerr ) {
+        DeltaPairReader radialReader = getDeltaPairReader( radialMode );
+
+        // dummy return - ignores any errors
+        return new ErrorReader( radialReader.getWordCount() 
+                                + ( hasTanerr ? 1 : 0 ), 0 ) {
+            public void calcErrors( double[] centre, double[] rawerrs,
+                                    double[][] errors ) {
+            }
+        };
+    }
+
+    /**
+     * Returns a DeltaPairReader object suitable for a given ErrorMode.
      *
      * @param  mode  error mode
      * @return   error reader
      */
-    private static ErrorReader getErrorReader( ErrorMode mode ) {
+    private static DeltaPairReader getDeltaPairReader( ErrorMode mode ) {
         if ( ErrorMode.SYMMETRIC.equals( mode ) ) {
-            return new ErrorReader( 1 ) {
+            return new DeltaPairReader( 1 ) {
                 protected void convert( double[] rawErrors, double[] deltas ) {
                     deltas[ 0 ] = rawErrors[ 0 ];
                     deltas[ 1 ] = rawErrors[ 0 ];
@@ -104,7 +137,7 @@ public class SphericalPolarPointStore implements PointStore {
             };
         }
         else if ( ErrorMode.LOWER.equals( mode ) ) {
-            return new ErrorReader( 1 ) {
+            return new DeltaPairReader( 1 ) {
                 protected void convert( double[] rawErrors, double[] deltas ) {
                     deltas[ 0 ] = rawErrors[ 0 ];
                     deltas[ 1 ] = 0.0;
@@ -112,7 +145,7 @@ public class SphericalPolarPointStore implements PointStore {
             };
         }
         else if ( ErrorMode.UPPER.equals( mode ) ) {
-            return new ErrorReader( 1 ) {
+            return new DeltaPairReader( 1 ) {
                 protected void convert( double[] rawErrors, double[] deltas ) {
                     deltas[ 0 ] = 0.0;
                     deltas[ 1 ] = rawErrors[ 0 ];
@@ -120,7 +153,7 @@ public class SphericalPolarPointStore implements PointStore {
             };
         }
         else if ( ErrorMode.BOTH.equals( mode ) ) {
-            return new ErrorReader( 2 ) {
+            return new DeltaPairReader( 2 ) {
                 protected void convert( double[] rawErrors, double[] deltas ) {
                     deltas[ 0 ] = rawErrors[ 0 ];
                     deltas[ 1 ] = rawErrors[ 1 ];
@@ -129,7 +162,7 @@ public class SphericalPolarPointStore implements PointStore {
         }
         else {
             assert ErrorMode.NONE.equals( mode );
-            return new ErrorReader( 0 ) {
+            return new DeltaPairReader( 0 ) {
                 public void readErrors( ValueStore store, int off,
                                         double[] deltas ) {
                     deltas[ 0 ] = 0.0;
@@ -143,13 +176,65 @@ public class SphericalPolarPointStore implements PointStore {
     }
 
     /**
+     * Interface defining the conversion between raw errors and error point
+     * coordinates.
+     */
+    private static abstract class ErrorReader {
+
+        private final int nword_;
+        private final int nerr_;
+
+        /**
+         * Constructor.
+         *
+         * @param  nword  number of raw error words representing error data
+         * @param  nerr   number of error points calculated
+         */
+        public ErrorReader( int nword, int nerr ) {
+            nword_ = nword;
+            nerr_ = nerr;
+        }
+
+        /**
+         * Returns the number of raw error words used for input.
+         *
+         * @return   raw word count
+         */
+        public int getWordCount() {
+            return nword_;
+        }
+
+        /**
+         * Returns the number of error points generated.
+         *
+         * @return   error point count
+         */
+        public int getNerror() {
+            return nerr_;
+        }
+
+        /**
+         * Calculates the errors for a given point given a set of 
+         * raw error values.
+         *
+         * @param  centre  position of central point in Cartesian coords
+         * @param  rawerrs  wordCount-element array of raw error values
+         * @param  errors  nerror-element array of 3-element vectors,
+         *         all to be filled on exit; nulls may be inserted for
+         *         no error values
+         */
+        public abstract void calcErrors( double[] centre, double[] rawerrs,
+                                         double[][] errors );
+    }
+
+    /**
      * Helper class which decodes error information from the ValueStore.
      * When it comes time to read error information from the store,
      * the reader is pointed at the offset into it at which the error
      * information is found, and then reads some number of values to
-     * determine the upper and lower errors.
+     * determine the upper and lower deltas.
      */
-    private static abstract class ErrorReader {
+    private static abstract class DeltaPairReader {
         private final double[] buf_;
 
         /**
@@ -158,7 +243,7 @@ public class SphericalPolarPointStore implements PointStore {
          * @param   wordCount  number of words from the value store which
          *          are used to store error information for each datum
          */
-        public ErrorReader( int wordCount ) {
+        public DeltaPairReader( int wordCount ) {
             buf_ = new double[ wordCount ];
         }
 
