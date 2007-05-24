5040dfc13e5f8a8962364661f6644ac80c8d5d12
Mark Taylor <m.b.taylor@bristol.ac.uk>
2007-05-24 10:52:04 +0000

    Add calculation of quantiles.  Currently median, quartiles, .001, .01, .99 and .999 are offered.  I was initially reluctant to introduce this because it can be inefficient (effectively requires loading all columns into memory and sorting on all columns).  However, in practice, for a few 10^5 rows it seems to work OK.  OutOfMemoryErrors can result for larger tables, but I've attempted to catch them and write a comprehensible error message.

--- topcat/src/main/uk/ac/starlink/topcat/StatsWindow.java
+++ topcat/src/main/uk/ac/starlink/topcat/StatsWindow.java
@@ -19,6 +19,7 @@ import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 import javax.swing.JTable;
@@ -41,6 +42,7 @@ import uk.ac.starlink.table.gui.NumericCellRenderer;
 import uk.ac.starlink.table.gui.ProgressBarStarTable;
 import uk.ac.starlink.table.gui.StarJTable;
 import uk.ac.starlink.table.gui.StarTableColumn;
+import uk.ac.starlink.ttools.filter.QuantCalc;
 
 /**
  * A window which displays statistics for a RowSubset in the table.
@@ -59,6 +61,7 @@ public class StatsWindow extends AuxWindow {
     private final JComboBox subSelector_;
     private final AbstractTableModel statsTableModel_;
     private final BitSet hideColumns_ = new BitSet();
+    private final Action recalcAct_;
     private StatsCalculator activeCalculator_;
     private StatsCalculator lastCalc_;
     private SaveTableQueryWindow saveWindow_;
@@ -74,6 +77,9 @@ public class StatsWindow extends AuxWindow {
         new DefaultValueInfo( "subset", String.class,
                               "Name of row subset over which statistics " +
                               "were gathered" );
+    private static final Map QUANTILE_NAMES = quantileNames();
+    private static final double[] QUANTILES =
+        { 0.001, 0.01, 0.25, 0.5, 0.75, 0.99, 0.999, };
 
     /**
      * Constructs a StatsWindow to report on the statistics of data in a
@@ -105,7 +111,7 @@ public class StatsWindow extends AuxWindow {
 
         /* Customise the JTable's column model to provide control over
          * which columns are displayed. */
-        MetaColumnModel statsColumnModel =
+        final MetaColumnModel statsColumnModel =
             new MetaColumnModel( jtab_.getColumnModel(), statsTableModel_ );
         jtab_.setColumnModel( statsColumnModel );
 
@@ -116,6 +122,25 @@ public class StatsWindow extends AuxWindow {
                 statsColumnModel.removeColumn( i );
             }
         }
+
+        /* Watch the column model to see whether any quantile rows are
+         * introduced.  If they are, a recalculation may be required,
+         * since (because of the expense) quantiles are not calculated
+         * unless explicitly asked for. */
+        statsColumnModel.addColumnModelListener( new TableColumnModelAdapter() {
+            public void columnAdded( TableColumnModelEvent evt ) {
+                if ( getMetaColumn( evt.getToIndex() )
+                     instanceof QuantileColumn ) {
+                    StatsCalculator calc = activeCalculator_;
+                    if ( calc == null ) {
+                        calc = lastCalc_;
+                    }
+                    if ( calc != null && ! calc.hasQuant ) {
+                        recalcAct_.actionPerformed( null );
+                    }
+                }
+            }
+        } );
 
         /* Construct and place a widget for selecting which subset to
          * present results for. */
@@ -153,9 +178,9 @@ public class StatsWindow extends AuxWindow {
         };
 
         /* Provide an action for requesting a recalculation. */
-        Action recalcAct = new BasicAction( "Recalculate", ResourceIcon.REDO,
-                                            "Recalculate the statistics for " +
-                                            "the current subset" ) {
+        recalcAct_ = new BasicAction( "Recalculate", ResourceIcon.REDO,
+                                      "Recalculate the statistics for " +
+                                      "the current subset" ) {
             public void actionPerformed( ActionEvent evt ) {
                 RowSubset rset = (RowSubset) subSelector_.getSelectedItem();
                 calcMap_.remove( rset );
@@ -166,7 +191,7 @@ public class StatsWindow extends AuxWindow {
         /* Add actions to toolbar. */
         getToolBar().add( importAct );
         getToolBar().add( saveAct );
-        getToolBar().add( recalcAct );
+        getToolBar().add( recalcAct_ );
         getToolBar().addSeparator();
 
         /* Add export-type actions to File menu. */
@@ -176,7 +201,7 @@ public class StatsWindow extends AuxWindow {
         /* Add a menu for statistics operations. */
         JMenu statsMenu = new JMenu( "Statistics" );
         statsMenu.setMnemonic( KeyEvent.VK_S );
-        statsMenu.add( new JMenuItem( recalcAct ) );
+        statsMenu.add( new JMenuItem( recalcAct_ ) );
         getJMenuBar().add( statsMenu );
 
         /* Add a menu for controlling column display. */
@@ -242,6 +267,14 @@ public class StatsWindow extends AuxWindow {
             return;
         }
 
+        /* Work out if the requested statistics include any quantiles. */
+        boolean hasQuant = false;
+        for ( int icol = 0; icol < jtab_.getColumnCount() && ! hasQuant; 
+              icol++ ) {
+            hasQuant = hasQuant
+                    || getMetaColumn( icol ) instanceof QuantileColumn;
+        }
+
         /* If we have already done this calculation, display the results
          * directly. */
         if ( calcMap_.containsKey( rset ) ) {
@@ -251,7 +284,7 @@ public class StatsWindow extends AuxWindow {
         /* Otherwise, kick off a new thread which will perform the
          * calculations and display the results in due course. */
         else {
-            activeCalculator_ = new StatsCalculator( rset );
+            activeCalculator_ = new StatsCalculator( rset, hasQuant );
             activeCalculator_.start();
         }
     }
@@ -596,6 +629,12 @@ public class StatsWindow extends AuxWindow {
             }
         } );
 
+        /* Quantiles. */
+        for ( int iq = 0; iq < QUANTILES.length; iq++ ) {
+            hideColumns_.set( metas.size() );
+            metas.add( new QuantileColumn( QUANTILES[ iq ] ) );
+        }
+
         /* Construct a new TableModel based on these meta columns. */
         final MetaColumnTableModel tmodel = new MetaColumnTableModel( metas ) {
             public int getRowCount() {
@@ -642,6 +681,18 @@ public class StatsWindow extends AuxWindow {
     }
 
     /**
+     * Returns the MetaColumn providing the metadata and data for a given
+     * column in the JTable displayed by this window.
+     *
+     * @param   icol  JTable column index
+     * @return  metacolumn object
+     */
+    private MetaColumn getMetaColumn( int icol ) {
+        int jcol = jtab_.getColumnModel().getColumn( icol ).getModelIndex();
+        return ((MetaColumnTableModel) jtab_.getModel()).getMeta( jcol );
+    }
+
+    /**
      * Returns a StarTable giving the results of the most recent statistics
      * calculations.
      *
@@ -660,12 +711,9 @@ public class StatsWindow extends AuxWindow {
          * of statistical data. */
         int ncol = jtab_.getColumnCount();
         ColumnInfo[] infos = new ColumnInfo[ ncol ];
-        MetaColumnTableModel statsModel =
-            (MetaColumnTableModel) jtab_.getModel();
         for ( int icol = 0; icol < ncol; icol++ ) {
-            int jcol = jtab_.getColumnModel().getColumn( icol ).getModelIndex();
             infos[ icol ] =
-                new ColumnInfo( statsModel.getMeta( jcol ).getColumnInfo() );
+                new ColumnInfo( getMetaColumn( icol ).getColumnInfo() );
         }
 
         /* Construct an empty table with these columns. */
@@ -703,6 +751,69 @@ public class StatsWindow extends AuxWindow {
     }
 
     /**
+     * Map providing human-readable names for quantiles.
+     * There ought to be an entry for every entry in the {@link #QUANTILES}
+     * array.
+     *
+     * @return   Double -&gt; String map
+     */
+    private static Map quantileNames() {
+        Map map = new HashMap();
+        map.put( new Double( 0.5 ), "Median" );
+        map.put( new Double( 0.25 ), "Quartile1" );
+        map.put( new Double( 0.75 ), "Quartile3" );
+        map.put( new Double( 0.001 ), "Q001" );
+        map.put( new Double( 0.01 ), "Q01" );
+        map.put( new Double( 0.99 ), "Q99" );
+        map.put( new Double( 0.999 ), "Q999" );
+        return map;
+    }
+
+    /**
+     * Metacolumn subclass which can display quantile values.
+     */
+    private class QuantileColumn extends MetaColumn {
+        private final Double key_;
+        private final double quant_;
+
+        /**
+         * Constructor.
+         *
+         * @param   quant   value at which quantile is calculated (0-1)
+        QuantileColumn( double quant ) {
+            this( quant, (String) QUANTILE_NAMES.get( new Double( quant ) ) );
+        }
+
+        /**
+         * Constructor with name.
+         *
+         * @param   quant   value at which quantile is calculated (0-1)
+         * @param  name   column name
+         */
+        QuantileColumn( double quant, String name ) {
+            super( name, Number.class, "Quantile corresponding to " + quant );
+            quant_ = quant;
+            key_ = new Double( quant );
+        }
+
+        public Object getValue( int irow ) {
+            int jcol = getModelIndexFromRow( irow );
+            if ( lastCalc_.hasQuant ) {
+                Map quantiles = lastCalc_.quantiles[ jcol ];
+                if ( quantiles == null || ! quantiles.containsKey( key_ ) ) {
+                    return null;
+                }
+                else {
+                    return quantiles.get( key_ );
+                }
+            }
+            else {
+                return null;
+            }
+        }
+    }
+
+    /**
      * Helper class which performs the calculations in its own thread,
      * and displays the results in the StatsWindow when it's done.
      * A maximum of one active instance of this is maintained by each 
@@ -711,6 +822,7 @@ public class StatsWindow extends AuxWindow {
     private class StatsCalculator extends Thread {
 
         private final RowSubset rset;
+        private final boolean hasQuant;
 
         int ncol;
         long ngoodrow;
@@ -737,16 +849,20 @@ public class StatsWindow extends AuxWindow {
         double[] sum3s;
         double[] sum4s;
         int[] cards;
+        Map[] quantiles;
+        QuantCalc[] quantCalcs;
 
         /**
          * Constructs a calculator object which can calculate the statistics
          * of the table owned by this StatsWindow over a given RowSubset.
          *
          * @param  rset the RowSubset to do calculations for
+         * @param  hasQuant  true if quantiles need calculating
          */
-        public StatsCalculator( RowSubset rset ) {
+        public StatsCalculator( RowSubset rset, boolean hasQuant ) {
             super( "StatsCalculator" );
             this.rset = rset;
+            this.hasQuant = hasQuant; 
         }
 
         /**
@@ -773,6 +889,28 @@ public class StatsWindow extends AuxWindow {
                         displayCalculations( StatsCalculator.this );
                     }
                 } );
+            }
+            catch ( OutOfMemoryError e ) {
+                if ( hasQuant ) {
+                    quantCalcs = null;
+                    final Object msg = new String[] {
+                        "Out of memory while calculating quantiles.",
+                        "",
+                        "Quantiles (median, quartiles, percentiles etc)",
+                        "are much more expensive to calculate than other",
+                        "statistical quantities.",
+                        "To calculate statistics for this table you will",
+                        "need to undisplay these columns, or start again",
+                        "with more memory.",
+                    };
+                    SwingUtilities.invokeLater( new Runnable() {
+                        public void run() {
+                            JOptionPane.showMessageDialog(
+                                    StatsWindow.this, msg, "Calculation failed",
+                                    JOptionPane.ERROR_MESSAGE );
+                        }
+                    } );
+                }
             }
             catch ( IOException e ) {
                 // no other action
@@ -826,6 +964,8 @@ public class StatsWindow extends AuxWindow {
             sum3s = new double[ ncol ];
             sum4s = new double[ ncol ];
             cards = new int[ ncol ];
+            quantiles = new Map[ ncol ];
+            quantCalcs = new QuantCalc[ ncol ];
 
             boolean[] badcompars = new boolean[ ncol ];
             double[] dmins = new double[ ncol ];
@@ -833,6 +973,20 @@ public class StatsWindow extends AuxWindow {
             Set[] valuesets = new Set[ ncol ];
             Arrays.fill( dmins, Double.MAX_VALUE );
             Arrays.fill( dmaxs, -Double.MAX_VALUE );
+
+            /* If we are going to calculate quantiles it's useful to work out
+             * how many rows there are in the subset up front.  This is a
+             * bit risky - the count may be incorrect if subset definition
+             * has changed.  Hmm.  It's not essential to use this value,
+             * but doing it will make it easier to catch, e.g.,
+             * OutOfMemoryErrors. */
+            long nr = -1;
+            if ( hasQuant ) {
+                Object num = tcModel_.getSubsetCounts().get( rset );
+                if ( num instanceof Number ) {
+                    nr = ((Number) num).longValue();
+                }
+            }
 
             /* See which columns we can sensibly gather statistics on. */
             for ( int icol = 0; icol < ncol; icol++ ) {
@@ -845,6 +999,9 @@ public class StatsWindow extends AuxWindow {
                 isCardinal[ icol ] = ! clazz.equals( Boolean.class );
                 if ( isCardinal[ icol ] ) {
                     valuesets[ icol ] = new HashSet();
+                }
+                if ( hasQuant && isNumber[ icol ] ) {
+                    quantCalcs[ icol ] = QuantCalc.createInstance( clazz, nr );
                 }
             }
 
@@ -919,6 +1076,9 @@ public class StatsWindow extends AuxWindow {
                                     sum2s[ icol ] += s2;
                                     sum3s[ icol ] += s3;
                                     sum4s[ icol ] += s4;
+                                    if ( hasQuant ) {
+                                        quantCalcs[ icol ].acceptDatum( val );
+                                    }
                                 }
                             }
                             else if ( isComparable[ icol ] ) {
@@ -1061,6 +1221,17 @@ public class StatsWindow extends AuxWindow {
                             isCardinal[ icol ] = false;
                             valuesets[ icol ] = null;
                         }
+                    }
+                    QuantCalc quantCalc = quantCalcs[ icol ];
+                    if ( quantCalc != null ) {
+                        quantCalc.ready();
+                        quantiles[ icol ] = new HashMap();
+                        for ( int iq = 0; iq < QUANTILES.length; iq++ ) {
+                            double q = QUANTILES[ iq ];
+                            quantiles[ icol ].put( new Double( q ),
+                                                   quantCalc.getQuantile( q ) );
+                        }
+                        quantCalcs[ icol ] = null;
                     }
                 }
                 else {
