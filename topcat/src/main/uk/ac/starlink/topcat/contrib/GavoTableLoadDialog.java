d719d80a017a1e40ddb39bc0afa1c03549db0747
Mark Taylor <m.b.taylor@bristol.ac.uk>
2007-06-07 14:15:08 +0000

    Improved UI so that the databases are listed by name not URL. Removed requirement to input name/password when used for public database. Now performs table load out of the event dispatch thread. Adds some metadata to returned table (name of DB, text of query).

--- topcat/src/main/uk/ac/starlink/topcat/contrib/GavoTableLoadDialog.java
+++ topcat/src/main/uk/ac/starlink/topcat/contrib/GavoTableLoadDialog.java
@@ -1,13 +1,19 @@
+package uk.ac.starlink.topcat.contrib;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.util.List;
 import java.util.Vector;
+import java.util.logging.Logger;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
@@ -20,32 +26,34 @@ import javax.swing.JPasswordField;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 
+import uk.ac.starlink.table.DefaultValueInfo;
+import uk.ac.starlink.table.DescribedValue;
 import uk.ac.starlink.table.StarTable;
 import uk.ac.starlink.table.StarTableFactory;
+import uk.ac.starlink.table.ValueInfo;
 import uk.ac.starlink.table.gui.BasicTableLoadDialog;
 import uk.ac.starlink.table.gui.LabelledComponentStack;
 
 public class GavoTableLoadDialog extends BasicTableLoadDialog {
 
     private List queries = new Vector();
-    private CSVTableParser csvParser = new CSVTableParser();
-    public class GAVOTableSupplier implements TableSupplier{
-     final StarTable table;
-        GAVOTableSupplier(StarTable table)
-        {
-            this.table = table;
-        }
-        
-        public StarTable getTable(StarTableFactory arg0, String arg1)
-                throws IOException {
-            // TODO Auto-generated method stub
-            return this.table;
-        }
-        public String getTableID() {
-            // TODO Auto-generated method stub
-            return table.getName();
-        }
-    }
+    private GavoCSVTableParser csvParser = new GavoCSVTableParser();
+
+    private static final Database[] DATABASES = new Database[] {
+        new Database( "Millennium",
+                      "http://www.g-vo.org/Millennium?action=doQuery&SQL=",
+                      false ),
+        new Database( "MyMillennium",
+                      "http://www.g-vo.org/MyMillennium?action=doQuery&SQL=",
+                      true ),
+    };
+
+    private static final ValueInfo URL_INFO =
+        new DefaultValueInfo( "Database", String.class,
+                              "Base URL of database providing results" );
+    private static final ValueInfo SQL_INFO =
+        new DefaultValueInfo( "SQL", String.class,
+                              "Text of SQL query" );
     
     private final JComboBox urlField_;
     private final JTextField userField_;
@@ -60,11 +68,7 @@ public class GavoTableLoadDialog extends BasicTableLoadDialog {
         super( "GAVO query", "Queries GAVO database" );
 
         /* Set up fields for user interaction. */
-        String[] availableURLs = new String[]{
-            "http://www.g-vo.org/MyMillennium?action=doQuery&SQL=",
-            "http://www.g-vo.org/Millennium?action=doQuery&SQL="
-        }; // TODO may invent nicer configuration
-        urlField_ = new JComboBox(availableURLs);
+        urlField_ = new JComboBox(DATABASES);
         urlField_.setEditable(true);
         userField_ = new JTextField();
         passField_ = new JPasswordField();
@@ -75,9 +79,26 @@ public class GavoTableLoadDialog extends BasicTableLoadDialog {
         LabelledComponentStack stack = new LabelledComponentStack();
         stack.addLine( "Query URL", urlField_ );
         stack.addLine( "User", userField_ );
+        final JLabel userLabel =
+            stack.getLabels()[ stack.getLabels().length - 1 ];
         stack.addLine( "Password", passField_ );
+        final JLabel passLabel =
+            stack.getLabels()[ stack.getLabels().length - 1 ];
 //        stack.addLine( "Is numeric", isNumericCB_ );
         setBorder( BorderFactory.createEmptyBorder( 5, 5, 5, 5 ) );
+
+        ActionListener urlListener = new ActionListener() {
+            public void actionPerformed( ActionEvent evt ) {
+                Database db = (Database) urlField_.getSelectedItem();
+                boolean requiresAuth = db != null && db.requiresAuth_;
+                userLabel.setEnabled( requiresAuth );
+                userField_.setEnabled( requiresAuth );
+                passLabel.setEnabled( requiresAuth );
+                passField_.setEnabled( requiresAuth );
+            }
+        };
+        urlListener.actionPerformed( null );
+        urlField_.addActionListener( urlListener );
 
         JComponent sqlHolder = new JPanel( new BorderLayout() );
         Box labelBox = Box.createVerticalBox();
@@ -110,9 +131,13 @@ public class GavoTableLoadDialog extends BasicTableLoadDialog {
     protected TableSupplier getTableSupplier() {
 
         /* Get state. */
-        final String url = (String)urlField_.getSelectedItem();
-        final String user = userField_.getText();
-        final String pass = new String( passField_.getPassword() );
+        Database db = (Database) urlField_.getSelectedItem();
+        final boolean requiresAuth = db.requiresAuth_;
+        final String url = db.url_;
+        final String user = requiresAuth ? userField_.getText() : "";
+        final String pass = requiresAuth
+                          ? new String( passField_.getPassword() )
+                          : null;
         final String sql = sqlField_.getText();
         queries.add(sql);
 
@@ -120,45 +145,64 @@ public class GavoTableLoadDialog extends BasicTableLoadDialog {
          * tell straight away that this isn't going to result in a readable
          * table. */
         if ( url == null || url.trim().length() == 0 ||
-             user == null || user.trim().length() == 0 ||
-             pass == null || pass.trim().length() == 0 ||
-             sql == null || sql.trim().length() == 0 ) {
+             sql == null || sql.trim().length() == 0 ||
+             requiresAuth && ( user == null || user.trim().length() == 0 ||
+                               pass == null || pass.trim().length() == 0 ) ) {
             throw new IllegalArgumentException(
                 "All fields must be filled in" );
         }
 
-        /* Construct an object which can read the data.  DataSource is 
-         * basically a way of wrapping a stream so that it can be re-read.
-         * CSV files need to be read in two passes (once to determine the
-         * type of data in each column, and once to read the actual data).
-         * If the data was returned in a format such as FITS or VOTable 
-         * there would be more efficent ways of doing it. */
-        // GL: replaced original code with a straightforward parse of the stream by
-        // the custom parser tailored ot the streams coming form the
-        // GAVO web databases
-        
+        String sqlEncoding;
         try {
-                String sqlEncoding = URLEncoder.encode(sql, "UTF-8");
-                String userPassword = user + ":" + pass;
-                String encoding = new sun.misc.BASE64Encoder().encode (userPassword.getBytes());
-                String urlString = url+sqlEncoding;
-                URLConnection uc = new URL( urlString ).openConnection();
-                uc.setRequestProperty ("Authorization", "Basic " + encoding);
+            sqlEncoding = URLEncoder.encode(sql, "UTF-8");
+        }
+        catch ( UnsupportedEncodingException e ) {
+            throw new AssertionError( "Do what?" );
+        }
+        String userPassword = user + ":" + pass;
+        final String encoding = new sun.misc.BASE64Encoder()
+                               .encode (userPassword.getBytes());
+        String urlString = url+sqlEncoding;
+        final URL queryUrl;
+        try {
+            queryUrl = new URL( urlString );
+        }
+        catch ( MalformedURLException e ) {
+            // shouldn't happen
+            throw (IllegalArgumentException)
+                  new IllegalArgumentException( "Bad url: " + url )
+                 .initCause( e );
+        }
+        final String id = db.toString() + " query " + queries.size();
+        return new TableSupplier() {
+            public StarTable getTable( StarTableFactory tabFact, String fmt )
+                    throws IOException {
+                URLConnection uc = queryUrl.openConnection();
+                if ( requiresAuth ) {
+                    uc.setRequestProperty ("Authorization",
+                                           "Basic " + encoding);
+                }
                 InputStream stream = uc.getInputStream();
-                StarTable starTable;
-                System.out.println("Content type = "+uc.getContentType());
-                starTable = csvParser.parse(stream);
-                starTable.setName("Query "+queries.size());
-                return new GAVOTableSupplier(starTable);
-        }
-        catch(IllegalArgumentException iae)
-        {
-            throw iae;
-        }
-        catch(Exception e)
-        {
-            throw new IllegalArgumentException(e.getMessage());
-        }
+                Logger.getLogger( "uk.ac.starlink.topcat.contrib" )
+                      .info( "Content type = " + uc.getContentType() );
+                StarTable table;
+                try {
+                    table = csvParser.parse(stream);
+                }
+                catch ( Throwable e ) {
+                    throw asIOException( e );
+                }
+                finally {
+                    stream.close();
+                }
+                table.setParameter( new DescribedValue( URL_INFO, url ) );
+                table.setParameter( new DescribedValue( SQL_INFO, sql ) );
+                return table;
+            }
+            public String getTableID() {
+                return id;
+            }
+        };
     }
 
     /**
@@ -173,5 +217,22 @@ public class GavoTableLoadDialog extends BasicTableLoadDialog {
         userField_.setEnabled( enabled );
         passField_.setEnabled( enabled );
         sqlField_.setEnabled( enabled );
+    }
+
+    /**
+     * Encapsulates the available target databases.
+     */
+    private static class Database {
+        final String name_;
+        final String url_;
+        final boolean requiresAuth_;
+        Database( String name, String url, boolean requiresAuth ) {
+            name_ = name;
+            requiresAuth_ = requiresAuth;
+            url_ = url;
+        }
+        public String toString() {
+            return name_;
+        }
     }
 }
