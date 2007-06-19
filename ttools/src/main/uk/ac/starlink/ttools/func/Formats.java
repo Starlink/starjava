826a23a076c7b0104941826dd4a505524623b3df
Mark Taylor <m.b.taylor@bristol.ac.uk>
2007-06-19 09:41:29 +0000

    Add locale-specific formatDecimal methods

--- ttools/src/main/uk/ac/starlink/ttools/func/Formats.java
+++ ttools/src/main/uk/ac/starlink/ttools/func/Formats.java
@@ -8,6 +8,7 @@ package uk.ac.starlink.ttools.func;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 // Need threads.
@@ -41,7 +42,7 @@ public class Formats {
 
     /**
      * Turns a floating point value into a string with a given number of
-     * decimal places.
+     * decimal places using standard settings.
      *
      * @example  <code>formatDecimal(PI,0) = "3."</code>
      * @example  <code>formatDecimal(0,10) = ".0000000000"</code>
@@ -52,13 +53,33 @@ public class Formats {
      * @return  formatted string
      */
     public static String formatDecimal( double value, int dp ) {
-        return getDpFormat( dp ).format( value );
+        return getDpFormat( dp, false ).format( value );
     }
 
     /**
-     * Turns a floating point value into a formatted string.
+     * Turns a floating point value into a string using current locale settings.
+     * For instance if language is set to French, decimal points will
+     * be represented as a comma "," instead of a full stop ".".
+     * Otherwise behaves the same as the corresponding 
+     * <code>formatDecimal</code> function.
+     *
+     * @example  <code>formatDecimal(PI,0) = "3,"</code>
+     * @example  <code>formatDecimal(0,10) = ",0000000000"</code>
+     * @example  <code>formatDecimal(E*10,3) = "27,183"</code>
+     *
+     * @param   value   value to format
+     * @param   dp      number of decimal places (digits after the decmal point)
+     * @return  formatted string
+     */
+    public static String formatDecimalLocal( double value, int dp ) {
+        return getDpFormat( dp, true ).format( value );
+    }
+
+    /**
+     * Turns a floating point value into a formatted string using standard
+     * settings.
      * The <code>format</code> string is as defined by Java's 
-     * <a href="http://java.sun.com/j2se/1.4.2/docs/api/java/text/DecimalFormat"
+     * <a href="http://java.sun.com/j2se/1.4.2/docs/api/java/text/DecimalFormat.html"
      *    ><code>java.text.DecimalFormat</code></a> class.
      *
      * @example   <code>formatDecimal(99, "#.000") = "99.000"</code>
@@ -69,7 +90,26 @@ public class Formats {
      * @return formatted string
      */
     public static String formatDecimal( double value, String format ) {
-        return getFormat( format ).format( value );
+        return getFormat( format, false ).format( value );
+    }
+
+    /**
+     * Turns a floating point value into a formatted string using current
+     * locale settings.
+     * For instance if language is set to French, decimal points will
+     * be represented as a comma "," instead of a full stop ".".
+     * Otherwise behaves the same as the corresponding 
+     * <code>formatDecimal</code> function.
+     *
+     * @example   <code>formatDecimal(99, "#.000") = "99,000"</code>
+     * @example   <code>formatDecimal(PI, "+0.##;-0.##") = "+3,14"</code>
+     *
+     * @param  value   value to format
+     * @param  format  format specifier
+     * @return formatted string
+     */
+    public static String formatDecimalLocal( double value, String format ) {
+        return getFormat( format, true ).format( value );
     }
 
     /**
@@ -80,11 +120,13 @@ public class Formats {
      * @param  format  format specifier string
      * @return  format object
      */
-    private static DecimalFormat getFormat( String format ) {
-        Map fmap = getKit().fmap_;
+    private static DecimalFormat getFormat( String format, boolean local ) {
+        Map fmap = local ? getKit().localFormatMap_
+                         : getKit().englishFormatMap_;
         if ( ! fmap.containsKey( format ) ) {
             DecimalFormat dfmt;
-            NumberFormat nfmt = NumberFormat.getInstance();
+            Locale locale = local ? Locale.getDefault() : Locale.ENGLISH;
+            NumberFormat nfmt = NumberFormat.getInstance( locale );
             if ( nfmt instanceof DecimalFormat ) {
                 dfmt = ((DecimalFormat) nfmt);
                 dfmt.applyPattern( format );
@@ -106,7 +148,7 @@ public class Formats {
      * @param   dp  number of digits after the decimal point
      * @return   format object
      */
-    private static DecimalFormat getDpFormat( int dp ) {
+    private static DecimalFormat getDpFormat( int dp, boolean local ) {
         DecimalFormat[] dpFormats = getKit().dpFormats_;
         if ( dp >= dpFormats.length ) {
             dpFormats = new DecimalFormat[ dp + 1 ];
@@ -116,7 +158,7 @@ public class Formats {
                 for ( int j = 0; j < i; j++ ) {
                     format += "0";
                 }
-                dpFormats[ i ] = getFormat( format );
+                dpFormats[ i ] = getFormat( format, local );
             }
         }
         return dpFormats[ dp ];
@@ -132,13 +174,22 @@ public class Formats {
     }
 
     /**
+     * Resets the format kit - must be called if locale is changed for
+     * local formatting methods to work correctly.  Only currently 
+     * intended for testing use.
+     */
+    static void reset() {
+        kitHolder_.set( new FormatKit() );
+    }
+
+    /**
      * Helper class which contains all the items that are potentially
      * expensive to produce but cannot be shared by different threads.
      * An instance of this class is managed by a ThreadLocal.
      */
     private static class FormatKit {
-        final Map fmap_ = new HashMap();
+        final Map localFormatMap_ = new HashMap();
+        final Map englishFormatMap_ = new HashMap();
         DecimalFormat[] dpFormats_ = new DecimalFormat[ 0 ];
     }
-    
 }
