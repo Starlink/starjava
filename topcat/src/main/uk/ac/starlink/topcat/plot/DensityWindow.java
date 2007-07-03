43843c97a69c202c7f6eb6d109f16797e9af2b0f
Mark Taylor <m.b.taylor@bristol.ac.uk>
2007-07-03 09:57:59 +0000

    Retitle

--- topcat/src/main/uk/ac/starlink/topcat/plot/DensityWindow.java
+++ topcat/src/main/uk/ac/starlink/topcat/plot/DensityWindow.java
@@ -108,7 +108,7 @@ public class DensityWindow extends GraphicsWindow {
      * @param   parent   parent component (may be used for positioning)
      */
     public DensityWindow( Component parent ) {
-        super( "Density Plot", AXIS_NAMES, 0, new ErrorModeSelectionModel[ 0 ],
+        super( "Density Map", AXIS_NAMES, 0, new ErrorModeSelectionModel[ 0 ],
                parent );
 
         /* There's only one style set it makes sense to use for this window.
