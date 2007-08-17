581309b631bbf4fdfabe0718dfc61a81a0418ba6
Mark Taylor <m.b.taylor@bristol.ac.uk>
2007-08-17 22:40:23 +0000

    Add Wrapper-related utility methods

--- topcat/src/main/uk/ac/starlink/topcat/TopcatUtils.java
+++ topcat/src/main/uk/ac/starlink/topcat/TopcatUtils.java
@@ -321,6 +321,48 @@ public class TopcatUtils {
     }
 
     /**
+     * Returns the object on which a given object is based.
+     * If <code>obj</code> is a {@link Wrapper}, it is unwrapped as far
+     * as possible and the base object is returned.
+     * Otherwise <code>obj</code> itself is returned.
+     *
+     * @param  obj  test object
+     * @return   ultimate base object of <code>obj</code>
+     */
+    public static Object getWrapped( Object obj ) {
+        while ( obj instanceof Wrapper ) {
+            obj = ((Wrapper) obj).getBase();
+        }
+        return obj;
+    }
+
+    /**
+     * Attempts to return an object of a given class on which a given
+     * object is based.
+     * An object is unwrapped (see {@link Wrapper#getBase}) until an
+     * object of class <code>clazz</code> is found, at which point it
+     * is returned.  If no <code>clazz</code> object can be found,
+     * <code>null</code> is returned.
+     *
+     * @param  obj   test object
+     * @return   object within the wrapping hierarchy of class 
+     *           <code>clazz</code>, or null
+     */
+    public static Object getWrapped( Object obj, Class clazz ) {
+        while ( true ) {
+            if ( clazz.isAssignableFrom( obj.getClass() ) ) {
+                return obj;
+            }
+            else if ( obj instanceof Wrapper ) {
+                obj = ((Wrapper) obj).getBase();
+            }
+            else {
+                return null;
+            }
+        }
+    }
+
+    /**
      * Indicates whether there are enough classes to make SoG work at runtime.
      *
      * @return  true iff it's safe to use a SoG-based viewer
