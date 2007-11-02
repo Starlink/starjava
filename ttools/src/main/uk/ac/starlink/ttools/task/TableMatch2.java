8c8f06eb18628debeb5bbe51fdcae39f70ae51fd
Mark Taylor <m.b.taylor@bristol.ac.uk>
2007-11-02 16:55:25 +0000

    Refine task description

--- ttools/src/main/uk/ac/starlink/ttools/task/TableMatch2.java
+++ ttools/src/main/uk/ac/starlink/ttools/task/TableMatch2.java
@@ -1,14 +1,15 @@
 package uk.ac.starlink.ttools.task;
 
 /**
- * Task implementation for pair matching task.
+ * Task implementation for generic pair matching task.
  *
  * @author   Mark Taylor
  * @since    2 Sep 2005
  */
 public class TableMatch2 extends FixedMapperTask {
     public TableMatch2() {
-        super( "Crossmatches 2 tables", new ChoiceMode(), true,
+        super( "Crossmatches 2 tables using flexible criteria",
+               new ChoiceMode(), true,
                new Match2Mapper(), 2, true );
     }
 }
