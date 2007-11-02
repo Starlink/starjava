2de69dcc1215b3d5adad793387239054c28d0dbb
Mark Taylor <m.b.taylor@bristol.ac.uk>
2007-11-02 14:04:21 +0000

    Now tests both Implementation and Service implementations of the CeaWriter

--- ttools/src/testcases/uk/ac/starlink/ttools/cea/CeaTest.java
+++ ttools/src/testcases/uk/ac/starlink/ttools/cea/CeaTest.java
@@ -14,41 +14,54 @@ public class CeaTest extends TestCase {
     public static final String APP_DESCRIPTION_SCHEMA =
         "http://software.astrogrid.org/schema/cea/CEAImplementation/v1.0/" +
         "CEAImplementation.xsd";
+    private static final String DUMMY_CMD =
+        "<" + CeaTest.class.getName() + ">";
+    private CeaTask[] tasks_;
 
     public CeaTest( String name ) {
         super( name );
     }
 
-    public void testAppDescription()
-           throws IOException, SAXException, LoadException {
-       File tmpFile = File.createTempFile( "app-description", ".xml" );
-       tmpFile.deleteOnExit();
-       try {
-           PrintStream out = new PrintStream( new FileOutputStream( tmpFile ) );
-           String dummyCmd = "<" + getClass().getName() + ">";
-           CeaWriter writer =
-               new ImplementationCeaWriter( out, CeaWriter.createTaskList(),
-                                            dummyCmd );
-           writer.configure( dummyCmd,
-                             new String[] { "-path", "/usr/bin/stilts", } );
-           writer.writeDocument();
-           out.close();
+    public void setUp() throws LoadException {
+        tasks_ = CeaWriter.createTaskList();
+    }
 
-           String schema = APP_DESCRIPTION_SCHEMA;
+    public void testImplementationDoc() throws Exception {
+        checkCeaOutput( new ImplementationCeaWriter( null, tasks_, DUMMY_CMD ),
+                        new String[] { "-path", "/usr/bin/stilts", } );
+    }
 
-           if ( Boolean.getBoolean( "tests.withnet" ) ) {
-               ValidationDriver validor = new ValidationDriver();
-               validor.loadSchema( validor.uriOrFileInputSource( schema ) );
-               assertTrue( validor.validate( validor
+    public void testServiceDoc() throws Exception {
+        checkCeaOutput( new ServiceCeaWriter( null, tasks_, DUMMY_CMD ),
+                        new String[ 0 ] );
+    }
+
+    private void checkCeaOutput( CeaWriter writer, String[] extraArgs )
+            throws Exception {
+        File tmpFile = File.createTempFile( "cea-doc", ".xml" );
+        tmpFile.deleteOnExit();
+        try {
+            PrintStream out =
+                new PrintStream( new FileOutputStream( tmpFile ) );
+            writer.setOut( out );
+            writer.configure( DUMMY_CMD, extraArgs );
+            writer.writeDocument();
+            out.close();
+
+            String schema = writer.getSchemaLocation();
+            if ( Boolean.getBoolean( "tests.withnet" ) ) {
+                ValidationDriver validor = new ValidationDriver();
+                validor.loadSchema( validor.uriOrFileInputSource( schema ) );
+                assertTrue( validor.validate( validor
                                             .fileInputSource( tmpFile ) ) );
-           }
-           else {
-               System.out.println( "Skipping network-dependent tests " 
-                                 + "(tests.withnet not set)" );
-           }
-       }
-       finally {
-           tmpFile.delete();
-       }
+            }
+            else {
+                System.out.println( "Skipping network-dependent tests " 
+                                  + "(tests.withnet not set)" );
+            }
+        }
+        finally {
+            tmpFile.delete();
+        }
     }
 }
