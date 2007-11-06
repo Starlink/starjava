5e3655acf17a19892ad986990d52e443f2306df0
Mark Taylor <m.b.taylor@bristol.ac.uk>
2007-11-06 18:01:33 +0000

    Add tests for tskymatch2

--- ttools/src/testcases/uk/ac/starlink/ttools/task/SkyMatchTest.java
+++ ttools/src/testcases/uk/ac/starlink/ttools/task/SkyMatchTest.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import uk.ac.starlink.table.ColumnData;
+import uk.ac.starlink.table.ColumnInfo;
 import uk.ac.starlink.table.ColumnPermutedStarTable;
 import uk.ac.starlink.table.ColumnStarTable;
 import uk.ac.starlink.table.DefaultValueInfo;
@@ -12,6 +13,7 @@ import uk.ac.starlink.table.StarTable;
 import uk.ac.starlink.table.StarTableOutput;
 import uk.ac.starlink.table.Tables;
 import uk.ac.starlink.table.ValueInfo;
+import uk.ac.starlink.task.ExecutionException;
 import uk.ac.starlink.ttools.TableTestCase;
 import uk.ac.starlink.ttools.func.Coords;
 
@@ -28,7 +30,7 @@ public class SkyMatchTest extends TableTestCase {
     public SkyMatchTest( String name ) {
         super( name );
 
-        t0 = getTestTable();
+        t0 = createTestTable();
         t1 = new ColumnPermutedStarTable( t0, new int[] { 0, 1, 2, 5 } );
         t2 = new ColumnPermutedStarTable( t0, new int[] { 0, 3, 4, 5 } );
     }
@@ -56,32 +58,88 @@ public class SkyMatchTest extends TableTestCase {
         env.setValue( "params", "10" );
         env.setValue( "join", "1and2" );
 
-        StarTable result = match2( env, t1, "ra1 dec1 known_error*0.49",
-                                        t2, "ra2 dec2 known_error*0.49" );
-        assertEquals( 1, match2( env, t1, "ra1 dec1 known_error*0.49",
-                                      t2, "ra2 dec2 known_error*0.49" )
+        StarTable result = tmatch2( env, t1, "ra1 dec1 known_error*0.49",
+                                         t2, "ra2 dec2 known_error*0.49" );
+        assertEquals( 1, tmatch2( env, t1, "ra1 dec1 known_error*0.49",
+                                       t2, "ra2 dec2 known_error*0.49" )
                         .getRowCount() );
 
-        assertEquals( 1000, match2( env, t1, "ra1 dec1 known_error*0.51",
-                                         t2, "ra2 dec2 known_error*0.51" )
+        assertEquals( 1000, tmatch2( env, t1, "ra1 dec1 known_error*0.51",
+                                          t2, "ra2 dec2 known_error*0.51" )
                            .getRowCount() );
 
         env.setValue( "params", "5" );
-        assertEquals( 550, match2( env, t1, "ra1 dec1 known_error*0.51",
-                                        t2, "ra2 dec2 known_error*0.51" )
+        assertEquals( 550, tmatch2( env, t1, "ra1 dec1 known_error*0.51",
+                                         t2, "ra2 dec2 known_error*0.51" )
                           .getRowCount() );
 
-        assertEquals( 550, match2( env, t1, "ra1 dec1 known_error*1.01",
-                                        t2, "ra2 dec2 0" )
+        assertEquals( 550, tmatch2( env, t1, "ra1 dec1 known_error*1.01",
+                                         t2, "ra2 dec2 0" )
                           .getRowCount() );
 
-        assertEquals( 550, match2( env, t1, "ra1 dec1 known_error*1.01",
-                                        t2, "ra2 dec2 NULL" )
+        assertEquals( 550, tmatch2( env, t1, "ra1 dec1 known_error*1.01",
+                                         t2, "ra2 dec2 NULL" )
                           .getRowCount() );
     }
 
-    private StarTable match2( MapEnvironment env, StarTable table1,
-                              String values1, StarTable table2, String values2 )
+    public void testSkyColumns() throws Exception {
+        StarTable ta = createTestTable();
+        StarTable tb = createTestTable();
+
+        ColumnInfo ra1col = ta.getColumnInfo( 1 );
+        ColumnInfo dec1col = ta.getColumnInfo( 2 );
+        ColumnInfo ra2col = tb.getColumnInfo( 3 );
+        ColumnInfo dec2col = tb.getColumnInfo( 4 );
+        assertEquals( "RA1", ra1col.getName() );
+        assertEquals( "DEC1", dec1col.getName() );
+        assertEquals( "RA2", ra2col.getName() );
+        assertEquals( "DEC2", dec2col.getName() );
+
+        assertEquals( 550,
+                      skyCount( ta, "ra1", "dec1", tb, "ra2", "dec2", 5 ) );
+        try {
+            skyCount( ta, null, null, tb, "RA2", "DEC2", 5 );
+            fail();
+        }
+        catch ( ExecutionException e ) {
+            assertTrue( e.getMessage().indexOf( "Failed to identify" ) >= 0 );
+        }
+
+        ra1col.setUCD( "POS_EQ_RA" );
+        dec1col.setUCD( "POS_EQ_DEC_MAIN" );
+        assertEquals( 550,
+                      skyCount( ta, null, null, tb, "RA2+0", "DEC2*1", 5 ) );
+
+        ra2col.setName( "RA" );
+        dec2col.setName( "DEC" );
+        assertEquals( 550, skyCount( ta, null, null, tb, "", "", 5 ) );
+
+        ra2col.setUnitString( "radians" );
+        dec2col.setUnitString( "radians" );
+        assertEquals( 0, skyCount( ta, "ra1", "dec1",
+                                   tb, "", "", 5 ) );
+    }
+
+    private int skyCount( StarTable table1, String ra1, String dec1,
+                          StarTable table2, String ra2, String dec2,
+                          double error )
+            throws Exception {
+        MapEnvironment env = new MapEnvironment()
+            .setValue( "in1", table1 )
+            .setValue( "in2", table2 )
+            .setValue( "ra1", ra1 )
+            .setValue( "dec1", dec1 )
+            .setValue( "ra2", ra2 )
+            .setValue( "dec2", dec2 )
+            .setValue( "error", Double.toString( error ) );
+        new SkyMatch2().createExecutable( env ).execute();
+        StarTable match = env.getOutputTable( "omode" );
+        return (int) match.getRowCount();
+    }
+
+    private StarTable tmatch2( MapEnvironment env,
+                               StarTable table1, String values1,
+                               StarTable table2, String values2 )
             throws Exception {
         MapEnvironment env12 = new MapEnvironment( env );
         env12.setValue( "in1", table1 );
@@ -107,16 +165,55 @@ public class SkyMatchTest extends TableTestCase {
         return m12;
     }
 
+    private StarTable skymatch2( MapEnvironment env,
+                                 StarTable table1, String ra1, String dec1,
+                                 StarTable table2, String ra2, String dec2,
+                                 double error )
+            throws Exception {
+        MapEnvironment env12 = new MapEnvironment( env );
+        env12.setValue( "in1", table1 );
+        env12.setValue( "in2", table2 );
+        env12.setValue( "ra1", ra1 );
+        env12.setValue( "dec1", dec1 );
+        env12.setValue( "ra2", ra2 );
+        env12.setValue( "dec2", dec2 );
+        env12.setValue( "error", Double.toString( error ) );
+        new SkyMatch2().createExecutable( env12 ).execute();
+        StarTable m12 = env12.getOutputTable( "omode" );
+
+        MapEnvironment env21 = new MapEnvironment( env );
+        env21.setValue( "in2", table1 );
+        env21.setValue( "in1", table2 );
+        env21.setValue( "ra2", ra1 );
+        env21.setValue( "dec2", dec1 );
+        env21.setValue( "ra1", ra2 );
+        env21.setValue( "dec1", dec2 );
+        env21.setValue( "error", Double.toString( error ) );
+        new SkyMatch2().createExecutable( env21 ).execute();
+        StarTable m21 = env21.getOutputTable( "omode" );
+
+        assertTrue( m21 != null );
+        assertEquals( m12.getRowCount(), m21.getRowCount() );
+        assertEquals( m12.getColumnCount(), m21.getColumnCount() );
+        Tables.checkTable( m12 );
+        Tables.checkTable( m21 );
+        return m12;
+    }
+
     private int countMatches( double tol ) throws Exception {
         MapEnvironment env = new MapEnvironment()
            .setValue( "params", Double.toString( tol ) )
            .setValue( "matcher", "sky" );
-        StarTable result = match2( env, t1, "ra1 dec1", t2, "ra2 dec2" );
+        StarTable tResult = tmatch2( env, t1, "ra1 dec1", t2, "ra2 dec2" );
+        StarTable skyResult =
+            skymatch2( new MapEnvironment(), t1, "ra1", "dec1",
+                                             t2, "ra2", "dec2", tol );
+        assertSameData( tResult, skyResult );
 
-        RowSequence rseq = result.getRowSequence();
+        RowSequence rseq = tResult.getRowSequence();
 
-        assertEquals( "ID_1", result.getColumnInfo( 0 ).getName() );
-        assertEquals( "ID_2", result.getColumnInfo( 4 ).getName() );
+        assertEquals( "ID_1", tResult.getColumnInfo( 0 ).getName() );
+        assertEquals( "ID_2", tResult.getColumnInfo( 4 ).getName() );
         boolean[] got = new boolean[ NROW ];
         while ( rseq.next() ) {
             Object[] row = rseq.getRow();
@@ -130,10 +227,10 @@ public class SkyMatchTest extends TableTestCase {
         assertTrue( ! rseq.next() );
         rseq.close();
 
-        return (int) result.getRowCount();
+        return (int) tResult.getRowCount();
     }
 
-    public static StarTable getTestTable() {
+    public static StarTable createTestTable() {
         DefaultValueInfo idInfo =
             new DefaultValueInfo( "ID", Integer.class );
 
@@ -156,7 +253,6 @@ public class SkyMatchTest extends TableTestCase {
         dec2Info.setUnitString( "degrees" );
         errInfo.setUnitString( "arcsec" );
 
-        
         final ColumnData idcol = new ColumnData( idInfo ) {
             public Object readValue( long irow ) {
                 return new Integer( (int) irow );
@@ -212,7 +308,7 @@ public class SkyMatchTest extends TableTestCase {
     }
 
     public static void main( String[] args ) throws IOException {
-        new StarTableOutput().writeStarTable( getTestTable(), "-",
+        new StarTableOutput().writeStarTable( createTestTable(), "-",
                                               "votable-binary" );
     }
     
