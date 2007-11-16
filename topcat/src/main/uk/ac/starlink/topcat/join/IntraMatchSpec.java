97a9fe745daf466ef3853979afc497db27b3a11b
Mark Taylor <m.b.taylor@bristol.ac.uk>
2007-11-16 13:21:27 +0000

    Refactored so that IntraMatchSelector uses an external Match1TypeSelector object to determine the kind of output table to generate from an internal match.  This uses the new uk.ac.starlink.table.join.Match1Type class, which is now used in common by the equivalent STILTS functionality.

--- topcat/src/main/uk/ac/starlink/topcat/join/IntraMatchSpec.java
+++ topcat/src/main/uk/ac/starlink/topcat/join/IntraMatchSpec.java
@@ -1,39 +1,17 @@
 package uk.ac.starlink.topcat.join;
 
 import java.awt.Component;
-import java.awt.event.ActionEvent;
 import java.io.IOException;
-import java.util.BitSet;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
 import java.util.logging.Logger;
-import javax.swing.AbstractAction;
-import javax.swing.Action;
 import javax.swing.Box;
-import javax.swing.ButtonGroup;
-import javax.swing.JComponent;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.JRadioButton;
-import javax.swing.JSpinner;
-import javax.swing.SpinnerNumberModel;
-import uk.ac.starlink.table.JoinFixAction;
-import uk.ac.starlink.table.JoinStarTable;
-import uk.ac.starlink.table.RowPermutedStarTable;
 import uk.ac.starlink.table.StarTable;
-import uk.ac.starlink.table.Tables;
 import uk.ac.starlink.table.join.LinkSet;
 import uk.ac.starlink.table.join.MatchEngine;
-import uk.ac.starlink.table.join.MatchStarTables;
 import uk.ac.starlink.table.join.ProgressIndicator;
-import uk.ac.starlink.table.join.RowLink;
 import uk.ac.starlink.table.join.RowMatcher;
-import uk.ac.starlink.table.join.RowRef;
 import uk.ac.starlink.topcat.AuxWindow;
-import uk.ac.starlink.topcat.BitsRowSubset;
 import uk.ac.starlink.topcat.ControlWindow;
-import uk.ac.starlink.topcat.RowSubset;
 import uk.ac.starlink.topcat.TopcatModel;
 
 /**
@@ -44,27 +22,14 @@ import uk.ac.starlink.topcat.TopcatModel;
  */
 public class IntraMatchSpec extends MatchSpec {
 
-    private final TupleSelector tupleSelector;
-    private final JSpinner widthSelector;
-    private final MatchEngine engine;
-    private StarTable result;
-    private int matchCount;
+    private final TupleSelector tupleSelector_;
+    private final Match1TypeSelector type1Selector_;
+    private final MatchEngine engine_;
+    private StarTable result_;
+    private int matchCount_;
 
     private final static Logger logger =
         Logger.getLogger( "uk.ac.starlink.topcat.join" );
-
-    private final static String IDENTIFY = 
-        "Mark Groups of Rows";
-    private final static String ELIMINATE_0 =
-        "Eliminate All Grouped Rows";
-    private final static String ELIMINATE_1 =
-        "Eliminate All But First of Each Group";
-    private final static String WIDE =
-        "New Table With Groups of Size ";
-    private final static String[] OPTIONS = new String[] {
-        IDENTIFY, ELIMINATE_0, ELIMINATE_1, WIDE,
-    };
-    private String option;
 
     /**
      * Constructs a new IntraMatchSpec.
@@ -72,176 +37,65 @@ public class IntraMatchSpec extends MatchSpec {
      * @param  engine the match engine defining the match type
      */
     public IntraMatchSpec( MatchEngine engine ) {
-        this.engine = engine;
+        engine_ = engine;
         Box main = Box.createVerticalBox();
         add( main );
 
         /* Set up a table/column selector panel for the sole table. */
-        tupleSelector = new TupleSelector( engine );
-        tupleSelector.setBorder( AuxWindow.makeTitledBorder( "Table" ) );
-        main.add( tupleSelector );
-
-        /* Set up a selector for group width. */
-        SpinnerNumberModel widthModel = new SpinnerNumberModel();
-        widthModel.setValue( new Integer( 2 ) );
-        widthModel.setMinimum( new Integer( 2 ) );
-        widthSelector = new JSpinner( widthModel );
+        tupleSelector_ = new TupleSelector( engine_ );
+        tupleSelector_.setBorder( AuxWindow.makeTitledBorder( "Table" ) );
+        main.add( tupleSelector_ );
 
         /* Set up action options. */
-        JComponent actBox = Box.createVerticalBox();
-        ButtonGroup buttGrp = new ButtonGroup();
-        for ( int i = 0; i < OPTIONS.length; i++ ) {
-            final String opt = OPTIONS[ i ];
-            Action buttAct = new AbstractAction() {
-                public void actionPerformed( ActionEvent evt ) {
-                    option = opt;
-                    widthSelector.setEnabled( opt == WIDE );
-                }
-            };
-            JRadioButton butt = new JRadioButton( buttAct );
-            buttGrp.add( butt );
-            Box line = Box.createHorizontalBox();
-            line.add( butt );
-            line.add( new JLabel( " " + opt ) );
-            if ( opt.equals( WIDE ) ) {
-                line.add( widthSelector );
-            }
-            line.add( Box.createHorizontalGlue() );
-            actBox.add( line );
-            if ( i == 0 ) {
-                butt.doClick();
-            }
-        }
-        actBox.setBorder( AuxWindow.makeTitledBorder( "Action" ) );
-        main.add( actBox );
+        type1Selector_ = new Match1TypeSelector();
+        type1Selector_.setBorder( AuxWindow.makeTitledBorder( "Action" ) );
+        main.add( type1Selector_ );
     }
 
     public void checkArguments() {
-        tupleSelector.getEffectiveTable();
-    }
-
-    /**
-     * Returns the currently selected option.  This defines what the user
-     * would like done with the results of the completed match.
-     *
-     * @return  match option, one of IDENTIFY, ELIMINATE_0, ELIMINATE_1, WIDE
-     */
-    private String getOption() {
-        return option;
-    }
-
-    /**
-     * Returns the width of the output table as a multiple of the width
-     * of the input table.  This only makes sense for option==WIDE.
-     *
-     * @return  output table width
-     */
-    private int getTableWideness() {
-        return ((Number) widthSelector.getValue()).intValue();
+        tupleSelector_.getEffectiveTable();
     }
 
     public void calculate( ProgressIndicator indicator )
             throws IOException, InterruptedException {
-        result = null;
+        result_ = null;
 
         /* Interrogate components for tables to operate on. */
-        StarTable effTable = tupleSelector.getEffectiveTable();
-        StarTable appTable = tupleSelector.getTable().getApparentStarTable();
+        StarTable effTable = tupleSelector_.getEffectiveTable();
+        StarTable appTable = tupleSelector_.getTable().getApparentStarTable();
 
         /* Do the matching. */
         RowMatcher matcher = 
-            new RowMatcher( engine, new StarTable[] { effTable } );
+            new RowMatcher( engine_, new StarTable[] { effTable } );
         matcher.setIndicator( indicator );
         LinkSet matches = matcher.findInternalMatches( false );
         if ( ! matches.sort() ) {
             logger.warning( "Can't sort matches - matched table rows may be "
                           + "in an unhelpful order" );
         }
-        matchCount = matches.size();
+        matchCount_ = matches.size();
 
         /* Construct a result table. */
-        if ( matchCount == 0 ) {
-            result = null;
-        }
-        else {
-            result = makeResultTable( appTable, matches, option );
-        }
-    }
-
-    /**
-     * Constructs a new StarTable from the results of a match operation
-     * which has been performed.
-     *
-     * @param  inTable  the input (apparent) table
-     * @param  matches  set of RowLinks describing matches found
-     * @param  option   one of the matching options (IDENTIFY, ELIMINATE_0,
-     *                  ELIMINATE_1, WIDE)
-     * @return  new StarTable formed as a result of this match operation
-     */
-    private StarTable makeResultTable( StarTable inTable, LinkSet matches,
-                                       String option ) {
-        if ( option.equals( IDENTIFY ) ) {
-            long nrow = inTable.getRowCount();
-            StarTable grpTable = MatchStarTables
-                                .makeInternalMatchTable( 0, matches, nrow );
-            JoinFixAction[] fixActs = new JoinFixAction[] {
-                JoinFixAction.makeRenameDuplicatesAction( "_old" ),
-                JoinFixAction.NO_ACTION,
-            };
-            return new JoinStarTable( new StarTable[] { inTable, grpTable },
-                                      fixActs );
-        }
-        else if ( option.equals( ELIMINATE_0 ) ||
-                  option.equals( ELIMINATE_1 ) ) {
-            int startFrom = option.equals( ELIMINATE_0 ) ? 0 : 1;
-            BitSet bits = new BitSet();
-            bits.set( 0, checkedLongToInt( inTable.getRowCount() ) );
-            for ( Iterator it = matches.iterator(); it.hasNext(); ) {
-                RowLink link = (RowLink) it.next();
-                int nref = link.size();
-                assert nref > 1;
-                for ( int i = startFrom; i < nref; i++ ) {
-                    RowRef ref = link.getRef( i );
-                    assert ref.getTableIndex() == 0;
-                    bits.clear( checkedLongToInt( ref.getRowIndex() ) );
-                }
-            }
-            int nbit = bits.cardinality();
-            long[] rowMap = new long[ nbit ];
-            int i = 0;
-            for ( int ipos = bits.nextSetBit( 0 ); ipos >= 0;
-                  ipos = bits.nextSetBit( ipos + 1 ) ) {
-                rowMap[ i++ ] = (long) ipos;
-            }
-            assert i == nbit;
-            return new RowPermutedStarTable( inTable, rowMap );
-        }
-        else if ( option.equals( WIDE ) ) {
-            int width = getTableWideness();
-            return MatchStarTables
-                  .makeParallelMatchTable( inTable, 0, matches,
-                                           width, width, width,
-                                           getDefaultFixActions( width ) );
-        }
-        else {
-            throw new AssertionError();
-        }
+        result_ = matchCount_ == 0
+                ? null
+                : type1Selector_.getType1()
+                                .createMatchTable( appTable, matches );
     }
 
     public void matchSuccess( Component parent ) {
         Object msg;
         String title = "Match Successful";
         int msgType = JOptionPane.INFORMATION_MESSAGE;
-        if ( result == null || result.getRowCount() == 0L ) {
+        if ( result_ == null || result_.getRowCount() == 0L ) {
             msg = "No internal matches were found";
             title = "Match Failed";
             msgType = JOptionPane.WARNING_MESSAGE;
         }
         else {
             TopcatModel tcModel = ControlWindow.getInstance()
-                                 .addTable( result, "matched", true );
+                                 .addTable( result_, "matched", true );
             msg = new String[] { 
-                matchCount + " match groups located",
+                matchCount_ + " match groups located",
                 "New table created by match: " + tcModel,
             };
             title = "Match Successful";
@@ -250,9 +104,5 @@ public class IntraMatchSpec extends MatchSpec {
 
         /* Alert the user that the matching is done. */
         JOptionPane.showMessageDialog( parent, msg, title, msgType );
-    }
-
-    private static int checkedLongToInt( long lval ) {
-        return Tables.checkedLongToInt( lval );
     }
 }
---
+++ topcat/src/main/uk/ac/starlink/topcat/join/Match1TypeSelector.java
@@ -0,0 +1,184 @@
+package uk.ac.starlink.topcat.join;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+import uk.ac.starlink.table.join.Match1Type;
+
+/**
+ * Component which allows the user to select a 
+ * {@link uk.ac.starlink.table.join.Match1Type} from a list of options.
+ *
+ * @author   Mark Taylor
+ * @since    16 Nov 2007
+ */
+public class Match1TypeSelector extends JPanel {
+
+    private TypeOption option_;
+
+    private static final TypeOption IDENTIFY =
+        new FixedTypeOption( "Mark Groups of Rows",
+                             Match1Type.createIdentifyType() );
+    private static final TypeOption ELIMINATE_0 =
+        new FixedTypeOption( "Eliminate All Grouped Rows",
+                             Match1Type.createEliminateMatchesType( 0 ) );
+    private static final TypeOption ELIMINATE_1 =
+        new FixedTypeOption( "Eliminate All But First of Each Group",
+                             Match1Type.createEliminateMatchesType( 1 ) );
+    private static final TypeOption WIDE =
+        new WideTypeOption( "New Table With Groups of Size " );
+
+    /** Options offered by this class. */
+    private static final TypeOption[] OPTIONS = new TypeOption[] {
+        IDENTIFY, ELIMINATE_0, ELIMINATE_1, WIDE,
+    };
+
+    /**
+     * Constructor.
+     */
+    public Match1TypeSelector() {
+        setLayout( new BoxLayout( this, BoxLayout.Y_AXIS ) );
+
+        /* Lay out one button for each option. */
+        ButtonGroup buttGrp = new ButtonGroup();
+        for ( int i = 0; i < OPTIONS.length; i++ ) {
+            final TypeOption opt = OPTIONS[ i ];
+            Action buttAct = new AbstractAction() {
+                public void actionPerformed( ActionEvent evt ) {
+                    option_ = opt;
+                }
+            };
+            JRadioButton butt = new JRadioButton( buttAct );
+            buttGrp.add( butt );
+            Box line = Box.createHorizontalBox();
+            line.add( butt );
+            line.add( new JLabel( " " + opt.getDescription() ) );
+            Component[] extras = opt.getExtras();
+            for ( int j = 0; j < extras.length; j++ ) {
+                line.add( extras[ j ] );
+            }
+            line.add( Box.createHorizontalGlue() );
+            add( line );
+            if ( i == 0 ) {
+                butt.doClick();
+            }
+        }
+    }
+
+    /**
+     * Returns the match option which the user has selected.
+     * Will not be null.
+     *
+     * @return  internal match type
+     */
+    public Match1Type getType1() {
+        return option_.getType1();
+    }
+
+    /**
+     * Abstract helper class defining one of the match type options.
+     */
+    private static abstract class TypeOption {
+
+        private final String description_;
+
+        /**
+         * Constructor.
+         *
+         * @param  description  option description, used as button annotation
+         */
+        TypeOption( String description ) {
+            description_ = description;
+        }
+
+        /**
+         * Returns the button option description, suitable for use as
+         * button annotation.
+         *
+         * @return  description
+         */
+        public String getDescription() {
+            return description_;
+        }
+
+        /**
+         * Returns any additional components to display on the line containing
+         * the button selecting this option.
+         *
+         * @return  array, possibly empty, of additional UI components
+         */
+        public abstract Component[] getExtras();
+
+        /**
+         * Returns the internal match type selected by this option.
+         *
+         * @return  match type
+         */
+        public abstract Match1Type getType1();
+    }
+
+    /**
+     * TypeOption implementation for a fixed match type.
+     */
+    private static class FixedTypeOption extends TypeOption {
+        private final Match1Type type1_;
+
+        /**
+         * Constructor.
+         *
+         * @param   option description
+         * @param   match type
+         */
+        FixedTypeOption( String description, Match1Type type1 ) {
+            super( description );
+            type1_ = type1;
+        }
+
+        public Match1Type getType1() {
+            return type1_;
+        }
+
+        public Component[] getExtras() {
+            return new Component[ 0 ];
+        }
+    }
+
+    /**
+     * Type option implementation which generates a wide table containing
+     * one internal N-way matches per row.
+     */
+    private static class WideTypeOption extends TypeOption {
+        private final JSpinner widthSelector_;
+
+        /**
+         * Constructor.
+         *
+         * @param   description  description
+         */
+        WideTypeOption( String description ) {
+            super( description );
+            SpinnerNumberModel widthModel = new SpinnerNumberModel();
+            widthModel.setValue( new Integer( 2 ) );
+            widthModel.setMinimum( new Integer( 2 ) );
+            widthSelector_ = new JSpinner( widthModel );
+        }
+
+        public Match1Type getType1() {
+            int wideness = ((Number) widthSelector_.getValue()).intValue();
+            return Match1Type.createWideType( wideness );
+        }
+
+        public Component[] getExtras() {
+            return new Component[] { widthSelector_, };
+        }
+    }
+}
