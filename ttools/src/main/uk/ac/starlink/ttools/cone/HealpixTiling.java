c036ec4963c3b4f6891b9d940c3859c11bec1cad
Mark Taylor <m.b.taylor@bristol.ac.uk>
2007-12-21 14:49:09 +0000

    Change constructor parameter to log2nside. Also introduce temporary (?) workaround for pixtools library failing.

--- ttools/src/main/uk/ac/starlink/ttools/cone/HealpixTiling.java
+++ ttools/src/main/uk/ac/starlink/ttools/cone/HealpixTiling.java
@@ -20,11 +20,15 @@ public class HealpixTiling implements SkyTiling {
     /**
      * Constructor.
      *
-     * @param  nside  HEALPix Nside parameter
+     * @param  log2nside  log to base 2 of HEALPix Nside parameter
      * @param  nest  true for nesting scheme, false for ring scheme
      */
-    public HealpixTiling( long nside, boolean nest ) {
-        nside_ = nside;
+    public HealpixTiling( int log2nside, boolean nest ) {
+        if ( log2nside > 63 ) {
+            throw new IllegalArgumentException( "log2nside " + log2nside
+                                              + " too large" );
+        }
+        nside_ = 1L << log2nside;
         nest_ = nest;
         pixTools_ = new PixTools();
     }
@@ -36,10 +40,36 @@ public class HealpixTiling implements SkyTiling {
     }
 
     public long[] getTileRange( double ra, double dec, double radius ) {
-        List tileList =
-            pixTools_.query_disc( nside_, toVector( ra, dec ),
-                                  Math.toRadians( radius ), 
-                                  nest_ ? 1 : 0, 1 );
+        List tileList;
+        if ( nest_ ) {
+
+            /* The following code should do the trick, but tends to generate 
+             * an exception:
+             *    java.lang.IllegalArgumentException: 
+             *        xy2pix_nest: iy out of range [0, nside-1]
+             *    at gov.fnal.eag.healpix.PixTools.xy2pix_nest(Unknown Source)
+             * in the current version of PixTools. */
+            // tileList = pixTools_.query_disc( nside_, toVector( ra, dec ),
+            //                                  Math.toRadians( radius ), 
+            //                                  1, 1 );
+
+            /* So work around it by calculating in the ring scheme and
+             * translating pixel by pixel to nest. */
+            tileList = pixTools_.query_disc( nside_, toVector( ra, dec ),
+                                             Math.toRadians( radius ), 
+                                             0, 1 );
+            int ntile = tileList.size();
+            for ( int itile = 0; itile < ntile; itile++ ) {
+                long ringIndex = ((Number) tileList.get( itile )).longValue();
+                long nestIndex = pixTools_.ring2nest( nside_, ringIndex );
+                tileList.set( itile, new Long( nestIndex ) );
+            }
+        }
+        else {
+            tileList = pixTools_.query_disc( nside_, toVector( ra, dec ),
+                                             Math.toRadians( radius ),
+                                             0, 1 );
+        }
         long lo = Long.MAX_VALUE;
         long hi = Long.MIN_VALUE;
         for ( Iterator it = tileList.iterator(); it.hasNext(); ) {
