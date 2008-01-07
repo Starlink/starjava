4bc5b0f08ac14cce1643f8c3891c8ce0a03d784c
Mark Taylor <m.b.taylor@bristol.ac.uk>
2008-01-07 16:47:49 +0000

    Changed name of log2nside parameter to k, following usage in HEALPix paper (Gorski et al 2005). Some additional rejigging, including adding a hook for improved efficiency (throwing out query_disc tiles which are not actually overlapped).

--- ttools/src/main/uk/ac/starlink/ttools/cone/HealpixTiling.java
+++ ttools/src/main/uk/ac/starlink/ttools/cone/HealpixTiling.java
@@ -20,15 +20,14 @@ public class HealpixTiling implements SkyTiling {
     /**
      * Constructor.
      *
-     * @param  log2nside  log to base 2 of HEALPix Nside parameter
+     * @param  k  log to base 2 of HEALPix Nside parameter
      * @param  nest  true for nesting scheme, false for ring scheme
      */
-    public HealpixTiling( int log2nside, boolean nest ) {
-        if ( log2nside > 63 ) {
-            throw new IllegalArgumentException( "log2nside " + log2nside
-                                              + " too large" );
+    public HealpixTiling( int k, boolean nest ) {
+        if ( k > 63 ) {
+            throw new IllegalArgumentException( "k " + k + " too large" );
         }
-        nside_ = 1L << log2nside;
+        nside_ = 1L << k;
         nest_ = nest;
         pixTools_ = new PixTools();
     }
@@ -40,24 +39,16 @@ public class HealpixTiling implements SkyTiling {
     }
 
     public long[] getTileRange( double ra, double dec, double radius ) {
-        List tileList;
+        double radiusRad = Math.toRadians( radius );
+
+        /* Calculate tile indices in the ring scheme, then convert if
+         * necessary to the nest scheme.  The query_disc method allows
+         * to calculate directly in the nest scheme, but early versions
+         * of PixTools gave the wrong answer for this and later versions
+         * are much slower. */
+        List tileList = pixTools_.query_disc( nside_, toVector( ra, dec ),
+                                              radiusRad, 0, 1 );
         if ( nest_ ) {
-
-            /* The following code should do the trick, but tends to generate 
-             * an exception:
-             *    java.lang.IllegalArgumentException: 
-             *        xy2pix_nest: iy out of range [0, nside-1]
-             *    at gov.fnal.eag.healpix.PixTools.xy2pix_nest(Unknown Source)
-             * in the current version of PixTools. */
-            // tileList = pixTools_.query_disc( nside_, toVector( ra, dec ),
-            //                                  Math.toRadians( radius ), 
-            //                                  1, 1 );
-
-            /* So work around it by calculating in the ring scheme and
-             * translating pixel by pixel to nest. */
-            tileList = pixTools_.query_disc( nside_, toVector( ra, dec ),
-                                             Math.toRadians( radius ), 
-                                             0, 1 );
             int ntile = tileList.size();
             for ( int itile = 0; itile < ntile; itile++ ) {
                 long ringIndex = ((Number) tileList.get( itile )).longValue();
@@ -65,17 +56,21 @@ public class HealpixTiling implements SkyTiling {
                 tileList.set( itile, new Long( nestIndex ) );
             }
         }
-        else {
-            tileList = pixTools_.query_disc( nside_, toVector( ra, dec ),
-                                             Math.toRadians( radius ),
-                                             0, 1 );
-        }
+
+        /* query_disc in inclusive mode returns a list which may include
+         * more tiles than are actually overlapped by the given cone.
+         * This is especially true if radius is much smaller than the
+         * tile size.  The PixTools algorithm is basically to find all the
+         * tiles whose centres lie within (radius+tileSize/2) - safe but
+         * not efficient. */
         long lo = Long.MAX_VALUE;
         long hi = Long.MIN_VALUE;
         for ( Iterator it = tileList.iterator(); it.hasNext(); ) {
             long tile = ((Number) it.next()).longValue();
-            lo = Math.min( lo, tile );
-            hi = Math.max( hi, tile );
+            if ( discIntersectsTile( ra, dec, radiusRad, tile ) ) {
+                lo = Math.min( lo, tile );
+                hi = Math.max( hi, tile );
+            }
         }
         return lo <= hi ? new long[] { lo, hi }
                         : null;
@@ -91,5 +86,24 @@ public class HealpixTiling implements SkyTiling {
     private Vector3d toVector( double ra, double dec ) {
         double theta = Math.PI * 0.5 - Math.toRadians( dec );
         return pixTools_.Ang2Vec( theta, Math.toRadians( ra ) );
+    }
+
+    /**
+     * Returns true if the given disc is fully or partially contained in a
+     * given tile.  False positives are permitted, but not false negatives.
+     * The supplied cone can be assumed to be "close to" the supplied tile.
+     *
+     * @param   ra   right ascension of disc centre
+     * @param   dec  declination of disc centre
+     * @param   radius   disc radius in radians
+     * @param   tile  tile index
+     * @return   true if the disc may be fully or partially within tile;
+     *           false if it is definitely outside
+     */
+    private boolean discIntersectsTile( double ra, double dec, double radiusRad,
+                                        long tile ) {
+
+        /* Correct but not optimal return - all cones may be inside tile. */
+        return true;
     }
 }
