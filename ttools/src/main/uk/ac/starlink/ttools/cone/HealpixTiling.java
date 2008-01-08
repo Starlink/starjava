133d278faa777a964d546bf46a6810d45c31ac28
Mark Taylor <m.b.taylor@bristol.ac.uk>
2008-01-08 15:55:05 +0000

    Add protection against attempting to calculate too-large tile ranges

--- ttools/src/main/uk/ac/starlink/ttools/cone/HealpixTiling.java
+++ ttools/src/main/uk/ac/starlink/ttools/cone/HealpixTiling.java
@@ -4,6 +4,7 @@ import gov.fnal.eag.healpix.PixTools;
 import java.util.Iterator;
 import java.util.List;
 import javax.vecmath.Vector3d;
+import uk.ac.starlink.ttools.func.Coords;
 
 /**
  * Tiling implementation based on the HEALPix scheme.
@@ -16,6 +17,7 @@ public class HealpixTiling implements SkyTiling {
     private final long nside_;
     private final boolean nest_;
     private final PixTools pixTools_;
+    private final double resolution_;
 
     /**
      * Constructor.
@@ -30,6 +32,7 @@ public class HealpixTiling implements SkyTiling {
         nside_ = 1L << k;
         nest_ = nest;
         pixTools_ = new PixTools();
+        resolution_ = pixTools_.PixRes( nside_ ) / 3600;
     }
 
     public long getPositionTile( double ra, double dec ) {
@@ -39,7 +42,12 @@ public class HealpixTiling implements SkyTiling {
     }
 
     public long[] getTileRange( double ra, double dec, double radius ) {
-        double radiusRad = Math.toRadians( radius );
+
+        /* If the radius is too big, don't attempt the calculation -
+         * range determination might be slow. */
+        if ( radius > resolution_ * 50 ) {
+            return null;
+        }
 
         /* Calculate tile indices in the ring scheme, then convert if
          * necessary to the nest scheme.  The query_disc method allows
@@ -47,7 +55,7 @@ public class HealpixTiling implements SkyTiling {
          * of PixTools gave the wrong answer for this and later versions
          * are much slower. */
         List tileList = pixTools_.query_disc( nside_, toVector( ra, dec ),
-                                              radiusRad, 0, 1 );
+                                              Math.toRadians( radius ), 0, 1 );
         if ( nest_ ) {
             int ntile = tileList.size();
             for ( int itile = 0; itile < ntile; itile++ ) {
@@ -67,7 +75,7 @@ public class HealpixTiling implements SkyTiling {
         long hi = Long.MIN_VALUE;
         for ( Iterator it = tileList.iterator(); it.hasNext(); ) {
             long tile = ((Number) it.next()).longValue();
-            if ( discIntersectsTile( ra, dec, radiusRad, tile ) ) {
+            if ( discIntersectsTile( ra, dec, radius, tile ) ) {
                 lo = Math.min( lo, tile );
                 hi = Math.max( hi, tile );
             }
@@ -93,17 +101,57 @@ public class HealpixTiling implements SkyTiling {
      * given tile.  False positives are permitted, but not false negatives.
      * The supplied cone can be assumed to be "close to" the supplied tile.
      *
-     * @param   ra   right ascension of disc centre
-     * @param   dec  declination of disc centre
-     * @param   radius   disc radius in radians
+     * @param   ra   right ascension of disc centre in degrees
+     * @param   dec  declination of disc centre in degrees
+     * @param   radius   disc radius in degrees
      * @param   tile  tile index
      * @return   true if the disc may be fully or partially within tile;
      *           false if it is definitely outside
      */
-    private boolean discIntersectsTile( double ra, double dec, double radiusRad,
+    private boolean discIntersectsTile( double ra, double dec, double radius,
                                         long tile ) {
 
-        /* Correct but not optimal return - all cones may be inside tile. */
+    // Experimental code - it does cut out a few pixels, but not many,
+    // and I'm not certain that it's correct.  The spherical geometry is
+    // hard (pixel boundaries are not even geodesics).  Comment this out
+    // for now, maybe return to it (or write from scratch) if this looks
+    // like a bottleneck which is really worth fixing.
+
+    //  /* If the disc is large compared to the resulution of this tiling,
+    //   * assume that the given tile is probably correct. */
+    //  if ( radius > resolution_ * 2 ) {
+    //      return true;
+    //  }
+    //
+    //  /* Otherwise, work out the maximum radius of the tile if it were a
+    //   * disc rather than a sort of square by looking at its vertices.
+    //   * This is inferior to doing the spherical geometry properly,
+    //   * but easier (at least, I don't know how to do it properly). */
+    //  double[] centreAng = nest_ ? pixTools_.pix2ang_nest( nside_, tile )
+    //                             : pixTools_.pix2ang_ring( nside_, tile );
+    //  double centreRa = centreAng[ 1 ];
+    //  double centreDec = Math.PI * 0.5 - centreAng[ 0 ];
+    //  double tileMaxRadius = 0.0;
+    //  double[][] vertices = nest_ ? pixTools_.pix2vertex_nest( nside_, tile )
+    //                              : pixTools_.pix2vertex_ring( nside_, tile );
+    //  for ( int i = 0; i < 4; i++ ) {
+    //      Vector3d vec = new Vector3d( vertices[ 0 ][ i ],
+    //                                   vertices[ 1 ][ i ],
+    //                                   vertices[ 2 ][ i ] );
+    //      double[] ang = pixTools_.Vect2Ang( vec );
+    //      double vertRa = ang[ 1 ];
+    //      double vertDec = Math.PI * 0.5 - ang[ 0 ];
+    //      double vertRadius =
+    //          Coords.skyDistance( centreRa, centreDec, vertRa, vertDec );
+    //      tileMaxRadius = Math.max( tileMaxRadius, vertRadius );
+    //  }
+    //  if ( Coords.skyDistance( Math.toRadians( ra ), Math.toRadians( dec ),
+    //                           centreRa, centreDec )
+    //       > tileMaxRadius + Math.toRadians( radius ) ) {
+    //      return false;
+    //  }
+
+        /* If no reason to reject so far, accept. */
         return true;
     }
 }
