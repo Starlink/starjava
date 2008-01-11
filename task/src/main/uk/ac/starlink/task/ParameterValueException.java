bdb9285d90b990d4cd74d0325c647c7da1b56cae
Mark Taylor <m.b.taylor@bristol.ac.uk>
2008-01-11 10:46:01 +0000

    Add getParameter method

--- task/src/main/uk/ac/starlink/task/ParameterValueException.java
+++ task/src/main/uk/ac/starlink/task/ParameterValueException.java
@@ -7,17 +7,34 @@ package uk.ac.starlink.task;
  * its first argument.
  */
 public class ParameterValueException extends UsageException {
+
+    private final Parameter param_;
+
     public ParameterValueException( Parameter par ) {
         super( "Bad value for parameter " + par );
+        param_ = par;
     }
     public ParameterValueException( Parameter par, String message ) {
         super( "Bad value for parameter " + par + ": " + message );
+        param_ = par;
     }
     public ParameterValueException( Parameter par, String message, 
                                     Throwable cause ) {
         super( "Bad value for parameter " + par + ": " + message, cause );
+        param_ = par;
     }
     public ParameterValueException( Parameter par, Throwable cause ) {
-        super( "Bad value for parameter " + par, cause );
+        super( "Bad value for parameter " + par + " - " + describe( cause ),
+               cause );
+        param_ = par;
+    }
+
+    /**
+     * Returns the parameter with which this exception is associated.
+     *
+     * @return  parameter
+     */
+    public Parameter getParameter() {
+        return param_;
     }
 }
