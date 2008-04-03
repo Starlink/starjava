228af589b99587e2f3fcd25fc2839cd526838705
Mark Taylor <m.b.taylor@bristol.ac.uk>
2008-04-03 12:27:57 +0000

    Remove redundant import

--- topcat/src/main/uk/ac/starlink/topcat/plot/DensityWindow.java
+++ topcat/src/main/uk/ac/starlink/topcat/plot/DensityWindow.java
@@ -4,7 +4,6 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.Shape;
