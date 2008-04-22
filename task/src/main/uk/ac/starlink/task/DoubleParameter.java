f60d647b0a03edaecee97419045c792eed8fd5fb
Mark Taylor <m.b.taylor@bristol.ac.uk>
2008-04-22 13:53:03 +0000

    Provide for possible use of null (=NaN) values

--- task/src/main/uk/ac/starlink/task/DoubleParameter.java
+++ task/src/main/uk/ac/starlink/task/DoubleParameter.java
@@ -5,7 +5,7 @@ package uk.ac.starlink.task;
  */
 public class DoubleParameter extends Parameter {
 
-    private double doubleval;
+    private double doubleval_;
 
     public DoubleParameter( String name ) {
         super( name );
@@ -14,23 +14,32 @@ public class DoubleParameter extends Parameter {
 
     public void setValueFromString( Environment env, String stringval )
             throws TaskException {
-        try {
-            doubleval = Double.parseDouble( stringval );
+        double dval;
+        if ( isNullPermitted() &&
+             ( stringval == null || stringval.trim().length() == 0 ) ) {
+            dval = Double.NaN;
         }
-        catch ( NumberFormatException e ) {
-            throw new ParameterValueException( this, e.getMessage() );
+        else {
+            try {
+                dval = Double.parseDouble( stringval );
+            }
+            catch ( NumberFormatException e ) {
+                throw new ParameterValueException( this, e.getMessage() );
+            }
         }
+        doubleval_ = dval;
         super.setValueFromString( env, stringval );
     }
 
     /**
      * Returns the value of this parameter as a <tt>double</tt>.
+     * A null string value, if permitted, will give a NaN result.
      *
      * @param   env  execution environment
      * @return   double value
      */
     public double doubleValue( Environment env ) throws TaskException {
         checkGotValue( env );
-        return doubleval;
+        return doubleval_;
     }
 }
