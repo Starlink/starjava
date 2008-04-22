19bc43b071f4371ecd4e7996fb7d31756e5f4242
Mark Taylor <m.b.taylor@bristol.ac.uk>
2008-04-22 14:04:12 +0000

    Plot export interface

---
+++ topcat/src/main/uk/ac/starlink/tptask/Painter.java
@@ -0,0 +1,19 @@
+package uk.ac.starlink.tptask;
+
+import javax.swing.JComponent;
+
+/**
+ * Interface for plot output.
+ *
+ * @author   Mark Taylor
+ * @since    22 Apr 2008
+ */
+public interface Painter {
+
+    /**
+     * Export the graphics displayed by a Swing component in some way.
+     *
+     * @param   plot  component to export
+     */
+    void paintPlot( JComponent plot );
+}
