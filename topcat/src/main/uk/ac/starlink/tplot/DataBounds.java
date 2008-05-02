129a9b3b6536c69b4431adef2d413c93b204968a
Mark Taylor <m.b.taylor@bristol.ac.uk>
2008-05-02 16:22:15 +0000

    Data bounds object

---
+++ topcat/src/main/uk/ac/starlink/tplot/DataBounds.java
@@ -0,0 +1,42 @@
+package uk.ac.starlink.tplot;
+
+/**
+ * Encapsulates what is known about the ranges along each axis of a data set.
+ *
+ * @author   Mark Taylor
+ * @since    2 May 2008
+ */
+public class DataBounds {
+
+    private final Range[] ranges_;
+    private final int npoint_;
+
+    /**
+     * Constructor.
+     *
+     * @param  ranges  array of data ranges, one for each axis
+     * @param  npoint  number of points in the data set
+     */
+    public DataBounds( Range[] ranges, int npoint ) {
+        ranges_ = (Range[]) ranges.clone();
+        npoint_ = npoint;
+    }
+
+    /**
+     * Returns the array of data ranges, one for each axis.
+     *
+     * @return  data range array
+     */
+    public Range[] getRanges() {
+        return ranges_;
+    }
+
+    /**
+     * Returnst the number of points in the data set.
+     *
+     * @param  point count
+     */
+    public int getPointCount() {
+        return npoint_;
+    }
+}
