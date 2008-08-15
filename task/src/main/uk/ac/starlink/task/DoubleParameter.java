47e99806c94251e3fd163bd94fa3d4eeee8d90a9
Mark Taylor <m.b.taylor@bristol.ac.uk>
2008-08-15 15:02:15 +0000

    Add setMaximum/setMinimum methods

--- task/src/main/uk/ac/starlink/task/DoubleParameter.java
+++ task/src/main/uk/ac/starlink/task/DoubleParameter.java
@@ -2,14 +2,46 @@ package uk.ac.starlink.task;
 
 /**
  * Parameter representing a double precision number.
+ *
+ * @author   Mark Taylor
  */
 public class DoubleParameter extends Parameter {
 
     private double doubleval_;
+    private Double min_;
+    private Double max_;
+    private boolean minInclusive_;
+    private boolean maxInclusive_;
 
+    /**
+     * Constructor.
+     *
+     * @param   name  parameter name
+     */
     public DoubleParameter( String name ) {
         super( name );
         setUsage( "<float-value>" );
+    }
+
+    /**
+     * Sets the minimum acceptable value for this parameter.
+     *
+     * @param   min   minimum value
+     * @param   inclusive  true iff <code>min</code> itself is permitted
+     */
+    public void setMinimum( double min, boolean inclusive ) {
+        min_ = Double.isNaN( min ) ? null : new Double( min );
+        minInclusive_ = inclusive;
+    }
+
+    /**
+     * Sets the maximum acceptable value for this parameter.
+     *
+     * @param   max  maximum value
+     * @param   inclusive   true iff <code>max</code> itself is permitted
+     */
+    public void setMaximum( double max, boolean inclusive ) {
+        max_ = Double.isNaN( max ) ? null : new Double( max );
     }
 
     public void setValueFromString( Environment env, String stringval )
@@ -25,6 +57,36 @@ public class DoubleParameter extends Parameter {
             }
             catch ( NumberFormatException e ) {
                 throw new ParameterValueException( this, e.getMessage() );
+            }
+        }
+        if ( min_ != null ) {
+            double dmin = min_.doubleValue();
+            if ( minInclusive_ ) {
+                if ( dval < dmin ) {
+                    throw new ParameterValueException(
+                            this, dval + " < minimum value " + dmin );
+                }
+            }
+            else {
+                if ( dval <= dmin ) {
+                    throw new ParameterValueException(
+                            this, dval + " <= minimum value " + dmin );
+                }
+            }
+        }
+        if ( max_ != null ) {
+            double dmax = max_.doubleValue();
+            if ( maxInclusive_ ) {
+                if ( dval > dmax ) {
+                    throw new ParameterValueException( 
+                            this, dval + " > maximum value " + dmax );
+                }
+            }
+            else {
+                if ( dval >= dmax ) {
+                    throw new ParameterValueException(
+                            this, dval + " >= maximum value " + dmax );
+                }
             }
         }
         doubleval_ = dval;
