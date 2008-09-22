5bed5009a79c75c62d2eda75df355b5caf3bbe70
Mark Taylor <m.b.taylor@bristol.ac.uk>
2008-09-22 14:21:36 +0000

    Modify action names to match browser help actions

--- topcat/src/main/uk/ac/starlink/topcat/HelpAction.java
+++ topcat/src/main/uk/ac/starlink/topcat/HelpAction.java
@@ -37,7 +37,8 @@ public class HelpAction extends AbstractAction {
     public HelpAction( String helpID, Component parent ) {
         helpID_ = helpID;
         parent_ = parent;
-        putValue( NAME, helpID == null ? "Help" : "Help for window" );
+        putValue( NAME, helpID == null ? "Help for Application"
+                                       : "Help for Window" );
         putValue( SMALL_ICON, helpID == null ? ResourceIcon.MANUAL
                                              : ResourceIcon.HELP );
         putValue( SHORT_DESCRIPTION,
