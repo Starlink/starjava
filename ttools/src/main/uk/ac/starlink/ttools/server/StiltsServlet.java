65bba8b84664c789f5f3c4edf504e36268b595fd
Mark Taylor <m.b.taylor@bristol.ac.uk>
2008-10-07 10:48:04 +0000

    Add much more helpful information if tasks fail etc

--- ttools/src/main/uk/ac/starlink/ttools/server/StiltsServlet.java
+++ ttools/src/main/uk/ac/starlink/ttools/server/StiltsServlet.java
@@ -3,18 +3,25 @@ package uk.ac.starlink.ttools.server;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.Comparator;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import uk.ac.starlink.table.StarTable;
 import uk.ac.starlink.table.StarTableFactory;
 import uk.ac.starlink.table.StarTableOutput;
 import uk.ac.starlink.table.jdbc.JDBCAuthenticator;
 import uk.ac.starlink.task.Executable;
+import uk.ac.starlink.task.InvokeUtils;
+import uk.ac.starlink.task.Parameter;
 import uk.ac.starlink.task.Task;
 import uk.ac.starlink.task.TaskException;
 import uk.ac.starlink.ttools.Stilts;
+import uk.ac.starlink.util.IOUtils;
 import uk.ac.starlink.util.LoadException;
 import uk.ac.starlink.util.ObjectFactory;
 
@@ -82,58 +89,217 @@ public class StiltsServlet extends HttpServlet {
                 task = (Task) taskFactory_.createObject( taskName );
             }
             catch ( LoadException e ) {
-                response.sendError( 500, serialize( e ) );
+                replyError( response, 500, e );
                 return;
             }
-            Executable exec;
-            try {
-                exec = task.createExecutable( env );
+
+            /* If we have a request for help information, write help. */
+            if ( env.isHelp() ) {
+                response.setStatus( 200 );
+                response.setContentType( "text/html" );
+                ServletOutputStream out = response.getOutputStream();
+                out.println( "<html>" );
+                out.println( "<head><title>STILTS " + taskName
+                           + "</title></head>" );
+                out.println( "<body>" );
+                writeTaskHelp( request.getServletPath(), out, taskName, task );
+                out.println( "</body>" );
+                out.println( "</html>" );
+                out.flush();
             }
-            catch ( TaskException e ) {
-                response.sendError( 400, serialize( e ) );
-                return;
-            }
-            catch ( Throwable e ) {
-                e.printStackTrace( System.err );
-                response.sendError( 500, serialize( e ) );
-                return;
-            }
-            try {
-                exec.execute();
-            }
-            catch ( TaskException e ) {
-                if ( ! response.isCommitted() ) {
-                    response.sendError( 400, serialize( e ) );
+
+            /* Otherwise, try to execute the task with the given parameters. */
+            else {
+                Executable exec;
+                try {
+                    exec = task.createExecutable( env );
                 }
-                else {
-                    throw new ServletException( "Error during task execution",
-                                                e );
+                catch ( TaskException e ) {
+                    replyError( response, 400, e );
+                    return;
+                }
+                catch ( Throwable e ) {
+                    e.printStackTrace( System.err );
+                    replyError( response, 500, e );
+                    return;
+                }
+                try {
+                    exec.execute();
+                }
+                catch ( TaskException e ) {
+                    if ( ! response.isCommitted() ) {
+                        replyError( response, 400, e );
+                    }
+                    else {
+                        throw new ServletException(
+                            "Error during task execution", e );
+                    }
                 }
             }
         }
+
+        /* If no task was identified, write some help text. */
         else {
-            response.sendError( 400, "No such task " + taskName );
+            response.setContentType( "text/html" );
+            ServletOutputStream out = response.getOutputStream();
+
+            /* If it looked like a help request, write lots of useful
+             * information. */
+            if ( env.isHelp() ) {
+                response.setStatus( 200 );
+                out.println( "<html>" );
+                out.println( "<head><title>STILTS</title></head>" );
+                out.println( "<body>" );
+                out.println( "<h2>STILTS</h2>" );
+                out.println( "<p>This is STILTS, the STIL Tool Set.<br />" );
+                out.println( "Running in server mode.</p>" );
+                out.println( "<p><b>STILTS version:</b> "
+                           + Stilts.getVersion() + "<br />" );
+                out.println( "<b>STIL version:</b> "
+                           + IOUtils.getResourceContents( StarTable.class,
+                                                          "stil.version" )
+                           + "<br />" );
+                out.println( "<b>JVM:</b> " + InvokeUtils.getJavaVM() );
+                out.println( "</p>" );
+                out.println( "<p><b>Author:</b> "
+                           + "<a href='http://www.star.bris.ac.uk/~mbt/'>"
+                           + "Mark Taylor" + "</a><br />" );
+                out.println( "<b>WWW:</b> "
+                           + "<a href='http://www.starlink.ac.uk/stilts/'>"
+                           + "http://www.starlink.ac.uk/stilts/</a>" );
+                out.println( "</p>" );
+                writeStiltsHelp( request.getServletPath(), out );
+                out.println( "</body>" );
+                out.println( "</html>" );
+            }
+
+            /* Otherwise an error message with some other pointers. */
+            else {
+                response.setStatus( 400 );
+                out.println( "<html>" );
+                out.println( "<head><title>No such task: "
+                           + taskName + "</title></head>" );
+                out.println( "<body>" );
+                out.println( "<h2>No such task: " + taskName + "</h2>" );
+                writeStiltsHelp( request.getServletPath(), out );
+                out.println( "</body>" );
+                out.println( "</html>" );
+            }
+            out.flush();
+            out.close();
         }
     }
 
     /**
-     * Turns a throwable into a string which can be returned to the caller
-     * in a servlet response.
+     * Writes error information to the response.
      *
-     * @param  error  throwable to serialize
-     * @return   string incorporating stacktrace
+     * @param   response  destination
+     * @param   code   3-digit HTTP response code
+     * @param   error  exception to be passed to caller
      */
-    private static String serialize( Throwable error ) {
-        try {
-            ByteArrayOutputStream bufout = new ByteArrayOutputStream();
-            PrintStream pout = new PrintStream( bufout );
+    private void replyError( HttpServletResponse response, int code,
+                             Throwable error )
+            throws IOException, ServletException {
+        if ( response.isCommitted() ) {
+            throw new ServletException( "Error after response commit", error );
+        }
+        else {
+            response.setStatus( code );
+            response.setContentType( "text/plain" );
+            PrintStream pout = new PrintStream( response.getOutputStream() );
             error.printStackTrace( pout );
             pout.flush();
             pout.close();
-            return new String( bufout.toByteArray(), "UTF-8" );
         }
-        catch ( IOException e ) {
-            return error.toString();
+    }
+
+    /**
+     * Writes HTML information about known tasks to an output stream.
+     *
+     * @param  baseUrl  main servlet URL
+     * @param  out  response output stream
+     */
+    private void writeStiltsHelp( String baseUrl, ServletOutputStream out )
+            throws IOException {
+        out.println( "<p>Known tasks:</p>" );
+        out.println( "<ul>" );
+        String[] taskNames = taskFactory_.getNickNames();
+        for ( int i = 0; i < taskNames.length; i++ ) {
+            String taskName = taskNames[ i ];
+            out.print( "<li>" );
+            out.print( "<a href='" + baseUrl + "/" + taskName + "'>"
+                       + taskName + "</a>" );
+            out.print( "</li>" );
+            out.println();
         }
+        out.println( "</ul>" );
+    }
+
+    /**
+     * Writes HTML information about a given task to an output stream.
+     *
+     * @param  baseUrl  main servlet URL
+     * @param  out  response output stream
+     * @param  taskName  task name
+     * @param  task   task object
+     */
+    private void writeTaskHelp( String baseUrl, ServletOutputStream out,
+                                String taskName, Task task )
+            throws IOException {
+        out.println( "<h2>STILTS task <code>" + taskName + "</code></h2>" );
+
+        out.println( "<h3>Purpose</h3>" );
+        out.println( task.getPurpose() );
+
+        out.println( "<h3>Usage</h3>" );
+        out.println( "<p><code>" );
+        out.println( baseUrl + "/" + taskName );
+        out.println( "<blockquote>" );
+        Parameter[] params = task.getParameters();
+        for ( int i = 0; i < params.length; i++ ) {
+            Parameter param = params[ i ];
+            out.print( " <font color='green'>"
+                     + ( i > 0 ? "&amp;" : "?" )
+                     + "</font> " );
+            out.print( "<a href='#" + param.getName() + "'>"
+                     + escape( param.getName() ) + "</a>" + "="
+                     + "<font color='brown'>" + escape( param.getUsage() )
+                     + "</font>" );
+            out.println( "<br />" );
+        }
+        out.println( "</blockquote>" );
+        out.println( "</code></p>" );
+
+        out.println( "<h3>Parameters</h3>" );
+        out.println( "<dl>" );
+        Arrays.sort( params, new Comparator() {
+            public int compare( Object o1, Object o2 ) {
+                Parameter p1 = (Parameter) o1;
+                Parameter p2 = (Parameter) o2;
+                return ((Parameter) o1).getName()
+                      .compareTo( ((Parameter) o2).getName() ); 
+            }
+        } );
+        for ( int i = 0; i < params.length; i++ ) {
+            Parameter param = params[ i ];
+            out.println( "<dt><b><a name='" + param.getName() + "'>"
+                       + escape( param.getName() + " = " + param.getUsage() )
+                       + "</a></b></dt>" );
+            out.println( "<dd>" + param.getDescription() );
+            out.println( "</dd>" );
+        }
+        out.println( "</dl>" );
+    }
+
+    /**
+     * Escapes a plain text string for use in HTML output.
+     *
+     * @param  text  input
+     * @return   escaped text
+     */
+    private static String escape( String text ) {
+        return text.replaceAll( "&", "&amp;" )
+                   .replaceAll( "<", "&lt;" )
+                   .replaceAll( ">", "&gt;" );
     }
 }
