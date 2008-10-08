da7dffc8f84df6ff435163a675a4055ce7bef14b
Mark Taylor <m.b.taylor@bristol.ac.uk>
2008-10-08 15:31:23 +0000

    Add fallback page

--- ttools/src/main/uk/ac/starlink/ttools/task/StiltsServer.java
+++ ttools/src/main/uk/ac/starlink/ttools/task/StiltsServer.java
@@ -3,8 +3,13 @@ package uk.ac.starlink.ttools.task;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.List;
 import org.mortbay.http.HttpContext;
+import org.mortbay.http.HttpRequest;
+import org.mortbay.http.HttpResponse;
 import org.mortbay.http.HttpServer;
+import org.mortbay.http.handler.NotFoundHandler;
 import org.mortbay.jetty.servlet.ServletHandler;
 import org.mortbay.util.InetAddrPort;
 import org.mortbay.util.Resource;
@@ -14,6 +19,8 @@ import uk.ac.starlink.task.IntegerParameter;
 import uk.ac.starlink.task.Parameter;
 import uk.ac.starlink.task.Task;
 import uk.ac.starlink.task.TaskException;
+import uk.ac.starlink.ttools.Stilts;
+import uk.ac.starlink.ttools.server.FormServlet;
 import uk.ac.starlink.ttools.server.ServletEnvironment;
 import uk.ac.starlink.ttools.server.StiltsServlet;
 
@@ -76,9 +83,16 @@ public class StiltsServer implements Task {
                 server.addListener( new InetAddrPort( port ) );
                 HttpContext context = server.getContext( "/" );
                 ServletHandler handler = new ServletHandler();
+                context.addHandler( handler );
+                List baseList = new ArrayList();
                 handler.addServlet( "STILTS", base + "/*",
                                     StiltsServlet.class.getName() );
-                context.addHandler( handler );
+                baseList.add( base + "/" );
+                handler.addServlet( "forms", "/forms/*",
+                                    FormServlet.class.getName() );
+                baseList.add( "/forms/" );
+                String[] bases = (String[]) baseList.toArray( new String[ 0 ] );
+                context.addHandler( new FallbackHandler( bases ) );
                 try {
                     server.start();
                     String url = "http://"
@@ -95,5 +109,54 @@ public class StiltsServer implements Task {
                 }
             }
         };
+    }
+
+    /**
+     * Handler which will be used if the requested URL does not match any
+     * other registered.
+     * It tells the user where to go.
+     */
+    private static class FallbackHandler extends NotFoundHandler {
+        private final String[] paths_;
+
+        /**
+         * Constructor.
+         *
+         * @param  paths  array of relative URLs giving suggested starting
+         *                points for a user
+         */
+        FallbackHandler( String[] paths ) {
+            paths_ = paths;
+        }
+
+        public void handle( String pathInContext, String pathParams,
+                            HttpRequest request, HttpResponse response )
+                throws IOException {
+            response.setContentType( "text/html" );
+            response.setStatus( 404 );
+            response.setReason( "Not Found" );
+            PrintStream out = new PrintStream( response.getOutputStream() );
+            out.println( "<html>" );
+            out.println( "<head><title>404 Not Found</title></head>" );
+            out.println( "<body>" );
+            out.println( "<h2>No such resource</h2>" );
+            out.println( "Try one of these:" );
+            out.println( "<ul>" );
+            for ( int i = 0; i < paths_.length; i++ ) {
+                String path = paths_[ i ];
+                out.println( "<li><a href='" + path + "'>" + path
+                           + "</a></li>" );
+            }
+            out.println( "</ul>" );
+            out.println( "<p><i>"
+                       + "<a href='http://www.starlink.ac.uk/stilts/'>"
+                       + "STILTS</a>"
+                       + " Server " + Stilts.getVersion()
+                       + "</i></p>" );
+            out.println( "<body>" );
+            out.println( "</html>" );
+            out.close();
+            response.commit();
+        }
     }
 }
