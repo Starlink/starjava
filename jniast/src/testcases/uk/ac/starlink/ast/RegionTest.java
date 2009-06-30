871613c6c92b98304cc589bfdc37859248b0e364
Mark Taylor <m.b.taylor@bristol.ac.uk>
2009-06-30 13:10:58 +0000

    Remove withdrawn PointList.points method

--- jniast/src/testcases/uk/ac/starlink/ast/RegionTest.java
+++ jniast/src/testcases/uk/ac/starlink/ast/RegionTest.java
@@ -95,9 +95,6 @@ public class RegionTest extends TestCase {
         double[][] points = { { 1, 2, 3, 4 }, { 101, 102, 103, 104 } };
         PointList plist = new PointList( frm, 4, points, null );
         assertEquals( 4, plist.getListSize() );
-        assertArrayEquals( points[ 0 ], plist.points()[ 0 ] );
-        assertArrayEquals( points[ 1 ], plist.points()[ 1 ] );
-        assertEquals( 2, plist.points().length );
         assertBounds( plist, 1, 101, 4, 104 );
         plist = new PointList( frm, 4, points, box( .01, .01, .01, .01 ) );
 
