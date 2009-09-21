f7bae3405a372b41e09ae7b6aa837e0999868d70
Mark Taylor <m.b.taylor@bristol.ac.uk>
2009-09-21 14:54:31 +0000

    vo: registry resource table can now be sorted by column (by user click)

--- vo/src/main/uk/ac/starlink/vo/RegistryTable.java
+++ vo/src/main/uk/ac/starlink/vo/RegistryTable.java
@@ -1,8 +1,13 @@
 package uk.ac.starlink.vo;
 
 import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.Arrays;
 import java.util.List;
+import javax.swing.Icon;
+import javax.swing.JLabel;
 import javax.swing.JTable;
 import javax.swing.JViewport;
 import javax.swing.SwingUtilities;
@@ -10,6 +15,8 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.TableColumnModelEvent;
 import javax.swing.event.TableColumnModelListener;
+import javax.swing.table.JTableHeader;
+import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumn;
 import javax.swing.table.TableColumnModel;
 import javax.swing.table.TableModel;
@@ -23,6 +30,10 @@ import uk.ac.starlink.table.gui.StarJTable;
  * It provides a number of convenience features for making sure that
  * the column widths are and stay reasonably sensible.
  *
+ * <p>The user can click the column headers to control sorting of the table.
+ * By default it is sorted on the first column; this can be reset 
+ * programmatically using {@link #setSorting}.
+ *
  * @author   Mark Taylor (Starlink)
  * @since    21 Dec 2004
  */
@@ -30,6 +41,8 @@ public class RegistryTable extends JTable {
 
     private final ResourceTableModel tModel_;
     private final MetaColumnModel colModel_;
+    private int iSortcol_;
+    private boolean descending_;
     private static String[] DEFAULT_COLUMNS = new String[] {
         "shortName",
         "title",
@@ -72,6 +85,13 @@ public class RegistryTable extends JTable {
             public void columnSelectionChanged( ListSelectionEvent evt ) {}
         } );
         setColumnModel( colModel_ );
+
+        /* Set up the table header to control sort by column. */
+        JTableHeader tHeader = getTableHeader();
+        tHeader.addMouseListener( new SortMouseListener() );
+        TableCellRenderer headRenderer = tHeader.getDefaultRenderer();
+        tHeader.setDefaultRenderer( new SortingHeaderRenderer( headRenderer ) );
+        setSorting( 0, false );
     }
 
     /**
@@ -81,6 +101,9 @@ public class RegistryTable extends JTable {
      */
     public void setData( RegResource[] data ) {
         tModel_.setResources( data );
+        if ( iSortcol_ >= 0 ) {
+            tModel_.sortByColumn( iSortcol_, descending_ );
+        }
         colModel_.purgeEmptyColumns();
         StarJTable.configureColumnWidths( this, 1000, 10000 );
     }
@@ -156,5 +179,121 @@ public class RegistryTable extends JTable {
             mcmodel.removeColumn( mcmodel.getColumn( ic ) );
         }
         return mcmodel;
+    }
+
+    /**
+     * Mouse listener to be installed on the table header which arranges
+     * to sort by columns according to clicks.
+     */
+    private class SortMouseListener extends MouseAdapter {
+        public void mouseClicked( MouseEvent evt ) {
+            JTableHeader head = (JTableHeader) evt.getSource();
+            TableColumnModel colModel = head.getColumnModel();
+            int iViewcol = head.columnAtPoint( evt.getPoint() );
+            int icol = colModel.getColumn( iViewcol ).getModelIndex();
+            if ( icol > -1 ) {
+                if ( iSortcol_ == icol ) {
+                    setSorting( icol, ! descending_ );
+                }
+                else {
+                    setSorting( icol, false );
+                }
+            }
+        }
+    }
+
+    /**
+     * Configures how resource sorting will be done.
+     *
+     * @param  icol  column for sort key; use -1 for no sorting
+     * @param  descending   false for sort up; true for sort down
+     */
+    public void setSorting( int icol, boolean descending ) {
+        iSortcol_ = icol;
+        descending_ = descending;
+        tModel_.sortByColumn( icol, descending );
+        getTableHeader().repaint();
+    }
+
+    /**
+     * Renderer for table header which paints an indication of which 
+     * column is controlling the table data sort.
+     */
+    private class SortingHeaderRenderer implements TableCellRenderer {
+
+        private final TableCellRenderer baseRenderer_;
+
+        /**
+         * Constructor.
+         *
+         * @param  baseRenderer   renderer doing basic header display
+         */
+        public SortingHeaderRenderer( TableCellRenderer baseRenderer ) {
+            baseRenderer_ = baseRenderer;
+        }
+
+        public Component getTableCellRendererComponent( JTable table,
+                                                        Object value,
+                                                        boolean isSelected,
+                                                        boolean hasFocus,
+                                                        int irow, int icol ) {
+            Component comp = baseRenderer_
+                .getTableCellRendererComponent( table, value, isSelected,
+                                                hasFocus, irow, icol );
+            if ( comp instanceof JLabel ) {
+                JLabel label = (JLabel) comp;
+                label. setHorizontalTextPosition( JLabel.RIGHT );
+                int iModelcol = table.convertColumnIndexToModel( icol );
+                Icon icon = iModelcol == iSortcol_
+                          ? new ArrowIcon( descending_,
+                                           label.getFont().getSize() )
+                          : null;
+                label.setIcon( icon );
+            }
+            return comp;
+        }
+    }
+
+    /**
+     * Paints a little up or down arrow.
+     * Code largely pinched from
+     * http://java.sun.com/docs/books/tutorial/uiswing/examples/components/TableSorterDemoProject/src/components/TableSorter.java.
+     */
+    private static class ArrowIcon implements Icon {
+        private final boolean descending_;
+        private final int size_;
+
+        /**
+         * Constructor.
+         *
+         * @param   descending  false for up, true for down
+         * @param   size  font size in pixels
+         */
+        ArrowIcon( boolean descending, int size ) {
+            descending_ = descending;
+            size_ = size;
+        }
+
+        public void paintIcon( Component c, Graphics g, int x, int y ) {
+            int dx = ( size_ + 1 ) / 2;
+            int dy = descending_ ? dx : -dx;
+            y += 5 * size_ / 6 + ( descending_ ? -dy : 0 );
+            int shift = descending_ ? 1 : -1;
+            g.translate( x, y );
+            g.drawLine( dx / 2, dy, 0, 0 );
+            g.drawLine( dx / 2, dy + shift, 0, shift );
+            g.drawLine( dx / 2, dy, dx, 0 );
+            g.drawLine( dx / 2, dy + shift, dx, shift );
+            g.drawLine( dx, 0, 0, 0 );
+            g.translate( -x, -y );
+        }
+
+        public int getIconWidth() {
+            return size_;
+        }
+
+        public int getIconHeight() {
+            return size_;
+        }
     }
 }
