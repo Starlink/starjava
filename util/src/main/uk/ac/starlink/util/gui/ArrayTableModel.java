a8b0e4eae58e69d1fd48fcc9b01c52ba5da580b0
Mark Taylor <m.b.taylor@bristol.ac.uk>
2009-10-14 16:44:22 +0000

    util: add GUI classes to facilitate sort-by-clickable JTables

---
+++ util/src/main/uk/ac/starlink/util/gui/ArrayTableModel.java
@@ -0,0 +1,167 @@
+package uk.ac.starlink.util.gui;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ * TableModel which contains its data as an array of objects, one per row.
+ * The {@link ArrayTableSorter} class can be used in conjunction with 
+ * this class to provide tables which can be sorted by clicking on 
+ * column headers.
+ * The model works by having an array of column objects which know how to 
+ * interrogate the array of data objects in such a way as to return cell
+ * values.  The implementation of the column and data item objects 
+ * is therefore interlinked.
+ *
+ * @author   Mark Taylor
+ * @since    14 Oct 2009
+ */
+public class ArrayTableModel extends AbstractTableModel {
+
+    private ArrayTableColumn[] columns_;
+    private Object[] items_;
+
+    /**
+     * Constructs a model with no columns or rows.
+     */
+    public ArrayTableModel() {
+        this( new ArrayTableColumn[ 0 ], new Object[ 0 ] );
+    }
+
+    /**
+     * Constructs a model with a given set of columns and rows.
+     *
+     * @param   columns   array of column specifiers
+     * @param   items     array of row data objects
+     */
+    public ArrayTableModel( ArrayTableColumn[] columns, Object[] items ) {
+        columns_ = columns;
+        items_ = items;
+    }
+
+    /**
+     * Sets the column specifiers to be used by this table.
+     *
+     * @param   columns  column specifiers
+     */
+    public void setColumns( ArrayTableColumn[] columns ) {
+        columns_ = columns;
+        fireTableStructureChanged();
+    }
+
+    /**
+     * Returns the column specifiers used by this table.
+     *
+     * @return   column specifiers
+     */
+    public ArrayTableColumn[] getColumns() {
+        return columns_;
+    }
+
+    /**
+     * Sets the row data items to be used by this table.
+     *
+     * @param  items  row data items, one per row
+     */
+    public void setItems( Object[] items ) {
+        items_ = items;
+        fireTableDataChanged();
+    }
+
+    /**
+     * Returns the row data items used by this model.
+     *
+     * @return  row data items, one per row
+     */
+    public Object[] getItems() {
+        return items_;
+    }
+
+    /**
+     * Sorts the rows in this table according to the ordering of the 
+     * data in one of the columns.
+     * The ordering may not be maintained if the data changes.
+     *
+     * @param  icol  index of column to sort by
+     * @param  descending  true to sort down, false to sort up
+     */
+    public void sortByColumn( int icol, boolean descending ) {
+        ArrayTableColumn col = columns_[ icol ];
+        if ( Comparable.class.isAssignableFrom( col.getContentClass() ) ) {
+            Arrays.sort( items_, new ColumnComparator( col, descending ) );
+            fireTableDataChanged();
+        }
+    }
+
+    public int getColumnCount() {
+        return columns_.length;
+    }
+
+    public int getRowCount() {
+        return items_.length;
+    }
+
+    public Object getValueAt( int irow, int icol ) {
+        return columns_[ icol ].getValue( items_[ irow ] );
+    }
+
+    public String getColumnName( int icol ) {
+        return columns_[ icol ].getName();
+    }
+
+    public Class getColumnClass( int icol ) {
+        return columns_[ icol ].getContentClass();
+    }
+
+    /**
+     * Can compare two row data items according to the value of a column.
+     */
+    private static class ColumnComparator implements Comparator {
+        private final ArrayTableColumn col_;
+        private final int sense_;
+
+        /**
+         * Constructor.
+         *
+         * @param   col   column whose contents determines sort order
+         * @param   descending  false for ascending, true for descending;
+         *                      nulls are always at the bottom
+         */
+        public ColumnComparator( ArrayTableColumn col, boolean descending ) {
+            col_ = col;
+            sense_ = descending ? -1 : +1;
+        }
+
+        public int compare( Object o1, Object o2 ) {
+            Comparable c1 = (Comparable) col_.getValue( o1 );
+            Comparable c2 = (Comparable) col_.getValue( o2 );
+            boolean blank1 = isBlank( c1 );
+            boolean blank2 = isBlank( c2 );
+            if ( blank1 && blank2 ) {
+                return 0;
+            }
+            else if ( blank1 ) {
+                return +1;
+            }
+            else if ( blank2 ) {
+                return -1;
+            }
+            else {
+                return sense_ * c1.compareTo( c2 );
+            }
+        }
+
+        /**
+         * Returns true if an object counts as empty for sort purposes.
+         *
+         * @param  o  object
+         * @return   true if this comparator's column value is to be treated
+         *           as blank
+         */
+        private boolean isBlank( Object o ) {
+            return o == null
+                || ( o instanceof String && ((String) o).trim().length() == 0 );
+        }
+    }
+}
---
+++ util/src/main/uk/ac/starlink/util/gui/ArrayTableSorter.java
@@ -0,0 +1,187 @@
+package uk.ac.starlink.util.gui;
+
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.Icon;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.SwingUtilities;
+import javax.swing.table.JTableHeader;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumnModel;
+
+/**
+ * Handles GUI aspects of allowing the user to sort columns in a table
+ * by clicking on the column header.
+ * After construction you have to install it on a suitable table header.
+ * The table must be represented by a {@link ArrayTableModel}.
+ *
+ * @author   Mark Taylor
+ * @since    14 Oct 2009
+ */
+public class ArrayTableSorter {
+
+    private final ArrayTableModel model_;
+    private final MouseListener mouseListener_;
+    private int iSortcol_;
+    private boolean descending_;
+
+    /**
+     * Constructor.
+     *
+     * @param  model  table model
+     * @param  header
+     */
+    public ArrayTableSorter( ArrayTableModel model ) {
+        model_ = model;
+        mouseListener_ = new SortMouseListener();
+        iSortcol_ = -1;
+    }
+
+    /**
+     * Sets this object up to allow sorting its model by clicking on a
+     * given JTable header.
+     *
+     * @param  header   table header component
+     */
+    public void install( JTableHeader header ) {
+        TableCellRenderer rend0 = header.getDefaultRenderer();
+        header.setDefaultRenderer( new SortingHeaderRenderer( rend0 ) );
+        header.addMouseListener( mouseListener_ );
+    }
+
+    /**
+     * Reverses the action of a previous {@link install} action.
+     *
+     * @param  header   table header component
+     */
+    public void uninstall( JTableHeader header ) {
+        TableCellRenderer rend1 = header.getDefaultRenderer();
+        if ( rend1 instanceof SortingHeaderRenderer ) {
+            TableCellRenderer rend0 =
+                ((SortingHeaderRenderer) rend1).baseRenderer_;
+            header.setDefaultRenderer( rend0 );
+        }
+        header.removeMouseListener( mouseListener_ );
+    }
+
+    /**
+     * Performs a sort on a given column.
+     * Note, the header should be repainted following a call to this method.
+     *
+     * @param   icol  column index
+     * @param   descending  true sort down, false sort up
+     */
+    public void setSorting( int icol, boolean descending ) {
+        iSortcol_ = icol;
+        descending_ = descending;
+        model_.sortByColumn( icol, descending );
+    }
+
+    /**
+     * Mouse listener to be installed on the table header which arranges
+     * to sort by columns according to clicks.
+     */
+    private class SortMouseListener extends MouseAdapter {
+        public void mouseClicked( MouseEvent evt ) {
+            JTableHeader header = (JTableHeader) evt.getSource();
+            TableColumnModel colModel = header.getColumnModel();
+            int iViewcol = header.columnAtPoint( evt.getPoint() );
+            int icol = colModel.getColumn( iViewcol ).getModelIndex();
+            if ( icol > -1 ) {
+                if ( iSortcol_ == icol ) {
+                    setSorting( icol, ! descending_ );
+                }
+                else {
+                    setSorting( icol, false );
+                }
+                header.repaint();
+            }
+        }
+    }
+
+    /**
+     * Renderer for table header which paints an indication of which
+     * column is controlling the table data sort.
+     */
+    private class SortingHeaderRenderer implements TableCellRenderer {
+
+        private final TableCellRenderer baseRenderer_;
+
+        /**
+         * Constructor.
+         *
+         * @param  baseRenderer   renderer doing basic header display
+         */
+        public SortingHeaderRenderer( TableCellRenderer baseRenderer ) {
+            baseRenderer_ = baseRenderer;
+        }
+
+        public Component getTableCellRendererComponent( JTable table,
+                                                        Object value,
+                                                        boolean isSelected,
+                                                        boolean hasFocus,
+                                                        int irow, int icol ) {
+            Component comp = baseRenderer_
+                .getTableCellRendererComponent( table, value, isSelected,
+                                                hasFocus, irow, icol );
+            if ( comp instanceof JLabel ) {
+                JLabel label = (JLabel) comp;
+                label. setHorizontalTextPosition( JLabel.RIGHT );
+                int iModelcol = table.convertColumnIndexToModel( icol );
+                Icon icon = iModelcol == iSortcol_
+                          ? new ArrowIcon( descending_,
+                                           label.getFont().getSize() )
+                          : null;
+                label.setIcon( icon );
+            }
+            return comp;
+        }
+    }
+
+    /**
+     * Paints a little up or down arrow.
+     * Code largely pinched from
+     * http://java.sun.com/docs/books/tutorial/uiswing/examples/components/TableSorterDemoProject/src/components/TableSorter.java.
+     */
+    private static class ArrowIcon implements Icon {
+        private final boolean descending_;
+        private final int size_;
+
+        /**
+         * Constructor.
+         *
+         * @param   descending  false for up, true for down
+         * @param   size  font size in pixels
+         */
+        ArrowIcon( boolean descending, int size ) {
+            descending_ = descending;
+            size_ = size;
+        }
+
+        public void paintIcon( Component c, Graphics g, int x, int y ) {
+            int dx = ( size_ + 1 ) / 2;
+            int dy = descending_ ? dx : -dx;
+            y += 5 * size_ / 6 + ( descending_ ? -dy : 0 );
+            int shift = descending_ ? 1 : -1;
+            g.translate( x, y );
+            g.drawLine( dx / 2, dy, 0, 0 );
+            g.drawLine( dx / 2, dy + shift, 0, shift );
+            g.drawLine( dx / 2, dy, dx, 0 );
+            g.drawLine( dx / 2, dy + shift, dx, shift );
+            g.drawLine( dx, 0, 0, 0 );
+            g.translate( -x, -y );
+        }
+
+        public int getIconWidth() {
+            return size_;
+        }
+
+        public int getIconHeight() {
+            return size_;
+        }
+    }
+}
