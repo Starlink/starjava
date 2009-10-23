0fe5f3d987a0aa2eff983a3a4b90b091d1ba7156
Mark Taylor <m.b.taylor@bristol.ac.uk>
2009-10-23 15:35:38 +0000

    datanode: copy code from previous version of FitsTableBuilder to prevent Treeview from breaking.  Treeview probably relies on undocumented and bad detailed behaviour of the old implementation, but it's unsupported - copying it here is better than nothing.

--- datanode/src/main/uk/ac/starlink/datanode/nodes/TableHDUDataNode.java
+++ datanode/src/main/uk/ac/starlink/datanode/nodes/TableHDUDataNode.java
@@ -12,9 +12,14 @@ import nom.tam.fits.TableData;
 import nom.tam.fits.TableHDU;
 import nom.tam.fits.Header;
 import nom.tam.util.ArrayDataInput;
+import nom.tam.util.RandomAccess;
+import uk.ac.starlink.fits.BintableStarTable;
+import uk.ac.starlink.fits.FitsConstants;
+import uk.ac.starlink.fits.FitsStarTable;
 import uk.ac.starlink.fits.FitsTableBuilder;
 import uk.ac.starlink.table.StarTable;
 import uk.ac.starlink.util.DataSource;
+import uk.ac.starlink.util.IOUtils;
 
 /**
  * An implementation of the {@link DataNode} interface for 
@@ -88,8 +93,7 @@ public class TableHDUDataNode extends HDUDataNode {
                 ArrayDataInput istrm = hdudata.getArrayData();
                 DataSource datsrc = hdudata.getDataSource();
                 long offset = hdudata.getOffset();
-                starTable = FitsTableBuilder
-                           .attemptReadTable( istrm, true, datsrc,
+                starTable = attemptReadTable( istrm, datsrc,
                                               new long[] { offset } );
             }
             catch ( FitsException e ) {
@@ -143,4 +147,76 @@ public class TableHDUDataNode extends HDUDataNode {
         }
     }
 
+    /**
+     * Reads the next header, and if it represents a table HDU, makes a
+     * StarTable out of it and returns.  If it is some other kind of HDU, 
+     * <tt>null</tt> is returned.  In either case, the stream is advanced
+     * the end of that HDU.
+     *
+     * NOTE: This code has been copied from a slightly earlier version of
+     * uk.ac.starlink.fits.FitsTableBuilder.attemptReadTable.
+     * That code was rationalised, and that stopped this class from working
+     * properly (attempting to access table data usually resulted in 
+     * MappedFile.seek() out of bounds errors).  
+     * I haven't looked into it in full detail, but probably
+     * this class was relying for its behaviour on undocumented misfeatures
+     * of the implementation below.  Since the datanode package is 
+     * unsupported at time of writing, a proper fix is not on the cards.
+     * Copying the 'working' code here so that existing behaviour continues
+     * correctly is therefore the best option.  - MBT 23 Oct 2009
+     * 
+     * @param  strm  stream to read from, positioned at the start of an HDU
+     *         (before the header)
+     * @param  datsrc  a DataSource which can supply the data 
+     *         in <tt>strm</tt>
+     * @param  pos  a 1-element array holding the position in <tt>datsrc</tt>
+     *         at which <tt>strm</tt> is positioned -
+     *         it's an array so it can be updated by this routine (sorry)
+     * @return   a StarTable made from the HDU at the start of <tt>strm</tt>
+     *           or null
+     */
+    private static StarTable attemptReadTable( ArrayDataInput strm,
+                                               DataSource datsrc, long[] pos )
+        throws FitsException, IOException {
+
+        /* Read the header. */
+        Header hdr = new Header();
+        int headsize = FitsConstants.readHeader( hdr, strm );
+        long datasize = FitsConstants.getDataSize( hdr );
+        long datpos = pos[ 0 ] + headsize;
+        pos[ 0 ] += headsize + datasize;
+        String xtension = hdr.getStringValue( "XTENSION" );
+          
+        /* If it's a BINTABLE HDU, make a BintableStarTable out of it. */ 
+        if ( "BINTABLE".equals( xtension ) ) {
+            if ( strm instanceof RandomAccess ) {
+                return BintableStarTable
+                      .makeRandomStarTable( hdr, (RandomAccess) strm );
+            }
+            else {
+                return BintableStarTable
+                      .makeSequentialStarTable( hdr, datsrc, datpos );
+            }
+
+            // BinaryTable tdata = new BinaryTable( hdr );
+            // tdata.read( strm );
+            // TableHDU thdu = new BinaryTableHDU( hdr, (Data) tdata );
+            // return new FitsStarTable( thdu );
+        }
+
+        /* If it's a TABLE HDU (ASCII table), make a FitsStarTable. */
+        else if ( "TABLE".equals( xtension ) ) {
+            AsciiTable tdata = new AsciiTable( hdr );
+            tdata.read( strm );
+            tdata.getData();
+            TableHDU thdu = new AsciiTableHDU( hdr, (Data) tdata );
+            return new FitsStarTable( thdu );
+        }
+
+        /* It's not a table HDU - just skip over it and return null. */
+        else {
+            IOUtils.skipBytes( strm, datasize );
+            return null;
+        }
+    }
 }
