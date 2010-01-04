d5eecfc251cb64c43660d490d49fb2d90ca3cad9
Mark Taylor <m.b.taylor@bristol.ac.uk>
2010-01-04 18:28:31 +0000

    ttools: no longer try to talk to TOPCAT using (long-deprecated) SOAP interface; Axis et al. no longer required

--- ttools/src/main/uk/ac/starlink/ttools/mode/TopcatMode.java
+++ ttools/src/main/uk/ac/starlink/ttools/mode/TopcatMode.java
@@ -4,18 +4,11 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.net.ConnectException;
 import java.net.URI;
-import java.rmi.RemoteException;
 import java.util.Iterator;
 import java.util.Random;
 import java.util.logging.Logger;
 import javax.swing.SwingUtilities;
-import javax.xml.rpc.ParameterMode;
-import javax.xml.rpc.ServiceException;
-import org.apache.axis.client.Call;
-import org.apache.axis.client.Service;
-import org.apache.axis.encoding.XMLType;
 import org.astrogrid.samp.Message;
 import org.astrogrid.samp.Metadata;
 import org.astrogrid.samp.Response;
@@ -29,7 +22,6 @@ import org.astrogrid.samp.xmlrpc.StandardClientProfile;
 import org.astrogrid.samp.xmlrpc.XmlRpcKit;
 import org.votech.plastic.PlasticHubListener;
 import uk.ac.starlink.plastic.PlasticUtils;
-import uk.ac.starlink.soap.util.RemoteUtilities;
 import uk.ac.starlink.table.StarTable;
 import uk.ac.starlink.table.StarTableWriter;
 import uk.ac.starlink.table.StoragePolicy;
@@ -57,14 +49,6 @@ public class TopcatMode implements ProcessingMode {
 
     private static Logger logger_ =
         Logger.getLogger( "uk.ac.starlink.tttools.mode" );
-
-    /**
-     * Fail with an undeclared throwable at load time if this class isn't
-     * going to be able to function.
-     */
-    static {
-        checkRequisites();
-    }
 
     public Parameter[] getAssociatedParameters() {
         return new Parameter[ 0 ];
@@ -150,25 +134,6 @@ public class TopcatMode implements ProcessingMode {
             }
             catch ( IOException e ) {
                 logger_.info( "... PLASTIC broadcast failed " + e );
-            }
-        }
-
-        if ( ! done ) {
-            try {
-                logger_.info( "Trying SOAP ..." );
-                soapDisplay( table, srcName );
-                logger_.info( "... sent via SOAP" );
-                done = true;
-            }
-            catch ( ConnectException e ) {
-                logger_.info( "... SOAP connection failed " + e );
-            }
-            catch ( ServiceException e ) {
-                throw (IOException) new IOException( e.getMessage() )
-                                   .initCause( e );
-            }
-            catch ( Throwable e ) {
-                logger_.info( "... SOAP connection failed " + e );
             }
         }
 
@@ -296,54 +261,6 @@ public class TopcatMode implements ProcessingMode {
     }
 
     /**
-     * Attempts to display a table in a running TOPCAT SOAP server.
-     *
-     * @param  table  table to display
-     * @param  srcName   label for the table's source
-     */
-    private void soapDisplay( StarTable table, String srcName )
-            throws ConnectException, ServiceException, IOException {
-
-        Object[] tcServ = RemoteUtilities.readContactFile( "topcat" );
-        if ( tcServ == null ) {
-            throw new ConnectException( "No contact file - looks like " 
-                                      + "no TOPCAT server is running" );
-        }
-        String host = (String) tcServ[ 0 ];
-        int port = ((Integer) tcServ[ 1 ]).intValue();
-        String cookie = (String) tcServ[ 2 ];
-        String endpoint = "http://" + host + ":" + port + 
-                          "/services/TopcatSOAPServices";
-
-        Call call = (Call) new Service().createCall();
-        call.setTargetEndpointAddress( endpoint );
-        VOTableSerialization.configureCall( call );
-        call.setOperationName( "displayTable" );
-        call.addParameter( "cookie", XMLType.SOAP_STRING, ParameterMode.IN );
-        call.addParameter( "table", VOTableSerialization.QNAME_VOTABLE,
-                           ParameterMode.IN );
-        call.addParameter( "location", XMLType.SOAP_STRING, ParameterMode.IN );
-        call.setReturnType( XMLType.AXIS_VOID );
-        try {
-            call.invoke( new Object[] { cookie, table, srcName } );
-        }
-        catch ( RemoteException e ) {
-            Throwable e2 = e.getCause();
-            if ( e2 instanceof ConnectException ) {
-                String msg = "Connection refused: TOPCAT server not running?";
-                throw (ConnectException) new ConnectException( msg )
-                                        .initCause( e2 );
-            }
-            else if ( e2 instanceof IOException ) {
-                throw (IOException) e2;
-            }
-            else {
-                throw e;
-            }
-        }
-    }
-
-    /**
      * Starts up a new TOPCAT instance and displays the given table in it.
      *
      * @param  table  randomTable to display, must have random access
@@ -382,15 +299,6 @@ public class TopcatMode implements ProcessingMode {
         catch ( InterruptedException e ) {
             logger_.info( "Interrupted waiting for table load" );
         }
-    }
-
-    /**
-     * Throws an exception if there aren't enough classes on the classpath
-     * to be able to attempt processing in this mode.
-     */
-    private static void checkRequisites() {
-        RemoteUtilities.class.getName();
-        org.apache.axis.encoding.Target.class.getName();
     }
 
     /**
