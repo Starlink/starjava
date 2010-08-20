c2ac9bd37468919d870a84457453729b99d9b522
Mark Taylor <m.b.taylor@bristol.ac.uk>
2010-08-20 17:26:42 +0000

    table: remove NioDataAccess class; hide functionality inside MappedColumnStore

--- table/src/main/uk/ac/starlink/table/storage/MappedColumnStore.java
+++ table/src/main/uk/ac/starlink/table/storage/MappedColumnStore.java
@@ -1,5 +1,6 @@
 package uk.ac.starlink.table.storage;
 
+import java.io.DataOutput;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
@@ -15,7 +16,7 @@ class MappedColumnStore implements ColumnStore {
 
     private final Codec codec_;
     private final int itemSize_;
-    private final NioDataAccess access_;
+    private final BufferIOAccess access_;
 
     /**
      * Constructor.
@@ -28,7 +29,7 @@ class MappedColumnStore implements ColumnStore {
     public MappedColumnStore( Codec codec, ByteBuffer bbuf ) {
         codec_ = codec;
         itemSize_ = codec.getItemSize();
-        access_ = new NioDataAccess( bbuf );
+        access_ = new BufferIOAccess( bbuf );
         if ( itemSize_ < 0 ) {
             throw new IllegalArgumentException( "Must have fixed size codec" );
         }
@@ -48,5 +49,80 @@ class MappedColumnStore implements ColumnStore {
     }
 
     public void dispose() {
+    }
+
+    /**
+     * ByteBuffer adapter which implements both ByteStoreAccess and DataOut.
+     */
+    private static class BufferIOAccess extends SingleNioAccess 
+                                        implements DataOutput {
+
+        /**
+         * Constructor.
+         *
+         * @param   bbuf  backing buffer
+         */
+        BufferIOAccess( ByteBuffer bbuf ) {
+            super( bbuf );
+        }
+
+        public void write( int b ) throws IOException {
+            getBuffer( 1 ).put( (byte) b );
+        }
+
+        public void write( byte[] b, int off, int len ) throws IOException {
+            getBuffer( len ).put( b, off, len );
+        }
+
+        public void write( byte[] b ) throws IOException {
+            getBuffer( b.length ).put( b );
+        }
+
+        public void writeBoolean( boolean val ) throws IOException {
+            getBuffer( 1 ).put( val ? (byte) 1 : (byte) 0 );
+        }
+
+        public void writeByte( int val ) throws IOException {
+            getBuffer( 1 ).put( (byte) val );
+        }
+
+        public void writeChar( int val ) throws IOException {
+            getBuffer( 2 ).putChar( (char) val );
+        }
+
+        public void writeShort( int val ) throws IOException {
+            getBuffer( 2 ).putShort( (short) val );
+        }
+
+        public void writeInt( int val ) throws IOException {
+            getBuffer( 4 ).putInt( val );
+        }
+
+        public void writeLong( long val ) throws IOException {
+            getBuffer( 8 ).putLong( val );
+        }
+
+        public void writeFloat( float val ) throws IOException {
+            getBuffer( 4 ).putFloat( val );
+        }
+
+        public void writeDouble( double val ) throws IOException {
+            getBuffer( 8 ).putDouble( val );
+        }
+
+        public void writeBytes( String val ) throws IOException {
+            throw new UnsupportedOperationException(
+                          "Incomplete DataOutput implementation" );
+        }
+
+        public void writeChars( String val ) throws IOException {
+            throw new UnsupportedOperationException(
+                          "Incomplete DataOutput implementation" );
+        }
+
+        public void writeUTF( String val ) throws IOException {
+            throw new UnsupportedOperationException(
+                          "Incomplete DataOutput implementation" );
+        }
     }
 }
