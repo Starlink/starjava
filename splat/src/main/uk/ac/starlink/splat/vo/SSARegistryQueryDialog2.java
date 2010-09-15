ae270b5c12ff1ae1611d4b89e662f799db7fe4eb
Mark Taylor <m.b.taylor@bristol.ac.uk>
2010-09-15 14:12:46 +0000

    splat: use new table load dialogue framework

---
+++ splat/src/main/uk/ac/starlink/splat/vo/SSARegistryQueryDialog2.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2005 Central Laboratory of the Research Councils
+ *
+ *  History:
+ *     04-APR-2005 (Peter W. Draper):
+ *       Original version, based on Mark's Dialog in vo package.
+ *     15-SEP-2010 (Mark Taylor):
+ *       Adjusted to use revised load dialog framework.
+ */
+package uk.ac.starlink.splat.vo;
+
+import java.awt.Component;
+import java.beans.IntrospectionException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import uk.ac.starlink.table.BeanStarTable;
+import uk.ac.starlink.table.DescribedValue;
+import uk.ac.starlink.table.StarTable;
+import uk.ac.starlink.table.StarTableFactory;
+import uk.ac.starlink.table.load.TableLoader;
+import uk.ac.starlink.vo.RegResource;
+import uk.ac.starlink.vo.RegistryQuery;
+import uk.ac.starlink.vo.RegistryQueryPanel;
+import uk.ac.starlink.vo.RegistryTableLoadDialog2;
+
+/**
+ * Dialog for performing a simple query on a registry for its SSAP servers.
+ * The user can choose which registry to use. A StarTable is returned which
+ * contains all the details of the resources found.
+ *
+ * @author Peter W. Draper
+ * @author Mark Taylor (Starlink)
+ * @version $Id: SSARegistryQueryDialog.java 9052 2009-12-09 18:48:37Z mbt $
+ */
+public class SSARegistryQueryDialog2
+    extends RegistryTableLoadDialog2
+{
+    private RegistryQueryPanel rqPanel_;
+    private static Boolean available_;
+    private StarTable table_;
+
+    /** The SSAP query. */
+    public static String[] defaultQuery_ = new String[]
+        {
+            "capability/@standardID = 'ivo://ivoa.net/std/SSA'"
+        };
+
+    public String getName()
+    {
+        return "SSAP Registry Query";
+    }
+
+    public String getDescription()
+    {
+        return "Query a registry for all known SSAP services";
+    }
+
+    protected Component createQueryComponent()
+    {
+        rqPanel_ = new RegistryQueryPanel();
+        rqPanel_.setPresetQueries( defaultQuery_ );
+        return rqPanel_;
+    }
+
+    public TableLoader createTableLoader()
+    {
+        try {
+            final RegistryQuery query = rqPanel_.getRegistryQuery();
+            return new TableLoader()
+                {
+                    public StarTable[] loadTables( StarTableFactory factory )
+                            throws IOException
+                    {
+                        RegResource[] resources = query.getQueryResources();
+                        BeanStarTable st;
+                        try {
+                            st = new BeanStarTable( RegResource.class );
+                        }
+                        catch ( IntrospectionException e ) {
+                            throw (IOException)
+                                  new IOException( e.getMessage() )
+                                 .initCause( e );
+                        }
+                        DescribedValue[] metadata = query.getMetadata();
+                        for ( int i = 0; i < metadata.length; i++ ) {
+                            st.setParameter( metadata[ i ] );
+                        }
+                        st.setData( resources );
+                        return new StarTable[] { st };
+                    }
+                    public String getLabel()
+                    {
+                        return query.toString();
+                    }
+                };
+        }
+        catch ( MalformedURLException e ) {
+            throw new IllegalStateException( e.getMessage() );
+        }
+    }
+}
