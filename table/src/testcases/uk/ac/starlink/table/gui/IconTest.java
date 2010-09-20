d8b3812cc95a2d94775845f18b07129e059466b3
Mark Taylor <m.b.taylor@bristol.ac.uk>
2010-09-20 15:11:15 +0000

    table: update tests

--- table/src/testcases/uk/ac/starlink/table/gui/IconTest.java
+++ table/src/testcases/uk/ac/starlink/table/gui/IconTest.java
@@ -6,9 +6,11 @@ import junit.framework.TestCase;
 public class IconTest extends TestCase {
 
     public void testIcons() {
-        assert24( new SQLReadDialog().getIcon() );
-        assert24( new FileChooserLoader().getIcon() );
+        assert24( new LocationTableLoadDialog().getIcon() );
+        assert24( new SQLTableLoadDialog().getIcon() );
+        assert24( new FileChooserTableLoadDialog().getIcon() );
         assert24( new FilestoreTableLoadDialog().getIcon() );
+        assert24( SystemBrowser.getSystemBrowserIcon() );
     }
 
     private void assert24( Icon icon ) {
