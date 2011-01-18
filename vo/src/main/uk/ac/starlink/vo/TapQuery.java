c6517af1dbcd3024472526c6121d0c7c8b8e2ae7
Mark Taylor <m.b.taylor@bristol.ac.uk>
2011-01-18 10:53:36 +0000

    vo: add UWS and TAP control classes

---
+++ vo/src/main/uk/ac/starlink/vo/TapQuery.java
@@ -0,0 +1,209 @@
+package uk.ac.starlink.vo;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathException;
+import javax.xml.xpath.XPathFactory;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+import uk.ac.starlink.table.DefaultValueInfo;
+import uk.ac.starlink.table.DescribedValue;
+import uk.ac.starlink.table.StarTable;
+import uk.ac.starlink.table.StarTableFactory;
+import uk.ac.starlink.table.ValueInfo;
+import uk.ac.starlink.util.DataSource;
+import uk.ac.starlink.util.URLDataSource;
+
+/**
+ * Query using the Table Access Protocol.
+ * UWS communications are handled by a supplied UwsJob, and the
+ * TAP-specific aspects of the protocol are provided by this class.
+ * The supplied UwsJob should not in general be invoked by the user of
+ * this class.
+ *
+ * @see <a href="http://www.ivoa.net/Documents/TAP/">IVOA TAP Recommendation</a>
+ */
+public class TapQuery {
+
+    private final UwsJob uwsJob_;
+    private static final Logger logger_ =
+        Logger.getLogger( "uk.ac.starlink.vo" );
+
+    /**
+     * Constructor.
+     *
+     * @param  uwsJob  UWS job representing the TAP query;
+     *         
+     */
+    public TapQuery( UwsJob uwsJob ) {
+        uwsJob_ = uwsJob;
+    }
+
+    /**
+     * Returns the UWS job on which this query is based.
+     *
+     * @return   UWS job
+     */
+    public UwsJob getJob() {
+        return uwsJob_;
+    }
+
+    /**
+     * Runs the TAP query represented by this object's UWS job, and
+     * returns a table based on the result.  In case of job failure,
+     * an exception will be thrown instead.
+     *
+     * @param   tfact  table factory for table creation from result document
+     * @param   poll   polling interval in milliseconds 
+     */
+    public StarTable execute( StarTableFactory tfact, long poll )
+            throws IOException, InterruptedException {
+        try {
+            String phase = uwsJob_.runToCompletion( poll );
+            if ( "COMPLETED".equals( phase ) ) {
+                URL resultUrl =
+                    new URL( uwsJob_.getJobUrl() + "/results/result" );
+                DataSource datsrc = new URLDataSource( resultUrl );
+
+                /* note this does take steps to follow redirects. */
+                return tfact.makeStarTable( datsrc, "votable" );
+            }
+            else if ( "ABORTED".equals( phase ) ||
+                      "HELD".equals( phase ) ) {
+                throw new IOException( "TAP query did not complete ("
+                                     + phase + ")" );
+            }
+            else if ( "ERROR".equals( phase ) ) {
+                String errText = null;
+
+                /* We read the error text from the /error job resource,
+                 * which TAP says has to be a VOTable.  This is always
+                 * present.  In some cases it might be better to read
+                 * from the errorSummary element of the job resource
+                 * (e.g. xpath /job/errorSummary/message/text()), since
+                 * that might contain a shorter and more human-friendly
+                 * message, but (a) many/most TAP servers do not supply
+                 * this at time of writing and (b) it's difficult to
+                 * navigate the UWS document in the presence of namespaces
+                 * which often refer to different/older versions of the
+                 * UWS protocol. */
+                try {
+                    errText = readErrorInfo( new URL( uwsJob_.getJobUrl()
+                                                    + "/error" ) );
+                }
+                catch ( Throwable e ) {
+                    throw (IOException)
+                          new IOException( "TAP Execution error"
+                                         + " (can't get detail)" )
+                         .initCause( e );
+                }
+                throw new IOException( "TAP execution error: " + errText );
+            }
+            else {
+                throw new IOException( "Unknown UWS execution phase " + phase );
+            }
+        }
+        finally {
+            new Thread( "UWS Job deletion" ) {
+                public void run() {
+                    try {
+                        uwsJob_.postDelete();
+                        logger_.info( "UWS job " + uwsJob_.getJobUrl()
+                                                 + " deleted" );
+                    }
+                    catch ( IOException e ) {
+                        logger_.warning( "UWS job deletion failed for "
+                                       + uwsJob_.getJobUrl() );
+                    }
+                }
+            }.start();
+        }
+    }
+
+    /**
+     * Returns an array of metadata items which describe this query.
+     *
+     * @return  some TAP query metadata items
+     */
+    public DescribedValue[] getQueryMetadata() {
+        List<DescribedValue> metaList = new ArrayList<DescribedValue>();
+        for ( Map.Entry<String,String> param :
+              uwsJob_.getParameterMap().entrySet() ) {
+            String pname = param.getKey();
+            String pvalue = param.getValue();
+            ValueInfo pinfo =
+                new DefaultValueInfo( pname, String.class,
+                                      "TAP " + pname + " parameter" );
+            metaList.add( new DescribedValue( pinfo, pvalue ) );
+        }
+        return metaList.toArray( new DescribedValue[ 0 ] );
+    }
+
+    /**
+     * Returns a short textual summary of this query.
+     *
+     * @return  query summary
+     */
+    public String getSummary() {
+        return "TAP query";
+    }
+
+    /**
+     * Returns the job list corresponding to a TAP service URL.
+     *
+     * @return  serviceUrl + "/async"
+     */
+    public static String jobListUrl( String serviceUrl ) {
+        return serviceUrl + "/async";
+    }
+
+    /**
+     * Returns a TapQuery given a serviceUrl and ADQL text.
+     *
+     * @param  serviceUrl  base service URL for TAP service (excluding "/async")
+     * @param  adql   text of ADQL query
+     */
+    public static TapQuery createAdqlQuery( String serviceUrl, String adql ) {
+        Map<String,String> tapMap = new LinkedHashMap<String,String>();
+        tapMap.put( "REQUEST", "doQuery" );
+        tapMap.put( "LANG", "ADQL" );
+        tapMap.put( "QUERY", adql );
+        return new TapQuery( new UwsJob( jobListUrl( serviceUrl ), tapMap ) );
+    }
+
+    /**
+     * Reads the error text as encoded in a TAP VOTable error document.
+     *
+     * @return   plain text error message
+     */
+    private static String readErrorInfo( URL voturl ) throws IOException {
+        try {
+            Document errDoc = DocumentBuilderFactory.newInstance()
+                             .newDocumentBuilder().parse( voturl.toString() );
+            XPath xpath = XPathFactory.newInstance().newXPath();
+            return xpath.evaluate( "VOTABLE/RESOURCE[@type='results']"
+                                 + "/INFO[@name='QUERY_STATUS']/text()",
+                                   errDoc );
+        }
+        catch ( ParserConfigurationException e ) {
+            throw (IOException) new IOException( "Error doc parse failure" )
+                               .initCause( e );
+        }
+        catch ( SAXException e ) {
+            throw (IOException) new IOException( "Error doc parse failure" )
+                               .initCause( e );
+        }
+        catch ( XPathException e ) {
+            throw (IOException) new IOException( "Error doc parse failure" )
+                               .initCause( e );
+        }
+    }
+}
---
+++ vo/src/main/uk/ac/starlink/vo/UwsJob.java
@@ -0,0 +1,371 @@
+package uk.ac.starlink.vo;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+import uk.ac.starlink.util.CgiQuery;
+
+/**
+ * Job submitted using the Universal Worker Service pattern.
+ *
+ * @author   Mark Taylor
+ * @since    18 Jan 2011
+ * @see <a href="http://www.ivoa.net/Documents/UWS/">IVOA UWS Recommendation</a>
+ */
+public class UwsJob {
+
+    private final String jobListUrl_;
+    private final Map<String,String> paramMap_;
+    private URL jobUrl_;
+    private boolean created_;
+    private volatile String phase_;
+    private volatile long phaseTime_;
+    private static final Logger logger_ =
+        Logger.getLogger( "uk.ac.starlink.vo" );
+
+    /**
+     * Constructs a UWS job with no parameters.
+     *
+     * @param  jobListUrl  base URL for UWS service (URL of job list)
+     */
+    public UwsJob( String jobListUrl ) {
+        this( jobListUrl, new HashMap<String,String>() );
+    }
+
+    /**
+     * Constructs a UWS job with a parameter map.
+     *
+     * @param  jobListUrl  base URL for UWS service (URL of job list)
+     * @param  paramMap   name-&gt;value map of UWS parameters
+     */
+    public UwsJob( String jobListUrl, Map paramMap ) {
+        jobListUrl_ = jobListUrl;
+        paramMap_ = new LinkedHashMap<String,String>();
+        paramMap_.putAll( paramMap );
+    }
+
+    /**
+     * Sets a parameter with a string value.
+     *
+     * @param   name   parameter name
+     * @param   value  parameter value
+     */
+    public void setParameter( String name, String value ) {
+        paramMap_.put( name, value );
+    }
+
+    /**
+     * Sets a parameter with an integer value.
+     *
+     * @param   name  parameter name
+     * @param   value  parameter value
+     */
+    public void setParameter( String name, long value ) {
+        setParameter( name, Long.toString( value ) );
+    }
+
+    /**
+     * Sets a parameter with a double precision floating point value.
+     * An effort is made to encode the value in a way unlikely to be#
+     * misunderstood by the service.
+     *
+     * @param  name   parameter name
+     * @param  value   parameter value
+     */
+    public void setParameter( String name, double value ) {
+        setParameter( name, CgiQuery.formatDouble( value ) );
+    }
+
+    /**
+     * Sets a parameter with a single precision floating point value.
+     * An effort is made to encode the value in a way unlikely to be#
+     * misunderstood by the service.
+     *
+     * @param  name   parameter name
+     * @param  value   parameter value
+     */
+    public void setParameter( String name, float value ) {
+        setParameter( name, CgiQuery.formatFloat( value ) );
+    }
+
+    /**
+     * Returns the current parameter map.  This may be manipulated directly.
+     *
+     * @return  live parameter map
+     */
+    public Map<String,String> getParameterMap() {
+        return paramMap_;
+    }
+
+    /**
+     * Returns the base URL for the UWS service (the job list URL).
+     *
+     * @return  job list URL
+     */
+    public String getJobListUrl() {
+        return jobListUrl_;
+    }
+
+    /**
+     * Returns the URL for this job.  This will normally be a child of the
+     * job list URL, and contains a representation of the job state.
+     *
+     * @return   job URL
+     */
+    public URL getJobUrl() {
+        return jobUrl_;
+    }
+
+    /**
+     * Returns the most recently read job phase.  Invoking this method
+     * does not cause the phase to be read.
+     *
+     * @return  phase
+     */
+    public String getLastPhase() {
+        return phase_;
+    }
+
+    /**
+     * Returns the epoch at which the job phase was last read.
+     * The return value is the value of <code>System.currentTimeMillis()</code>
+     * at that epoch.
+     *
+     * @return   phase read time epoch in milliseconds
+     * @see   #getLastPhase
+     */
+    public long getLastPhaseTime() {
+        return phaseTime_;
+    }
+
+    /**
+     * Creates this job within UWS.  The current parameter set is posted
+     * and a job URL is assigned.  No phase is posted.
+     * The phase following this method is expected to be PENDING.
+     *
+     * @throws  IllegalArgumentException  if creation has already occurred
+     */
+    public void postCreate() throws IOException {
+        synchronized ( this ) {
+            if ( created_ ) {
+                throw new IllegalStateException( "Job already created" );
+            }
+            created_ = true;
+        }
+        HttpURLConnection hconn = postForm( jobListUrl_, paramMap_ );
+        int code = hconn.getResponseCode();
+        if ( code != HttpURLConnection.HTTP_SEE_OTHER ) {
+            throw new IOException( "Non-303 response: " + code + " " +
+                                   hconn.getResponseMessage() );
+        }
+        String location = hconn.getHeaderField( "Location" );
+        if ( location == null ) {
+            throw new IOException( "No Location field in 303 response" );
+        }
+        logger_.info( "UWS job at: " + location );
+        jobUrl_ = new URL( location );
+    }
+
+    /**
+     * Posts a phase for this job.
+     * The job must have been created first.
+     *
+     * @param  phase  UWS job phase to assign
+     */
+    public void postPhase( String phase ) throws IOException {
+        Map paramMap = new HashMap();
+        paramMap.put( "PHASE", phase );
+        HttpURLConnection hconn =
+            postForm( jobUrl_ + "/phase", "PHASE", phase );
+        int code = hconn.getResponseCode();
+        if ( code != HttpURLConnection.HTTP_SEE_OTHER ) {
+            throw new IOException( "Non-303 response: " + code + " " +
+                                   hconn.getResponseMessage() );
+        }
+    }
+
+    /**
+     * Creates, runs this job and blocks until it has completed with
+     * a success or error code.  This convenience method is a one-stop-shop
+     * for running a UWS job.
+     *
+     * @param   poll   polling time in milliseconds to assess job completion
+     * @return   final phase
+     */
+    public String runToCompletion( final long poll )
+            throws IOException, InterruptedException {
+        postCreate();
+        postPhase( "RUN" );
+        Object lock = new Object();
+        synchronized ( lock ) {
+            while ( ! isCompletionPhase( readPhase() ) ) {
+                lock.wait( poll );
+            }
+        }
+        return phase_;
+    }
+
+    /**
+     * Reads the current UWS phase for this job from the server.
+     * The {@link #getLastPhase lastPhase} and associated
+     * {@link #getLastPhaseTime lastPhaseTime} are also set.
+     *
+     * @return   current phase
+     */
+    public String readPhase() throws IOException {
+        URL phaseUrl = new URL( jobUrl_ + "/phase" );
+        InputStream in = new BufferedInputStream( phaseUrl.openStream() );
+        StringBuffer sbuf = new StringBuffer();
+        for ( int c; ( c = in.read() ) >= 0; ) {
+            sbuf.append( (char) ( c & 0xff ) );
+        }
+        in.close();
+        String phase = sbuf.toString();
+        phase_ = phase;
+        phaseTime_ = System.currentTimeMillis();
+        return phase;
+    }
+
+    /**
+     * Returns the DOM document representing the current state of this job.
+     * This is read from the XML text at the job URL.
+     *
+     * @return  job status
+     */
+    public Document readJob() throws IOException {
+        try {
+            return DocumentBuilderFactory.newInstance()
+                                         .newDocumentBuilder()
+                                         .parse( jobUrl_.toString() );
+        }
+        catch ( SAXException e ) {
+            throw (IOException) new IOException( "Job document parse failure" )
+                               .initCause( e );
+        }
+        catch ( ParserConfigurationException e ) {
+            throw (IOException) new IOException( "Job document parse failure" )
+                               .initCause( e );
+        }
+    }
+
+    /**
+     * Posts deletion of this job to the server.
+     */
+    public void postDelete() throws IOException {
+        URLConnection connection = jobUrl_.openConnection();
+        if ( ! ( connection instanceof HttpURLConnection ) ) {
+            throw new IOException( "Not an HTTP URL?" );
+        }
+        HttpURLConnection hconn = (HttpURLConnection) connection;
+        hconn.setRequestMethod( "DELETE" );
+        hconn.setInstanceFollowRedirects( false );
+        hconn.connect();
+    }
+
+    /**
+     * Convenience method to assess whether a UWS phase is a completion phase.
+     * A completion phase is defined as one which will not change further
+     * without some stimulus from the client.
+     *
+     * @param   phase  phase text
+     * @return   true iff <code>phase</code> is final
+     */
+    public static boolean isCompletionPhase( String phase ) {
+        return "COMPLETED".equals( phase )
+            || "ERROR".equals( phase )
+            || "ABORTED".equals( phase )
+            || "HELD".equals( phase );
+    }
+
+    /**
+     * Performs an HTTP form POST with a single name, value pair.
+     * This convenience method invokes {@link #postForm}.
+     *
+     * @param   url   destination URL
+     * @param   name   parameter name
+     * @param   value  parameter value
+     */
+    private static HttpURLConnection postForm( String url, String name,
+                                               String value )
+            throws IOException {
+        Map paramMap = new HashMap();
+        paramMap.put( name, value );
+        return postForm( url, paramMap );
+    }
+
+    /**
+     * Performs an HTTP form POST with a name-&gt;value map of parameters.
+     * They are posted with MIME type "application/x-www-form-urlencoded".
+     *
+     * @param   url  destination URL
+     * @param   paramMap   name, value map of parameters
+     */
+    private static HttpURLConnection postForm( String url,
+                                               Map<String,String> paramMap )
+            throws IOException {
+        URLConnection connection = new URL( url ).openConnection();
+        if ( ! ( connection instanceof HttpURLConnection ) ) {
+            throw new IOException( "Not an HTTP URL?" );
+        }
+        HttpURLConnection hconn = (HttpURLConnection) connection;
+        byte[] postBytes = toPostedBytes( paramMap );
+        hconn.setRequestMethod( "POST" );
+        hconn.setRequestProperty( "Content-Type",
+                                  "application/x-www-form-urlencoded" );
+        hconn.setRequestProperty( "Content-Length",
+                                  Integer.toString( postBytes.length ) );
+        hconn.setInstanceFollowRedirects( false );
+        hconn.setDoOutput( true );
+        hconn.connect();
+        OutputStream hout = hconn.getOutputStream();
+        hout.write( postBytes );
+        hout.close();
+        return hconn;
+    }
+
+    /**
+     * Encodes a name-&gt;value mapping as an array of bytes suitable for
+     * "application/x-www-form-urlencoded" transmission.
+     *
+     * @param   paramMap  name-&gt;value mapping
+     */
+    private static byte[] toPostedBytes( Map<String,String> paramMap ) {
+        String utf8 = "UTF-8";
+        StringBuffer sbuf = new StringBuffer();
+        for ( Map.Entry<String,String> entry : paramMap.entrySet() ) {
+            if ( sbuf.length() != 0 ) {
+                sbuf.append( '&' );
+            }
+            try {
+                sbuf.append( URLEncoder.encode( entry.getKey(), utf8 ) )
+                    .append( '=' )
+                    .append( URLEncoder.encode( entry.getValue(), utf8 ) );
+            }
+            catch ( UnsupportedEncodingException e ) {
+                throw new AssertionError( "No " + utf8 + "??" );
+            }
+        }
+        int nc = sbuf.length();
+        byte[] bbuf = new byte[ nc ];
+        for ( int i = 0; i < nc; i++ ) {
+            char c = sbuf.charAt( i );
+            assert (int) c == (int) ( c & 0x7f );
+            bbuf[ i ] = (byte) sbuf.charAt( i );
+        }
+        return bbuf;
+    }
+}
