36478a428e55e2290946156c16a81de1e811fb16
Mark Taylor <m.b.taylor@bristol.ac.uk>
2011-03-31 15:58:32 +0000

    vo: add basic TAP tests

---
+++ vo/src/testcases/uk/ac/starlink/vo/TapTester.java
@@ -0,0 +1,36 @@
+package uk.ac.starlink.vo;
+
+import java.io.IOException;
+import junit.framework.TestCase;
+import org.xml.sax.SAXException;
+
+public class TapTester extends TestCase {
+
+    public void testExamples() throws IOException, SAXException {
+        AdqlExample[] examples = AbstractAdqlExample.createSomeExamples();
+        TableMeta[] tables =
+            TableSetSaxHandler
+           .readTableSet( TapTest.class
+                         .getResource( "gavo_tables.xml" ) );
+        TapCapability tcap =
+            TapCapability
+           .readTapCapability( TapTest.class
+                              .getResource( "gavo_capabilities.xml" ) );
+        showExamples( examples, tables, tcap );
+    }
+
+    private void showExamples( AdqlExample[] examples, TableMeta[] tables,
+                              TapCapability tcap ) {
+        String[] langs = tcap.getLanguages();
+        String lang = langs != null && langs.length > 0 ? langs[ 0 ] : null;
+        for ( int ie = 0; ie < examples.length; ie++ ) {
+            AdqlExample example = examples[ ie ];
+            String exTxt =
+                example.getText( true, lang, tcap, tables, tables[ 0 ] );
+            if ( exTxt != null ) {
+                System.out.println( example.getName() );
+                System.out.println( exTxt.replaceAll( "(?m)^", "   " ) );
+            }
+        }
+    }
+}
