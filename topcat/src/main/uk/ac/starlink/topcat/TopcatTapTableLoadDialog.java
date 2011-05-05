aa42aabdc5bbec0f26e519fe1cf749540504ab4b
Mark Taylor <m.b.taylor@bristol.ac.uk>
2011-05-05 10:36:12 +0000

    topcat: add configurable TAP job deletion behaviour (by menu)

--- topcat/src/main/uk/ac/starlink/topcat/TopcatTapTableLoadDialog.java
+++ topcat/src/main/uk/ac/starlink/topcat/TopcatTapTableLoadDialog.java
@@ -1,12 +1,25 @@
 package uk.ac.starlink.topcat;
 
 import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.ButtonGroup;
+import javax.swing.JMenu;
+import javax.swing.JRadioButtonMenuItem;
 import javax.swing.ListModel;
 import uk.ac.starlink.table.DescribedValue;
 import uk.ac.starlink.table.StarTable;
 import uk.ac.starlink.table.StarTableFactory;
+import uk.ac.starlink.table.StoragePolicy;
 import uk.ac.starlink.table.TableSequence;
+import uk.ac.starlink.table.Tables;
+import uk.ac.starlink.vo.TapQuery;
 import uk.ac.starlink.vo.TapTableLoadDialog;
 import uk.ac.starlink.vo.UwsJob;
 
@@ -18,6 +31,7 @@ import uk.ac.starlink.vo.UwsJob;
  */
 public class TopcatTapTableLoadDialog extends TapTableLoadDialog {
     private final RegistryDialogAdjuster adjuster_;
+    private volatile DeletionPolicy deletionPolicy_;
 
     public TopcatTapTableLoadDialog() {
         adjuster_ = new RegistryDialogAdjuster( this, "tap", false );
@@ -26,6 +40,32 @@ public class TopcatTapTableLoadDialog extends TapTableLoadDialog {
     public Component createQueryComponent() {
         Component comp = super.createQueryComponent();
         adjuster_.adjustComponent();
+
+        /* Add menu for configurable job deletion. */
+        List<JMenu> menuList =
+            new ArrayList<JMenu>( Arrays.asList( super.getMenus() ) );
+        JMenu delMenu = new JMenu( "Deletion" );
+        delMenu.setMnemonic( KeyEvent.VK_D );
+        ButtonGroup delButtGroup = new ButtonGroup();
+        DeletionPolicy[] delPolicies = DeletionPolicy.values();
+        for ( int i = 0; i < delPolicies.length; i++ ) {
+            final DeletionPolicy policy = delPolicies[ i ];
+            Action act = new AbstractAction( policy.name_ ) {
+                public void actionPerformed( ActionEvent evt ) {
+                    deletionPolicy_ = policy;
+                }
+            };
+            act.putValue( Action.SHORT_DESCRIPTION, policy.description_ );
+            JRadioButtonMenuItem menuItem = new JRadioButtonMenuItem( act );
+            delButtGroup.add( menuItem );
+            delMenu.add( menuItem );
+            if ( policy == DeletionPolicy.EXIT ) { // default
+                menuItem.doClick();
+            }
+        }
+        menuList.add( 0, delMenu );
+        setMenus( menuList.toArray( new JMenu[ 0 ] ) );
+
         return comp;
     }
 
@@ -89,15 +129,90 @@ public class TopcatTapTableLoadDialog extends TapTableLoadDialog {
         throw new IllegalArgumentException( sbuf.toString() );
     }
 
+    @Override
     protected TableSequence createTableSequence( StarTableFactory tfact,
-                                                 UwsJob tapJob,
+                                                 final UwsJob tapJob,
                                                  DescribedValue[] tapMetadata )
             throws IOException {
 
-        // Currently this just does the same as the superclass, which is
-        // effectively delete jobs on JVM exit (actually quite useful for
-        // TAP service debugging).  I may want to do something
-        // smarter than that.
-        return super.createTableSequence( tfact, tapJob, tapMetadata );
+        /* This isn't quite right - the deletionPolicy is polled at table
+         * creation time, but really it should be polled at job dispatch
+         * time.  They are likely to be close together though. */
+        DeletionPolicy deletionPolicy = deletionPolicy_;
+
+        StoragePolicy storage = tfact.getStoragePolicy();
+        if ( deletionPolicy.deleteOnExit_ ) {
+            tapJob.setDeleteOnExit( true );
+        }
+        tapJob.start();
+        try {
+
+            /* Note it's essential to make sure that the table has been
+             * read (is stored locally) before deleting the job, since
+             * otherwise subsequent attempts might be made to read it from
+             * its URL, which would no longer be there. */
+            StarTable table = TapQuery.waitForResult( tapJob, storage, 4000 );
+            if ( table != null ) {
+                table = storage.randomTable( table );
+            }
+            table.getParameters().addAll( Arrays.asList( tapMetadata ) );
+            return Tables.singleTableSequence( table );
+        }
+        catch ( InterruptedException e ) {
+            throw (IOException) new IOException( "Interrupted" ).initCause( e );
+        }
+        finally {
+            if ( deletionPolicy.deleteOnCompletion_ ) {
+                Thread delThread = new Thread( "Delete TAP job" ) {
+                    public void run() {
+                        tapJob.attemptDelete();
+                    }
+                };
+                delThread.setDaemon( true );
+                delThread.start();
+            }
+        }
+    }
+
+    /**
+     * Enumeration which codifies under what circumstances a UWS job should
+     * be deleted.
+     */
+    private static enum DeletionPolicy {
+
+        /** Delete when the job has finished. */
+        FINISHED( "On completion",
+                  "Delete jobs on completion, either successful or failed",
+                  true, false ),
+
+        /** Delete on application exit. */
+        EXIT( "On Exit", "Delete jobs on application exit", false, true ),
+
+        /** Delete never. */
+        NEVER( "Never", "Do not delete jobs", false, false );
+
+        private final String name_;
+        private final String description_;
+        private final boolean deleteOnCompletion_;
+        private final boolean deleteOnExit_;
+
+        /**
+         * Constructor.
+         *
+         * @param  name  name, used for action name
+         * @param  description  description, used for tooltip
+         * @param  deleteOnCompletion  true to attempt job deletion when it
+         *         has completed and its result table (if any) has been read
+         * @param  deleteOnExit  true to attempt job deletion at application
+         *         exit time
+         */
+        private DeletionPolicy( String name, String description,
+                                boolean deleteOnCompletion,
+                                boolean deleteOnExit ) {
+            name_ = name;
+            description_ = description;
+            deleteOnCompletion_ = deleteOnCompletion;
+            deleteOnExit_ = deleteOnExit;
+        }
     }
 }
