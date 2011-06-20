2aff3a931d0a712562e87143cbbba7f756f883eb
Mark Taylor <m.b.taylor@bristol.ac.uk>
2011-06-20 17:09:15 +0000

    ttools: add more queries to TAP test

--- ttools/src/main/uk/ac/starlink/ttools/taplint/QueryStage.java
+++ ttools/src/main/uk/ac/starlink/ttools/taplint/QueryStage.java
@@ -2,9 +2,12 @@ package uk.ac.starlink.ttools.taplint;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import uk.ac.starlink.table.ColumnInfo;
 import uk.ac.starlink.table.StarTable;
@@ -59,23 +62,6 @@ public abstract class QueryStage implements Stage {
     }
 
     /**
-     * Chooses one of a given set of tables to use for examples.
-     * Some attempt is made to use a representative table.
-     *
-     * @param  tmetas  table list
-     * @return  representative element of <code>tmetas</code>
-     */
-    private static TableMeta getExampleTable( TableMeta[] tmetas ) {
-        TableMeta[] sorted = (TableMeta[]) tmetas.clone();
-        Arrays.sort( sorted, new Comparator<TableMeta>() {
-            public int compare( TableMeta tm1, TableMeta tm2 ) {
-                return tm1.getColumns().length - tm2.getColumns().length;
-            }
-        } );
-        return sorted[ sorted.length / 2 ];
-    }
-
-    /**
      * Service method to create an instance of this class given some
      * previously-run stages which might have acquired table metadata.
      *
@@ -103,8 +89,9 @@ public abstract class QueryStage implements Stage {
     private class Querier implements Runnable {
         private final Reporter reporter_;
         private final URL serviceUrl_;
-        private final TableMeta[] tmetas_;
         private final TableMeta tmeta1_;
+        private final TableMeta tmeta2_;
+        private final TableMeta tmeta3_;
 
         /**
          * Constructor.
@@ -116,25 +103,54 @@ public abstract class QueryStage implements Stage {
         Querier( Reporter reporter, URL serviceUrl, TableMeta[] tmetas ) {
             reporter_ = reporter;
             serviceUrl_ = serviceUrl;
-            tmetas_ = tmetas;
-            tmeta1_ = getExampleTable( tmetas );
+
+            /* Pick some representative tables for later testing.
+             * Do it by choosing tables with column counts near the median. */
+            tmetas = (TableMeta[]) tmetas.clone();
+            Arrays.sort( tmetas, new Comparator<TableMeta>() {
+                public int compare( TableMeta tm1, TableMeta tm2 ) {
+                    return tm1.getColumns().length - tm2.getColumns().length;
+                }
+            } );
+            int imed = tmetas.length / 2;
+            tmeta1_ = tmetas[ imed ];
+            tmeta2_ = tmetas[ Math.min( imed + 1, tmetas.length - 1 ) ];
+            tmeta3_ = tmetas[ Math.min( imed + 2, tmetas.length - 1 ) ];
         }
 
         /**
          * Invoked to perform queries.
          */
         public void run() {
-            runAllColumns();
+            runAllColumns( tmeta1_ );
+            runSomeColumns( tmeta2_ );
+            runJustMeta( tmeta3_ );
+            // make sure GET and POST work
         }
 
         /**
          * Runs some tests using all the columns in an example table.
+         *
+         * @param  tmeta  table to test
          */
-        private void runAllColumns() {
+        private void runAllColumns( TableMeta tmeta ) {
             int nr0 = 10;
-            String name1 = tmeta1_.getName();
+            String name1 = tmeta.getName();
+
+            /* Prepare an array of column specifiers corresponding to all
+             * the columns in the table. */
+            ColumnMeta[] cmetas = tmeta.getColumns();
+            int ncol = cmetas.length;
+            ColSpec[] cspecs = new ColSpec[ ncol ];
+            for ( int ic = 0; ic < ncol; ic++ ) {
+                cspecs[ ic ] = new ColSpec( cmetas[ ic ] );
+            }
+
+            /* Check that MAXREC limit works. */
             StarTable t1 = runCheckedQuery( "SELECT * FROM " + name1, nr0,
-                                            tmeta1_.getColumns(), -1 );
+                                            cspecs, -1 );
+
+            /* Get a lower bound for the number of rows in the result. */
             long nr1 = t1 != null ? t1.getRowCount() : 0;
             assert nr1 >= 0;
             final boolean over;
@@ -147,9 +163,11 @@ public abstract class QueryStage implements Stage {
                 over = false;
                 nr2 = 3;
             }
+
+            /* Check that limiting using TOP works. */
             StarTable t2 =
                 runCheckedQuery( "SELECT TOP " + nr2 + " * FROM " + name1, -1,
-                                 tmeta1_.getColumns(), (int) nr2 );
+                                 cspecs, (int) nr2 );
             if ( t2 != null && over && ! tapRunner_.isOverflow( t2 ) ) {
                 String msg = new StringBuffer()
                    .append( "Overflow not marked - " )
@@ -161,15 +179,87 @@ public abstract class QueryStage implements Stage {
         }
 
         /**
+         * Runs some tests using a selection of the columns in an example
+         * table.
+         *
+         * @param  tmeta  table to run tests on
+         */
+        private void runSomeColumns( TableMeta tmeta ) {
+
+            /* Assemble column specifiers for the query. */
+            String talias = tmeta.getName().substring( 0, 1 );
+            List<ColSpec> clist = new ArrayList<ColSpec>();
+            int ncol = tmeta.getColumns().length;
+            int step = Math.max( 1, ncol / 11 );
+            int ix = 0;
+            for ( int icol = ncol - 1; icol >= 0; icol -= step ) {
+                ColSpec cspec = new ColSpec( tmeta.getColumns()[ icol ] );
+                if ( ix % 2 == 1 ) {
+                    cspec.setRename( "c_" + ( ix + 1 ) );
+                }
+                if ( ix % 3 == 2 ) {
+                    cspec.setQuoted( true );
+                }
+                if ( ix % 5 == 3 ) {
+                    cspec.setTableAlias( talias );
+                }
+                clist.add( cspec );
+                ix++;
+            }
+            ColSpec[] cspecs = clist.toArray( new ColSpec[ 0 ] );
+
+            /* Assemble the ADQL text. */
+            int nr = 8;
+            StringBuffer abuf = new StringBuffer();
+            abuf.append( "SELECT " )
+                .append( "TOP " )
+                .append( nr )
+                .append( " " );
+            for ( Iterator<ColSpec> colIt = clist.iterator();
+                  colIt.hasNext(); ) {
+                ColSpec col = colIt.next();
+                abuf.append( col.getQueryText() );
+                if ( colIt.hasNext() ) {
+                    abuf.append( "," );
+                }
+                abuf.append( " " );
+            }
+            abuf.append( " FROM " )
+                .append( tmeta.getName() )
+                .append( " AS " )
+                .append( talias );
+            String adql = abuf.toString();
+
+            /* Run the query. */
+            runCheckedQuery( adql, -1, cspecs, nr );
+        }
+
+        /**
+         * Test that a query with MAXREC=0 works (it should return metadata).
+         *
+         * @param  tmeta  table to run tests on
+         */
+        private void runJustMeta( TableMeta tmeta ) {
+            ColSpec cspec = new ColSpec( tmeta.getColumns()[ 0 ] );
+            String adql = new StringBuffer()
+                .append( "SELECT " )
+                .append( cspec.getQueryText() )
+                .append( " FROM " )
+                .append( tmeta.getName() )
+                .toString();
+            runCheckedQuery( adql, 0, new ColSpec[] { cspec }, -1 );
+        }
+
+        /**
          * Executes a TAP query and checks the results.
          *
          * @param  adql  query string
-         * @param  maxrec  value of MAXREC limit
-         * @param  colMetas  expected column metadata of result
+         * @param  maxrec  value of MAXREC limit; if negative none applied
+         * @param  colSpecs  expected column metadata of result
          * @param  maxrow  expected maximum row count, different from MAXREC
          */
         private StarTable runCheckedQuery( String adql, int maxrec,
-                                           ColumnMeta[] colMetas, int maxrow ) {
+                                           ColSpec[] colSpecs, int maxrow ) {
             Map<String,String> extraParams = new HashMap<String,String>();
             if ( maxrec >= 0 ) {
                 extraParams.put( "MAXREC", Integer.toString( maxrec ) );
@@ -208,7 +298,7 @@ public abstract class QueryStage implements Stage {
                        .toString();
                     reporter_.report( Reporter.Type.ERROR, "NROW", msg );
                 }
-                checkMeta( adql, colMetas, table );
+                checkMeta( adql, colSpecs, table );
             }
             return table;
         }
@@ -217,14 +307,14 @@ public abstract class QueryStage implements Stage {
          * Checks metadata from a TAP result matches what's expected.
          *
          * @param  adql  text of query
-         * @param  colMetas   metadata of expected columns in result
+         * @param  colSpecs   metadata of expected columns in result
          * @param  table  actual result table
          */
-        private void checkMeta( String adql, ColumnMeta[] colMetas,
+        private void checkMeta( String adql, ColSpec[] colSpecs,
                                 StarTable table ) {
 
             /* Check column counts match. */
-            int qCount = colMetas.length;
+            int qCount = colSpecs.length;
             int rCount = table.getColumnCount();
             if ( qCount != rCount ) {
                 String msg = new StringBuffer()
@@ -243,9 +333,9 @@ public abstract class QueryStage implements Stage {
 
             /* Check column names match. */
             for ( int ic = 0; ic < ncol; ic++ ) {
-                ColumnMeta cmeta = colMetas[ ic ];
+                ColSpec cspec = colSpecs[ ic ];
                 ColumnInfo cinfo = table.getColumnInfo( ic );
-                String qName = cmeta.getName();
+                String qName = cspec.getResultName();
                 String rName = cinfo.getName();
                 if ( ! qName.equalsIgnoreCase( rName ) ) {
                     String msg = new StringBuffer()
@@ -266,7 +356,7 @@ public abstract class QueryStage implements Stage {
                                 : "#" + ic;
 
                 /* Check column types match. */
-                String qType = cmeta.getDataType();
+                String qType = cspec.getColumnMeta().getDataType();
                 String rType =
                     (String) cinfo.getAuxDatumValue( VOStarTable.DATATYPE_INFO,
                                                      String.class );
@@ -286,6 +376,96 @@ public abstract class QueryStage implements Stage {
                     reporter_.report( Reporter.Type.ERROR, "CTYP", msg );
                 }
             }
+        }
+    }
+
+    /**
+     * Specifies a column for an ADQL query.
+     */
+    private static class ColSpec {
+        private final ColumnMeta cmeta_;
+        private String talias_;
+        private String rename_;
+        private boolean quote_;
+
+        /**
+         * Constructor.
+         *
+         * @param   cmeta  column metadata on which this queried column is based
+         */
+        ColSpec( ColumnMeta cmeta ) {
+            cmeta_ = cmeta;
+        }
+
+        /**
+         * Returns the column metadata object this column is based on.
+         *
+         * @return  col meta
+         */
+        ColumnMeta getColumnMeta() {
+            return cmeta_;
+        }
+
+        /**
+         * Sets whether the column name should be quoted in ADQL.
+         *
+         * @param  quote   true for quoting, false for not
+         */
+        public void setQuoted( boolean quote ) {
+            quote_ = quote;
+        }
+
+        /**
+         * Sets an optional table alias by which the table this column
+         * comes from is known.
+         *
+         * @param  talias  table alias, may be null
+         */
+        public void setTableAlias( String talias ) {
+            talias_ = talias;
+        }
+
+        /**
+         * Sets an optional changed name by which this column will be queried
+         * as.  If null, the name will be taken from the column metadata.
+         *
+         * @param  rename  column alias, or null
+         */
+        public void setRename( String rename ) {
+            rename_ = rename;
+        }
+
+        /**
+         * Returns the ADQL text for the column list in the SELECT statement
+         * corresponding to this column.
+         *
+         * @return  query column specifier
+         */
+        String getQueryText() {
+            StringBuffer sbuf = new StringBuffer();
+            String q = quote_ ? "\"" : "";
+            sbuf.append( q );
+            if ( talias_ != null ) {
+                sbuf.append( talias_ )
+                    .append( "." );
+            }
+            sbuf.append( cmeta_.getName() );
+            sbuf.append( q );
+            if ( rename_ != null ) {
+                sbuf.append( " AS " )
+                    .append( rename_ );
+            }
+            return sbuf.toString();
+        }
+
+        /**
+         * Returns the name of the column that should be returned from the
+         * query for this column.
+         *
+         * @return   result column name
+         */
+        String getResultName() {
+            return rename_ == null ? cmeta_.getName() : rename_;
         }
     }
 }
