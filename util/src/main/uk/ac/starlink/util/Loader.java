52a5bd1f5caba312583add89f481fad274259871
Mark Taylor <m.b.taylor@bristol.ac.uk>
2011-11-15 15:46:19 +0000

    util: downgrade warning about non-Sun JREs

--- util/src/main/uk/ac/starlink/util/Loader.java
+++ util/src/main/uk/ac/starlink/util/Loader.java
@@ -489,10 +489,11 @@ public class Loader {
     public static void checkJ2seVendor() {
         String vendorProp = "java.vendor";
         String vendor = System.getProperty( vendorProp );
-        String recommendedDownload = "http://sun.java.com/javase/downloads/";
+        String recommendedDownload = "http://java.sun.com/javase/downloads/";
         final Level vendorLevel;
         final String[] vendorMsgs;
         if ( vendor.matches( "[Ss]un\\b.*" ) ||
+             vendor.matches( "[Oo]racle\\b.*" ) ||
              vendor.matches( "[Aa]pple\\b.*" ) ) {
             vendorLevel = Level.CONFIG;
             vendorMsgs = new String[ 0 ];
@@ -500,17 +501,19 @@ public class Loader {
         else if ( vendor.matches( ".*Free Software Foundation.*" ) ||
                   vendor.matches( ".*\\bFSF\\b.*" ) ||
                   vendor.matches( ".*\\bGNU\\b.*" ) ) {
-            vendorLevel = Level.WARNING;
+            vendorLevel = Level.INFO;
             vendorMsgs = new String[] {
                 "You appear to be running GNU/FSF Java",
-                "At time of writing, this is an incomplete implementation",
+                "In at least some versions, "
+                + "this is an incomplete implementation",
                 "The application may work incorrectly, or not at all",
-                "You are advised to use Sun's Java implementation instead:",
+                "You are advised to use Sun/Oracle's Java implementation "
+                + "instead:",
                 recommendedDownload,
             };
         }
         else {
-            vendorLevel = Level.WARNING;
+            vendorLevel = Level.INFO;
             vendorMsgs = new String[] {
                 "JRE is not Sun's - may or may not work properly",
             };
