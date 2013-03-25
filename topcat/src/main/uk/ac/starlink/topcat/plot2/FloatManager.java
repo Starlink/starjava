3c087a5214200e94dedc7a0e6dd9e37f5d86c429
Mark Taylor <m.b.taylor@bristol.ac.uk>
2013-03-25 15:50:16 +0000

    topcat: add code to put floatable panel inside a JToolBar; but can't get it working

--- topcat/src/main/uk/ac/starlink/topcat/plot2/FloatManager.java
+++ topcat/src/main/uk/ac/starlink/topcat/plot2/FloatManager.java
@@ -1,17 +1,22 @@
 package uk.ac.starlink.topcat.plot2;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Dialog;
 import java.awt.Frame;
 import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.HierarchyEvent;
+import java.awt.event.HierarchyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JSplitPane;
+import javax.swing.JToolBar;
 import javax.swing.SwingUtilities;
+import javax.swing.plaf.basic.BasicToolBarUI;
 import uk.ac.starlink.topcat.ResourceIcon;
 import uk.ac.starlink.topcat.ToggleButtonModel;
 
@@ -23,6 +28,26 @@ import uk.ac.starlink.topcat.ToggleButtonModel;
  * @since    25 Mar 2013
  */
 public abstract class FloatManager {
+
+    private final JComponent container_;
+    private final JComponent fixedPanel_;
+    private final JComponent floatablePanel_;
+
+    /**
+     * Constructor.
+     *
+     * @param  container   containing panel which contains one or both of
+     *                     the others
+     * @param  fixedPanel  component which is always inside container
+     * @param  floatablePanel  component which may be inside container
+     *                         or may be floated out of it
+     */
+    protected FloatManager( JComponent container, JComponent fixedPanel,
+                            JComponent floatablePanel ) {
+        container_ = container;
+        fixedPanel_ = fixedPanel;
+        floatablePanel_ = floatablePanel;
+    }
 
     /**
      * Returns a toggle button model which can be used to control float status.
@@ -37,6 +62,37 @@ public abstract class FloatManager {
      * Called to initialise this manager when the components are populated.
      */
     public abstract void init();
+
+    /**
+     * Returns the floatable panel.
+     *
+     * @return  floatable panel
+     */
+    protected JComponent getFloatablePanel() {
+        return floatablePanel_;
+    }
+
+    /**
+     * Configures the container panel for floating or non-floating status.
+     * The floating panel itself is not affected.
+     *
+     * @param    floating  true for floating, false for not
+     */
+    protected void configureContainer( boolean floating ) {
+        container_.removeAll();
+        if ( floating ) {
+            container_.add( fixedPanel_ );
+        }
+        else {
+            JSplitPane splitter =
+                    new JSplitPane( JSplitPane.VERTICAL_SPLIT,
+                                    fixedPanel_, floatablePanel_ );
+            splitter.setResizeWeight( 0.75 );
+            splitter.setOneTouchExpandable( true );
+            container_.add( splitter, BorderLayout.CENTER );
+        }
+        container_.validate();
+    }
 
     /**
      * Returns an instance of this class.
@@ -60,7 +116,6 @@ public abstract class FloatManager {
      */
     private static class ActionFloatManager extends FloatManager {
         private final JComponent container_;
-        private final JComponent fixedPanel_;
         private final JComponent floatablePanel_;
         private final ToggleButtonModel floatModel_;
         private JDialog floater_;
@@ -75,8 +130,8 @@ public abstract class FloatManager {
          */
         ActionFloatManager( JComponent container, JComponent fixedPanel,
                             JComponent floatablePanel ) {
+            super( container, fixedPanel, floatablePanel );
             container_ = container;
-            fixedPanel_ = fixedPanel;
             floatablePanel_ = floatablePanel;
             floatModel_ =
                 new ToggleButtonModel( "Float Controls", ResourceIcon.FLOAT,
@@ -90,13 +145,13 @@ public abstract class FloatManager {
         }
 
         @Override
-        public void init() {
-            placeControls();
+        public ToggleButtonModel getFloatToggle() {
+            return floatModel_;
         }
 
         @Override
-        public ToggleButtonModel getFloatToggle() {
-            return floatModel_;
+        public void init() {
+            placeControls();
         }
 
         /**
@@ -106,14 +161,13 @@ public abstract class FloatManager {
          */       
         private void placeControls() {
             boolean external = floatModel_.isSelected();
-            container_.removeAll();
+            configureContainer( external );
             if ( floater_ != null ) {
                  floater_.getContentPane().removeAll();
                  floater_.dispose();
                 floater_ = null;
             }
             if ( external ) {
-                container_.add( fixedPanel_ );
 
                 /* This should possibly be a JFrame rather than a JDialog.
                  * If it was a JFrame it could go under its controlling window,
@@ -133,15 +187,66 @@ public abstract class FloatManager {
                     }
                 } );
             }
-            else {
-                JSplitPane splitter =
-                    new JSplitPane( JSplitPane.VERTICAL_SPLIT,
-                                    fixedPanel_, floatablePanel_ );
-                splitter.setResizeWeight( 0.75 );
-                splitter.setOneTouchExpandable( true );
-                container_.add( splitter, BorderLayout.CENTER );
-            }
-            container_.validate();
+        }
+    }
+
+    /**
+     * FloatManager which places the floatable component in a JToolBar.
+     * The point of this is that JToolBars have floatable grab-handles
+     * built into the GUI.
+     * This could count as abuse of JToolBar, since nothing else about
+     * this usage is toolbarish.
+     * 
+     * <p>This code is not quite working.  Maybe more effort could get it
+     * going.
+     *
+     * @deprecated  Not working
+     */
+    private static class ToolbarFloatManager extends FloatManager {
+
+        /**
+         * Constructor.
+         *
+         * @param  container   containing panel which contains one or both of
+         *                     the others
+         * @param  fixedPanel  component which is always inside container
+         * @param  floatablePanel  component which may be inside container
+         */
+        ToolbarFloatManager( JComponent container, JComponent fixedPanel,
+                             JComponent floatablePanel ) {
+            super( container, fixedPanel, new JToolBar() );
+            final JToolBar bar = (JToolBar) getFloatablePanel();
+            bar.setLayout( new BorderLayout() );
+            bar.setFloatable( true );
+            bar.removeAll();
+            bar.add( floatablePanel );
+            final BasicToolBarUI ui = (BasicToolBarUI) bar.getUI();
+            bar.addHierarchyListener( new HierarchyListener() {
+                boolean wasFloating;
+                public void hierarchyChanged( HierarchyEvent evt ) {
+                    if ( ( evt.getChangeFlags()
+                           & HierarchyEvent.PARENT_CHANGED ) != 0 ) {
+                        final boolean isFloating = ui.isFloating();
+                        if ( isFloating ) {
+                            setResizable( bar );
+                        }
+                        if ( isFloating ^ wasFloating ) {
+                            wasFloating = isFloating;
+                                    configureContainer( isFloating ); 
+                        }
+                    }
+                }
+            } );
+        }
+
+        @Override
+        public void init() {
+            configureContainer( false );
+        }
+
+        @Override
+        public ToggleButtonModel getFloatToggle() {
+            return null;
         }
     }
 
@@ -162,6 +267,21 @@ public abstract class FloatManager {
         }
         else {
             return new JDialog();
+        }
+    }
+
+    /**
+     * Sets the containing window of a given component.
+     *
+     * @param  comp   component
+     */
+    private static void setResizable( JComponent comp ) {
+        Window win = SwingUtilities.windowForComponent( comp );
+        if ( win instanceof Frame ) {
+            ((Frame) win).setResizable( true );
+        }
+        else if ( win instanceof Dialog ) {
+            ((Dialog) win).setResizable( true );
         }
     }
 }
