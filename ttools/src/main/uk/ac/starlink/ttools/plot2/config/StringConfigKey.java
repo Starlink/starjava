2695eb60e1f556600269f00254bb65eb7aea2f30
Mark Taylor <m.b.taylor@bristol.ac.uk>
2013-07-24 13:52:53 +0100

    ttools: refactor TextFieldSpecifier.
    Move it to a new class to be used by Double and String config keys,
    also available for use by other keys (like Time).

--- ttools/src/main/uk/ac/starlink/ttools/plot2/config/StringConfigKey.java
+++ ttools/src/main/uk/ac/starlink/ttools/plot2/config/StringConfigKey.java
@@ -1,8 +1,5 @@
 package uk.ac.starlink.ttools.plot2.config;
 
-import javax.swing.JComponent;
-import javax.swing.JTextField;
-
 /**
  * Config key for use with String values.
  *
@@ -30,26 +27,6 @@ public class StringConfigKey extends ConfigKey<String> {
     }
 
     public Specifier<String> createSpecifier() {
-        final String dflt = getDefaultValue();
-        return new SpecifierPanel<String>( true ) {
-            private final JTextField txtField_;
-            /** Constructor. */ {
-                txtField_ = new JTextField();
-                if ( dflt != null ) {
-                    txtField_.setText( dflt );
-                }
-                txtField_.addActionListener( getActionForwarder() );
-            }
-            protected JComponent createComponent() {
-                return txtField_;
-            }
-            public String getSpecifiedValue() {
-                return txtField_.getText();
-            }
-            public void setSpecifiedValue( String value ) {
-                txtField_.setText( value );
-                fireAction();
-            }
-        };
+        return new TextFieldSpecifier( this, null );
     }
 }
---
+++ ttools/src/main/uk/ac/starlink/ttools/plot2/config/TextFieldSpecifier.java
@@ -0,0 +1,59 @@
+package uk.ac.starlink.ttools.plot2.config;
+
+import javax.swing.JComponent;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+/**
+ * SpecifierPanel subclass that uses a text field for input.
+ *
+ * @author   Mark Taylor
+ * @since    17 Jul 2013
+ */
+public class TextFieldSpecifier<V> extends SpecifierPanel<V> {
+
+    private final ConfigKey<V> key_;
+    private final JTextField txtField_;
+    private V badValue_;
+
+    /**
+     * Constructor.
+     *
+     * @param  key  config key for which this panel will gather values
+     * @param  badValue  value to be returned if a ConfigException is
+     *                   encountered when parsing the entered text
+     */
+    public TextFieldSpecifier( ConfigKey<V> key, V badValue ) {
+        super( true );
+        key_ = key;
+        badValue_ = badValue;
+        txtField_ = new JTextField();
+        V dflt = key.getDefaultValue();
+        if ( dflt != null ) {
+            txtField_.setText( key.valueToString( dflt ) );
+        }
+    }
+
+    protected JComponent createComponent() {
+        txtField_.addActionListener( getActionForwarder() );
+        return txtField_;
+    }
+
+    public V getSpecifiedValue() {
+        try {
+            return key_.stringToValue( txtField_.getText() );
+        }
+        catch ( ConfigException e ) {
+            JOptionPane.showMessageDialog( txtField_, e.getMessage(),
+                                           "Bad Value",
+                                           JOptionPane.ERROR_MESSAGE );
+            txtField_.setText( "" );
+            return null;
+        }
+    }
+
+    public void setSpecifiedValue( V value ) {
+        txtField_.setText( key_.valueToString( value ) );
+        fireAction();
+    }
+}
