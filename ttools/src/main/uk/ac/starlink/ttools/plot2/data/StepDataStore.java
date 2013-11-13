9305ef3bab47a028906b89f268ad030708de1aeb
Mark Taylor <m.b.taylor@bristol.ac.uk>
2013-11-13 10:49:30 +0000

    topcat: more responsive large/slow plots
    When navigating (pan/zoom) round plots that take more than a significant
    fraction of a second to draw, a plot of an evenly spaced subsample is
    prepared during the navigation, and displayed at intermediate positions.
    This can give a somewhat jumpy appearance to the plot during navigation
    (the number of points changes according to whether you're within a
    navigation sequence or at rest), but it gives you better visual feedback
    overall, since you see frames more often.

---
+++ ttools/src/main/uk/ac/starlink/ttools/plot2/data/StepDataStore.java
@@ -0,0 +1,57 @@
+package uk.ac.starlink.ttools.plot2.data;
+
+/**
+ * Wrapper data store implementation that dispenses all column data
+ * as evenly spaced subsamples by row.
+ *
+ * @author   Mark Taylor
+ * @since    13 Nov 2013
+ */
+public class StepDataStore implements DataStore {
+
+    private final DataStore baseStore_;
+    private final int step_;
+
+    /**
+     * Constructor.
+     *
+     * @param   base  base data store to which most behaviour is delegated
+     * @param   step  stride indicating the size of the subsample;
+     *                step=1 means all rows, step=2 means every other one etc
+     *                
+     */
+    public StepDataStore( DataStore base, int step ) {
+        baseStore_ = base;
+        step_ = step;
+    }
+
+    public TupleSequence getTupleSequence( DataSpec spec ) {
+        final TupleSequence baseSeq = baseStore_.getTupleSequence( spec );
+        return new TupleSequence() {
+            public boolean next() {
+                for ( int i = 0; i < step_; i++ ) {
+                    if ( ! baseSeq.next() ) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            public long getRowIndex() {
+                return baseSeq.getRowIndex();
+            }
+            public Object getObjectValue( int icol ) {
+                return baseSeq.getObjectValue( icol );
+            }
+            public double getDoubleValue( int icol ) {
+                return baseSeq.getDoubleValue( icol );
+            }
+            public boolean getBooleanValue( int icol ) {
+                return baseSeq.getBooleanValue( icol );
+            }
+        };
+    }
+
+    public boolean hasData( DataSpec spec ) {
+        return baseStore_.hasData( spec );
+    }
+}
