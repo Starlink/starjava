079cadac45a365ac1fc03f21419c9a02093b11db
Mark Taylor <m.b.taylor@bristol.ac.uk>
2013-11-22 14:44:00 +0000

    topcat: refactor SimplePositionCoordPanel
    Use composition/wrapping rather than inheritance.
    This will make it easier to provide other options, like multi-position
    instances.

--- topcat/src/main/uk/ac/starlink/topcat/plot2/SimplePositionCoordPanel.java
+++ topcat/src/main/uk/ac/starlink/topcat/plot2/SimplePositionCoordPanel.java
@@ -1,6 +1,8 @@
 package uk.ac.starlink.topcat.plot2;
 
+import java.awt.event.ActionListener;
 import javax.swing.JComponent;
+import uk.ac.starlink.topcat.TopcatModel;
 import uk.ac.starlink.ttools.plot2.DataGeom;
 
 /**
@@ -10,19 +12,19 @@ import uk.ac.starlink.ttools.plot2.DataGeom;
  * @author   Mark Taylor
  * @since    13 Mar 2013
  */
-public class SimplePositionCoordPanel extends CoordPanel
-                                      implements PositionCoordPanel {
+public class SimplePositionCoordPanel implements PositionCoordPanel {
+
+    private final CoordPanel coordPanel_;
     private final DataGeom geom_;
 
     /**
      * Constructor.
      *
+     * @param  coordPanel  panel providing actual coordinate entry
      * @param  geom  fixed data geom
-     * @param  autoPopulate  whether to attempt to fill in non-blank
-     *                       data values automatically
      */
-    public SimplePositionCoordPanel( DataGeom geom, boolean autoPopulate ) {
-        super( geom.getPosCoords(), autoPopulate );
+    public SimplePositionCoordPanel( CoordPanel coordPanel, DataGeom geom ) {
+        coordPanel_ = coordPanel;
         geom_ = geom;
     }
 
@@ -31,6 +33,37 @@ public class SimplePositionCoordPanel extends CoordPanel
     }
 
     public JComponent getComponent() {
-        return this;
+        return coordPanel_;
+    }
+
+    public void setTable( TopcatModel tcModel ) {
+        coordPanel_.setTable( tcModel );
+    }
+
+    public GuiCoordContent[] getContents() {
+        return coordPanel_.getContents();
+    }
+
+    public void addActionListener( ActionListener listener ) {
+        coordPanel_.addActionListener( listener );
+    }
+
+    public void removeActionListener( ActionListener listener ) {
+        coordPanel_.removeActionListener( listener );
+    }
+
+    /**
+     * Constructs a position coord panel based on a given DataGeom.
+     * Each positional coordinate from the DataGeom is represented once.
+     *
+     * @param  geom   provides description of positional coordinates
+     * @param  autoPopulate  if true, some attempt will be made to
+     *                       fill in the fields with non-blank values
+     *                       when a table is selected
+     */
+    public static SimplePositionCoordPanel createPanel( DataGeom geom,
+                                                        boolean autoPopulate ) {
+        CoordPanel cpanel = new CoordPanel( geom.getPosCoords(), autoPopulate );
+        return new SimplePositionCoordPanel( cpanel, geom );
     }
 }
--- topcat/src/main/uk/ac/starlink/topcat/plot2/TimePlotWindow.java
+++ topcat/src/main/uk/ac/starlink/topcat/plot2/TimePlotWindow.java
@@ -37,9 +37,8 @@ public class TimePlotWindow
             return new TimeAxisControl( stack );
         }
         public PositionCoordPanel createPositionCoordPanel() {
-            return new SimplePositionCoordPanel( PLOT_TYPE
-                                                .getPointDataGeoms()[ 0 ],
-                                                 true );
+            return SimplePositionCoordPanel
+                  .createPanel( PLOT_TYPE.getPointDataGeoms()[ 0 ], true );
         }
     }
 }
