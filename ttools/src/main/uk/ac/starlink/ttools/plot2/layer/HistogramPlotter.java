4195ff490d5e475fafa51bf1248719660a7b7c2c
Mark Taylor <m.b.taylor@bristol.ac.uk>
2014-01-10 16:49:16 +0000

    ttools: add bar shape config options for histogram
    Add config options BarForm, line thickness and line dash.
    Non-default bar forms also require some additional work to plot with
    a flipped Y axis, since the classic plot BarForm class relies on the
    bars pointing upwards.

--- ttools/src/main/uk/ac/starlink/ttools/plot2/layer/HistogramPlotter.java
+++ ttools/src/main/uk/ac/starlink/ttools/plot2/layer/HistogramPlotter.java
@@ -2,10 +2,15 @@ package uk.ac.starlink.ttools.plot2.layer;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.geom.AffineTransform;
+import java.util.Arrays;
 import java.util.Map;
 import javax.swing.Icon;
 import uk.ac.starlink.ttools.gui.ResourceIcon;
+import uk.ac.starlink.ttools.plot.BarStyle;
 import uk.ac.starlink.ttools.plot.Range;
 import uk.ac.starlink.ttools.plot.Style;
 import uk.ac.starlink.ttools.plot2.AuxScale;
@@ -42,7 +47,8 @@ public class HistogramPlotter
     private final FloatingCoord xCoord_;
     private final SliceDataGeom histoDataGeom_;
 
-    private static final AuxScale COUNT_SCALE = new AuxScale( "Count" );
+    private static final ConfigKey<Integer> THICK_KEY =
+        StyleKeys.createThicknessKey( 2 );
 
     /**
      * Constructor.
@@ -79,14 +85,22 @@ public class HistogramPlotter
     public ConfigKey[] getStyleKeys() {
         return new ConfigKey[] {
             StyleKeys.COLOR,
+            StyleKeys.BAR_FORM,
+            THICK_KEY,
+            StyleKeys.DASH,
         };
     }
 
     public HistoStyle createStyle( ConfigMap config ) {
         Color color = config.get( StyleKeys.COLOR );
+        BarStyle.Form barForm = config.get( StyleKeys.BAR_FORM );
+        BarStyle.Placement placement = BarStyle.PLACE_OVER;
+        int thick = config.get( THICK_KEY );
+        float[] dash = config.get( StyleKeys.DASH );
         BinSizer sizer = BinSizer.createCountBinSizer( 20 );
         double binBase = 0;
-        return new HistoStyle( color, sizer, binBase );
+        return new HistoStyle( color, barForm, placement, thick, dash,
+                               sizer, binBase );
     }
 
     /**
@@ -118,6 +132,15 @@ public class HistogramPlotter
                     final double xlo = xlimits[ 0 ];
                     final double xhi = xlimits[ 1 ];
                     final double binWidth = sizer.getWidth( xlog, xlo, xhi );
+
+                    /* We can't work out what other histogram data sets are
+                     * being plotted or where this one fits in - that level
+                     * of inter-layer communication is not provided by the
+                     * plotting framework.  Would take some effort to add it.
+                     * For now, just treat each histogram layer as if it's
+                     * the only one. */
+                    final int iseq = 0;
+                    final int nseq = 1;
                     return new Drawing() {
                         public Object calculatePlan( Object[] knownPlans,
                                                      DataStore dataStore ) {
@@ -142,7 +165,8 @@ public class HistogramPlotter
                             final BinBag binBag = hPlan.binBag_;
                             paperType.placeDecal( paper, new Decal() {
                                 public void paintDecal( Graphics g ) {
-                                    paintBins( pSurf, binBag, style, g );
+                                    paintBins( pSurf, binBag, style,
+                                               iseq, nseq, g );
                                 }
                                 public boolean isOpaque() {
                                     return isOpaque;
@@ -227,58 +251,134 @@ public class HistogramPlotter
      * @param  surface  plot surface
      * @param  binBag   calculated histogram data
      * @param  style    style
+     * @param  iseq     index of this histogram in the plot
+     * @param  nseq     total number of histograms in the plot
      * @param  g        graphics context
      */
     private void paintBins( PlaneSurface surface, BinBag binBag,
-                            HistoStyle style, Graphics g ) {
+                            HistoStyle style, int iseq, int nseq, Graphics g ) {
         Color color0 = g.getColor();
         g.setColor( style.color_ );
+        BarStyle barStyle = style.barStyle_;
+        Rectangle clip = surface.getPlotBounds();
+        int xClipMin = clip.x - 64;
+        int xClipMax = clip.x + clip.width + 64;
+        int yClipMin = clip.y - 64;
+        int yClipMax = clip.y + clip.height + 64;
         double[][] dataLimits = surface.getDataLimits();
-        double dxlo = dataLimits[ 0 ][ 0 ];
-        double dxhi = dataLimits[ 0 ][ 1 ];
-        double dylo = dataLimits[ 1 ][ 0 ];
-        double dyhi = dataLimits[ 1 ][ 1 ];
+        double dxMin = dataLimits[ 0 ][ 0 ];
+        double dxMax = dataLimits[ 0 ][ 1 ];
+        double dyMin = dataLimits[ 1 ][ 0 ];
+        double dyMax = dataLimits[ 1 ][ 1 ];
+        boolean[] flipFlags = surface.getFlipFlags();
+        final boolean xflip = flipFlags[ 0 ];
+        final boolean yflip = flipFlags[ 1 ];
         Point p0 = new Point();
         Point p1 = new Point();
-        for ( BinBag.Bin bin : binBag.getBins() ) {
-            double xmin = bin.getXMin();
-            double xmax = bin.getXMax();
-            double y = bin.getY();
-            if ( xmin <= dxhi && xmax >= dxlo ) {
-                double ymin = 0;
-                double ymax = 0;
-                if ( y > 0 ) {
-                    ymax = y;
-                }
-                else if ( y < 0 ) {
-                    ymin = y;
-                }
-                ymin = Math.max( ymin, dylo );
-                ymax = Math.min( ymax, dyhi );
-                if ( ! ( ymin == ymax ) &&
-                     surface.dataToGraphics( new double[] { xmin, ymin },
-                                             false, p0 ) &&
-                     surface.dataToGraphics( new double[] { xmax, ymax },
-                                             false, p1 ) ) {
-                    int rx = Math.min( p0.x, p1.x );
-                    int ry = Math.min( p0.y, p1.y );
-                    int rw = Math.abs( p1.x - p0.x );
-                    int rh = Math.abs( p1.y - p0.y );
+        double[] dpos0 = new double[ 2 ];
+        double[] dpos1 = new double[ 2 ];
+        int lastGx1 = xflip ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+        int lastGy1 = 0;
+        int commonGy0 = 0;
 
-                    /* Ensure that there is at least one pixel for non-zero
-                     * data extents, rather than rounding down to zero. */
-                    if ( rw == 0 ) {
-                        rw += 1;
+        /* Iterate over bins, plotting each one individually. */
+        for ( BinBag.Bin bin : binBag.getBins() ) {
+
+            /* Get bin data. */
+            double dxlo = bin.getXMin();
+            double dxhi = bin.getXMax();
+            double dy = bin.getY();
+
+            /* Only plot those bins that fall at least partly in the X range. */
+            if ( dxlo <= dxMax && dxhi >= dxMin && dy != 0 ) {
+
+                 /* Transform the corners of each bar to graphics coords. */
+                 dpos0[ 0 ] = dxlo;
+                 dpos0[ 1 ] = 0;
+                 dpos1[ 0 ] = dxhi;
+                 dpos1[ 1 ] = dy;
+                 if ( surface.dataToGraphics( dpos0, false, p0 ) &&
+                      surface.dataToGraphics( dpos1, false, p1 ) ) {
+
+                    /* Clip them so they are not too far off the plot region;
+                     * attempting to draw ridiculously large rectangles can
+                     * give AWT a headache. */
+                    int gx0 = clip( p0.x, xClipMin, xClipMax );
+                    int gx1 = clip( p1.x, xClipMin, xClipMax );
+                    int gy0 = clip( p0.y, yClipMin, yClipMax );
+                    int gy1 = clip( p1.y, yClipMin, yClipMax );
+
+                    /* Draw the trailing edge of the previous bar if
+                     * necessary. */
+                    if ( lastGx1 != gx0 ) {
+                        barStyle.drawEdge( g, lastGx1, lastGy1, gy0,
+                                           iseq, nseq );
+                        lastGy1 = gy0;
                     }
-                    if ( rh == 0 ) {
-                        ry -= 1;
-                        rh += 1;
-                    }
-                    g.fillRect( rx, ry, rw, rh );
+
+                    /* Draw the leading edge of the current bar. */
+                    barStyle.drawEdge( g, gx0, lastGy1, gy1, iseq, nseq );
+                    lastGx1 = gx1;
+                    lastGy1 = gy1;
+                    commonGy0 = gy0;
+
+                    /* Draw the bar. */
+                    int gxlo = xflip ? gx1 : gx0;
+                    int gxhi = xflip ? gx0 : gx1;
+                    drawGeneralBar( barStyle, g, gxlo, gxhi, gy0, gy1,
+                                    iseq, nseq );
                 }
             }
         }
+
+        /* Draw the trailing edge of the final bar. */
+        barStyle.drawEdge( g, lastGx1, lastGy1, commonGy0, iseq, nseq );
         g.setColor( color0 );
+    }
+
+    /**
+     * Clips a value to a given range.
+     *
+     * @param  p  input value
+     * @param  lo  minimum acceptable value
+     * @param  hi  maximum acceptable value
+     * @return   input value clipped to given limits
+     */
+    private static int clip( int p, int lo, int hi ) {
+        return Math.max( Math.min( p, hi ), lo );
+    }
+
+    /**
+     * Draws a bar which may point upwards or downwards in a histogram.
+     *
+     * @param   g  graphics context
+     * @param   xlo  lower bound in X direction
+     * @param   xhi  upper bound in X direction
+     * @param   y0   Y coordinate of the base of the bar
+     * @param   y1   Y coordinate of the data level of the bar
+     * @param   iseq  index of the set being plotted
+     * @param   nseq  number of sets being plotted for this bar
+     */
+    private void drawGeneralBar( BarStyle barStyle, Graphics g,
+                                 int xlo, int xhi, int y0, int y1,
+                                 int iseq, int nseq ) {
+
+        /* The work is done by the BarStyle.drawBar method.
+         * However, that method can only draw bars with their base
+         * facing down and the data level facing up.  If we need to
+         * do it the other way round (Y axis inverted) do it by
+         * applying a coordinate transformation to the graphics context. */
+        if ( y0 >= y1 ) {
+            barStyle.drawBar( g, xlo, xhi, y1, y0, iseq, nseq );
+        }
+        else {
+            Graphics2D g2 = (Graphics2D) g;
+            AffineTransform trans0 = g2.getTransform();
+            g2.translate( 0, y0 + y1 );
+            g2.scale( 1, -1 );
+            barStyle.drawBar( g, xlo, xhi, y0, y1, iseq, nseq );
+            g2.setTransform( trans0 );
+        }
     }
 
     /**
@@ -286,30 +386,54 @@ public class HistogramPlotter
      */
     public static class HistoStyle implements Style {
         private final Color color_;
+        private final BarStyle.Form barForm_;
+        private final BarStyle.Placement placement_;
+        private final int thick_;
+        private final float[] dash_;
         private final BinSizer sizer_;
         private final double base_;
+
+        private final BarStyle barStyle_;
 
         /**
          * Constructor.
          *
          * @param  color   bar colour
+         * @param  barForm  bar form
+         * @param  placement  bar placement
+         * @param  thick   line thickness (only relevant for some forms)
+         * @param  dash    line dash pattern (only relevant for some forms)
          * @param  sizer   determines bin widths
          * @param  base    bin reference point
          */
-        public HistoStyle( Color color, BinSizer sizer, double base ) {
+        public HistoStyle( Color color, BarStyle.Form barForm,
+                           BarStyle.Placement placement,
+                           int thick, float[] dash,
+                           BinSizer sizer, double base ) {
             color_ = color;
+            barForm_ = barForm;
+            placement_ = placement;
+            thick_ = thick;
+            dash_ = dash;
             sizer_ = sizer;
             base_ = base;
+            barStyle_ = new BarStyle( color, barForm, placement );
+            barStyle_.setLineWidth( thick );
+            barStyle_.setDash( dash );
         }
 
         public Icon getLegendIcon() {
-            return ResourceIcon.PLOT_HISTO;
+            return barStyle_;
         }
 
         @Override
         public int hashCode() {
             int code = 55012;
             code = 23 * code + color_.hashCode();
+            code = 23 * code + barForm_.hashCode();
+            code = 23 * code + placement_.hashCode();
+            code = 23 * code + thick_;
+            code = 23 * code + Arrays.hashCode( dash_ );
             code = 23 * code + sizer_.hashCode();
             code = 23 * code + Float.floatToIntBits( (float) base_ );
             return code;
@@ -320,6 +444,10 @@ public class HistogramPlotter
             if ( o instanceof HistoStyle ) {
                 HistoStyle other = (HistoStyle) o;
                 return this.color_.equals( other.color_ )
+                    && this.barForm_.equals( other.barForm_ )
+                    && this.placement_.equals( other.placement_ )
+                    && this.thick_ == other.thick_
+                    && Arrays.equals( this.dash_, other.dash_ )
                     && this.sizer_.equals( other.sizer_ )
                     && this.base_ == other.base_;
             }
