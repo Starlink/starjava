e1a5300b50dae147c2f6cc3452d950308f8b572f
Mark Taylor <m.b.taylor@bristol.ac.uk>
2014-01-10 17:48:30 +0000

    ttools: improve options for histogram bin width sizing
    Use round numbers for bin widths, and allow explicit setting of fixed
    bin width sizes.

--- ttools/src/main/uk/ac/starlink/ttools/plot2/layer/BinSizer.java
+++ ttools/src/main/uk/ac/starlink/ttools/plot2/layer/BinSizer.java
@@ -1,5 +1,6 @@
 package uk.ac.starlink.ttools.plot2.layer;
 
+import uk.ac.starlink.ttools.plot.Rounder;
 import uk.ac.starlink.ttools.plot2.Equality;
 
 /**
@@ -23,47 +24,112 @@ public abstract class BinSizer {
     public abstract double getWidth( boolean xlog, double xlo, double xhi );
 
     /**
-     * Returns a bin sizer instance which divides the axis range up into
-     * a fixed number of equal intervals.
+     * Returns a bin sizer instance which always returns the same fixed
+     * value.
      *
-     * @param   nbin   number of intervals to divide the axis into
-     * @return  bin sizer instance
+     * @param  binWidth  fixed bin width
+     * @return  bin sizer
      */
-    public static BinSizer createCountBinSizer( int nbin ) {
-        return new CountBinSizer( nbin );
+    public static BinSizer createFixedBinSizer( double binWidth ) {
+        return new FixedBinSizer( binWidth );
     }
 
     /**
-     * Primitive BinSizer implementation that just chops the data range
-     * up into a fixed number of equal intervals.
+     * Returns a bin sizer instance which divides the axis range up into
+     * a fixed number of equal intervals.  If the rounding flag is true,
+     * the number is approximate, and bin widths returned are round numbers.
+     *
+     * @param   nbin   number of intervals to divide the axis into
+     * @param   rounding   if true, only round numbers are returned
+     * @return  bin sizer instance
+     */
+    public static BinSizer createCountBinSizer( int nbin, boolean rounding ) {
+        return new CountBinSizer( nbin, rounding );
+    }
+
+    /**
+     * BinSizer implementation that always returns a fixed value.
+     */
+    private static class FixedBinSizer extends BinSizer {
+        private final double binWidth_;
+
+        /**
+         * Constructor.
+         *
+         * @param  binWidth  fixed bin width
+         */
+        FixedBinSizer( double binWidth ) {
+            binWidth_ = binWidth;
+        }
+
+        public double getWidth( boolean xlog, double xlo, double xhi ) {
+            return binWidth_;
+        }
+
+        @Override
+        public int hashCode() {
+            return Float.floatToIntBits( (float) binWidth_ );
+        }
+
+        @Override
+        public boolean equals( Object o ) {
+            if ( o instanceof FixedBinSizer ) {
+                FixedBinSizer other = (FixedBinSizer) o;
+                return this.binWidth_ == other.binWidth_;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+
+    /**
+     * BinSizer implementation that chops the data range
+     * up into a fixed number of equal intervals, with optional rounding
+     * of the resulting width.
      */
     private static class CountBinSizer extends BinSizer {
         private final int nbin_;
+        private final boolean rounding_;
 
         /**
          * Constructor.
          *
          * @param  nbin  number of intervals
+         * @param  rounding  true to round the output bin widths to sensible
+         *                   values
          */
-        CountBinSizer( int nbin ) {
+        CountBinSizer( int nbin, boolean rounding ) {
             nbin_ = nbin;
+            rounding_ = rounding;
         }
 
         public double getWidth( boolean xlog, double xlo, double xhi ) {
-            return xlog ? Math.exp( Math.log( xhi / xlo ) / nbin_ )
-                        : ( xhi - xlo ) / nbin_;
+            double width0 = xlog ? Math.exp( Math.log( xhi / xlo ) / nbin_ )
+                                 : ( xhi - xlo ) / nbin_;
+            if ( rounding_ ) {
+                Rounder rounder = xlog ? Rounder.LOG : Rounder.LINEAR;
+                return rounder.round( width0 );
+            }
+            else {
+                return width0;
+            }
         }
 
         @Override
         public int hashCode() {
-            return nbin_ * 44301;
+            int code = 44301;
+            code = 23 * code + nbin_;
+            code = 23 * code + ( rounding_ ? 5 : 7 );
+            return code;
         }
 
         @Override
         public boolean equals( Object o ) {
             if ( o instanceof CountBinSizer ) {
                 CountBinSizer other = (CountBinSizer) o;
-                return this.nbin_ == other.nbin_;
+                return this.nbin_ == other.nbin_
+                    && this.rounding_ == other.rounding_;
             }
             else {
                 return false;
