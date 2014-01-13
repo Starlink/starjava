837ad849d91bc641d686402fbc2e79480300e836
Mark Taylor <m.b.taylor@bristol.ac.uk>
2014-01-13 12:49:45 +0000

    ttools: move SliderSpecifier out of DoubleConfigKey class

--- ttools/src/main/uk/ac/starlink/ttools/plot2/config/DoubleConfigKey.java
+++ ttools/src/main/uk/ac/starlink/ttools/plot2/config/DoubleConfigKey.java
@@ -3,7 +3,6 @@ package uk.ac.starlink.ttools.plot2.config;
 import javax.swing.JCheckBox;
 import javax.swing.JComponent;
 import javax.swing.JOptionPane;
-import javax.swing.JSlider;
 
 /**
  * Config key for double precision values.
@@ -158,71 +157,6 @@ public abstract class DoubleConfigKey extends ConfigKey<Double> {
                 checkBox_.setSelected( true );
             }
             fireAction();
-        }
-    }
-
-    /**
-     * Specifier that uses a slider to choose a value in the range
-     * between two given values.
-     */
-    private static class SliderSpecifier extends SpecifierPanel<Double> {
-        private final double lo_;
-        private final double hi_;
-        private final boolean log_;
-        private final JSlider slider_;
-        private static final int MIN = 0;
-        private static final int MAX = 10000;
-
-        /**
-         * Constructor.
-         *
-         * @param   lo   slider lower bound
-         * @param   hi   slider upper bound
-         * @param  log  true for logarithmic slider scale, false for linear
-         */
-        SliderSpecifier( double lo, double hi, boolean log ) {
-            super( true );
-            lo_ = lo;
-            hi_ = hi;
-            log_ = log;
-            slider_ = new JSlider( MIN, MAX );
-        }
-
-        protected JComponent createComponent() {
-            slider_.addChangeListener( getChangeForwarder() );
-            return slider_;
-        }
-
-        public Double getSpecifiedValue() {
-            return scale( slider_.getValue() );
-        }
-
-        public void setSpecifiedValue( Double dval ) {
-            slider_.setValue( unscale( dval.doubleValue() ) );
-        }
-
-        /**
-         * Turns a slider value into a specified value.
-         *
-         * @param  ival  slider position
-         * @return  specifier value
-         */
-        private double scale( int ival ) {
-            double f = ( ival - MIN ) / (double) ( MAX - MIN );
-            return log_ ? lo_ * Math.pow( hi_ / lo_, f )
-                        : lo_ + ( hi_ - lo_ ) * f;
-        }
-
-        /**
-         * Turns a specified value into a slider value.
-         *
-         * @param  dval  specifier value
-         * @return  slider position
-         */
-        private int unscale( double dval ) {
-            double s = log_ ? Math.log( dval / lo_ ) / Math.log( hi_ / lo_ )
-                            : ( dval - lo_ ) / ( hi_ - lo_ );
-            return (int) Math.round( s * ( MAX - MIN ) ) + MIN;
         }
     }
 }
