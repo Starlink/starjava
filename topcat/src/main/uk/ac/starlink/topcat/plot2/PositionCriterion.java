27434d5f0dd5daaa00fc5600fd2fabe5325c25ac
Mark Taylor <m.b.taylor@bristol.ac.uk>
2014-01-29 10:13:57 +0000

    topcat: move StackPlotWindow.PositionIncluder to PositionCriterion
    Refactor to an external class to be more tidy and for re-use of
    implementations.

---
+++ topcat/src/main/uk/ac/starlink/topcat/plot2/PositionCriterion.java
@@ -0,0 +1,95 @@
+package uk.ac.starlink.topcat.plot2;
+
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.Shape;
+import uk.ac.starlink.ttools.plot2.Surface;
+
+/**
+ * Defines a true/false test on a data position.
+ *
+ * @author   Mark Taylor
+ * @since    29 Jan 2014
+ */
+public abstract class PositionCriterion {
+
+    /**
+     * Evaluates the test for a data position.
+     *
+     * @param  dpos  data space coordinate array
+     * @return  true iff the given position fits this criterion
+     */
+    public abstract boolean isIncluded( double[] dpos );
+
+    /**
+     * Returns a criterion to test whether point positions fall within
+     * the plot bounds of a given surface.
+     *
+     * @param  surface   plot surface
+     * @return  new criterion
+     */
+    public static PositionCriterion
+            createBoundsCriterion( final Surface surface ) {
+        return new PositionCriterion() {
+            final Point gp = new Point();
+            public boolean isIncluded( double[] dpos ) {
+                return surface.dataToGraphics( dpos, true, gp );
+            }
+        };
+    }
+
+    /**
+     * Returns a criterion to test whether partial positions fall within
+     * the plot bounds of a given surface.  These partial positions are
+     * things like histogram data, which have an X but not a Y graphics
+     * coordinate.  For this case, either X or Y coordinate within the
+     * plot bounds counts as success.
+     *
+     * @param  surface   plot surface
+     * @return  new criterion
+     */
+    public static PositionCriterion
+            createPartialBoundsCriterion( final Surface surface ) {
+        Rectangle plotBounds = surface.getPlotBounds();
+        final int gxlo = plotBounds.x;
+        final int gxhi = plotBounds.x + plotBounds.width;
+        final int gylo = plotBounds.y;
+        final int gyhi = plotBounds.y + plotBounds.height;
+        return new PositionCriterion() {
+            final Point gp = new Point();
+            public boolean isIncluded( double[] dpos ) {
+                return surface.dataToGraphics( dpos, false, gp )
+                    && ( ( gxlo < gp.x && gp.x < gxhi ) ||
+                         ( gylo < gp.y && gp.y < gyhi ) );
+            }
+        };
+    }
+
+    /**
+     * Returns a criterion to test whether point positions fall within a given
+     * shape in graphics coordinates.
+     *
+     * @param  surface   plot surface
+     * @param  blob   test shape in graphics coordinates
+     * @return  new criterion
+     */
+    public static PositionCriterion
+            createBlobCriterion( final Surface surface, final Shape blob ) {
+
+        /* Test for inclusion within the shape bounding box before testing
+         * the shape itself.  This does not change the result, it's an
+         * optimisation; for a complicated shape the first test should
+         * be fast and may throw out a high proportion of points.
+         * I haven't tested to see whether this actually does speed things up, 
+         * but at least I can't see it slowing things down significantly. */
+        final Rectangle blobBounds = blob.getBounds();
+        return new PositionCriterion() {
+            final Point gp = new Point();
+            public boolean isIncluded( double[] dpos ) {
+                return surface.dataToGraphics( dpos, true, gp )
+                    && blobBounds.contains( gp )
+                    && blob.contains( gp );
+            }
+        };
+    }
+}
