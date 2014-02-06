ec4faa4f1441e0fbcd3ab228afe8d6fc37254c17
Mark Taylor <m.b.taylor@bristol.ac.uk>
2014-02-06 10:12:10 +0000

    ttools: add 2MASS PSC raw ASCII file input handler to examples
    The examples sub-package now contains all the classes required to
    read data from the DVD/FTP version of the 2MASS Point Source Catalogue.
    Previously the PostgresAsciiStarTable class was in the formats
    subpackage, but the additional information to actually use it was
    not in the repository.  This can act as a template for other custom
    large text file readers.

---
+++ ttools/src/testcases/uk/ac/starlink/ttools/example/TwomassTest.java
@@ -0,0 +1,28 @@
+package uk.ac.starlink.ttools.example;
+
+import java.io.IOException;
+import java.net.URL;
+import junit.framework.TestCase;
+import uk.ac.starlink.table.StarTable;
+import uk.ac.starlink.table.StoragePolicy;
+import uk.ac.starlink.table.Tables;
+import uk.ac.starlink.util.URLDataSource;
+
+public class TwomassTest extends TestCase {
+
+    public void testRead() throws IOException {
+        URL url = TwomassTest.class.getResource( "psc_aaa.tail10.gz" );
+        StoragePolicy storage = StoragePolicy.PREFER_MEMORY;
+        StarTable table =
+            new TwoMassPscTableBuilder()
+           .makeStarTable( new URLDataSource( url ), true, storage );
+        assertTrue( ! table.isRandom() );
+        table = storage.copyTable( table );
+        assertEquals( 10, table.getRowCount() );
+        assertEquals( 60, table.getColumnCount() );
+        assertEquals( "ra", table.getColumnInfo( 0 ).getName() );
+        assertEquals( "coadd", table.getColumnInfo( 59 ).getName() );
+        assertEquals( new Short( (short) 127 ), table.getCell( 0, 59 ) );
+        assertEquals( new Short( (short) 115 ), table.getCell( 9, 59 ) );
+    }
+}
