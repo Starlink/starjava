e748c0b9d629cdc44a2ddb072c6247699c4cf3db
Mark Taylor <m.b.taylor@bristol.ac.uk>
2014-06-12 10:34:02 +0100

    task: allow null value for IntegerParameter
    The return value of intValue is now Integer not int, but that's
    transparent at the source code level because of java 1.5 auto-unboxing.

--- task/src/main/uk/ac/starlink/task/IntegerParameter.java
+++ task/src/main/uk/ac/starlink/task/IntegerParameter.java
@@ -9,7 +9,7 @@ public class IntegerParameter extends Parameter {
     private boolean odd;
     private int min = Integer.MIN_VALUE;
     private int max = Integer.MAX_VALUE;
-    private int intval;
+    private Integer intval;
 
     public IntegerParameter( String name ) {
         super( name );
@@ -49,37 +49,46 @@ public class IntegerParameter extends Parameter {
     }
 
     /**
-     * Returns the value of this parameter as an <tt>int</tt>.
+     * Returns the value of this parameter as an <tt>Integer</tt>.
+     * The result may be null only if this parameter has nullPermitted.
      *
      * @param  env  execution environment
      * @return   integer value
      */
-    public int intValue( Environment env ) throws TaskException {
+    public Integer intValue( Environment env ) throws TaskException {
         checkGotValue( env );
         return intval;
     }
 
     public void setValueFromString( Environment env, String stringval ) 
             throws TaskException {
-        try {
-            intval = Integer.parseInt( stringval );
+        if ( isNullPermitted() && 
+             ( stringval == null || stringval.trim().length() == 0 ) ) {
+            intval = null;
         }
-        catch ( NumberFormatException e ) {
-            throw new ParameterValueException( this, e.getMessage() );
-        }
-        if ( odd && intval % 2 == 0 ) {
-            throw new ParameterValueException( this, intval + " is not odd" );
-        }
-        if ( even && intval % 2 == 1 ) {
-            throw new ParameterValueException( this, intval + " is not even" );
-        }
-        if ( intval < min ) {
-            throw new ParameterValueException( this, intval 
-                                             + " < minimum value " + min );
-        }
-        if ( intval > max ) {
-            throw new ParameterValueException( this, intval
-                                             + " < maximum value " + max );
+        else {
+            try {
+                intval = Integer.parseInt( stringval );
+            }
+            catch ( NumberFormatException e ) {
+                throw new ParameterValueException( this, e.getMessage() );
+            }
+            if ( odd && intval % 2 == 0 ) {
+                throw new ParameterValueException( this,
+                                                   intval + " is not odd" );
+            }
+            if ( even && intval % 2 == 1 ) {
+                throw new ParameterValueException( this,
+                                                   intval + " is not even" );
+            }
+            if ( intval < min ) {
+                throw new ParameterValueException( this, intval 
+                                                 + " < minimum value " + min );
+            }
+            if ( intval > max ) {
+                throw new ParameterValueException( this, intval
+                                                 + " < maximum value " + max );
+            }
         }
         super.setValueFromString( env, stringval );
     }
