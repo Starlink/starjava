103070afd92d56aef1dd4fd947f15a440381c49b
Mark Taylor <m.b.taylor@bristol.ac.uk>
2014-06-17 12:59:21 +0100

    topcat: deprecate multicone in favour of CDS X-Match
    The topcat multicone toolbar button is replaced by the CDS X-Match one.
    
    The stilts coneskymatch task now issues a deprecation warning on execution.

--- ttools/src/main/uk/ac/starlink/ttools/task/MultiCone.java
+++ ttools/src/main/uk/ac/starlink/ttools/task/MultiCone.java
@@ -1,5 +1,9 @@
 package uk.ac.starlink.ttools.task;
 
+import java.util.logging.Logger;
+import uk.ac.starlink.task.Environment;
+import uk.ac.starlink.task.Executable;
+import uk.ac.starlink.task.TaskException;
 import uk.ac.starlink.ttools.cone.ConeSearchConer;
 import uk.ac.starlink.ttools.cone.ParallelResultRowSequence;
 import uk.ac.starlink.ttools.cone.SkyConeMatch2;
@@ -11,9 +15,18 @@ import uk.ac.starlink.ttools.cone.SkyConeMatch2;
  * @since    4 Jul 2006
  */
 public class MultiCone extends SkyConeMatch2 {
+    private static final Logger logger_ =
+        Logger.getLogger( "uk.ac.starlink.ttools.task" );
     public MultiCone() {
         super( "Crossmatches table on sky position against remote cone service",
                new ConeSearchConer(), 
                ParallelResultRowSequence.getMaxParallelism() );
+    }
+    @Override
+    public Executable createExecutable( Environment env ) throws TaskException {
+        logger_.warning( "Multi-Cone is somewhat deprecated in favour of "
+                       + "TAP or CDS X-Match" );
+        logger_.warning( "There is probably a much faster way to do this" );
+        return super.createExecutable( env );
     }
 }
