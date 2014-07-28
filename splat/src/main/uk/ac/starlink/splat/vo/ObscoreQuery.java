6ee16c6ee547a5cf12aae99e9958c14948a6efeb
Margarida Castro Neves <mcneves@ari.uni-heidelberg.de>
2014-07-28 10:50:03 +0200

    ObsCore support for SPLAT

---
+++ splat/src/main/uk/ac/starlink/splat/vo/ObscoreQuery.java
@@ -0,0 +1,18 @@
+package uk.ac.starlink.splat.vo;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Map;
+
+import uk.ac.starlink.table.StarTable;
+import uk.ac.starlink.vo.TapQuery;
+
+public class ObscoreQuery extends TapQuery 
+{
+
+    public ObscoreQuery(URL arg0, String arg1, Map<String, String> arg2,
+            Map<String, StarTable> arg3, long arg4) throws IOException {
+        super(arg0, arg1, arg2, arg3, arg4);
+    }
+
+}
