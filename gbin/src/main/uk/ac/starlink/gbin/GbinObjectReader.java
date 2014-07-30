f2e803fbc4f852e22f35f8b4d8bb4a6d4c044136
Mark Taylor <m.b.taylor@bristol.ac.uk>
2014-07-30 23:02:59 +0100

    gbin: new package GBIN for Gaia internal data
    The GBIN package attempts to read GBIN files, which are the
    java-serialised-object based data files used internally within
    DPAC for Gaia data storage.
    
    For avoidance of confusion: this is Gaia the astrometry satellite,
    not Gaia the image analysis tool.

---
+++ gbin/src/main/uk/ac/starlink/gbin/GbinObjectReader.java
@@ -0,0 +1,202 @@
+package uk.ac.starlink.gbin;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class GbinObjectReader {
+
+    private final Object gbinReaderObj_;
+    private final Method hasNextMethod_;
+    private final Method nextMethod_;
+    private final Method getCountMethod_;
+    private static final Object[] ARGS0 = new Object[ 0 ];
+    private static boolean isGaiaToolsInit_;
+    private static final Logger logger_ =
+        Logger.getLogger( "uk.ac.starlink.gbin" );
+
+    public GbinObjectReader( Object gbinReaderObj ) {
+        gbinReaderObj_ = gbinReaderObj;
+        Class clazz = gbinReaderObj.getClass();
+        hasNextMethod_ = getNoArgMethod( clazz, "hasNext", boolean.class );
+        nextMethod_ = getNoArgMethod( clazz, "next", null );
+        getCountMethod_ = getNoArgMethod( clazz, "getCount", long.class );
+    }
+
+    public boolean hasNext() throws IOException {
+        return Boolean.TRUE.equals( invoke( hasNextMethod_ ) );
+    }
+
+    public Object next() throws IOException {
+        return invoke( nextMethod_ );
+    }
+
+    public long getCount() {
+        try {
+            return ((Number) invoke( getCountMethod_ )).longValue();
+        }
+        catch ( Exception e ) {
+            return -1;
+        }
+    }
+
+    private Object invoke( Method method ) throws IOException {
+        try {
+            return method.invoke( gbinReaderObj_, ARGS0 );
+        }
+        catch ( IllegalAccessException e ) {
+            throw new AssertionError( e );
+        }
+        catch ( IllegalArgumentException e ) {
+            throw new AssertionError( e );
+        }
+        catch ( InvocationTargetException e ) {
+            Throwable targetEx = e.getTargetException();
+            if ( targetEx instanceof ClassNotFoundException ) {
+                throw (IOException)
+                      new IOException( "Missing GBIN object class"
+                                     + " - probably lacking DM jars" )
+                     .initCause( e );
+            }
+            else {
+                throw (IOException)
+                      new IOException( targetEx.getMessage() )
+                     .initCause( e );
+            }
+        }
+    }
+
+    private static Method getNoArgMethod( Class<?> clazz, String name,
+                                          Class<?> retClazz ) {
+        Method method;
+        try {
+            method = clazz.getMethod( name, new Class[ 0 ] );
+            int mods = method.getModifiers();
+            if ( Modifier.isStatic( mods ) ||
+                 ! Modifier.isPublic( mods ) ||
+                 ( retClazz != null &&
+                   ! retClazz.equals( method.getReturnType() ) ) ) {
+                method = null;
+            }
+        }
+        catch ( NoSuchMethodException e ) {
+            method = null;
+        }
+        if ( method == null ) {
+            throw new IllegalArgumentException( "Object of class "
+                                              + clazz.getName()
+                                              + " is not a GbinReader"
+                                              + " (no suitable " + name + "()"
+                                              + " method)" );
+        }
+        return method;
+    }
+
+    public static GbinObjectReader createReader( InputStream in )
+            throws IOException {
+        initGaiaTools();
+        return new GbinObjectReader( createGbinReaderObject( in ) );
+    }
+
+    public static boolean isMagic( byte[] intro ) {
+        if ( intro.length < 13 ) {
+            return false;
+        }
+        long magic = ((intro[ 7 ] & 0xffL) << 56)
+                   | ((intro[ 6 ] & 0xffL) << 48)
+                   | ((intro[ 5 ] & 0xffL) << 40)
+                   | ((intro[ 4 ] & 0xffL) << 32)
+                   | ((intro[ 3 ] & 0xffL) << 24)
+                   | ((intro[ 2 ] & 0xffL) << 16)
+                   | ((intro[ 1 ] & 0xffL) <<  8)
+                   | ((intro[ 0 ] & 0xffL) <<  0);
+
+        /* Pre-V3 GBIN file - no magic number, but we can see if it's
+         * a java serialization stream. */
+        if ( ( magic & 0xffff ) == 0xedac ) {
+            int jserVers = ((intro[ 2 ] & 0xff) << 8)
+                         | ((intro[ 3 ] & 0xff) << 0);
+            logger_.info( "Java serialised stream, may be GBIN v2 file"
+                        + " (java serialization version " + jserVers + ")" );
+            return true;
+        }
+
+        /* V3 or later GBIN file - see GAIA-C1-TN-ESAC-AH-004-1. */
+        else if ( magic == 0x1a0a0d4e49424789L ) {
+            int gbinVers = ((intro[  9 ] & 0xff) << 24)
+                         | ((intro[ 10 ] & 0xff) << 16)
+                         | ((intro[ 11 ] & 0xff) <<  8)
+                         | ((intro[ 12 ] & 0xff) <<  0);
+            logger_.info( "Post-v2 GBIN file identified"
+                        + " (GBIN version " + gbinVers + ")" );
+            return true;
+        }
+
+        /* No known GBIN. */
+        return false;
+    }
+
+    /**
+     * Called by createReader.
+     */
+    public static synchronized void initGaiaTools() {
+        if ( ! isGaiaToolsInit_ ) {
+            String loaderClassName = "gaia.cu1.tools.util.props.PropertyLoader";
+            String loaderMethodName = "load";
+            String sig = loaderClassName + "." + loaderMethodName + "()";
+            logger_.info( "Invoking " + sig );
+            try {
+                Class.forName( loaderClassName )
+                     .getMethod( loaderMethodName, new Class[ 0 ] )
+                     .invoke( null, new Object[ 0 ] );
+            }
+            catch ( Throwable e ) {
+                logger_.log( Level.WARNING, "Failed to invoke " + sig, e );
+            }
+            isGaiaToolsInit_ = true;
+        }
+    }
+
+    private static Object createGbinReaderObject( InputStream in )
+            throws IOException {
+        try {
+            Class factClazz =
+                Class.forName( "gaia.cu1.tools.dal.gbin.GbinFactory" );
+            Method getReaderMethod =
+                factClazz.getMethod( "getGbinReader",
+                                     new Class[] { InputStream.class } );
+            try {
+                return getReaderMethod.invoke( null, in );
+            }
+            catch ( IllegalArgumentException e ) {
+                throw (IOException)
+                      new IOException( "Problem with GaiaTools classes" )
+                     .initCause( e );
+            }
+        }
+        catch ( ClassNotFoundException e ) {
+            throw (IOException)
+                  new IOException( "GaiaTools classes not available?" )
+                 .initCause( e );
+        }
+        catch ( NoSuchMethodException e ) {
+            throw (IOException)
+                  new IOException( "Problem with GaiaTools classes" )
+                 .initCause( e );
+        }
+        catch ( IllegalAccessException e ) {
+            throw (IOException)
+                  new IOException( "Problem with GaiaTools classes" )
+                 .initCause( e );
+        } 
+        catch ( InvocationTargetException e ) {
+            throw (IOException)
+                  new IOException( e.getTargetException().getMessage() )
+                 .initCause( e );
+        } 
+    }
+}
---
+++ gbin/src/main/uk/ac/starlink/gbin/GbinTableReader.java
@@ -0,0 +1,227 @@
+package uk.ac.starlink.gbin;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import uk.ac.starlink.table.ColumnInfo;
+import uk.ac.starlink.table.RowSequence;
+
+public class GbinTableReader implements RowSequence {
+
+    private final InputStream in_;
+    private final GbinObjectReader reader_;
+    private final Object gobj0_;
+    private final int ncol_;
+    private final long nrow_;
+    private final ItemReader[] itemReaders_;
+    private final Map<ItemReader,Object> itemMap_;
+    private final ColumnInfo[] colInfos_;
+    private boolean started_;
+
+    private static final String NAME_SEP = "_";
+    private static final Map<Class,Class> primitiveMap_ = createPrimitiveMap();
+    private static final Set<String> boringGetters_ = createBoringGetters();
+
+    public GbinTableReader( InputStream in ) throws IOException {
+        in_ = in;
+        reader_ = GbinObjectReader.createReader( in );
+        if ( ! reader_.hasNext() ) {
+            throw new IOException( "No objects in GBIN file" );
+        }
+        gobj0_ = reader_.next();
+        itemReaders_ = createItemReaders( gobj0_.getClass() );
+        ncol_ = itemReaders_.length;
+        colInfos_ = new ColumnInfo[ ncol_ ];
+        for ( int ic = 0; ic < ncol_; ic++ ) {
+            ItemReader irdr = itemReaders_[ ic ];
+            colInfos_[ ic ] =
+                new ColumnInfo( irdr.getItemName(), irdr.getItemContentClass(),
+                                null );
+        }
+        itemMap_ = new HashMap<ItemReader,Object>();
+        nrow_ = reader_.getCount();
+    }
+
+    public int getColumnCount() {
+        return colInfos_.length;
+    }
+
+    public ColumnInfo getColumnInfo( int icol ) {
+        return colInfos_[ icol ];
+    }
+
+    public long getRowCount() {
+        return nrow_;
+    }
+
+    public boolean next() throws IOException {
+        itemMap_.clear();
+        if ( started_ ) {
+            if ( reader_.hasNext() ) {
+                itemMap_.put( ItemReader.ROOT, reader_.next() );
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            started_ = true;
+            itemMap_.put( ItemReader.ROOT, gobj0_ );
+            return true;
+        }
+    }
+
+    public Object getCell( int icol ) throws IOException {
+        return itemReaders_[ icol ].readItem( itemMap_ );
+    }
+
+    public Object[] getRow() throws IOException {
+        Object[] row = new Object[ ncol_ ];
+        for ( int ic = 0; ic < ncol_; ic++ ) {
+            row[ ic ] = itemReaders_[ ic ].readItem( itemMap_ );
+        }
+        return row;
+    }
+
+    public void close() throws IOException {
+        in_.close();
+    }
+
+    private static ItemReader[] createItemReaders( Class rootClazz ) {
+        List<ItemReader> rdrList = new ArrayList<ItemReader>();
+        addItemReaders( rootClazz, ItemReader.ROOT, rdrList );
+        return rdrList.toArray( new ItemReader[ 0 ] );
+    }
+
+    private static void addItemReaders( Class parentClazz, ItemReader parentRdr,
+                                        List<ItemReader> rdrList ) {
+        Method[] methods = parentClazz.getMethods();
+        for ( int i = 0; i < methods.length; i++ ) {
+            Method method = methods[ i ];
+            if ( isGetterMethod( method ) ) {
+                ItemReader rdr = new ItemReader( parentRdr, method );
+                Class clazz = rdr.getItemContentClass();
+                if ( isColumnType( clazz ) ) {
+                    rdrList.add( rdr );
+                }
+                else {
+                    addItemReaders( clazz, rdr, rdrList );
+                }
+            }
+        }
+    }
+
+    private static boolean isGetterMethod( Method method ) {
+        String name = method.getName();
+        int mods = method.getModifiers();
+        return name.startsWith( "get" )
+            && ! boringGetters_.contains( name )
+            && Modifier.isPublic( mods )
+            && ! Modifier.isStatic( mods )
+            && method.getParameterTypes().length == 0;
+    }
+
+    private static boolean isColumnType( Class clazz ) {
+        return primitiveMap_.values().contains( clazz )
+            || clazz.equals( String.class )
+
+    // Arrays of non-primitive objects are not decomposed into primitive
+    // values.  Hard to see how they could be, but it means you may end
+    // up with column content types that are arrays of non-primitive
+    // objects, which most STIL output handlers will not cope well with.
+            || clazz.getComponentType() != null;
+    }
+
+    private static Map<Class,Class> createPrimitiveMap() {
+        Map<Class,Class> map = new LinkedHashMap<Class,Class>();
+        map.put( boolean.class, Boolean.class );
+        map.put( char.class, Character.class );
+        map.put( byte.class, Byte.class );
+        map.put( short.class, Short.class );
+        map.put( int.class, Integer.class );
+        map.put( long.class, Long.class );
+        map.put( float.class, Float.class );
+        map.put( double.class, Double.class );
+        return map;
+    }
+
+    private static Set<String> createBoringGetters() {
+        return new HashSet<String>( Arrays.asList( new String[] {
+            "getClass",
+            "getField",
+            "getStringValue",
+            "getGTDescription",
+            "getParamMaxValues",
+            "getParamMinValues",
+            "getParamOutOfRangeValues",
+        } ) );
+    }
+
+    private static class ItemReader {
+
+        private final ItemReader parentReader_;
+        private final Method method_;
+        public static final ItemReader ROOT = new ItemReader( null, null );
+
+        public ItemReader( ItemReader parentReader, Method method ) {
+            parentReader_ = parentReader;
+            method_ = method;
+        }
+        public String getItemName() {
+            StringBuffer sbuf = new StringBuffer();
+            if ( parentReader_ != ROOT ) {
+                sbuf.append( parentReader_.getItemName() )
+                    .append( NAME_SEP );
+            }
+            sbuf.append( method_.getName().substring( 3 ) );
+            return sbuf.toString();
+        }
+        public Class getItemContentClass() {
+            Class clazz = method_.getReturnType();
+            return primitiveMap_.containsKey( clazz )
+                 ? primitiveMap_.get( clazz )
+                 : clazz;
+        }
+        public Object readItem( Map<ItemReader,Object> itemMap )
+                throws IOException {
+            if ( ! itemMap.containsKey( this ) ) {
+                Object parentItem = parentReader_.readItem( itemMap );
+                Object item = parentItem == null
+                            ? null
+                            : invokeMethod( parentItem );
+                itemMap.put( this, item );
+            }
+            assert itemMap.containsKey( this );
+            return itemMap.get( this );
+        }
+        private Object invokeMethod( Object parentItem ) throws IOException {
+            try {
+                return method_.invoke( parentItem );
+            }
+            catch ( IllegalAccessException e ) {
+                throw (IOException) new IOException( "Reflection trouble" )
+                                   .initCause( e );
+            }
+            catch ( IllegalArgumentException e ) {
+                throw (IOException) new IOException( "Reflection trouble" )
+                                   .initCause( e );
+            }
+            catch ( InvocationTargetException e ) {
+                throw (IOException)
+                      new IOException( e.getTargetException().getMessage() )
+                     .initCause( e );
+            }
+        }
+    }
+}
