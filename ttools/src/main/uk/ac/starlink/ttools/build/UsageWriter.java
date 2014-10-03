126ca4709d2f5f3cfb0346e431949bc427e9f827
Mark Taylor <m.b.taylor@bristol.ac.uk>
2014-10-03 15:18:57 +0100

    ttools: don't reference javadocs for private classes
    Parameter usage messages have hrefs to javadocs for parameter value types.
    If those classes are private, the links are broken (not to mention useless).
    Just write them as plain text (also useless, but less misleading, and
    doesn't give build errors).

--- ttools/src/main/uk/ac/starlink/ttools/build/UsageWriter.java
+++ ttools/src/main/uk/ac/starlink/ttools/build/UsageWriter.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.lang.reflect.Modifier;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.logging.Level;
@@ -154,8 +155,11 @@ public class UsageWriter {
         else {
             docset = null;
         }
+        boolean isPublic = Modifier.isPublic( clazz.getModifiers() );
         StringBuffer sbuf = new StringBuffer();
-        if ( pkgName.length() == 0 || pkgName.startsWith( "java.lang" ) ) {
+        if ( pkgName.length() == 0 ||
+             pkgName.startsWith( "java.lang" ) ||
+             ! isPublic ) {
             sbuf.append( unqName )
                 .append( arraySuffix );
         }
