dbe2c7d1a460754ce02f875caf3a73d87aa6750f
Mark Taylor <m.b.taylor@bristol.ac.uk>
2014-11-07 12:33:59 +0000

    ttools: modify Coverage user functions
    Add new function nearMoc, required to find out if a position is
    within a given radius of a MOC, often more useful than whether
    it strictly falls within it.  Also change the way that MOC names
    are interpreted, so you can use a VizieR table ID as an alternative
    to MOC FITS file URL/filename.

--- ttools/src/main/uk/ac/starlink/ttools/func/Coverage.java
+++ ttools/src/main/uk/ac/starlink/ttools/func/Coverage.java
@@ -5,30 +5,47 @@
 
 package uk.ac.starlink.ttools.func;
 
-import cds.moc.HealpixImpl;
-import cds.moc.HealpixMoc;
-import java.io.InputStream;
+import java.io.IOException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
 import java.util.logging.Logger;
-import uk.ac.starlink.ttools.cone.PixtoolsHealpix;
-import uk.ac.starlink.util.DataSource;
+import uk.ac.starlink.ttools.cone.Coverage.Amount;
+import uk.ac.starlink.ttools.cone.MocCoverage;
+import uk.ac.starlink.ttools.cone.UrlMocCoverage;
+import uk.ac.starlink.util.URLUtils;
 
 /**
  * Functions related to coverage and footprints.
- * One coverage standard is Multi-Order Coverage maps, described at
+ *
+ * <p>One coverage standard is Multi-Order Coverage maps, described at
  * <a href="http://www.ivoa.net/Documents/MOC/"
  *         >http://www.ivoa.net/Documents/MOC/</a>.
+ * MOC positions are always defined in ICRS equatorial coordinates.
+ *
+ * <p>MOC locations may be given as either the filename or the URL of
+ * a MOC FITS file.  Alternatively, they may be the identifier of a VizieR
+ * table, for instance "<code>V/139/sdss9</code>" (SDSS DR9).
+ * A list of all the MOCs available from VizieR can
+ * currently be found at
+ * <a href="http://alasky.u-strasbg.fr/footprints/tables/vizier/"
+ *         >http://alasky.u-strasbg.fr/footprints/tables/vizier/</a>.
+ * You can search for VizieR table identifiers from the
+ * VizieR web page
+ * (<a href="http://vizier.u-strasbg.fr/"
+ *          >http://vizier.u-strasbg.fr/</a>);
+ * note you must use
+ * the <em>table</em> identifier (like "<code>V/139/sdss9</code>")
+ * and not the <em>catalogue</em> identifier (like "<code>V/139</code>").
  *
  * @author   Mark Taylor
  * @since    29 May 2012
  */
 public class Coverage {
 
-    private static final Map<String,HealpixMoc> mocMap_ =
-        new HashMap<String,HealpixMoc>();
-    private static final HealpixImpl healpix_ = PixtoolsHealpix.getInstance();
-    private static final HealpixMoc EMPTY_MOC = new HealpixMoc();
+    private static final Map<String,MocCoverage> mocMap_ =
+        new HashMap<String,MocCoverage>();
     private static final Logger logger_ =
         Logger.getLogger( "uk.ac.starlink.ttools.func" );
 
@@ -39,65 +56,107 @@ public class Coverage {
     }
 
     /**
-     * Indicates whether a given sky position is within a given MOC
+     * Indicates whether a given sky position falls strictly within a given MOC
      * (Multi-Order Coverage map).
      * If the given <code>mocLocation</code> value does not represent
      * a MOC (for instance no file exists or the file is not in MOC format)
      * a warning will be issued the first time it's referenced, and
      * the result will be false.
      *
-     * @param  mocLocation  location of a FITS MOC file, either as a filename
-     *                      or a URL
-     * @param  ra     right ascension in degrees
-     * @param  dec    declination in degrees
+     * @param  mocLocation  location of a FITS MOC file:
+     *                      a filename, a URL, or a VizieR table name
+     * @param  ra     ICRS right ascension in degrees
+     * @param  dec    ICRS declination in degrees
      * @return   true iff the given position falls within the given MOC
      */
     public static boolean inMoc( String mocLocation, double ra, double dec ) {
-        HealpixMoc moc = getMoc( mocLocation );
-        try {
-            return moc.contains( healpix_, ra, dec );
-        }
-        catch ( Exception e ) {
-            return false;
-        }
+        return nearMoc( mocLocation, ra, dec, 0 );
     }
 
     /**
-     * Returns a (possibly cached) MOC object for a given location.
+     * Indicates whether a given sky position either falls within,
+     * or is within a certain distance of the edge of,
+     * a given MOC (Multi-Order Coverage map).
+     * If the given <code>mocLocation</code> value does not represent
+     * a MOC (for instance no file exists or the file is not in MOC format)
+     * a warning will be issued the first time it's referenced, and
+     * the result will be false.
+     *
+     * @param  mocLocation  location of a FITS MOC file:
+     *                      a filename, a URL, or a VizieR table name
+     * @param  ra     ICRS right ascension in degrees
+     * @param  dec    ICRS declination in degrees
+     * @param  distanceDeg   permitted distance from MOC boundary in degrees
+     * @return   true iff the given position is within <code>distance</code>
+     *           degrees of the given MOC
+     */
+    public static boolean nearMoc( String mocLocation, double ra, double dec,
+                                   double distanceDeg ) {
+        MocCoverage cov = getMocCoverage( mocLocation );
+
+        /* Note we fail to false not true here in case of no data.
+         * It's documented like that, so we have to.  This is
+         * (probably) a good idea since people will realise the
+         * coverage isn't working if they get no results,
+         * but probably wouldn't know they'd got the location wrong
+         * otherwise - clients of this class are expected to be mostly
+         * humans, not machines. */
+        return cov == null || ( cov.getAmount() == Amount.NO_DATA )
+             ? false
+             : cov.discOverlaps( ra, dec, distanceDeg );
+    }
+
+    /**
+     * Returns a (possibly cached) coverage object for a given location.
+     * Any coverage object returned is ready for use (initialised),
+     * but the return value may be null if no data is available.
      *
      * @param  loc  MOC FITS file location - filename or URL
-     * @return  MOC, not null
+     * @return  initialised coverage object, may be null if not known
      */
-    private static HealpixMoc getMoc( String loc ) {
-        HealpixMoc moc = mocMap_.get( loc );
-        if ( moc == null ) {
-            moc = readFitsMoc( loc );
-            mocMap_.put( loc, moc );
+    private static MocCoverage getMocCoverage( String loc ) {
+        if ( ! mocMap_.containsKey( loc ) ) {
+            mocMap_.put( loc, createMocCoverage( loc ) );
         }
-        return moc;
+        return mocMap_.get( loc );
     }
 
     /**
-     * Reads a MOC from a FITS file at the given location.
-     * If the read fails for whatever reason, a message is issued through
-     * the logging system, and an empty MOC is returned.
+     * Creates a MOC from a string, which may be either a VizieR table ID
+     * or the URL or filename of a MOC file.
      *
-     * @param   loc  MOC FITS file location - filename or URL
-     * @return  MOC, not null
+     * @param   loc  MOC location
+     * @return  initialised coverage object, or null
      */
-    private static HealpixMoc readFitsMoc( String loc ) {
-        HealpixMoc moc = new HealpixMoc();
+    private static MocCoverage createMocCoverage( String loc ) {
         try {
-            InputStream in = DataSource.getInputStream( loc );
-            moc.readFits( in );
-            in.close();
-            logger_.info( "Loaded MOC at " + loc );
-            return moc;
+            URL url = URLUtils.makeURL( loc );
+            MocCoverage cov = new UrlMocCoverage( url );
+            cov.initCoverage();
+            if ( cov.getAmount() != Amount.NO_DATA ) {
+                return cov;
+            }
+            else {
+                logger_.config( "No MOC at location: " + loc );
+            }
         }
         catch ( Exception e ) {
-            logger_.warning( "No MOC at " + loc
-                           + " (" + e.toString() + ")" );
-            return EMPTY_MOC;
+            logger_.log( Level.INFO, "No MOC at location: " + loc, e );
         }
+        try {
+            MocCoverage cov = UrlMocCoverage.getVizierMoc( loc, -1 );
+            cov.initCoverage();
+            if ( cov.getAmount() != Amount.NO_DATA ) {
+                return cov;
+            }
+            else {
+                logger_.config( "No VizieR MOC: " + loc );
+            }
+        }
+        catch ( IOException e ) {
+            logger_.log( Level.INFO, "No VizieR MOC: " + loc, e );
+        }
+        logger_.warning( "Unknown MOC: " + loc + " - assume no coverage" );
+        return null;
     }
 }
