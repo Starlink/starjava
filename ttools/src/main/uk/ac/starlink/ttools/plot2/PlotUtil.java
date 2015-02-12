Starlink/starjava

commit c51d802157b4fe51f867fb52b36fef45ab58874a

ttools: treat logs of negative values correctly in plots
Because I currently treat graphics coordinates as integer values in plot2,
graphics positions corresponding to logs of negative values were getting
calculated as NaNs and then turned into integer zero values by casting
NaN to int.  Although most such NaN coordinates were thrown out before
plotting, some, such as error bars terminating below zero on logarithmic
axes, were getting shown (since in this case Surface.dataToGraphics is
called with visibleOnly false), in obviously anomalous places.

Fix the code to check for NaNs before int casting is done.

This leaves the somewhat related problem that negative sense error bars
that would extend to negative values will not be displayed if the
relevant axis is logarithmic.  Probably it would be better to extend
them down infinitely.  This doesn't look very easy to fix; wait until
somebody complains till I worry about it.

topcat/src/docs/sun253.xml
@@ -20474,6 +20474,9 @@ introduced since the last version:
     <ul>
     <li>Fix some problems to do with zooming to very large/small
         plot axis ranges.</li>
+    <li>Hide error bars (etc) that would extend to negative values
+        on logarithmic axes; previously they were being drawn in anomalous
+        places.</li>
     </ul>
     </p></dd>
 

ttools/src/main/uk/ac/starlink/ttools/plot2/geom/CubeSurface.java
@@ -193,6 +193,9 @@ public class CubeSurface implements Surface {
         double sx = normalise( dataPos, 0 );
         double sy = normalise( dataPos, 1 );
         double sz = normalise( dataPos, 2 );
+        if ( Double.isNaN( sx ) || Double.isNaN( sy ) || Double.isNaN( sz ) ) {
+            return false;
+        }
         assert ( ! knownInCube )
                || ( isNormal( sx ) && isNormal( sy ) && isNormal( sz ) )
              : "(" + sx + ", " + sy + ", " + sz + ")";

ttools/src/main/uk/ac/starlink/ttools/plot2/geom/PlaneSurface.java
@@ -121,16 +121,23 @@ public class PlaneSurface implements Surface {
         if ( dpos == null ) {
             return false;
         }
-        int gx = xAxis_.dataToGraphics( dpos[ 0 ] );
-        int gy = yAxis_.dataToGraphics( dpos[ 1 ] );
-        if ( visibleOnly &&
-             ( gx < gxlo_ || gx >= gxhi_ || gy < gylo_ || gy >= gyhi_ ) ) {
+        double dx = xAxis_.dataToGraphics( dpos[ 0 ] );
+        double dy = yAxis_.dataToGraphics( dpos[ 1 ] );
+        if ( Double.isNaN( dx ) || Double.isNaN( dy ) ) {
             return false;
         }
         else {
-            gp.x = gx;
-            gp.y = gy;
-            return true;
+            int gx = (int) dx;
+            int gy = (int) dy;
+            if ( visibleOnly &&
+                 ( gx < gxlo_ || gx >= gxhi_ || gy < gylo_ || gy >= gyhi_ ) ) {
+                return false;
+            }
+            else {
+                gp.x = gx;
+                gp.y = gy;
+                return true;
+            }
         }
     }
 
@@ -162,14 +169,14 @@ public class PlaneSurface implements Surface {
             for ( int it = 0; it < xticks_.length; it++ ) {
                 Tick tick = xticks_[ it ];
                 if ( tick.getLabel() != null ) {
-                    int gx = xAxis_.dataToGraphics( tick.getValue() );
+                    int gx = (int) xAxis_.dataToGraphics( tick.getValue() );
                     g.drawLine( gx, gylo_, gx, gyhi_ );
                 }
             }
             for ( int it = 0; it < yticks_.length; it++ ) {
                 Tick tick = yticks_[ it ];
                 if ( tick.getLabel() != null ) {
-                    int gy = yAxis_.dataToGraphics( tick.getValue() );
+                    int gy = (int) yAxis_.dataToGraphics( tick.getValue() );
                     g.drawLine( gxlo_, gy, gxhi_, gy );
                 }
             }
@@ -480,7 +487,7 @@ public class PlaneSurface implements Surface {
      */
     public static String formatPosition( Axis axis, double dpos ) {
 
-        /* This could be implemented bettter.
+        /* This could be implemented better.
          * It would be better if the precision determination, and hence
          * the number of digits before and after the decimal point and
          * in the exponent, were determined by the axis (bounds and
@@ -489,7 +496,8 @@ public class PlaneSurface implements Surface {
 
         /* Work out pixel size in data coordinates by looking at the
          * data position of a point two pixels away. */
-        double dp2 = axis.graphicsToData( axis.dataToGraphics( dpos ) + 2 );
+        double dp2 =
+            axis.graphicsToData( (int) axis.dataToGraphics( dpos ) + 2 );
         double prec = Math.abs( dp2 - dpos ) / 2.;
 
         /* Work out the number of significant figures. */

ttools/src/main/uk/ac/starlink/ttools/plot2/geom/TimeSurface.java
@@ -116,16 +116,23 @@ public class TimeSurface implements Surface {
         if ( dpos == null ) {
             return false;
         }
-        int gx = tAxis_.dataToGraphics( dpos[ 0 ] );
-        int gy = yAxis_.dataToGraphics( dpos[ 1 ] );
-        if ( visibleOnly &&
-             ( gx < gxlo_ || gx >= gxhi_ || gy < gylo_ || gy >= gyhi_ ) ) {
+        double dx = tAxis_.dataToGraphics( dpos[ 0 ] );
+        double dy = yAxis_.dataToGraphics( dpos[ 1 ] );
+        if ( Double.isNaN( dx ) || Double.isNaN( dy ) ) {
             return false;
         }
         else {
-            gp.x = gx;
-            gp.y = gy;
-            return true;
+            int gx = (int) dx;
+            int gy = (int) dy;
+            if ( visibleOnly &&
+                 ( gx < gxlo_ || gx >= gxhi_ || gy < gylo_ || gy >= gyhi_ ) ) {
+                return false;
+            }
+            else {
+                gp.x = gx;
+                gp.y = gy;
+                return true;
+            }
         }
     }
 
@@ -158,14 +165,14 @@ public class TimeSurface implements Surface {
             for ( int it = 0; it < tticks_.length; it++ ) {
                 Tick tick = tticks_[ it ];
                 if ( tick.getLabel() != null ) {
-                    int gx = tAxis_.dataToGraphics( tick.getValue() );
+                    int gx = (int) tAxis_.dataToGraphics( tick.getValue() );
                     g.drawLine( gx, gylo_, gx, gyhi_ );
                 }
             }
             for ( int it = 0; it < yticks_.length; it++ ) {
                 Tick tick = yticks_[ it ];
                 if ( tick.getLabel() != null ) {
-                    int gy = yAxis_.dataToGraphics( tick.getValue() );
+                    int gy = (int) yAxis_.dataToGraphics( tick.getValue() );
                     g.drawLine( gxlo_, gy, gxhi_, gy );
                 }
             }

ttools/src/docs/sun256.xml
@@ -10041,6 +10041,9 @@ eds. C. Gabriel et al., ASP Conf. Ser. 351, p. 666 (2006)
     <ul>
     <li>Fix some problems to do with zooming to very large/small
         plot axis ranges.</li>
+    <li>Hide error bars (etc) that would extend to negative values
+        on logarithmic axes; previously they were being drawn in anomalous
+        places.</li>
     </ul>
     </p></dd>
 

ttools/src/main/uk/ac/starlink/ttools/plot2/Axis.java
@@ -41,7 +41,7 @@ public abstract class Axis {
      * @param   d  data coordinate
      * @return  graphics coordinate
      */
-    public abstract int dataToGraphics( double d );
+    public abstract double dataToGraphics( double d );
 
     /**
      * Converts a graphics position on this axis to a data coordinate.
@@ -137,7 +137,7 @@ public abstract class Axis {
         for ( int it = 0; it < ticks.length; it++ ) {
             Tick tick = ticks[ it ];
             String label = tick.getLabel();
-            double gx = dataToGraphics( tick.getValue() );
+            int gx = (int) dataToGraphics( tick.getValue() );
             double tx = invert ? ghi_ - gx : gx - glo_;
             AffineTransform tTrans =
                 AffineTransform.getTranslateInstance( tx, 0 );
@@ -280,8 +280,8 @@ public abstract class Axis {
             b_ = ( flip ? ghi : glo ) - a_ * dlo;
         }
 
-        public int dataToGraphics( double d ) {
-            return (int) ( b_ + a_ * d );
+        public double dataToGraphics( double d ) {
+            return b_ + a_ * d;
         }
 
         public double graphicsToData( int g ) {
@@ -328,8 +328,8 @@ public abstract class Axis {
             a1_ = 1.0 / a_;
         }
 
-        public int dataToGraphics( double d ) {
-            return (int) ( b_ + a_ * Math.log( d ) );
+        public double dataToGraphics( double d ) {
+            return b_ + a_ * Math.log( d );
         }
 
         public double graphicsToData( int g ) {

ttools/src/main/uk/ac/starlink/ttools/plot2/BasicTicker.java
@@ -400,7 +400,7 @@ public abstract class BasicTicker implements Ticker {
             Tick tick = ticks[ i ];
             String label = tick.getLabel();
             if ( label != null ) {
-                int gx = axis.dataToGraphics( tick.getValue() );
+                int gx = (int) axis.dataToGraphics( tick.getValue() );
                 Rectangle cbounds = captioner.getCaptionBounds( label );
                 AffineTransform oTrans =
                     orient.captionTransform( cbounds, cpad );

commit 7119d6072ef3ce5369d026da37f2c343b5c90030

ttools: allow NaN coordinates from Surface.dataToGraphics result
Clarify the contract and modify the PlaneSurface and CubeSurface
implementations of the Surface interface, so that input NaN data
coordinates can result in output NaN graphics coordinates *if*
visibleOnly is false.

Previously in some cases NaN outputs (perhaps resulting from NaN
inputs) resulted in summary failure (false return value).

That was problematic, since in some cases ("partial" coordinates,
used for things like histograms and spectrograms) the inputs
are supposed to be NaN in one coordinate and you still want a
non-failure result.  This was leading to failure of some things
that relied on partial coordinate processing in histogram/spectrogram
layers (Histogram/Time plot windows):

   - the "New subset from visible" button did nothing
   - the point count was always zero

That was a regression bug dating from commit c51d80215, so present
in v4.2-2 but not v4.2-1.

Following the fix, it's necessary to perform some more checking on
result graphics coordinates at the various places where
dataToGraphics (etc) is invoked with visibleOnly set false.

topcat/src/docs/sun253.xml
@@ -21042,6 +21042,10 @@ introduced since the last version:
 <dt>Next version</dt>
 <dd><p>
     <ul>
+    <li>Fix regression bug (from v4.2-2) that caused problems in Histogram
+        and Spectrogram layers:
+        <em>New subset from visible</em> action wasn't working,
+        and the displayed point count was always zero.</li>
     </ul>
     </p></dd>
 

ttools/src/main/uk/ac/starlink/ttools/plot2/PlotUtil.java
@@ -174,7 +174,7 @@ public class PlotUtil {
      * time and the time when this method is called.
      * If the elapsed time is zero, nothing is logged.
      *
-     * @param  logger   log message destaination
+     * @param  logger   log message destination
      * @param  phase  name of step to log time of
      * @param  start   start {@link java.lang.System#currentTimeMillis
      *                              currentTimeMillis}
@@ -231,6 +231,28 @@ public class PlotUtil {
      */
     public static boolean isFinite( double value ) {
         return ! Double.isNaN( value ) && ! Double.isInfinite( value );
+    }
+
+    /**
+     * Determines whether both coordinates of a graphics position are
+     * definite numbers.
+     *
+     * @param  gp  position to test
+     * @return  true  iff X and Y coordinates are both non-NaN and non-infinite
+     */
+    public static boolean isPointFinite( Point2D.Double gp ) {
+        return isFinite( gp.x ) && isFinite( gp.y );
+    }
+
+    /**
+     * Determines whether both coordinates of a graphics position are
+     * not NaNs.
+     *
+     * @param  gp  position to test
+     * @return  true iff X and Y coordinates are both non-NaN
+     */
+    public static boolean isPointReal( Point2D.Double gp ) {
+        return ! Double.isNaN( gp.x ) && ! Double.isNaN( gp.y );
     }
 
     /**

ttools/src/main/uk/ac/starlink/ttools/plot2/Surface.java
@@ -71,6 +71,16 @@ public interface Surface {
 
     /**
      * Converts a data space position to a graphics position.
+     * If <code>visibleOnly</code> is true, then if the return value
+     * is true, the exit value of <code>gPos</code> is guaranteed
+     * to be within the plot bounds of this surface.
+     *
+     * <p>If <code>visibleOnly</code> is false, there are no guarantees
+     * about the exit value of <code>gPos</code>, and its coordinates
+     * could be infinite or NaN.  In this case you might want to perform
+     * additional checking, for instance with the utility methods
+     * {@link PlotUtil#isPointFinite PlotUtil.isPointFinite} or
+     * {@link PlotUtil#isPointReal isPointReal}.
      *
      * @param  dataPos  dataDimCount-element array containing data space
      *                  coordinates

ttools/src/main/uk/ac/starlink/ttools/plot2/geom/CubeSurface.java
@@ -198,9 +198,6 @@ public class CubeSurface implements Surface {
         double sx = normalise( dataPos, 0 );
         double sy = normalise( dataPos, 1 );
         double sz = normalise( dataPos, 2 );
-        if ( Double.isNaN( sx ) || Double.isNaN( sy ) || Double.isNaN( sz ) ) {
-            return false;
-        }
         assert ( ! knownInCube )
                || ( isNormal( sx ) && isNormal( sy ) && isNormal( sz ) )
              : "(" + sx + ", " + sy + ", " + sz + ")";

ttools/src/main/uk/ac/starlink/ttools/plot2/geom/PlaneNavigator.java
@@ -184,13 +184,15 @@ public class PlaneNavigator implements Navigator<PlaneAspect> {
         int xoff = ! Double.isNaN( xAnchor_ ) &&
                    ( xAnchor_ > 0 || ! logFlags[ 0 ] ) &&
                    surface.dataToGraphics( new double[] { xAnchor_, d0[ 1 ] },
-                                           false, pc )
+                                           false, pc ) &&
+                   PlotUtil.isPointFinite( pc )
                  ? (int) Math.round( pc.x - refpos.x )
                  : 0;
         int yoff = ! Double.isNaN( yAnchor_ ) &&
                    ( yAnchor_ > 0 || ! logFlags[ 1 ] ) &&
                    surface.dataToGraphics( new double[] { d0[ 0 ], yAnchor_ },
-                                           false, pc )
+                                           false, pc ) &&
+                   PlotUtil.isPointFinite( pc )
                  ? (int) Math.round( pc.y - refpos.y )
                  : 0;
         return new int[] { xoff, yoff };

ttools/src/main/uk/ac/starlink/ttools/plot2/geom/PlaneSurface.java
@@ -121,19 +121,14 @@ public class PlaneSurface implements Surface {
                                    Point2D.Double gp ) {
         double gx = xAxis_.dataToGraphics( dpos[ 0 ] );
         double gy = yAxis_.dataToGraphics( dpos[ 1 ] );
-        if ( Double.isNaN( gx ) || Double.isNaN( gy ) ) {
-            return false;
+        if ( ! visibleOnly ||
+             ( gx >= gxlo_ && gx <= gxhi_ && gy >= gylo_ && gy <= gyhi_ ) ) {
+            gp.x = gx;
+            gp.y = gy;
+            return true;
         }
         else {
-            if ( visibleOnly &&
-                 ( gx < gxlo_ || gx >= gxhi_ || gy < gylo_ || gy >= gyhi_ ) ) {
-                return false;
-            }
-            else {
-                gp.x = gx;
-                gp.y = gy;
-                return true;
-            }
+            return false;
         }
     }
 
@@ -280,7 +275,7 @@ public class PlaneSurface implements Surface {
      */
     PlaneAspect center( double[] dpos, boolean xFlag, boolean yFlag ) {
         Point2D.Double gp = new Point2D.Double();
-        return dataToGraphics( dpos, false, gp )
+        return dataToGraphics( dpos, false, gp ) && PlotUtil.isPointFinite( gp )
              ? pan( gp, new Point2D.Double( ( gxlo_ + gxhi_ ) * 0.5,
                                             ( gylo_ + gyhi_ ) * 0.5 ),
                     xFlag, yFlag )

ttools/src/main/uk/ac/starlink/ttools/plot2/geom/TimeSurface.java
@@ -116,19 +116,14 @@ public class TimeSurface implements Surface {
                                    Point2D.Double gp ) {
         double gx = tAxis_.dataToGraphics( dpos[ 0 ] );
         double gy = yAxis_.dataToGraphics( dpos[ 1 ] );
-        if ( Double.isNaN( gx ) || Double.isNaN( gy ) ) {
-            return false;
+        if ( ! visibleOnly ||
+             ( gx >= gxlo_ && gx <= gxhi_ && gy >= gylo_ && gy <= gyhi_ ) ) {
+            gp.x = gx;
+            gp.y = gy;
+            return true;
         }
         else {
-            if ( visibleOnly &&
-                 ( gx < gxlo_ || gx >= gxhi_ || gy < gylo_ || gy >= gyhi_ ) ) {
-                return false;
-            }
-            else {
-                gp.x = gx;
-                gp.y = gy;
-                return true;
-            }
+            return false;
         }
     }
 
@@ -246,7 +241,7 @@ public class TimeSurface implements Surface {
      */
     TimeAspect center( double[] dpos, boolean tFlag, boolean yFlag ) {
         Point2D.Double gp = new Point2D.Double();
-        return dataToGraphics( dpos, false, gp )
+        return dataToGraphics( dpos, false, gp ) && PlotUtil.isPointFinite( gp )
              ? pan( gp, new Point2D.Double( ( gxlo_ + gxhi_ ) * 0.5,
                                             ( gylo_ + gyhi_ ) * 0.5 ),
                     tFlag, yFlag )

ttools/src/main/uk/ac/starlink/ttools/plot2/layer/FloatingCoordAuxReader.java
@@ -51,7 +51,8 @@ public class FloatingCoordAuxReader implements AuxReader {
         /* Convert data to graphics coordinates.  The resulting values are
          * not used, but this determines whether the points are plottable. */
         if ( geom_.readDataPos( tseq, 0, dpos_ ) &&
-             surface.dataToGraphics( dpos_, visibleOnly_, gpos_ ) ) {
+             surface.dataToGraphics( dpos_, visibleOnly_, gpos_ ) &&
+             ( visibleOnly_ || PlotUtil.isPointFinite( gpos_ ) ) ) {
 
             /* Read the coordinate value. */
             double value = coord_.readDoubleCoord( tseq, icol_ );

ttools/src/main/uk/ac/starlink/ttools/plot2/layer/FunctionPlotter.java
@@ -364,7 +364,8 @@ public class FunctionPlotter extends
                 double x = xs[ ip ];
                 double f = function.evaluate( x );
                 if ( axis.xfToData( surface_, x, f, dpos ) &&
-                     surface_.dataToGraphics( dpos, false, gpos ) ) {
+                     surface_.dataToGraphics( dpos, false, gpos ) &&
+                     PlotUtil.isPointReal( gpos ) ) {
                     tracer.addVertex( gpos.x, gpos.y );
                 }
             }

ttools/src/main/uk/ac/starlink/ttools/plot2/layer/HistogramPlotter.java
@@ -449,7 +449,9 @@ public class HistogramPlotter
                  dpos1[ 0 ] = dxhi;
                  dpos1[ 1 ] = dy;
                  if ( surface.dataToGraphics( dpos0, false, p0 ) &&
-                      surface.dataToGraphics( dpos1, false, p1 ) ) {
+                      ! Double.isNaN( p0.x ) &&
+                      surface.dataToGraphics( dpos1, false, p1 ) &&
+                      ! Double.isNaN( p1.x ) ) {
 
                     /* Clip them so they are not too far off the plot region;
                      * attempting to draw ridiculously large rectangles can

ttools/src/main/uk/ac/starlink/ttools/plot2/layer/LinePlotter.java
@@ -81,7 +81,8 @@ public class LinePlotter extends SimpleDecalPlotter<LineStyle> {
         TupleSequence tseq = dataStore.getTupleSequence( dataSpec );
         while ( tseq.next() ) {
             if ( geom.readDataPos( tseq, icPos, dpos ) &&
-                 surface.dataToGraphics( dpos, false, gp ) ) {
+                 surface.dataToGraphics( dpos, false, gp ) &&
+                 PlotUtil.isPointReal( gp ) ) {
                 tracer.addVertex( gp.x, gp.y );
             }
         }

ttools/src/main/uk/ac/starlink/ttools/plot2/layer/LinearFitPlotter.java
@@ -229,7 +229,8 @@ public class LinearFitPlotter extends AbstractPlotter<LineStyle> {
             double[] dpos = new double[ geom_.getDataDimCount() ];
             while ( tseq.next() ) {
                 if ( geom_.readDataPos( tseq, icPos, dpos ) &&
-                     surface_.dataToGraphics( dpos, visibleOnly, gp ) ) {
+                     surface_.dataToGraphics( dpos, visibleOnly, gp ) &&
+                     PlotUtil.isPointFinite( gp ) ) {
                     double x = xlog ? log( dpos[ 0 ] ) : dpos[ 0 ];
                     double y = ylog ? log( dpos[ 1 ] ) : dpos[ 1 ];
                     if ( hasWeight ) {
@@ -319,8 +320,10 @@ public class LinearFitPlotter extends AbstractPlotter<LineStyle> {
             Point2D.Double gp2 = new Point2D.Double();
             if ( surface.dataToGraphics( new double[] { dx1, dy1 },
                                          false, gp1 ) &&
+                 PlotUtil.isPointFinite( gp1 ) &&
                  surface.dataToGraphics( new double[] { dx2, dy2 },
-                                         false, gp2 ) ) {
+                                         false, gp2 ) &&
+                 PlotUtil.isPointFinite( gp2 ) ) {
                 LineTracer tracer =
                     style.createLineTracer( g, bounds, 2, false ); 
                 tracer.addVertex( gp1.x, gp1.y );

ttools/src/main/uk/ac/starlink/ttools/plot2/layer/MultiPointForm.java
@@ -444,7 +444,8 @@ public class MultiPointForm implements ShapeForm {
                 final int gy;
                 if ( surface_.dataToGraphicsOffset( dpos0, gpos0,
                                                     dposExtras[ ie ], false,
-                                                    gp_ ) ) {
+                                                    gp_ ) &&
+                     PlotUtil.isPointReal( gp_ ) ) {
                     gx = (int) Math.round( ( gp_.x - gx0 ) * scale_ );
                     gy = (int) Math.round( ( gp_.y - gy0 ) * scale_ );
                 }
@@ -537,13 +538,15 @@ public class MultiPointForm implements ShapeForm {
                     if ( geom.readDataPos( tseq, 0, dpos0 ) &&
                          surface.dataToGraphics( dpos0, scaleFromVisible_,
                                                  gpos0 ) &&
+                         PlotUtil.isPointFinite( gpos0 ) &&
                          extraCoordSet.readPoints( tseq, icExtra, dpos0,
                                                    dposExtras ) ) {
                         for ( int ie = 0; ie < nextra; ie++ ) {
                             if ( surface
                                 .dataToGraphicsOffset( dpos0, gpos0,
                                                        dposExtras[ ie ], false,
-                                                       gpos1 ) ) {
+                                                       gpos1 ) &&
+                                PlotUtil.isPointFinite( gpos1 ) ) {
                                 range.submit( gpos1.x - gpos0.x );
                                 range.submit( gpos1.y - gpos0.y );
                             }

ttools/src/main/uk/ac/starlink/ttools/plot2/layer/PairLinkForm.java
@@ -200,8 +200,10 @@ public class PairLinkForm implements ShapeForm {
                      * plot bounds. */
                     if ( geom.readDataPos( tseq, 0, dpos1 ) &&
                          surface.dataToGraphics( dpos1, false, gp1 ) &&
+                         PlotUtil.isPointFinite( gp1 ) &&
                          geom.readDataPos( tseq, npc, dpos2 ) &&
-                         surface.dataToGraphics( dpos2, false, gp2 ) ) {
+                         surface.dataToGraphics( dpos2, false, gp2 ) &&
+                         PlotUtil.isPointFinite( gp2 ) ) {
                         PlotUtil.quantisePoint( gp1, gp1i );
                         PlotUtil.quantisePoint( gp2, gp2i );
                         if ( lineMightCross( bounds, gp1i, gp2i ) ) {