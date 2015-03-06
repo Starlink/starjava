7d83bc2cb932c11c36d2544e0f1ff8a5268e3564
Mark Taylor <m.b.taylor@bristol.ac.uk>
2015-03-06 16:30:57 +0000

    topcat: display actual bin/smoothing width in GUI
    The Bars control of the histogram window now reports actual
    bin width/smoothing width alongside the width selection slider.
    
    Note this is specific to the histogram window.
    It should also be done for a histogram layer control in other plot
    types (e.g. Plane plot).

--- topcat/src/main/uk/ac/starlink/topcat/plot2/HistogramAxisController.java
+++ topcat/src/main/uk/ac/starlink/topcat/plot2/HistogramAxisController.java
@@ -1,15 +1,21 @@
 package uk.ac.starlink.topcat.plot2;
 
 import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import uk.ac.starlink.topcat.ResourceIcon;
 import uk.ac.starlink.ttools.plot.Style;
 import uk.ac.starlink.ttools.plot2.Navigator;
 import uk.ac.starlink.ttools.plot2.PlotLayer;
 import uk.ac.starlink.ttools.plot2.PlotUtil;
+import uk.ac.starlink.ttools.plot2.ReportKey;
+import uk.ac.starlink.ttools.plot2.ReportMap;
 import uk.ac.starlink.ttools.plot2.SurfaceFactory;
 import uk.ac.starlink.ttools.plot2.config.ConfigException;
 import uk.ac.starlink.ttools.plot2.config.ConfigKey;
 import uk.ac.starlink.ttools.plot2.config.ConfigMap;
+import uk.ac.starlink.ttools.plot2.config.SliderSpecifier;
+import uk.ac.starlink.ttools.plot2.config.Specifier;
 import uk.ac.starlink.ttools.plot2.config.StyleKeys;
 import uk.ac.starlink.ttools.plot2.geom.PlaneAspect;
 import uk.ac.starlink.ttools.plot2.geom.PlaneNavigator;
@@ -29,6 +35,9 @@ import uk.ac.starlink.ttools.plot2.layer.Pixel1dPlotter;
 public class HistogramAxisController
         extends CartesianAxisController<PlaneSurfaceFactory.Profile,
                                         PlaneAspect> {
+
+    private final BinSizer.BinSizerSpecifier binWidthSpecifier_;
+    private final BinSizer.BinSizerSpecifier smoothWidthSpecifier_;
 
     /**
      * Constructor.
@@ -98,10 +107,16 @@ public class HistogramAxisController
             HistogramPlotter.BINSIZER_KEY,
             HistogramPlotter.PHASE_KEY,
         } );
+        binWidthSpecifier_ =
+            getSliderSpecifier( hbarSpecifier, HistogramPlotter.BINSIZER_KEY );
+        assert binWidthSpecifier_ != null;
         ConfigSpecifier kbinSpecifier = new ConfigSpecifier( new ConfigKey[] {
             Pixel1dPlotter.SMOOTHSIZER_KEY,
             Pixel1dPlotter.KERNEL_KEY,
         } );
+        smoothWidthSpecifier_ =
+            getSliderSpecifier( kbinSpecifier, Pixel1dPlotter.SMOOTHSIZER_KEY );
+        assert smoothWidthSpecifier_ != null;
         ConfigSpecifier genSpecifier = new ConfigSpecifier( new ConfigKey[] {
             StyleKeys.CUMULATIVE,
             StyleKeys.NORMALISE,
@@ -163,6 +178,67 @@ public class HistogramAxisController
         }
     }
 
+    @Override
+    public void submitReports( Map<LayerId,ReportMap> reports ) {
+        updateBinSizerText( HistogramPlotter.BINWIDTH_KEY,
+                            binWidthSpecifier_, reports );
+        updateBinSizerText( Pixel1dPlotter.SMOOTHWIDTH_KEY,
+                            smoothWidthSpecifier_, reports );
+    }
+
+    /**
+     * Updates the state of the GUI to reflect the current actual
+     * bin/smoothing widths.  This is is useful visual feedback for
+     * the user, but can only be known as a result of performing
+     * the plot.
+     *
+     * @param   key  report key giving the actual width in data coordinates
+     *               extracted from a BinSizer config item
+     * @param   specifier   specifier from which the BinSizer value is acquired
+     * @param   reports   reports obtained from doing the plot
+     */
+    private static void
+            updateBinSizerText( ReportKey<Double> key,
+                                BinSizer.BinSizerSpecifier specifier,
+                                Map<LayerId,ReportMap> reports ) {
+
+        /* See if we have a value for the actual width for the relevant
+         * config item. */
+        double dval = Double.NaN;
+        for ( ReportMap report : reports.values() ) {
+            Double value = report == null ? null : report.get( key );
+            double dval0 = value == null ? Double.NaN : value.doubleValue();
+            if ( ! Double.isNaN( dval0 ) ) {
+                assert Double.isNaN( dval ) || dval0 == dval
+                     : key + " not unique?";
+                dval = dval0;
+            }
+        }
+
+        /* If so, pass it to the relevant specifier for display. */
+        if ( specifier != null ) {
+            specifier.displayBinWidth( dval );
+        }
+    }
+
+    /**
+     * Returns a typed specifier used to acquire the value for a BinSizer
+     * config item.  Null is returned if the specifier can't be found or
+     * is of the wrong type.
+     *
+     * @param  cs  config specifier expected to hold the relevant item
+     * @param  key   config key for the relevant item 
+     * @return    bin sizer specifier, or null
+     */
+    private static BinSizer.BinSizerSpecifier
+            getSliderSpecifier( ConfigSpecifier cs, ConfigKey<BinSizer> key ) {
+        Specifier<BinSizer> spec = cs.getSpecifier( key );
+        assert spec instanceof BinSizer.BinSizerSpecifier;
+        return spec instanceof BinSizer.BinSizerSpecifier
+             ? (BinSizer.BinSizerSpecifier) spec
+             : null;
+    }
+
     /**
      * Returns the common state of the histogram layers concerning
      * how the bars are laid out.
@@ -187,8 +263,9 @@ public class HistogramAxisController
             Boolean cumul1 = null;
             Normalisation norm1 = null;
             Style style = layer.getStyle();
+            final boolean layerHasBars;
             if ( style instanceof HistogramPlotter.HistoStyle ) {
-                hasBars = true;
+                layerHasBars = true;
                 HistogramPlotter.HistoStyle hstyle =
                     (HistogramPlotter.HistoStyle) style;
                 sizer1 = hstyle.getBinSizer();
@@ -196,18 +273,25 @@ public class HistogramAxisController
                 norm1 = hstyle.getNormalisation();
             }
             else if ( style instanceof KernelDensityPlotter.KDenseStyle ) {
-                hasBars = true;
+                layerHasBars = true;
                 KernelDensityPlotter.KDenseStyle dstyle =
                     (KernelDensityPlotter.KDenseStyle) style;
                 cumul1 = dstyle.isCumulative();
                 norm1 = dstyle.getNormalisation();
             }
-            assert sizer == null || sizer1 == null || sizer.equals( sizer1 );
-            assert cumul == null || cumul.equals( cumul1 );
-            assert norm == null || norm.equals( norm1 );
-            sizer = sizer1;
-            cumul = cumul1;
-            norm = norm1;
+            else {
+                layerHasBars = false;
+            }
+            if ( layerHasBars ) {
+                assert sizer == null || sizer1 == null
+                             || sizer.equals( sizer1 );
+                assert cumul == null || cumul.equals( cumul1 );
+                assert norm == null || norm.equals( norm1 );
+                sizer = sizer1;
+                cumul = cumul1;
+                norm = norm1;
+            }
+            hasBars = hasBars || layerHasBars;
         }
         return hasBars ? new BarState( sizer, cumul, norm ) : null;
     }
