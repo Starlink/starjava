facbceab9c74fb7ba7b3de5320108f648e3833c8
Mark Taylor <m.b.taylor@bristol.ac.uk>
2015-04-15 15:49:54 +0100

    vo: modify asynchronous TAP metadata reading
    Modify the way that queued runnables performing asynchronous TAP
    metadata acquisition make sure that the same reads are not done
    multiple times.
    
    I think this implementation is a bit cleaner than the previous one,
    and it is also better fitted to alternative ExecutorService
    implementations, for instance LIFO ones.

--- vo/src/main/uk/ac/starlink/vo/TapMetaManager.java
+++ vo/src/main/uk/ac/starlink/vo/TapMetaManager.java
@@ -1,30 +1,36 @@
 package uk.ac.starlink.vo;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.ThreadFactory;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.SwingUtilities;
 
 /**
- * Handles asynchronous population of the TAP metadata hierarchy.
+ * Handles asynchronous population of the TAP metadata hierarchy. 
  * It owns a TapMetaReader and invokes its methods as required to
  * populate the metadata hiearchy objects, providing methods that
  * allow callbacks to be invoked when the relevant items are available.
- *
+ *                  
+ * <p>This class is intended for use with Swing; some methods must be
+ * invoked from the Event Dispatch Thread as documented.
+ *                      
  * @author   Mark Taylor
  * @since    23 Mar 2015
  */
 public class TapMetaManager {
 
     private final TapMetaReader rdr_;
-    private final Map<Object,List<Runnable>> callbackMap_;
+    private final Map<Populator,Collection<Runnable>> runningMap_;
     private ExecutorService executor_;
 
     private static final Logger logger_ =
@@ -37,7 +43,7 @@ public class TapMetaManager {
      */
     public TapMetaManager( TapMetaReader rdr ) {
         rdr_ = rdr;
-        callbackMap_ = new HashMap<Object,List<Runnable>>();
+        runningMap_ = new HashMap<Populator,Collection<Runnable>>();
     }
 
     /**
@@ -60,38 +66,20 @@ public class TapMetaManager {
      * @return  true iff callback has been called synchronously
      */
     public boolean onTables( final SchemaMeta smeta, Runnable callback ) {
-        if ( smeta.getTables() != null ) {
-            callback.run();
-            return true;
-        }
-        else {
-            schedule( smeta, new Runnable() {
-                public void run() {
-                    TableMeta[] tmetas;
-                    try {
-                        tmetas = rdr_.readTables( smeta );
-                    }
-                    catch ( IOException e ) {
-                        logger_.log( Level.WARNING,
-                                     "Failed to read tables for schema "
-                                   + smeta.getName()
-                                   + " (" + rdr_.getSource() + ")", e );
-                        tmetas = new TableMeta[ 0 ];
-                    }
-                    final TableMeta[] tmetas0 = tmetas;
-
-                    /* Update the metadata on the EDT as well, otherwise
-                     * the change is not guaranteed to be visible from that
-                     * thread (Java Memory Model). */
-                    SwingUtilities.invokeLater( new Runnable() {
-                        public void run() {
-                            smeta.setTables( tmetas0 );
-                        }
-                    } );
-                }
-            }, callback );
-            return false;
-        }
+        return onData( callback,
+                       new Populator<TableMeta>( TableMeta.class, "tables",
+                                                 smeta ) {
+            public boolean hasData() {
+                return smeta.getTables() != null;
+            }
+            public TableMeta[] readData( TapMetaReader rdr )
+                    throws IOException {
+                return rdr.readTables( smeta );
+            }
+            public void updateData( TableMeta[] tmetas ) {
+                smeta.setTables( tmetas );
+            }
+        } );
     }
 
     /**
@@ -105,34 +93,20 @@ public class TapMetaManager {
      * @return  true iff callback has been called synchronously
      */
     public boolean onColumns( final TableMeta tmeta, Runnable callback ) {
-        if ( tmeta.getColumns() != null ) {
-            callback.run();
-            return true;
-        }
-        else {
-            schedule( new ColKey( tmeta ), new Runnable() {
-                public void run() {
-                    ColumnMeta[] cmetas;
-                    try {
-                        cmetas = rdr_.readColumns( tmeta );
-                    }
-                    catch ( IOException e ) {
-                        logger_.log( Level.WARNING,
-                                     "Failed to read columns for table "
-                                    + tmeta.getName()
-                                    + " (" + rdr_.getSource() + ")", e );
-                        cmetas = new ColumnMeta[ 0 ];
-                    }
-                    final ColumnMeta[] cmetas0 = cmetas;
-                    SwingUtilities.invokeLater( new Runnable() {
-                        public void run() {
-                            tmeta.setColumns( cmetas0 );
-                        }
-                    } );
-                }
-            }, callback );
-            return false;
-        }
+        return onData( callback,
+                       new Populator<ColumnMeta>( ColumnMeta.class, "columns",
+                                                  tmeta ) {
+            public boolean hasData() {
+                return tmeta.getColumns() != null;
+            }
+            public ColumnMeta[] readData( TapMetaReader rdr )
+                    throws IOException {
+                return rdr.readColumns( tmeta );
+            }
+            public void updateData( ColumnMeta[] cmetas ) {
+                tmeta.setColumns( cmetas );
+            }
+        } );
     }
 
     /**
@@ -146,34 +120,20 @@ public class TapMetaManager {
      * @return  true iff callback has been called synchronously
      */
     public boolean onForeignKeys( final TableMeta tmeta, Runnable callback ) {
-        if ( tmeta.getForeignKeys() != null ) {
-            callback.run();
-            return true;
-        }
-        else {
-            schedule( new ForKey( tmeta ), new Runnable() {
-                public void run() {
-                    ForeignMeta[] fmetas;
-                    try {
-                        fmetas = rdr_.readForeignKeys( tmeta );
-                    }
-                    catch ( IOException e ) {
-                        logger_.log( Level.WARNING,
-                                     "Failed to read foreign keys for table "
-                                   + tmeta.getName()
-                                   + " (" + rdr_.getSource() + ")", e );
-                        fmetas = new ForeignMeta[ 0 ];
-                    }
-                    final ForeignMeta[] fmetas0 = fmetas;
-                    SwingUtilities.invokeLater( new Runnable() {
-                        public void run() {
-                            tmeta.setForeignKeys( fmetas0 );
-                        }
-                    } );
-                }
-            }, callback );
-            return false;
-        }
+        return onData( callback,
+                       new Populator<ForeignMeta>( ForeignMeta.class,
+                                                   "foreign keys", tmeta ) {
+            public boolean hasData() {
+                return tmeta.getForeignKeys() != null;
+            }
+            public ForeignMeta[] readData( TapMetaReader rdr )
+                    throws IOException {
+                return rdr.readForeignKeys( tmeta );
+            }
+            public void updateData( ForeignMeta[] fmetas ) {
+                tmeta.setForeignKeys( fmetas );
+            }
+        } );
     }
 
     /**
@@ -189,116 +149,229 @@ public class TapMetaManager {
     }
 
     /**
-     * Schedules a callback to run on the Event Dispatch Thread
-     * following completion of a supplied runnable.
-     * This method, and the callback, are called on the EDT.
-     *
-     * @param   key   object identifying the populator;
-     *                populators with the same key do the same thing,
-     *                so it's not necessary to run more than one
-     * @param   populator  runnable to be completed before callback invocation;
-     *                     will be run asynchronously
-     * @param   callback  callback to be run on populator completion
-     */
-    private void schedule( final Object key, final Runnable populator,
-                           Runnable callback ) {
-
-        /* See if we're already running a populator that does the job in
-         * question.  If so, just add the callback to the list that will
-         * be run on poplulator completion. */
-        List<Runnable> callbacks = callbackMap_.get( key );
-        if ( callbacks != null ) {
-            callbacks.add( callback );
-        }
-
-        /* Otherwise, schedule the populator to run asynchronously. */
-        else {
-            callbacks = new ArrayList<Runnable>();
-            callbacks.add( callback );
-            callbackMap_.put( key, callbacks );
-            getExecutor().submit( new Runnable() {
-                public void run() {
-                    populator.run();
-
-                    /* On completion, run any associated callbacks. */
-                    SwingUtilities.invokeLater( new Runnable() {
-                        public void run() {
-                            for ( Runnable callback :
-                                  callbackMap_.remove( key ) ) {
-                                callback.run();
-                            }
-                        }
-                    } );
-                }
-            } );
-        }
-    }
-
-    /**
      * Lazily create and return an executor instance.
      *
      * @return  executor used for asynchronous queries by this object
      */
     public ExecutorService getExecutor() {
         if ( executor_ == null ) {
-            executor_ = Executors.newSingleThreadExecutor( new ThreadFactory() {
-                public Thread newThread( Runnable r ) {
-                    Thread th = new Thread( r, "TAP Metadata Query" );
-                    th.setDaemon( true );
-                    return th;
-                }
-            } );
+            executor_ = createExecutorService();
         }
         return executor_;
     }
 
     /**
-     * Characterises a column request job.
+     * Executes a given callback when data is available from a given populator.
+     * This method, and the supplied callback, are called on the
+     * Event Dispatch Thread.
+     *
+     * @param  callback  runnable to execute when data is available
+     * @param  populator   defines data acquisition
      */
-    private static class ColKey {
-        private final TableMeta tmeta_;
-
-        /**
-         * Constructor.
-         *
-         * @param  tmeta   table for which columns are requested
-         */
-        ColKey( TableMeta tmeta ) {
-            tmeta_ = tmeta;
+    private boolean onData( final Runnable callback,
+                            final Populator<?> populator ) {
+        if ( populator.hasData() ) {
+            callback.run();
+            return true;
         }
-        @Override
-        public int hashCode() {
-            return 99 * tmeta_.hashCode();
-        }
-        @Override
-        public boolean equals( Object other ) {
-            return other instanceof ColKey
-                && this.tmeta_ == ((ColKey) other).tmeta_;
+        else {
+            try {
+                getExecutor().submit( new Runnable() {
+                    public void run() {
+                        if ( populator.populateCompleted_ ) {
+                            SwingUtilities.invokeLater( callback );
+                        }
+                        else {
+                            populateAndCallback( populator, callback );
+                        }
+                    }
+                } );
+            }
+            catch ( RejectedExecutionException e ) {
+                logger_.log( Level.INFO,
+                             "Will not read TAP metadata this time"
+                            + " (" + populator.getDataDescription() + ")", e );
+            }
+            return false;
         }
     }
 
     /**
-     * Characterises a foreign key request job.
+     * Causes the data in a populator to be updated as required,
+     * and then calls a supplied callback.
+     * Manages the possibility of other equivalent populators being
+     * queried concurrently, so as to avoid the same work being done
+     * multiple times.
+     *
+     * <p>This method may be called from any thread.
+     *
+     * @param  populator   defines data acquisition
+     * @param  callback  runnable to execute on the EDT when data is available
      */
-    private static class ForKey {
-        private final TableMeta tmeta_;
+    private void populateAndCallback( final Populator<?> populator,
+                                      Runnable callback ) {
+        synchronized ( runningMap_ ) {
+
+            /* If an equivalent populator is already running, piggyback on it
+             * and just ensure that our callback gets run on completion. */
+            if ( runningMap_.containsKey( populator ) ) {
+                runningMap_.get( populator ).add( callback );
+                return;
+            }
+
+            /* Otherwise initiate a list of callbacks, with the requested one
+             * as the initial sole member. */
+            else {
+                List<Runnable> callbacks = new ArrayList<Runnable>();
+                callbacks.add( callback );
+                runningMap_.put( populator, callbacks );
+            }
+        }
+
+        /* Acquire the data synchronously. */
+        populator.populate( rdr_ );
+        populator.populateCompleted_ = true;
+
+        /* Get the list of callbacks dependent on the data.
+         * This will certainly contain the one requested  by this invocation,
+         * but may contain more added by other threads during the data
+         * acquisition. */
+        final Collection<Runnable> callbacks;
+        synchronized ( runningMap_ ) {
+            callbacks = runningMap_.remove( populator );
+        }
+
+        /* Invoke all the callbacks on the EDT. */
+        SwingUtilities.invokeLater( new Runnable() {
+            public void run() {
+                assert populator.hasData();
+                for ( Runnable cb : callbacks ) {
+                    cb.run();
+                }
+            }
+        } );
+    }
+
+    /**
+     * Creates an executor service suitable for use by this object.
+     *
+     * @return   new executor service
+     */
+    private static ExecutorService createExecutorService() {
+        ThreadFactory thFact = new ThreadFactory() {
+            public Thread newThread( Runnable r ) {
+                Thread th = new Thread( r, "TAP metadata query" );
+                th.setDaemon( true );
+                return th;
+            }
+        };
+        return Executors.newSingleThreadExecutor( thFact );
+    }
+
+    /**
+     * Defines data to be acquired.
+     * Instances of this class with the same behaviour are equal according
+     * to the <code>equals</code> method.
+     */
+    private static abstract class Populator<T> {
+
+        private final Class<T> clazz_;
+        private final String metasType_;
+        private final Object id_;
+        volatile boolean populateCompleted_;
 
         /**
          * Constructor.
          *
-         * @param  tmeta   table for which foreign keys are requested
+         * @param   clazz  element type of acquired data
+         * @param   metasType  string (for user messages) describing acquired
+         *                     data items, plural
+         * @param   id  object defining the identity (equality) for this object
          */
-        ForKey( TableMeta tmeta ) {
-            tmeta_ = tmeta;
+        Populator( Class<T> clazz, String metasType, Object id ) {
+            clazz_ = clazz;
+            metasType_ = metasType;
+            id_ = id;
         }
+
+        /**
+         * Indicates whether the data for this populator is already avaialable.
+         * Execute on the Event Dispatch Thread.
+         *
+         * @return   true iff data has been acquired
+         */
+        abstract boolean hasData();
+
+        /**
+         * Does the work of reading the data for this populator.
+         * Runs from any thread (not EDT).  May be time consuming.
+         *
+         * @param  rdr   reader object
+         * @return   acquired data
+         * @throws  IOException   in case of read error
+         */
+        abstract T[] readData( TapMetaReader rdr ) throws IOException;
+
+        /**
+         * Sets the data for this populator.
+         * Execute on the Event Dispatch Thread.
+         *
+         * @param  data   acquired data
+         */
+        abstract void updateData( T[] data );
+
+        /**
+         * Causes this populator's data to be populated.
+         * May be run from any thread, updates are performed on EDT.
+         *
+         * @param  rdr   reader object
+         */
+        void populate( TapMetaReader rdr ) {
+            final T[] data = getData( rdr );
+            SwingUtilities.invokeLater( new Runnable() {
+                public void run() {
+                    updateData( data );
+                }
+            } );
+        }
+
+        /**
+         * Reads data, returning an empty array in case of error.
+         *
+         * @param  rdr   reader object
+         * @return   acquired data or empty array, not null
+         */
+        private T[] getData( TapMetaReader rdr ) {
+            try {
+                return readData( rdr );
+            }
+            catch ( IOException e ) {
+                logger_.log( Level.WARNING,
+                             "Failed to read TAP metadata "
+                           + getDataDescription(), e );
+                @SuppressWarnings("unchecked")
+                T[] emptyArray = (T[]) Array.newInstance( clazz_, 0 );
+                return emptyArray;
+            }
+        }
+
+        /**
+         * Returns a user-directed description of whatever it is this
+         * populator is supposed to be reading.
+         */
+        public String getDataDescription() {
+            return metasType_ + " for " + id_;
+        }
+
         @Override
         public int hashCode() {
-            return 77 * tmeta_.hashCode();
+            return id_.hashCode();
         }
+
         @Override
-        public boolean equals( Object other ) {
-            return other instanceof ForKey
-                && this.tmeta_ == ((ForKey) other).tmeta_;
+        public boolean equals( Object o ) {
+            return getClass().equals( o.getClass() )
+                && this.id_.equals( ((Populator) o).id_ );
         }
     }
 }
