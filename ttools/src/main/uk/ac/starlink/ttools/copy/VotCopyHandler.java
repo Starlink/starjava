9c6a681d87a2e9b370e830c564a9c4057389abc0
Mark Taylor <m.b.taylor@bristol.ac.uk>
2015-08-04 15:24:03 +0100

    ttools: fix votcopy handling of unsignedByte columns
    Votcopy was changing the datatype for unsignedByte columns to type
    short during copying, on the assumption that this is what the
    VOSerializers do (because unsigned bytes are hard to handle in java).
    They used to do that, but they don't any longer, since the
    introduction of Tables.UBYTE_FLAG_INFO (cc7abd5d59)
    previous few commits).
    
    The result was slightly annoying for TABLEDATA output, since
    the datatype was unnecessarily changed.  For BINARY/BINARY2
    output, it generated unreadable VOTables.
    
    Remove that bit of code, so votcopy works for these columns again.
    
    The code is badly designed, or this sort of thing couldn't happen.
    The VOTable serialization code really needs rewriting so that
    output column types can be determined programmatically rather
    than requiring expert knowledge at different places in the code.
    
    But at least there is test code there now that should catch this
    sort of thing for votcopy.

--- ttools/src/main/uk/ac/starlink/ttools/copy/VotCopyHandler.java
+++ ttools/src/main/uk/ac/starlink/ttools/copy/VotCopyHandler.java
@@ -38,10 +38,6 @@ import uk.ac.starlink.votable.VOTableVersion;
  * unchanged apart from events within a DATA element, which are written
  * in one of the VOTable encodings as selected by the user.
  *
- * <p>One exception to the rule is that, for implementation-specific 
- * reasons, FIELD elements with <tt>datatype="unsignedByte"</tt> are
- * changed to have <tt>datatype="short"</tt> instead.
- *
  * @author   Mark Taylor (Starlink)
  * @since    18 Apr 2005
  */
@@ -65,7 +61,7 @@ public class VotCopyHandler
     private int iTable_;
 
     private final static Logger logger_ =
-        Logger.getLogger( "uk.ac.starlink.ttools" );
+        Logger.getLogger( "uk.ac.starlink.ttools.copy" );
 
     /**
      * Constructor.  The copy can be done in either cached or streamed
@@ -199,20 +195,6 @@ public class VotCopyHandler
         }
         else if ( "FIELD".equals( localName ) ) {
             String datatype = atts.getValue( "datatype" );
-
-            /* Unfortunately we have to translate unsignedByte datatypes
-             * to short ones here.  This is because the serializers in the
-             * VOTable package all use short as an internal representation
-             * for unsignedByte because of the difficulties of representing
-             * an unsigned value in Java. */
-            if ( "unsignedByte".equals( datatype ) ) {
-                AttributesImpl newAtts = new AttributesImpl( atts );
-                int itype = newAtts.getIndex( "datatype" );
-                newAtts.setValue( itype, "short" );
-                atts = newAtts;
-                log( Level.WARNING, "FIELD datatype has been changed from " +
-                                    "unsignedByte to short" );
-            }
 
             /* Fix up arraysize values here. */
             if ( ( "char".equals( datatype ) ||
