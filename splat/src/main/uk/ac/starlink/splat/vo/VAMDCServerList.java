b86cc2b9c5c64375114ab0d748261ca682b6f9d0
Margarida Castro Neves <mcneves@ari.uni-heidelberg.de>
2017-01-09 13:51:51 +0100

    splat: added class for dealing with vamdc services list

---
+++ splat/src/main/uk/ac/starlink/splat/vo/VAMDCServerList.java
@@ -0,0 +1,32 @@
+package uk.ac.starlink.splat.vo;
+
+import uk.ac.starlink.splat.util.SplatException;
+import uk.ac.starlink.table.StarTable;
+
+public class VAMDCServerList extends AbstractServerList {
+
+    
+    private final static String configFile = "VAMDCServerListV4.xml";
+    private final static String defaultFile = "VAMDCserverlist.xml";
+
+    public VAMDCServerList( ) throws SplatException  {
+        super();
+    }
+    
+    public VAMDCServerList(StarTable table )  {
+        super(table); 
+    }
+
+    @Override
+    public String getConfigFile() {
+        // TODO Auto-generated method stub
+        return configFile;
+    }
+
+    @Override
+    public String getDefaultFile() {
+        // TODO Auto-generated method stub
+        return defaultFile;
+    }
+
+}
