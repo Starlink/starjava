e139afc26169b7df48aa09b042e5dad969ee13d4
Mark Taylor <m.b.taylor@bristol.ac.uk>
2017-06-07 12:57:33 +0100

    gbin: refactor GBIN table reading classes
    Remove the class GbinTableReader and replace it with an abstract
    class GbinStarTable which has the same functionality, including
    some of the code that was previously in GbinTableBuilder.
    
    This localises the logic required for turning GaiaRoot objects
    into a StarTable in one place, GbinStarTable.
    The GbinTableBuilder class now mostly just contains the I/O
    required to harness that.
    
    This is a more logical arrangement, and makes it easy to
    add GbinStarTable implementations not based on a GBIN file
    as such.  It wasn't done that way in the first place because
    the reading is a bit fiddly, since you have to read the
    first item from the stream to find out what class it has
    before you can set up the table column metadata.
    But it's done now.

---
+++ gbin/src/main/uk/ac/starlink/gbin/GbinStarTable.java
@@ -0,0 +1,216 @@
+package uk.ac.starlink.gbin;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import uk.ac.starlink.table.AbstractStarTable;
+import uk.ac.starlink.table.ColumnInfo;
+import uk.ac.starlink.table.DescribedValue;
+import uk.ac.starlink.table.RowSequence;
+
+
+/**
+ * Partial StarTable implementation for use with GBIN files.
+ *
+ * @author   Mark Taylor
+ * @since    6 Jun 2017
+ */
+public abstract class GbinStarTable extends AbstractStarTable {
+
+    private final Class<?> gobjClazz_;
+    private final int ncol_;
+    private final ItemReader[] itemReaders_;
+    private final ColumnInfo[] colInfos_;
+
+    /**
+     * Constructor.
+     *
+     * @param  profile  configures how GBIN files will be mapped to a table
+     * @param  gobjClazz   class of all objects in GBIN file
+     */
+    protected GbinStarTable( GbinTableProfile profile, Class<?> gobjClazz ) {
+        gobjClazz_ = gobjClazz;
+
+        /* Table metadata. */
+        setParameter( new DescribedValue( GbinTableBuilder.CLASSNAME_INFO,
+                                          gobjClazz.getName() ) );
+
+        /* Get the "official" gaia table name from the class of this object,
+         * if it has one. */
+        String gaiaTableName = GbinMetadataReader.getGaiaTableName( gobjClazz );
+
+        /* Get additional metadata about this table from the classpath
+         * data model classes if available. */
+        final GaiaTableMetadata tmeta;
+        if ( gaiaTableName != null ) {
+            setName( gaiaTableName );
+            setParameter( new DescribedValue( GbinTableBuilder
+                                             .GAIATABLENAME_INFO,
+                                              gaiaTableName ) );
+            tmeta = GbinMetadataReader.getTableMetadata( gaiaTableName );
+        }
+        else {
+            tmeta = null;
+        }
+
+        /* Table description. */
+        if ( tmeta != null ) {
+            String descrip = tmeta.getTableDescription();
+            if ( descrip != null ) {
+                setParameter( new DescribedValue( GbinTableBuilder.DESCRIP_INFO,
+                                                  descrip ) );
+            }
+        }
+
+        /* Construct an array of readers, one for each column, based on
+         * the object class. */
+        itemReaders_ = ItemReader.createItemReaders( gobjClazz, profile );
+        ncol_ = itemReaders_.length;
+
+        /* Group readers by the leaf name of the items they read.
+         * The main purpose of this is so we can tell if there are
+         * duplicate names, which in turn will affect how columns
+         * are named. */
+        Map<String,List<ItemReader>> nameMap =
+            new HashMap<String,List<ItemReader>>();
+        boolean forceHier = profile.isHierarchicalNames();
+        String separator = profile.getNameSeparator();
+        if ( ! forceHier ) {
+            for ( int ic = 0; ic < ncol_; ic++ ) {
+                ItemReader irdr = itemReaders_[ ic ];
+                String name = irdr.getItemName();
+                if ( ! nameMap.containsKey( name ) ) {
+                    nameMap.put( name, new ArrayList<ItemReader>() );
+                }
+                nameMap.get( name ).add( irdr );
+            }
+        }
+
+        /* Construct a list of column infos based on what we have. */
+        colInfos_ = new ColumnInfo[ ncol_ ];
+        for ( int ic = 0; ic < ncol_; ic++ ) {
+
+            /* Basic column info. */
+            ItemReader irdr = itemReaders_[ ic ];
+            String cname =
+                ItemReader.getColumnName( irdr, nameMap, forceHier, separator );
+            ColumnInfo cinfo =
+                new ColumnInfo( cname, irdr.getItemContentClass(), null );
+            colInfos_[ ic ] = cinfo;
+
+            /* Additional column info from GbinMetadataReader if available. */
+            String archName = GbinMetadataReader
+                             .convertNameToArchiveFormat( cinfo.getName() );
+            if ( archName != null ) {
+                cinfo.setName( archName );
+            }
+            if ( tmeta != null ) {
+
+                /* Column description. */
+                String descrip = tmeta.getParameterDescription( cname );
+                if ( descrip != null && descrip.trim().length() > 0 ) {
+                    cinfo.setDescription( descrip );
+                }
+
+                /* Column UCDs. */
+                List<?> ucdList = tmeta.getUcds( cname );
+                if ( ucdList != null && ucdList.size() > 0 ) {
+                    StringBuffer ucdBuf = new StringBuffer();
+                    for ( Object ucd : ucdList ) {
+                        if ( ucdBuf.length() > 0 ) {
+                            ucdBuf.append( ";" );
+                        }
+                        ucdBuf.append( ucd );
+                    }
+                    if ( ucdBuf.length() > 0 ) {
+                        cinfo.setUCD( ucdBuf.toString() );
+                    }
+                }
+
+                /* Column extra detail (ends up in STIL but probably
+                 * not in any serialized formats). */
+                String detail =
+                    tmeta.getParameterDetailedDescription( cname );
+                if ( detail != null && detail.trim().length() > 0 ) {
+                    cinfo.setAuxDatum( new DescribedValue( GbinTableBuilder
+                                                          .COLDETAIL_INFO,
+                                                           detail ) );
+                }
+            }
+        }
+    }
+
+    public int getColumnCount() {
+        return ncol_;
+    }
+
+    public ColumnInfo getColumnInfo( int icol ) {
+        return colInfos_[ icol ];
+    }
+
+    /**
+     * Returns a row sequence for this table based on some required objects.
+     *
+     * @param   reader  gbin object reader positioned after the first object
+     *                  in the stream; must read objects suitable for this table
+     * @param   gobj0   first object read from reader
+     * @return  new row sequence for this table
+     */
+    public RowSequence createRowSequence( final GbinObjectReader reader,
+                                          final Object gobj0 ) {
+        final Map<ItemReader,Object> itemMap = new HashMap<ItemReader,Object>();
+        return new RowSequence() {
+            boolean started;
+            public boolean next() throws IOException {
+                itemMap.clear();
+                if ( started ) {
+                    if ( reader.hasNext() ) {
+                        itemMap.put( ItemReader.ROOT, reader.next() );
+                        return true;
+                    }
+                    else {
+                        return false;
+                    }
+                }
+                else {
+                    started = true;
+                    itemMap.put( ItemReader.ROOT, gobj0 );
+                    return true;
+                }
+            }
+            public Object getCell( int icol ) throws IOException {
+                return itemReaders_[ icol ].readItem( itemMap );
+            }
+            public Object[] getRow() throws IOException {
+                Object[] row = new Object[ ncol_ ];
+                for ( int ic = 0; ic < ncol_; ic++ ) {
+                    row[ ic ] = itemReaders_[ ic ].readItem( itemMap );
+                }
+                return row;
+            }
+            public void close() {
+            }
+        };
+    }
+
+    /**
+     * Returns the class of the elements contained in the GBIN file.
+     *
+     * @return  element class
+     */
+    public Class<?> getGaiaObjectClass() {
+        return gobjClazz_;
+    }
+
+    /**
+     * Returns the ItemReaders used by this table.
+     *
+     * @return   array of ItemReaders, one for each column
+     */
+    ItemReader[] getItemReaders() {
+        return itemReaders_;
+    }
+}
--- gbin/src/main/uk/ac/starlink/gbin/GbinTableBuilder.java
+++ gbin/src/main/uk/ac/starlink/gbin/GbinTableBuilder.java
@@ -8,8 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import uk.ac.starlink.table.AbstractStarTable;
-import uk.ac.starlink.table.ColumnInfo;
 import uk.ac.starlink.table.DefaultValueInfo;
 import uk.ac.starlink.table.DescribedValue;
 import uk.ac.starlink.table.RowSequence;
@@ -19,6 +17,7 @@ import uk.ac.starlink.table.TableBuilder;
 import uk.ac.starlink.table.TableFormatException;
 import uk.ac.starlink.table.TableSink;
 import uk.ac.starlink.table.ValueInfo;
+import uk.ac.starlink.table.WrapperRowSequence;
 import uk.ac.starlink.util.DataSource;
 
 /**
@@ -97,24 +96,31 @@ public class GbinTableBuilder implements TableBuilder {
 
     public void streamStarTable( InputStream in, TableSink sink, String pos )
             throws IOException {
-        GbinTableReader trdr = null;
         try {
-            trdr = new GbinTableReader( new BufferedInputStream( in ),
-                                        profile_ );
-            sink.acceptMetadata( new GbinStarTable( trdr, -1 ) {
+            GbinObjectReader reader = GbinObjectReader.createReader( in );
+            if ( ! reader.hasNext() ) {
+                throw new IOException( "No objects in GBIN file" );
+            }
+            Object gobj0 = reader.next();
+            GbinStarTable table =
+                    new GbinStarTable( profile_, gobj0.getClass() ) {
+                public long getRowCount() {
+                    return -1;
+                }
                 public RowSequence getRowSequence() {
                     throw new UnsupportedOperationException();
                 }
-            } );
-            while ( trdr.next() ) {
-                sink.acceptRow( trdr.getRow() );
+            };
+            sink.acceptMetadata( table );
+            RowSequence rseq = table.createRowSequence( reader, gobj0 );
+            while ( rseq.next() ) {
+                sink.acceptRow( rseq.getRow() );
             }
             sink.endRows();
+            rseq.close();
         }
         finally {
-            if ( trdr != null ) {
-                trdr.close();
-            }
+            in.close();
         }
     }
 
@@ -128,11 +134,14 @@ public class GbinTableBuilder implements TableBuilder {
             throw new TableFormatException( "Not GBIN" );
         }
 
-        /* Read the start of the file to obtain essential metadata. */
-        GbinTableReader trdr =
-            new GbinTableReader(
-                new BufferedInputStream( datsrc.getInputStream() ), profile_ );
-        trdr.close();
+        /* Read an object out of the file to find out the data type. */
+        InputStream in = new BufferedInputStream( datsrc.getInputStream() );
+        GbinObjectReader rdr = GbinObjectReader.createReader( in );
+        if ( ! rdr.hasNext() ) {
+            throw new IOException( "No objects in GBIN file" );
+        }
+        Class<?> gobjClazz = rdr.next().getClass();
+        in.close();
 
         /* If required, read the start of the file again to obtain
          * non-essential metadata.  This requires another read, since the
@@ -170,118 +179,31 @@ public class GbinTableBuilder implements TableBuilder {
 
         /* Construct a table with the metadata we have obtained,
          * and return it. */
-        StarTable table = new GbinStarTable( trdr, nrow ) {
+        final long nrow0 = nrow;
+        StarTable table = new GbinStarTable( profile_, gobjClazz ) {
             public RowSequence getRowSequence() throws IOException {
-                return new GbinTableReader(
-                           new BufferedInputStream( datsrc.getInputStream() ),
-                           profile_ );
+                final InputStream in =
+                    new BufferedInputStream( datsrc.getInputStream() );
+                GbinObjectReader reader = GbinObjectReader.createReader( in );
+                if ( ! reader.hasNext() ) {
+                    throw new IOException( "No objects in GBIN file" );
+                }
+                Object gobj0 = reader.next();
+                return new WrapperRowSequence( createRowSequence( reader,
+                                                                  gobj0 ) ) {
+                    @Override
+                    public void close() throws IOException {
+                        in.close();
+                    }
+                };
+            }
+            public long getRowCount() {
+                return nrow0;
             }
         };
         for ( DescribedValue param : params ) {
             table.setParameter( param );
         }
         return table;
-    }
-
-    /** 
-     * Partial StarTable implementation for use with GBIN files.
-     */
-    private static abstract class GbinStarTable extends AbstractStarTable {
-        private final ColumnInfo[] colInfos_;
-        private final long nrow_;
-
-        /**
-         * Constructor.
-         *
-         * @param  trdr  contains metadata about the table;
-         *               will not be used to read data (may be closed)
-         * @param  nrow  row count (-1 if not known)
-         */
-        GbinStarTable( GbinTableReader trdr, long nrow ) {
-            nrow_ = nrow;
-            setParameter( new DescribedValue( CLASSNAME_INFO,
-                                              trdr.getItemClass().getName() ) );
-
-            /* Get additional metadata about this table from the classpath
-             * data model classes if available. */
-            String gaiaTableName = trdr.getGaiaTableName();
-            final GaiaTableMetadata tmeta;
-            if ( gaiaTableName != null ) {
-                setName( gaiaTableName );
-                setParameter( new DescribedValue( GAIATABLENAME_INFO,
-                                                  gaiaTableName ) );
-                tmeta = GbinMetadataReader.getTableMetadata( gaiaTableName );
-            }
-            else {
-                tmeta = null;
-            }
-
-            /* Table description. */
-            if ( tmeta != null ) {
-                String descrip = tmeta.getTableDescription();
-                if ( descrip != null ) {
-                    setParameter( new DescribedValue( DESCRIP_INFO, descrip ) );
-                }
-            }
-
-            /* Column metadata. */
-            ColumnInfo[] cinfos = trdr.getColumnInfos();
-            int ncol = cinfos.length;
-            colInfos_ = new ColumnInfo[ ncol ];
-            for ( int icol = 0; icol < ncol; icol++ ) {
-                ColumnInfo cinfo = new ColumnInfo( cinfos[ icol ] );
-                String archName = GbinMetadataReader
-                                 .convertNameToArchiveFormat( cinfo.getName() );
-                if ( archName != null ) {
-                    cinfo.setName( archName );
-                }
-                if ( tmeta != null ) {
-                    String cname = cinfo.getName();
-
-                    /* Column description. */
-                    String descrip = tmeta.getParameterDescription( cname );
-                    if ( descrip != null && descrip.trim().length() > 0 ) {
-                        cinfo.setDescription( descrip );
-                    }
-
-                    /* Column UCDs. */
-                    List<?> ucdList = tmeta.getUcds( cname );
-                    if ( ucdList != null && ucdList.size() > 0 ) {
-                        StringBuffer ucdBuf = new StringBuffer();
-                        for ( Object ucd : ucdList ) {
-                            if ( ucdBuf.length() > 0 ) {
-                                ucdBuf.append( ";" );
-                            }
-                            ucdBuf.append( ucd );
-                        }
-                        if ( ucdBuf.length() > 0 ) {
-                            cinfo.setUCD( ucdBuf.toString() );
-                        }
-                    }
-
-                    /* Column extra detail (ends up in STIL but probably
-                     * not in any serialized formats). */
-                    String detail =
-                        tmeta.getParameterDetailedDescription( cname );
-                    if ( detail != null && detail.trim().length() > 0 ) {
-                        cinfo.setAuxDatum( new DescribedValue( COLDETAIL_INFO,
-                                                               detail ) );
-                    }
-                }
-                colInfos_[ icol ] = cinfo;
-            }
-        }
-
-        public int getColumnCount() {
-            return colInfos_.length;
-        }
-
-        public ColumnInfo getColumnInfo( int icol ) {
-            return colInfos_[ icol ];
-        }
-
-        public long getRowCount() {
-            return nrow_;
-        }
     }
 }
