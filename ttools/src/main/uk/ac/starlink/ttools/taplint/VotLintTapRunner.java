cddd704387267258b1c237c701d4b0dfd31a5086
Mark Taylor <m.b.taylor@bristol.ac.uk>
2017-11-30 15:30:29 +0000

    ttools: taplint now includes VOTable DTD/XSD parsing
    Fix taplint's VotLintTapRunner so that the VOTables it parses
    (when VOTable checking is configured) are validated against the
    relevant XML Schema/DTD alongside the custom votlint checks.
    I think it was always supposed to do that, but it looks like
    it hasn't been, at least in recent versions.

--- ttools/src/main/uk/ac/starlink/ttools/taplint/VotLintTapRunner.java
+++ ttools/src/main/uk/ac/starlink/ttools/taplint/VotLintTapRunner.java
@@ -28,11 +28,10 @@ import uk.ac.starlink.ttools.votlint.SaxMessager;
 import uk.ac.starlink.ttools.votlint.VersionDetector;
 import uk.ac.starlink.ttools.votlint.VotLintContentHandler;
 import uk.ac.starlink.ttools.votlint.VotLintContext;
-import uk.ac.starlink.ttools.votlint.VotLintEntityResolver;
+import uk.ac.starlink.ttools.votlint.VotLinter;
 import uk.ac.starlink.util.Compression;
 import uk.ac.starlink.util.ContentCoding;
 import uk.ac.starlink.util.DOMUtils;
-import uk.ac.starlink.util.MultiplexInvocationHandler;
 import uk.ac.starlink.util.StarEntityResolver;
 import uk.ac.starlink.vo.TapQuery;
 import uk.ac.starlink.vo.UwsJob;
@@ -268,25 +267,10 @@ public abstract class VotLintTapRunner extends TapRunner {
             SaxMessager messager = new ReporterSaxMessager( reporter );
             VotLintContext vlContext =
                 new VotLintContext( version, true, messager );
-            parser = createParser( reporter, vlContext );
-            ContentHandler vcHandler =
-                    new MultiplexInvocationHandler<ContentHandler>(
-                            new ContentHandler[] {
-                            new VotLintContentHandler( vlContext ),
-                            domHandler } )
-                   .createMultiplexer( ContentHandler.class );
-            parser.setErrorHandler( new ReporterErrorHandler( reporter ) );
-            parser.setContentHandler( vcHandler );
+            parser = new VotLinter( vlContext ).createParser( domHandler );
         }
         else {
-            parser = createParser( reporter );
-            parser.setEntityResolver( StarEntityResolver.getInstance() );
-            parser.setErrorHandler( new ErrorHandler() {
-                public void warning( SAXParseException err ) {}
-                public void error( SAXParseException err ) {}
-                public void fatalError( SAXParseException err ) {}
-            } );
-            parser.setContentHandler( domHandler );
+            parser = createBasicParser( reporter, domHandler );
         }
 
         /* Perform the parse and retrieve the resulting DOM. */
@@ -484,19 +468,26 @@ public abstract class VotLintTapRunner extends TapRunner {
     }
 
     /**
-     * Returns a basic SAX parser.  If it fails, the error will be reported
+     * Returns a basic SAX parser that uses a supplied content handler.
+     * Any SAX messages are ignored, not reported.
+     *
+     * <p>If the SAX parser can't be created, an error will be reported
      * and an exception will be thrown.
      *
      * @param  reporter  validation message destination
      * @return  basically configured SAX parser
      */
-    private static XMLReader createParser( Reporter reporter )
+    private static XMLReader createBasicParser( Reporter reporter,
+                                                ContentHandler contentHandler )
             throws SAXException {
+
+        /* Create an XMLReader. */
+        final XMLReader parser;
         try {
             SAXParserFactory spfact = SAXParserFactory.newInstance();
             spfact.setValidating( false );
             spfact.setNamespaceAware( true );
-            return spfact.newSAXParser().getXMLReader();
+            parser = spfact.newSAXParser().getXMLReader();
         }
         catch ( ParserConfigurationException e ) {
             reporter.report( FixedCode.F_CAPC,
@@ -504,38 +495,15 @@ public abstract class VotLintTapRunner extends TapRunner {
             throw (SAXException) new SAXException( e.getMessage() )
                                 .initCause( e );
         }
-    }
 
-    /**
-     * Returns a SAX parser suitable for VOTable checking.
-     * Its handlers are not set.
-     *
-     * @param  reporter   validation message destination
-     * @param  vlContext  information about votlint config
-     */
-    private static XMLReader createParser( Reporter reporter,
-                                           VotLintContext vlContext )
-            throws SAXException {
-        XMLReader parser = createParser( reporter );
-
-        /* Install a custom entity resolver.  This is also installed as
-         * a lexical handler, to guarantee that whatever is named in the
-         * DOCTYPE declaration is actually interpreted as the VOTable DTD. */
-        VotLintEntityResolver entityResolver =
-            new VotLintEntityResolver( vlContext );
-        try {
-            parser.setProperty( "http://xml.org/sax/properties/lexical-handler",
-                                entityResolver );
-            parser.setEntityResolver( entityResolver );
-        }
-        catch ( SAXException e ) {
-            parser.setEntityResolver( StarEntityResolver.getInstance() );
-            reporter.report( FixedCode.F_XENT,
-                             "Entity trouble - DTD validation may not be " +
-                             "done properly", e );
-        }
-
-        /* Return. */
+        /* Configure it as required. */
+        parser.setContentHandler( contentHandler );
+        parser.setEntityResolver( StarEntityResolver.getInstance() );
+        parser.setErrorHandler( new ErrorHandler() {
+            public void warning( SAXParseException err ) {}
+            public void error( SAXParseException err ) {}
+            public void fatalError( SAXParseException err ) {}
+        } );
         return parser;
     }
 
