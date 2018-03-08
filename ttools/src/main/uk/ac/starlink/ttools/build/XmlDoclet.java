03fc8ce700dc7ddc62f9fa22df4acf49283fc630
Mark Taylor <m.b.taylor@bristol.ac.uk>
2018-03-08 17:15:09 +0000

    ttools: allow <pre> in JEL function javadocs
    The XmlDoclet that transforms normal javadoc comments from the
    functions in ttools/func intended for the user expression language
    now copes with <pre> blocks as well as a few other HTML constructs.
    More could be added.

--- ttools/src/main/uk/ac/starlink/ttools/build/XmlDoclet.java
+++ ttools/src/main/uk/ac/starlink/ttools/build/XmlDoclet.java
@@ -123,7 +123,9 @@ public abstract class XmlDoclet extends MemberDoclet {
      */
     public static String doctorText( String text ) {
         text = text.replaceAll( "<a href=", "<webref plaintextref='yes' url=" )
-                   .replaceAll( "</a>", "</webref>" );
+                   .replaceAll( "</a>", "</webref>" )
+                   .replaceAll( "<pre>", "<verbatim>" )
+                   .replaceAll( "</pre>", "</verbatim>" );
         return pWrap( text );
     }
 
