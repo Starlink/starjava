7996e0c61c1925b6ff83c28ce1edf5b4b806e216
Mark Taylor <m.b.taylor@bristol.ac.uk>
2018-03-15 10:00:36 +0000

    ttools: add MinimalConfigMeta class
    Utility class that makes it easier to knock up a quick Plotter
    implementation for prototyping or tests.

---
+++ ttools/src/main/uk/ac/starlink/ttools/plot2/config/MinimalConfigMeta.java
@@ -0,0 +1,23 @@
+package uk.ac.starlink.ttools.plot2.config;
+
+/**
+ * ConfigMeta implementation that conforms minimally to the requirements.
+ * This is only intended for prototyping and test usage, not production code;
+ * config keys using instances of this class will not be properly documented
+ * in the user-facing documentation.
+ *
+ * @author   Mark Taylor
+ * @since    15 Mar 2018
+ */
+public class MinimalConfigMeta extends ConfigMeta {
+
+    /**
+     * Constructor.
+     *
+     * @param  name  basic name for this item
+     */
+    public MinimalConfigMeta( String name ) {
+        super( name, name );
+        setXmlDescription( "<p>(no description)</p>" );
+    }
+}
