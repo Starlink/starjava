97ad0aab39ae0fffb1806fab24bd197d7025bbd0
Margarida Castro Neves <mcneves@ari.uni-heidelberg.de>
2018-03-19 17:58:49 +0100

    splat: removed unused import

--- splat/src/main/uk/ac/starlink/splat/plot/PlotControl.java
+++ splat/src/main/uk/ac/starlink/splat/plot/PlotControl.java
@@ -33,7 +33,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.OptionalDouble;
 
 import javax.print.attribute.PrintRequestAttributeSet;
 import javax.swing.AbstractAction;
