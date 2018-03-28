11605e1ee9a0d2dbb3ac04e6e90db26b37536d7b
Mark Taylor <m.b.taylor@bristol.ac.uk>
2018-03-28 13:58:23 +0100

    topcat: ActionForwarder now implements ListDataListener
    As a convenience, ActionForwarder can now impersonate a ListDataListener
    as well as a ChangeListener and an ActionListener;

--- topcat/src/main/uk/ac/starlink/topcat/ActionForwarder.java
+++ topcat/src/main/uk/ac/starlink/topcat/ActionForwarder.java
@@ -7,17 +7,22 @@ import java.util.Iterator;
 import java.util.List;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import javax.swing.event.ListDataEvent;
+import javax.swing.event.ListDataListener;
 
 /**
  * ActionListener implementation which forwards all ActionEvents to a list
  * of clients.
- * It also implements ChangeListener, and any ChangeEvents it receives
- * will be turned into ActionEvents and forwarded as well.
+ * It also implements some other listener interfaces,
+ * currently <code>ChangeListener</code> and <code>ListDataListener</code>.
+ * Any events received throught those interfaces will be adapted into
+ * ActionEvents and forwarded as well.
  *
  * @author   Mark Taylor 
  * @since    28 Oct 2005
  */
-public class ActionForwarder implements ActionListener, ChangeListener {
+public class ActionForwarder
+             implements ActionListener, ChangeListener, ListDataListener {
 
     private final List listeners_ = new ArrayList();
 
@@ -48,5 +53,17 @@ public class ActionForwarder implements ActionListener, ChangeListener {
 
     public void stateChanged( ChangeEvent evt ) {
         actionPerformed( new ActionEvent( evt.getSource(), 0, "Change" ) );
+    }
+
+    public void contentsChanged( ListDataEvent evt ) {
+        actionPerformed( new ActionEvent( evt.getSource(), 0, "List" ) );
+    }
+
+    public void intervalAdded( ListDataEvent evt ) {
+        actionPerformed( new ActionEvent( evt.getSource(), 1, "List" ) );
+    }
+
+    public void intervalRemoved( ListDataEvent evt ) {
+        actionPerformed( new ActionEvent( evt.getSource(), 2, "List" ) );
     }
 }
