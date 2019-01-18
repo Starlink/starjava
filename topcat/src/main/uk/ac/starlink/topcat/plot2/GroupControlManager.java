Starlink/starjava

commit 67c75e50bf40f4c52c7ad865a6ebb3f43708ffe7

topcat: initialise plot with Healpix where appropriate
In the case that the current table can be identified as a HEALPix
index table, and a sky plot is requested, initialise the plot
with a HEALPix layer rather than the usual scatter plot layer.

This special casing is a bit questionable (might confuse users),
but it's pretty likely that this kind of table will not have sensible
lon/lat coordinates, so a normal scatter plot is not wanted.

topcat/src/main/uk/ac/starlink/topcat/plot2/GroupControlManager.java
@@ -12,6 +12,7 @@ import java.util.logging.Logger;
 import javax.swing.Action;
 import javax.swing.Icon;
 import uk.ac.starlink.table.ColumnData;
+import uk.ac.starlink.table.HealpixTableInfo;
 import uk.ac.starlink.topcat.BasicAction;
 import uk.ac.starlink.topcat.ColumnDataComboBoxModel;
 import uk.ac.starlink.topcat.ResourceIcon;
@@ -160,11 +161,34 @@ public class GroupControlManager implements ControlManager {
     }
 
     public Control createDefaultControl( TopcatModel tcModel ) {
-        Action act0 = stackActs_[ 0 ];
-        for ( int ia = 0; ia < stackActs_.length; ia++ ) {
-            if ( stackActs_[ ia ] instanceof LayerControlAction ) {
-                LayerControlAction cact = (LayerControlAction) stackActs_[ ia ];
-                LayerControl control = cact.createLayerControl();
+
+        /* For the special case where the table is known to contain HEALPix
+         * indices, and one of the layer controls can plot those, use that
+         * control.  In most cases, the other controls will not be appropriate
+         * for such a table. */
+        if ( HealpixTableInfo.isHealpix( tcModel.getDataModel()
+                                                .getParameters() ) ) {
+            for ( Action stackAct : stackActs_ ) {
+                if ( stackAct instanceof LayerControlAction ) {
+                    LayerControlAction cact = (LayerControlAction) stackAct;
+                    if ( cact.getPlotter() instanceof HealpixPlotter ) {
+                        LayerControl control = cact.createLayerControl();
+                        assert control instanceof HealpixLayerControl;
+                        if ( control instanceof HealpixLayerControl ) {
+                            ((HealpixLayerControl) control)
+                                                  .setTopcatModel( tcModel );
+                            return control;
+                        }
+                    }
+                }
+            }
+        }
+
+        /* Otherwise, just pick the first control. */
+        for ( Action stackAct : stackActs_ ) {
+            if ( stackAct instanceof LayerControlAction ) {
+                LayerControl control =
+                    ((LayerControlAction) stackAct).createLayerControl();
                 if ( control instanceof FormLayerControl ) {
                     ((FormLayerControl) control).setTopcatModel( tcModel );
                 }

commit 6dc6c544119f40ac1475bc3b0eca47f8a81a34b6

topcat: avoid recently introduced plot window NPE bug
Check for null TopcatModel when making tests of plotter type.
Opening a plot window with no selected table was failing with
a NullPointerException since 67c75e50bf40 (not in last release).

topcat/src/main/uk/ac/starlink/topcat/plot2/GroupControlManager.java
@@ -166,7 +166,8 @@ public class GroupControlManager implements ControlManager {
          * indices, and one of the layer controls can plot those, use that
          * control.  In most cases, the other controls will not be appropriate
          * for such a table. */
-        if ( HealpixTableInfo.isHealpix( tcModel.getDataModel()
+        if ( tcModel != null &&
+             HealpixTableInfo.isHealpix( tcModel.getDataModel()
                                                 .getParameters() ) ) {
             for ( Action stackAct : stackActs_ ) {
                 if ( stackAct instanceof LayerControlAction ) {