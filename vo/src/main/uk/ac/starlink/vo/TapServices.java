43ff355d6cabd7880e5a6bc807a6747ac82fc834
Mark Taylor <m.b.taylor@bristol.ac.uk>
2019-03-27 14:13:21 +0000

    vo: extract TapService instances from TapCapabilitiesDoc
    Provide methods in class TapServices to pull out a list of TapService
    instances from the content of a parsed TAP capabilities document.
    
    There are various ways of doing this: the current implementation
    pulls out one TapService for each interface with
    standardId of "ivo://ivoa.net/std/TAP", which corresponds to what
    will probably be in the TAP 1.1 standard, though that will be
    modified by declared securityMethods.

--- vo/src/main/uk/ac/starlink/vo/TapServices.java
+++ vo/src/main/uk/ac/starlink/vo/TapServices.java
@@ -2,7 +2,8 @@ package uk.ac.starlink.vo;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.logging.Level;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 /**
@@ -18,7 +19,13 @@ public class TapServices {
 
     /** GAVO RegTAP service is pretty reliable. */
     private static final TapService REGTAP =
-        initDefaultTapService( RegTapRegistryQuery.GAVO_REG );
+        createDefaultTapService( RegTapRegistryQuery.GAVO_REG );
+
+    /**
+     * Sole private constructor prevents instantiation.
+     */
+    private TapServices() {
+    }
 
     /**
      * Returns a default service  corresponding to a Relational Registry
@@ -35,37 +42,49 @@ public class TapServices {
     }
 
     /**
-     * Creates a TAP service from a string giving the base URL,
-     * with the endpoints in the default places.
+     * Creates a TAP 1.0 service given a base URL string.
+     * If the URL is bad, a warning is logged, and null is returned.
      *
-     * @param  baseUrl   base TAP URL
-     * @return   service using standard (v1.0) TAP endpoints
-     * @throws   IllegalArgumentException  in case of a bad URL
+     * @param   baseUrl   base TAP URL
+     * @return   service with standard (v1.0) TAP endpoints
      */
     public static TapService createDefaultTapService( String baseUrl ) {
         try {
             return createDefaultTapService( new URL( baseUrl ) );
         }
         catch ( MalformedURLException e ) {
-            throw (RuntimeException)
-                  new IllegalArgumentException( "Not a URL: " + baseUrl )
-                 .initCause( e );
+            logger_.warning( "Bad URL for TAP service: " + baseUrl );
+            return null;
         }
     }
 
     /**
-     * Creates a TAP service from a string giving the base URL,
+     * Creates a TAP 1.0 service given the base URL,
      * with the endpoints in the default places.
      *
-     * <p>This setup is more or less mandatory for
-     * TAP 1.0 services, but TAP 1.1 services can provide different
-     * sets of endpoints (capability/interface elements) for different
-     * purposes, for instance with different securityMethods.
-     *
      * @param  baseUrl   base TAP URL
-     * @return   TAP service with standard (v1.0) endpoints
+     * @return   service using standard (v1.0) TAP endpoints
+     * @throws   IllegalArgumentException  in case of a bad URL
      */
     public static TapService createDefaultTapService( URL baseUrl ) {
+        return createTapService( baseUrl, TapVersion.V10 );
+    }
+
+    /**
+     * Creates a TAP service given the base URL,
+     * with the endpoints in the default places and a specified TAP version.
+     *
+     * <p>This setup is more or less mandatory for
+     * TAP 1.0 services, but TAP 1.1 services may have more freedom
+     * to provide different sets of endpoints (capability/interface elements)
+     * for different purposes, for instance with different securityMethods.
+     *
+     * @param  baseUrl   base TAP URL
+     * @param  tapVersion  TAP protocol version
+     * @return   TAP service with standard endpoints
+     */
+    public static TapService
+            createTapService( URL baseUrl, final TapVersion tapVersion ) {
         final String identity = baseUrl.toString();
         final URL sync = appendPath( baseUrl, "/sync" );
         final URL async = appendPath( baseUrl, "/async" );
@@ -96,9 +115,83 @@ public class TapServices {
                 return examples;
             }
             public TapVersion getTapVersion() {
-                return TapVersion.V10;
+                return tapVersion;
             }
         };
+    }
+
+    /**
+     * Returns an array of TapService instances that are described by
+     * a supplied TAP capabilities document.
+     * The returned list will have at least one entry, but may have
+     * more if multiple TAP interfaces or security method variants
+     * have been declared.
+     *
+     * @param   baseUrl    base URL for TAP service (source of capabilities doc)
+     * @param   capsDoc    parsed capabilities document
+     * @return  array containing one or more TAP services
+     */
+    public static TapService[] createTapServices( URL baseUrl,
+                                                  TapCapabilitiesDoc capsDoc ) {
+        List<TapService> taps = new ArrayList<TapService>();
+        StdCapabilityInterface[] intfs = capsDoc.getInterfaces();
+        for ( StdCapabilityInterface intf : intfs ) {
+            URL accessUrl = getBaseUrl( intf.getAccessUrl() );
+            if ( "ivo://ivoa.net/std/TAP".equals( intf.getStandardId() ) &&
+                 "std".equals( intf.getRole() ) &&
+                 accessUrl != null ) {
+                TapVersion version = getTapVersion( intf.getVersion() );
+                taps.add( createTapService( accessUrl, version ) );
+            }
+        }
+        if ( taps.size() == 0 ) {
+            taps.add( createTapService( baseUrl, TapVersion.V10 ) );
+        }
+        return taps.toArray( new TapService[ 0 ] );
+    }
+
+    /**
+     * Turns a string giving a service access URL into a URL object.
+     * If it can't be done, null is returned and a warning is logged.
+     *
+     * @param  accessUrl  URL string
+     * @return  URL, or null
+     */
+    private static URL getBaseUrl( String accessUrl ) {
+        if ( accessUrl == null ) {
+            return null;
+        }
+        else {
+            try {
+                return new URL( accessUrl );
+            }
+            catch ( MalformedURLException e ) {
+                logger_.warning( "Ignore badly formed TAP base URL: "
+                               + accessUrl );
+                return null;
+            }
+        }
+    }
+
+    /**
+     * Returns the TAP version object corresponding to a version string.
+     * If it's unrecognised, log it and make something up.
+     *
+     * @param  version string
+     * @return  TAP version object, not null
+     */
+    private static TapVersion getTapVersion( String versStr ) {
+        if ( versStr == null || "1.0".equals( versStr ) ) {
+            return TapVersion.V10;
+        }
+        else if ( "1.1".equals( versStr ) ) {
+            return TapVersion.V11;
+        }
+        else {
+            logger_.warning( "Unrecognised TAP interface @version attribute \""
+                           + versStr + "\"; assume 1.0" );
+            return TapVersion.V10;
+        }
     }
 
     /**
@@ -117,25 +210,6 @@ public class TapServices {
             throw (RuntimeException)
                   new IllegalArgumentException( "Bad URL???" )
                  .initCause( e );
-        }
-    }
-
-    /**
-     * Initialises a default TAP service.
-     * If the URL is bad, a warning is logged, and null is returned.
-     *
-     * @param   baseUrl   base TAP URL
-     * @return   service with standard (v1.0) TAP endpoints
-     */
-    private static TapService initDefaultTapService( String baseUrl ) {
-        try {
-            return createDefaultTapService( new URL( baseUrl ) );
-        }
-        catch ( MalformedURLException e ) {
-            logger_.log( Level.WARNING,
-                         "Failed to get endpoints for TAP service: " + baseUrl,
-                         e );
-            return null;
         }
     }
 }
