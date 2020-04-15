ca2c5b270200f47e95a3f7b7ece6ba4c376ef1ab
Mark Taylor <m.b.taylor@bristol.ac.uk>
2020-04-15 16:25:03 +0100

    ttools: document use of time DomainMapper in plot2time

---
+++ ttools/src/main/uk/ac/starlink/ttools/build/MiscDoc.java
@@ -0,0 +1,54 @@
+package uk.ac.starlink.ttools.build;
+
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import uk.ac.starlink.task.Parameter;
+import uk.ac.starlink.ttools.plot2.data.Input;
+import uk.ac.starlink.ttools.plot2.geom.TimeDataGeom;
+import uk.ac.starlink.ttools.plot2.task.AbstractPlot2Task;
+
+/**
+ * Write some miscallaneous auto-documentation files for ingestion
+ * in the user document.
+ *
+ * <p>Invoke this from the class's <code>main</code> method.
+ *
+ * @author   Mark Taylor
+ * @since    15 Apr 2020
+ */
+public class MiscDoc {
+
+    /**
+     * Write text to a file in the current directory.
+     *
+     * @param  filename  relative filename
+     * @param  text   text to write
+     */
+    private static final void writeData( String filename, String text )
+            throws IOException {
+        File file = new File( new File( "." ), filename );
+        System.out.println( "Writing " + filename );
+        Writer out = new BufferedWriter(
+                         new OutputStreamWriter(
+                             new FileOutputStream( file ) ) );
+        out.write( text );
+        out.close();
+    }
+
+    /**
+     * Writes miscellaneous documentation files to the current directory.
+     */
+    public static void main( String[] args ) throws IOException {
+        Input timeInput = TimeDataGeom.T_COORD.getInput();
+        String timeSuffix = AbstractPlot2Task.EXAMPLE_LAYER_SUFFIX;
+        Parameter<?> ttypeParam = 
+            AbstractPlot2Task
+           .createDomainMapperParameter( timeInput, timeSuffix );
+        writeData( "ttypeN-param.xml",
+                   UsageWriter.xmlItem( ttypeParam, false ) );
+    }
+}
