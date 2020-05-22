29160b4ac026c1960e5a62b52f4c32fa4c098c61
Mark Taylor <m.b.taylor@bristol.ac.uk>
2020-05-22 14:09:11 +0100

    table: remove PixTools library from runtime
    The PixTools library of Nickolai Kuropatkine is no longer used anywhere
    in TABLE/STILTS/TOPCAT; it has been replaced with the cds-healpix-java
    library of F-X Pineau.  The jar files are removed from the build,
    though retained for comparison in the classpath used for unit
    testing of the TTOOLS package.
    
    Note the move from PixTools to cds-healpix-java in the topcat
    and stilts user document change logs.

--- table/src/testcases/uk/ac/starlink/table/join/RingBugTest.java
+++ table/src/testcases/uk/ac/starlink/table/join/RingBugTest.java
@@ -1,8 +1,10 @@
 package uk.ac.starlink.table.join;
 
-import gov.fnal.eag.healpix.PixTools;
+import cds.healpix.Healpix;
+import cds.healpix.HealpixNested;
+import cds.healpix.HealpixNestedBMOC;
+import cds.healpix.HealpixNestedFixedRadiusConeComputer;
 import java.util.List;
-import javax.vecmath.Vector3d;
 import junit.framework.TestCase;
 
 /**
@@ -16,58 +18,21 @@ public class RingBugTest extends TestCase {
         double scale = 0.0001 * PI;
         double decRad = PI / 18.0;
         double radius = scale * 4;
-        PixTools pt = new PixTools();
-        int nside = 1 << 2;
-        int inclusive = 0;
-        int nest = 0;
-        Vector3d v1 = pt.Ang2Vec( PI/2 + decRad, -0.1 * scale );
-        Vector3d v2 = pt.Ang2Vec( PI/2 + decRad, +0.1 * scale );
-        List tiles1 = pt.query_disc( nside, v1, radius, nest, inclusive );
-        List tiles2 = pt.query_disc( nside, v2, radius, nest, inclusive );
-        assertEquals( 1, tiles1.size() );
-        assertEquals( 1, tiles2.size() );
 
-        // In PixTools 2012-07-28, tiles1 contains pixel 120 instead of 104.
-        assertEquals( tiles2, tiles1 );
-
-        long itile = pt.nest2ring( nside, pt.vect2pix_nest( nside, v2 ) );
-        assertEquals( 104, itile );
-        assertEquals( itile, ((Long) tiles1.get( 0 )).longValue() );
-        assertEquals( itile, ((Long) tiles2.get( 0 )).longValue() );
-    }
-
-    public void testPoint4() {
-        int nside = 2;
-        PixTools pt = new PixTools();
-        for ( double theta = 0; theta < Math.PI; theta += 0.1 ) {
-            for ( double phi = -Math.PI; phi < Math.PI; phi += 0.1 ) {
-                Vector3d v = pt.Ang2Vec( theta, phi );
-                long irv1 = pt.vect2pix_ring( nside, v );
-                long inv1 = pt.vect2pix_nest( nside, v );
-                long irv2 = pt.nest2ring( nside, inv1 );
-
-                long ira1 = pt.ang2pix_ring( nside, theta, phi );
-                long ina1 = pt.ang2pix_nest( nside, theta, phi );
-                long ira2 = pt.nest2ring( nside, ina1 );
-
-                // This fails in some cases for 2012-07-28 PixTools version
-                assertEquals( irv1, irv2 );
-                assertEquals( ira1, ira2 );
-            }
-        }
-    }
-
-    public void testPoint5() {
-        PixTools pt = new PixTools();
-        double theta = 1.5;
-        double phi = -0.04;
-        Vector3d v = pt.Ang2Vec( theta, phi );
-        int nside = 2;
-        long inest = pt.vect2pix_nest( nside, v );
-        long iring = pt.vect2pix_ring( nside, v );
-        long iring2 = pt.nest2ring( nside, inest );
-
-        // Specifically, this fails for 2012-07-28 PixTools version.
-        assertEquals( iring, iring2 );
+        // This is not a particularly rigorous test,
+        // versions of PixTools 2012-07-28 got it wrong.
+        HealpixNested hn = Healpix.getNested( 2 );
+        HealpixNestedFixedRadiusConeComputer coner =
+            hn.newConeComputerApprox( radius );
+        HealpixNestedBMOC bmoc1 =
+            coner.overlappingCells( -0.1 * scale, - decRad );
+        HealpixNestedBMOC bmoc2 =
+            coner.overlappingCells( +0.1 * scale, - decRad );
+        assertEquals( 1, bmoc1.computeDeepSize() );
+        assertEquals( 1, bmoc2.computeDeepSize() );
+        long ring1 = hn.toRing( bmoc1.flatHashIterator().next() );
+        long ring2 = hn.toRing( bmoc2.flatHashIterator().next() );
+        assertEquals( ring1, ring2 );
+        assertEquals( 104, ring1 );
     }
 }
--- ttools/src/testcases/uk/ac/starlink/ttools/cone/SkyTilingTest.java
+++ ttools/src/testcases/uk/ac/starlink/ttools/cone/SkyTilingTest.java
@@ -1,11 +1,7 @@
 package uk.ac.starlink.ttools.cone;
 
-import gov.fnal.eag.healpix.PixTools;
 import java.util.BitSet;
-import java.util.Collections;
-import java.util.List;
 import java.util.Random;
-import javax.vecmath.Vector3d;
 import junit.framework.TestCase;
 import uk.ac.starlink.ttools.func.Tilings;
 
@@ -15,75 +11,6 @@ public class SkyTilingTest extends TestCase {
 
     public SkyTilingTest( String name ) {
         super( name );
-    }
-
-    public void testPixTools() {
-
-        /* Known to fail in early versions of PixTools. */
-        if ( false ) {
-            System.err.println( "Skipping test due to suspected PixTools bug" );
-        }
-        else {
-            int nside = 1 << 20;
-            Vector3d pos = new Vector3d( -0.704, 0.580, 0.408 );
-            double radius = 1.22E-7;
-            List nestPixels =
-                new PixTools().query_disc( nside, pos, radius, 1, 1 );
-            List ringPixels =
-                new PixTools().query_disc( nside, pos, radius, 0, 1 );
-            assertEquals( ringPixels.size(), nestPixels.size() );
-            for ( int i = 0; i < ringPixels.size(); i++ ) {
-                long iring = ((Number) ringPixels.get( i )).longValue();
-                long inest = new PixTools().ring2nest( nside, iring );
-                ringPixels.set( i, new Long( inest ) );
-            }
-            Collections.sort( nestPixels );
-            Collections.sort( ringPixels );
-            assertEquals( nestPixels, ringPixels );
-        }
-
-        /* Actually I'm not certain these should be 1-element arrays - but
-         * unless I've got very lucky with the vector position they should
-         * be. */
-        /* Following discussion with Nikolai Kouropatkine (Pixtools author) -
-         * this is probably not a bug, but it would be nice if it behaved
-         * otherwise (for performance reasons). */
-        /* Version 30 April 2008 - behaves better than the pervious version did.
-         * These queries now return 2 values rather than 4 values. */
-        if ( false ) {
-            System.err.println( "Skipping test due to known PixTools bug" );
-        }
-        else {
-
-//          assertEquals( 1, // would be nice, but not available yet
-//          assertTrue( 4 >= // 07-Jan-2008 version achieves this
-//          assertTrue( 2 >= // 30-Apr-2008 version achieves this
-            assertTrue( 4 >= // 28-Jul-2012 - reversion required by bugfix
-                new PixTools().query_disc( 1024,
-                                           new Vector3d( -0.704, 0.580, 0.408 ),
-                                           1.22E-12,
-                                           1, 1 ).size() );
-//          assertEquals( 1, // would be nice, but not available yet
-//          assertTrue( 4 >= // 07-Jan-2008 version achieves this
-//          assertTrue( 2 >= // 30-Apr-2008 version achieves this
-            assertTrue( 4 >= // 28-Jul-2012 - reversion required by bugfix
-                new PixTools().query_disc( 1024,
-                                           new Vector3d( -0.704, 0.580, 0.408 ),
-                                           1.22E-12,
-                                           0, 1 ).size() );
-        }
-    }
-
-    public void testGetNSide() {
-
-        /* Known to fail for early versions of PixTools. */
-        double radius = 0.25 / 3600;
-        for ( int i = 0; i < 20; i++ ) {
-            long nside = new PixTools().GetNSide( radius * 60 * 60 );
-            int l2n = (int) ( Math.log( nside ) / Math.log( 2 ) );
-            assertEquals( (int) Math.pow( 2, l2n ), nside );
-            radius *= 2;
-        }
     }
 
     public void testHtmTilings() {
@@ -123,30 +50,6 @@ public class SkyTilingTest extends TestCase {
                     new HtmTiling( level ).getPositionTile( ra, dec ),
                     Tilings.htmIndex( level, ra, dec ) );
             }
-        }
-    }
-
-    public void testPoints2() {
-        PixTools pt = new PixTools();
-        BitSet ringMap = new BitSet();
-        BitSet nestMap = new BitSet();
-        for ( int level = 0; level <= 2; level++ ) {
-            int npix = 12 * (int) Math.pow( 4, level );
-            int nside = 1 << level;
-            for ( long ipix = 0; ipix < npix; ipix++ ) {
-                long ipixNest = ipix;
-                long ipixRing = pt.nest2ring( nside, ipixNest );
-                assertEquals( ipixNest, pt.ring2nest( nside, ipixRing ) );
-                Vector3d pos = pt.pix2vect_nest( nside, ipix );
-                assertEquals( ipixNest, pt.vect2pix_nest( nside, pos ) );
-                assertEquals( ipixRing, pt.vect2pix_ring( nside, pos ) );
-                nestMap.set( (int) ipixNest );
-                ringMap.set( (int) ipixRing );
-            }
-            BitSet fullMap = new BitSet( npix );
-            fullMap.set( 0, npix );
-            assertEquals( fullMap, ringMap );
-            assertEquals( fullMap, nestMap );
         }
     }
 
