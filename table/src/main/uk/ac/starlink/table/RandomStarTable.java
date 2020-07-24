dee46f39357a0ebad1c8aac409c70305a9587047
Mark Taylor <m.b.taylor@bristol.ac.uk>
2020-07-24 14:45:09 +0100

    table: clarify requirements of RandomStarTable
    Improve the javadocs for RandomStarTable to make clear it should
    only be used for 'naturally' random-access table data.

--- table/src/main/uk/ac/starlink/table/RandomStarTable.java
+++ table/src/main/uk/ac/starlink/table/RandomStarTable.java
@@ -3,15 +3,19 @@ package uk.ac.starlink.table;
 import java.io.IOException;
 
 /**
- * Implements a StarTable based on random access.
- * The <tt>isRandom</tt> method always returns true, and the 
+ * Implements a StarTable based on row and cell accessor methods
+ * that are random access and thread-safe.
+ *
+ * <p>The <tt>isRandom</tt> method always returns true, and the 
  * <tt>getRowSequence</tt> method is implemented using the table's
- * (abstract) <tt>getCell</tt> and <tt>getRow</tt> methods.
- * <p>
- * Implementations of this object must supply a non-negative return value
- * for <tt>getColumnCount</tt> method, because the <tt>RowSequence</tt>
- * method requires this knowledge.
- * 
+ * (abstract) <tt>getCell</tt> and <tt>getRow</tt> methods,
+ * which must be safe for concurrent use from multiple threads.
+ * This implementation is only suitable where table data access is
+ * naturally thread-safe; in cases where synchronization or some
+ * other potentially expensive mechanism is used to secure thread-safety
+ * of <code>getCell</code>, it is better to use a different implementation
+ * with a better <code>getRowSequence</code> implementation.
+ *
  * @author   Mark Taylor (Starlink)
  */
 public abstract class RandomStarTable extends AbstractStarTable {
@@ -36,11 +40,15 @@ public abstract class RandomStarTable extends AbstractStarTable {
     }
 
     /**
-     * The number of rows in this table.  Implementations must supply
-     * a non-negative return value.
+     * Implementations must supply a non-negative return value.
      *
      * @return  the number of rows in the table
      */
     abstract public long getRowCount();
 
+    /**
+     * Implementations of this method must be safe for concurrent calls
+     * from multiple threads.
+     */
+    abstract public Object getCell( long irow, int icol ) throws IOException;
 }
