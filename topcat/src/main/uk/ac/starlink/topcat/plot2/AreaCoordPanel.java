090bdf66e8669c092c114968414e2830915ddaf9
Mark Taylor <m.b.taylor@bristol.ac.uk>
2020-09-22 12:32:05 +0100

    topcat: use AreaDomain.getProbableMapper for Area autopop
    When determining autopopulation of Area plot coordinates,
    use the AreaDomain.INSTANCE.getProbableMapper method rather
    than (re-)doing ad hoc guesses based on column metadata.
    AreaDomain is still a bit ad hoc, but at least this keeps
    the logic in one place.  It also improves current behaviour,
    e.g. allowing it to spot columns labelled with xtype="moc".

--- topcat/src/main/uk/ac/starlink/topcat/plot2/AreaCoordPanel.java
+++ topcat/src/main/uk/ac/starlink/topcat/plot2/AreaCoordPanel.java
@@ -2,11 +2,11 @@ package uk.ac.starlink.topcat.plot2;
 
 import uk.ac.starlink.table.ColumnData;
 import uk.ac.starlink.table.ColumnInfo;
-import uk.ac.starlink.table.Tables;
 import uk.ac.starlink.topcat.ColumnDataComboBoxModel;
 import uk.ac.starlink.ttools.plot2.PlotUtil;
 import uk.ac.starlink.ttools.plot2.config.ConfigKey;
 import uk.ac.starlink.ttools.plot2.data.AreaCoord;
+import uk.ac.starlink.ttools.plot2.data.AreaDomain;
 import uk.ac.starlink.ttools.plot2.data.Coord;
 
 /**
@@ -40,63 +40,16 @@ public abstract class AreaCoordPanel extends PositionCoordPanel {
             return;
         }
         int ncol = areaSelector.getSize();
-        int bestScore = 0;
-        ColumnData bestCdata = null;
         for ( int ic = 0; ic < ncol; ic++ ) {
             ColumnData cdata = areaSelector.getColumnDataAt( ic );
             if ( cdata != null ) {
                 ColumnInfo info = cdata.getColumnInfo();
-                if ( info != null ) {
-                    int score = getAreaScore( info );
-                    if ( score > bestScore ) {
-                        bestScore = score;
-                        bestCdata = cdata;
-                    }
+                if ( info != null &&
+                     AreaDomain.INSTANCE.getProbableMapper( info ) != null ) {
+                    areaSelector.setSelectedItem( cdata );
+                    return;
                 }
             }
         }
-        if ( bestCdata != null ) {
-            areaSelector.setSelectedItem( bestCdata );
-        }
-    }
-
-    /**
-     * Returns some kind of value which indicates how likely a column
-     * is to represent an area that can be plotted by this panel.
-     *
-     * @param   info  column metadata
-     * @return   score for area compability:
-     *           higher is better, zero means no use
-     */
-    private static int getAreaScore( ColumnInfo info ) {
-        int score = 0;
-        Class<?> clazz = info.getContentClass();
-        String name = info.getName();
-        String ucd = info.getUCD();
-        String xtype = Tables.getXtype( info );
-        boolean isString = clazz.equals( String.class );
-        boolean isNumarray = clazz.equals( float[].class )
-                          || clazz.equals( double[].class );
-        if ( isString || isNumarray ) {
-            if ( "s_region".equals( name ) ) {
-                score += 4;
-            }
-            if ( "pos.outline;obs.field".equals( ucd ) ) {
-                score += 3;
-            }
-            else if ( ucd != null && ucd.startsWith( "pos.outline" ) ) {
-                score += 2;
-            }
-        }
-        if ( isNumarray ) {
-            if ( "circle".equalsIgnoreCase( xtype ) ||
-                 "polygon".equalsIgnoreCase( xtype ) ) {
-                score += 5;
-            }
-            else if ( "point".equalsIgnoreCase( xtype ) ) {
-                score += 2;
-            }
-        }
-        return score;
     }
 }
