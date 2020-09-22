c4cb3ab37f7ed4844bab968c71f8c8f76ea3680b
Mark Taylor <m.b.taylor@bristol.ac.uk>
2020-09-22 14:48:18 +0100

    mirage: MirageTableWriter is now self-documenting

--- mirage/src/main/uk/ac/starlink/mirage/MirageTableWriter.java
+++ mirage/src/main/uk/ac/starlink/mirage/MirageTableWriter.java
@@ -6,9 +6,13 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import uk.ac.starlink.table.StarTable;
-import uk.ac.starlink.table.StreamStarTableWriter;
+import uk.ac.starlink.table.formats.DocumentedStreamStarTableWriter;
 
-public class MirageTableWriter extends StreamStarTableWriter {
+public class MirageTableWriter extends DocumentedStreamStarTableWriter {
+
+    public MirageTableWriter() {
+        super( new String[] { "mirage" } );
+    }
 
     public void writeStarTable( StarTable startab, OutputStream out )
             throws IOException {
@@ -20,15 +24,31 @@ public class MirageTableWriter extends StreamStarTableWriter {
         pstrm.flush();
     }
 
-    public boolean looksLikeFile( String filename ) {
-        return filename.endsWith( ".mirage" );
-    }
-
     public String getFormatName() {
         return "mirage";
     }
 
     public String getMimeType() {
         return "text/plain";
+    }
+
+    public boolean docIncludesExample() {
+        return true;
+    }
+
+    public String getXmlDescription() {
+        return String.join( "\n",
+            "<p>Mirage was a nice standalone tool for analysis of",
+            "multidimensional data, from which TOPCAT took some inspiration.",
+            "It was described in a 2007 paper",
+            "<a href='https://ui.adsabs.harvard.edu/abs/2007ASPC..371..391H/'"
+              + ">2007ASPC..371..391H</a>,",
+            "but no significant development seems to have taken place",
+            "since then.",
+            "This format is therefore probably obsolete, but you can still",
+            "write table output in Mirage-compatible format",
+            "if you like.",
+            "</p>",
+        "" );
     }
 }
