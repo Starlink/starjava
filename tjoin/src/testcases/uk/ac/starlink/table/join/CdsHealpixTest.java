d783b5fa81b24b541bd1e40d5b354c99b39a2e1b
Mark Taylor <m.b.taylor@bristol.ac.uk>
2021-04-19 11:34:06 +0100

    tjoin: cds-healpix-java bugfix update to v0.29.2
    Upgrade the cds-healpix-java library to v0.29.2.
    
    This avoids a numeric bug that could generate an
    ArrayIndexOutOfBoundsException for certain rare parameter values
    when calculating cells overlapping a cone.
    This could cause sky crossmatches to terminate with an error.
    
    This is in fact a resurfacing of about the same bug that provoked
    the last cds-healpix-java upgrade, but the fix this time is more robust
    (the data structure in question has now been made elastic rather
    than just increasing its maximum size as before), so it should not
    crop up again.
    See https://github.com/cds-astro/cds-healpix-java/issues/15.
    
    A unit test is added to check that the current library version
    has fixed this bug.
    
    The jar file was built from source, using the sources from
    https://github.com/cds-astro/cds-healpix-java,
    at tag v0.29.2 (743a57bdd2d6a).

--- tjoin/src/testcases/uk/ac/starlink/table/join/CdsHealpixTest.java
+++ tjoin/src/testcases/uk/ac/starlink/table/join/CdsHealpixTest.java
@@ -16,4 +16,28 @@ public class CdsHealpixTest extends TestCase {
                    .overlappingCells(0.5*Math.PI, -1.1617);
         assertEquals( 46, bmoc.size() );
     }
+
+    public void testBug2() {
+
+        // Prior to 0.29, this failed with an IllegalArgumentException.
+        // It's essentially the same as Bug1, but with different troublesome
+        // parameters.  According to F-X the fix applied this time
+        // (a data structure without a fixed size limit) ought to prevent
+        // similar behaviour for any parameter values in the future.
+        HealpixNestedBMOC bmoc =
+            Healpix.getNested(14)
+                   .newConeComputerApprox(0.001)
+                   .overlappingCells(0.002, -1.3);
+        assertEquals( 338, bmoc.size() );
+    }
+
+    void writeBmoc( HealpixNestedBMOC bmoc ) {
+        System.out.println( "Depth,Hash,Raw,Full" );
+        for ( HealpixNestedBMOC.CurrentValueAccessor acc : bmoc ) {
+            System.out.println( acc.getDepth() + ","
+                              + acc.getHash() + ","
+                              + acc.getRawValue() + ","
+                              + acc.isFull() );
+        }
+    }
 }
