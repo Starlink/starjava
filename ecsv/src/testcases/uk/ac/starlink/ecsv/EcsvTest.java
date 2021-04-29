c33ce83117fe5a8ca55eea387de9c2697e6ef08b
Mark Taylor <m.b.taylor@bristol.ac.uk>
2021-04-29 13:11:12 +0100

    ecsv: add more unit tests
    Add some tests confirming that table round-tripping gives
    byte-for-byte identical ECSV output for all known column types.
    This is a fairly good test of I/O handler mutual consistency.
    
    Also bring in a few new example files from the ECSV 1.0 update
    of the ECSV specification, that show how to use array subtypes:
    https://github.com/astropy/astropy-APEs/blob/master/APE6.rst
    
    Extract these from the text and check that they can be read correctly
    in unit tests.

--- ecsv/src/testcases/uk/ac/starlink/ecsv/EcsvTest.java
+++ ecsv/src/testcases/uk/ac/starlink/ecsv/EcsvTest.java
@@ -1,5 +1,6 @@
 package uk.ac.starlink.ecsv;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.URL;
@@ -9,8 +10,12 @@ import java.util.logging.Logger;
 import uk.ac.starlink.table.ColumnInfo;
 import uk.ac.starlink.table.DescribedValue;
 import uk.ac.starlink.table.StarTable;
+import uk.ac.starlink.table.StarTableFactory;
+import uk.ac.starlink.table.StarTableWriter;
 import uk.ac.starlink.table.StoragePolicy;
+import uk.ac.starlink.table.TableBuilder;
 import uk.ac.starlink.table.Tables;
+import uk.ac.starlink.table.TestTableScheme;
 import uk.ac.starlink.util.ByteArrayDataSource;
 import uk.ac.starlink.util.DataSource;
 import uk.ac.starlink.util.TestCase;
@@ -21,11 +26,12 @@ public class EcsvTest extends TestCase {
     private static final StoragePolicy STORAGE = StoragePolicy.PREFER_MEMORY;
 
     public EcsvTest() {
-        Logger.getLogger( "uk.ac.starlink.ecsv" ).setLevel( Level.WARNING );
+        Logger.getLogger( "uk.ac.starlink.ecsv" ).setLevel( Level.SEVERE );
     }
 
     public void testData2() throws IOException {
         StarTable table = readTable( "example2.ecsv" );
+        checkRoundTripSerialize( table );
         assertEquals( 2, table.getRowCount() );
         assertEquals( 2, table.getColumnCount() );
         assertEquals( new Double( 4.0 ), table.getCell( 1, 0 ) );
@@ -49,6 +55,7 @@ public class EcsvTest extends TestCase {
         StarTable table = readTable( "test1.ecsv" );
         table = Tables.randomTable( table );
         Tables.checkTable( table );
+        checkRoundTripSerialize( table );
         assertEquals( 12, table.getColumnCount() );
         assertEquals( new Double( 1.5 ), table.getCell( 1, 11 ) );
         assertEquals( "mbt",
@@ -57,6 +64,66 @@ public class EcsvTest extends TestCase {
         assertSameTable( table, t2 );
         StarTable t3 = roundTrip( table, EcsvTableWriter.COMMA_WRITER );
         assertSameTable( table, t3 );
+        assertEquals( 1, countDummy( table ) );
+        assertEquals( 1, countDummy( t3 ) );
+        Object[] blankRow = table.getRow( 2 );
+        for ( int i = 0; i < table.getColumnCount(); i++ ) {
+            assertTrue( blankRow[ i ] == null ||
+                        table.getColumnInfo( i ).getContentClass()
+                                                .equals( Boolean.class ) );
+        }
+    }
+
+    public void testScheme() throws IOException {
+        TestTableScheme tscheme = new TestTableScheme();
+        checkRoundTripSerialize( tscheme.createTable( "100,*" ) );
+    }
+
+    private void checkRoundTripSerialize( StarTable table ) throws IOException {
+        EcsvTableBuilder rdr = new EcsvTableBuilder();
+        roundTripSerialize( table, EcsvTableWriter.SPACE_WRITER, rdr );
+        roundTripSerialize( table, EcsvTableWriter.COMMA_WRITER, rdr );
+    }
+
+    private void roundTripSerialize( StarTable t0,
+                                     StarTableWriter writer,
+                                     TableBuilder reader )
+            throws IOException {
+        ByteArrayOutputStream out1 = new ByteArrayOutputStream();
+        writer.writeStarTable( t0, out1 );
+        out1.close();
+        byte[] buf1 = out1.toByteArray();
+        StarTableFactory tfact = new StarTableFactory();
+        tfact.setStoragePolicy( StoragePolicy.PREFER_MEMORY );
+        StarTable t1 =
+            tfact.makeStarTable( new ByteArrayInputStream( buf1 ), reader );
+        ByteArrayOutputStream out2 = new ByteArrayOutputStream();
+        writer.writeStarTable( t1, out2 );
+        out2.close();
+        byte[] buf2 = out2.toByteArray();
+        assertArrayEquals( buf1, buf2 );
+    }
+
+    public void testSubtypes() throws IOException {
+        // Tables from APE6 ECSV 1.0 update
+        // https://github.com/astropy/astropy-APEs/blob/master/APE6.rst
+        StarTable s1 = readTable( "subtype1.ecsv" );
+        StarTable s2 = readTable( "subtype2.ecsv" );
+        StarTable s3 = readTable( "subtype3.ecsv" );
+
+        ColumnInfo c1a = s1.getColumnInfo( 0 );
+        assertEquals( double[].class, c1a.getContentClass() );
+        assertArrayEquals( new int[] { 2, 3 }, c1a.getShape() );
+        assertEquals( 9.0, ((double[]) s1.getCell( 1, 0 ))[ 3 ] );
+
+        ColumnInfo c2a = s2.getColumnInfo( 0 );
+        assertEquals( long[].class, c2a.getContentClass() );
+        assertArrayEquals( new int[] { -1 }, c2a.getShape() );
+        assertArrayEquals( new long[] { 8, 9, 10 }, s2.getCell( 2, 0 ) );
+
+        ColumnInfo c3a = s3.getColumnInfo( 0 );
+        assertEquals( String.class, c3a.getContentClass() );
+        assertEquals( "true", s3.getCell( 2, 0 ) );
     }
 
     private void assertSameTable( StarTable t1, StarTable t2 ) {
@@ -76,11 +143,7 @@ public class EcsvTest extends TestCase {
             Object dummy1 = c1.getAuxDatumValueByName( "dummy", String.class );
             Object dummy2 = c2.getAuxDatumValueByName( "dummy", String.class );
             assertEquals( dummy1, dummy2 );
-            if ( dummy1 != null ) {
-                ndummy++;
-            }
         }
-        assertEquals( 1, ndummy );
 
         assertEquals( t1.getParameters().size(), t2.getParameters().size() );
         for ( DescribedValue param1 : t1.getParameters() ) {
@@ -95,6 +158,18 @@ public class EcsvTest extends TestCase {
                 assertEquals( pv1, pv2 );
             }
         }
+    }
+
+    private int countDummy( StarTable table ) {
+        int ndummy = 0;
+        for ( int ic = 0; ic < table.getColumnCount(); ic++ ) {
+            ColumnInfo cinfo = table.getColumnInfo( ic );
+            Object dummy = cinfo.getAuxDatumValueByName( "dummy", String.class);
+            if ( dummy != null ) {
+                ndummy++;
+            }
+        }
+        return ndummy;
     }
 
     private StarTable roundTrip( StarTable table, EcsvTableWriter writer )
