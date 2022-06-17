3e6b1b0aedab920bd0c7f9ff7f3c23a8c22dadf2
Mark Taylor <m.b.taylor@bristol.ac.uk>
2022-06-17 13:39:34 +0100

    fits: add sanitizeText option to CardFactory.Config
    The FITS CardFactory now has a pluggable option to check free text
    inputs (string values and comments, but not key names) and ensure
    that they conform to the FITS constraints, i.e. all characters
    are in the range 0x20-0x7e.  By default, it now replaces any
    out-of-range characters with a question mark '?'.
    
    The previous behaviour was to throw an IllegalArgumentException
    if illegal FITS header characters were encountered in input
    string values or comments.  That was pretty disastrous, since it
    meant that non-ASCII Unicode characters from input table metadata
    could (and did) cause the whole FITS output operation to fail.

--- fits/src/main/uk/ac/starlink/fits/CardFactory.java
+++ fits/src/main/uk/ac/starlink/fits/CardFactory.java
@@ -17,7 +17,7 @@ public class CardFactory {
 
     private final Config config_;
 
-    /** Instance with silent value trimming and no HIERARCH. */
+    /** Instance with value trimming, bad char replacement, no HIERARCH. */
     public static final CardFactory CLASSIC = new CardFactory( new Config() {
         public boolean allowHierarch() {
             return false;
@@ -25,9 +25,12 @@ public class CardFactory {
         public boolean allowTrim() {
             return true;
         }
+        public String sanitiseText( String txt ) {
+            return sanitiseByReplacement( txt, '?' );
+        }
     } );
 
-    /** Instance with value trimming and HIERARCH support. */
+    /** Instance with value trimming, bad char replacement, and HIERARCH. */
     public static final CardFactory HIERARCH = new CardFactory( new Config() {
         public boolean allowHierarch() {
             return true;
@@ -35,9 +38,12 @@ public class CardFactory {
         public boolean allowTrim() {
             return true;
         }
+        public String sanitiseText( String txt ) {
+            return sanitiseByReplacement( txt, '?' );
+        }
     } );
 
-    /** Instance with no value trimming and no HIERARCH. */
+    /** Instance with no value trimming, bad char rejection, no HIERARCH. */
     public static final CardFactory STRICT = new CardFactory( new Config() {
         public boolean allowHierarch() {
             return false;
@@ -45,9 +51,12 @@ public class CardFactory {
         public boolean allowTrim() {
             return false;
         }
+        public String sanitiseText( String txt ) {
+            return sanitiseAsError( txt );
+        }
     } );
 
-    /** Default instance. */
+    /** Default instance (currently {@link #CLASSIC}). */
     public static final CardFactory DEFAULT = CLASSIC;
 
     /** CardImage for terminating header ("END"). */
@@ -78,6 +87,7 @@ public class CardFactory {
     /**
      * Constructs a card containing the given exact text.
      * It is padded with spaces if necessary.
+     * No sanitisation or trimming is performed.
      *
      * @param  txt  literal card content, &lt;=80 characters
      * @return   new card
@@ -98,13 +108,13 @@ public class CardFactory {
 
     /**
      * Constructs a COMMENT card with the given comment.
-     * May be trimmed if too long according to policy.
+     * The text may be sanitised and trimmed if too long according to policy.
      *
      * @param  txt  comment text
      * @return   new card
      */
     public CardImage createCommentCard( String txt ) {
-        String ctxt = "COMMENT " + txt;
+        String ctxt = "COMMENT " + sanitiseText( txt );
         if ( ctxt.length() > 80 && config_.allowTrim() ) {
             ctxt = ctxt.substring( 0, 80 );
         }
@@ -113,7 +123,8 @@ public class CardFactory {
 
     /**
      * Constructs a key-value card with string content.
-     * The value may be trimmed if too long according to policy.
+     * The value and comment may be sanitised and trimmed if too long
+     * according to policy.
      *
      * @param  key  header keyword
      * @param  value  header value
@@ -125,18 +136,20 @@ public class CardFactory {
         StringBuffer content =
             new StringBuffer()
            .append( '\'' )
-           .append( value.replace( "'", "''" ) );
+           .append( sanitiseText( value ).replace( "'", "''" ) );
         if ( comment == null || comment.length() < 50 ) {
             while ( content.length() < 9 ) {
                 content.append( ' ' );
             }
         }
         content.append( '\'' );
-        return createLiteralCard( key, content.toString(), comment );
+        return createLiteralCard( key, content.toString(),
+                                  sanitiseText( comment ) );
     }
 
     /**
      * Constructs a key-value card with integer content.
+     * The comment may be sanitised and trimmed if too long according to policy.
      *
      * @param  key  header keyword
      * @param  value  header value
@@ -145,11 +158,13 @@ public class CardFactory {
      */
     public CardImage createIntegerCard( String key, long value,
                                         String comment ) {
-        return createLiteralCard( key, Long.toString( value ), comment );
+        return createLiteralCard( key, Long.toString( value ),
+                                  sanitiseText( comment ) );
     }
 
     /**
      * Constructs a key-value card with floating point content.
+     * The comment may be sanitised and trimmed if too long according to policy.
      *
      * @param  key  header keyword
      * @param  value  header value
@@ -158,11 +173,13 @@ public class CardFactory {
      */
     public CardImage createRealCard( String key, double value,
                                      String comment ) {
-        return createLiteralCard( key, Double.toString( value ), comment );
+        return createLiteralCard( key, Double.toString( value ),
+                                  sanitiseText( comment ) );
     }
 
     /**
-     * Constructs a key-value card with logical content
+     * Constructs a key-value card with logical content.
+     * The comment may be sanitised and trimmed if too long according to policy.
      *
      * @param  key  header keyword
      * @param  value  header value
@@ -171,18 +188,24 @@ public class CardFactory {
      */
     public CardImage createLogicalCard( String key, boolean value,
                                         String comment ) {
-        return createLiteralCard( key, value ? "T" : "F", comment );
+        return createLiteralCard( key, value ? "T" : "F",
+                                  sanitiseText( comment ) );
     }
 
     /**
      * Constructs a key-value card with given literal text for the value part.
      * No additional quoting or escaping is performed on the provided string,
-     * but it may be trimmed in a string-sensitive way according to policy.
+     * but both string and comment may be trimmed in a string-sensitive way
+     * according to policy.
+     *
+     * <p>The key must contain only legal key characters,
+     * and the literal and comment must contain only legal header characters.
      *
      * @param  key  header keyword
      * @param  literal  formatted value text
      * @param  comment  comment text, or null
      * @return  new card
+     * @throws  IllegalArgumentException  if inputs are not suitable for FITS
      */
     public CardImage createLiteralCard( String key, String literal,
                                         String comment ) {
@@ -251,10 +274,14 @@ public class CardFactory {
      * No additional quoting or escaping is performed on the provided string,
      * but it may be trimmed in a string-sensitive way according to policy.
      *
+     * <p>The key must contain only legal key characters,
+     * and the literal and comment must contain only legal header characters.
+     *
      * @param  key  header keyword starting with "HIERARCH"
      * @param  literal  formatted value text
      * @param  comment  comment text, or null
      * @return  new card
+     * @throws  IllegalArgumentException  if inputs are not suitable for FITS
      */
     private CardImage createLiteralHierarchCard( String key, String literal,
                                                  String comment ) {
@@ -326,6 +353,19 @@ public class CardFactory {
     }
 
     /**
+     * Ensures that a given string contains only legal FITS header characters
+     * in accordance with configuration.
+     *
+     * @param  txt  free text string
+     * @return  string like input but containing only legal FITS characters
+     * @throws  IllegalArgumentException  if this factory's config is unable
+     *                                    or unwilling to sanitise
+     */
+    private String sanitiseText( String txt ) {
+        return txt == null ? null : config_.sanitiseText( txt );
+    }
+
+    /**
      * Creates a copy of a CardImage that cannot be tampered with.
      *
      * @param  template  card to copy
@@ -338,6 +378,54 @@ public class CardFactory {
                 return super.getBytes().clone();
             }
         };
+    }
+
+    /**
+     * Utility string sanitisation function that replaces illegal characters
+     * by a given char.
+     *
+     * @param  txt  input text
+     * @param  replaceChar  character to replace non-FITS characters
+     * @return  same as <code>txt</code>, but with non-FITS characters
+     *          replaced by <code>replaceChar</code>
+     */
+    public static String sanitiseByReplacement( String txt, char replaceChar ) {
+        int nc = txt.length();
+        int nr = 0;
+        StringBuffer sbuf = new StringBuffer( txt );
+        for ( int i = 0; i < nc; i++ ) {
+            if ( ! FitsUtil.isFitsCharacter( sbuf.charAt( i ) ) ) {
+                sbuf.setCharAt( i, replaceChar );
+                nr++;
+            }
+        }
+        if ( nr > 0 ) {
+            logger_.info( "Replace " + nr + " non-FITS "
+                        + ( nr == 1 ? "char" : "chars" ) + " with '"
+                        + replaceChar + "' in \"" + txt + "\"" );
+        }
+        return nr == 0 ? txt : sbuf.toString();
+    }
+
+    /**
+     * Utility string sanitisation function that throws an
+     * IllegalArgumentException if any illegal characters are present.
+     *
+     * @param  txt  input text
+     * @return  input text
+     * @throws  IllegalArgumentException  if non-FITS characters are present
+     */
+    public static String sanitiseAsError( String txt ) {
+        int nc = txt.length();
+        for ( int ic = 0; ic < nc; ic++ ) {
+            char ch = txt.charAt( ic );
+            if ( ! FitsUtil.isFitsCharacter( ch ) ) {
+                throw new IllegalArgumentException( "Illegal FITS character 0x"
+                                                  + Integer.toHexString( ch )
+                                                  + " in \"" + txt + "\"" );
+            }
+        }
+        return txt;
     }
 
     /**
@@ -362,5 +450,27 @@ public class CardFactory {
          * @return  true  iff overlength string values may be truncated
          */
         boolean allowTrim();
+
+        /**
+         * This method is called on uncontrolled text inputs that will
+         * end up as text or comment content in output cards.
+         * The output value must contain only legal FITS header characters,
+         * that is in the range 0x20-0x7e inclusive
+         * (see {@link FitsUtil#isFitsCharacter}).
+         * If the input text contains no illegal characters, it should be
+         * returned unchanged.  If it does contain illegal characters,
+         * it should be adjusted in some way to remove them,
+         * or a RuntimeException may be thrown.
+         *
+         * <p>Example implementations are provided in
+         * {@link #sanitiseByReplacement sanitiseAsReplacement} and
+         * {@link #sanitiseAsError sanitiseAsError}.
+         *
+         * @param  txt  non-null free text string
+         * @return  string like input but containing only legal FITS characters
+         * @throws  IllegalArgumentException  if this config is unable
+         *                                    or unwilling to sanitise
+         */
+        String sanitiseText( String txt );
     }
 }
