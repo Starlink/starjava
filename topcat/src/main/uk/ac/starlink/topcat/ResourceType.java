ea370777c38a598b3040579a5c2177f7b8733b04
Mark Taylor <m.b.taylor@bristol.ac.uk>
2023-01-03 12:58:08 +0000

    topcat: use contentQualifier to guess resource type
    ResourceType.guessResourceType now takes advantage of the
    ResourceInfo.getContentQualifier item to improve its guesses
    about resource type.  Some other changes to the logic have
    also taken place.

--- topcat/src/main/uk/ac/starlink/topcat/ResourceType.java
+++ topcat/src/main/uk/ac/starlink/topcat/ResourceType.java
@@ -20,7 +20,7 @@ public enum ResourceType {
     /** DataLink {links}-response table. */
     DATALINK,
 
-    /** FITS file to be interpreted as image data. */
+    /** FITS file to be interpreted as 2D image data. */
     FITS_IMAGE,
 
     /** Some kind of spectrum. */
@@ -47,88 +47,114 @@ public enum ResourceType {
         if ( resInfo == null ) {
             return UNKNOWN;
         }
+
+        /* Unpack resource info. */
         String standardId = resInfo.getStandardId();
         String contentType = resInfo.getContentType();
+        String contentQualifier = resInfo.getContentQualifier();
         URL url = resInfo.getUrl();
+
+        /* Prepare information in a form convenient for matching. */
         ContentType ctype = ContentType.parseContentType( contentType );
-        if ( standardId != null ) {
-            String standardid = standardId.toLowerCase();
-            if ( standardid.startsWith( "ivo://ivoa.net/std/datalink" ) ) {
-                return DATALINK;
-            }
-            if ( standardid.startsWith( "ivo://ivoa.net/std/conesearch" ) ||
-                 standardid.startsWith( "ivo://ivoa.net/std/tap" ) ) {
-                return TABLE;
-            }
-            if ( standardid.startsWith( "ivo://ivoa.net/std/sia" ) ) {
-                if ( ctype != null ) {
-                    if ( ctype.matches( "image", "fits" ) ||
-                         ctype.matches( "application", "fits" ) ) {
-                        return FITS_IMAGE;
-                    }
-                }
-                return IMAGE;
-            }
-            if ( standardid.startsWith( "ivo://ivoa.net/std/ssa" ) ) {
-                return SPECTRUM;
-            }
-        }
-        if ( ctype != null ) {
-            if ( "datalink".equalsIgnoreCase( ctype.getParameters()
-                                                   .get( "content" ) ) ) {
-                return DATALINK;
-            }
-            if ( "votable".equalsIgnoreCase( ctype.getSubtype() ) ) {
-                return TABLE;
-            }
-            if ( "html".equalsIgnoreCase( ctype.getSubtype() ) ) {
-                return WEB;
-            }
-            if ( ctype.matches( "text", "xml" ) ||
-                 ctype.matches( "text", "csv" ) ) {
-                return TABLE;
-            }
-            if ( ctype.matches( "image", "fits" ) ) {
-                return FITS_IMAGE;
-            }
-            if ( "image".equalsIgnoreCase( ctype.getType() ) ) {
-                return IMAGE;
-            }
-        }
-        if ( contentType != null ) {
-            String contenttype = contentType.toLowerCase();
-            if ( contenttype.indexOf( "votable" ) >= 0 ||
-                 contenttype.indexOf( "xml" ) >= 0 ) {
-                return TABLE;
-            }
-            if ( contenttype.indexOf( "fits" ) >= 0 ) {
-                return FITS_IMAGE;
-            }
-        }
+        String contenttype = contentType == null ? null
+                                                 : contentType.toLowerCase();
+        String standardid = standardId == null ? null
+                                               : standardId.toLowerCase();
+        boolean isFits = ctype != null
+                      && ( ctype.matches( "image", "fits" ) ||
+                           ctype.matches( "application", "fits" ) );
+        final String extension;
         if ( url != null ) {
             String path = url.getPath();
             int idot = path.lastIndexOf( '.' );
-            if ( idot >= 0 ) {
-                String exten = path.substring( idot + 1 ).toLowerCase();
-                if ( Arrays.asList( new String[] { "fits", "fit" } )
-                           .contains( exten ) ) {
-                    return FITS_IMAGE;
-                }
-                if ( Arrays.asList( new String[] { "jpeg", "jpg",
-                                                   "png", "gif" } )
-                           .contains( exten ) ) {
-                    return IMAGE;
-                }
-                if ( Arrays.asList( new String[] { "vot", "votable", "xml",
-                                                   "csv", } )
-                           .contains( exten ) ) {
-                    return TABLE;
-                }
-                if ( Arrays.asList( new String[] { "html", "htm", } )
-                           .contains( exten ) ) {
-                    return WEB;
-                }
-            }
+            extension = idot >= 0 ? path.substring( idot + 1 ).toLowerCase()
+                                  : null;
+        }
+        else {
+            extension = null;
+        }
+
+        /* Note that a product type of e.g. "spectrum" does not necessarily
+         * point at a spectrum data file, it could point to another
+         * datalink links file describing spectral data. */
+        String prodtype = ( contentQualifier != null &&
+                            contentQualifier.startsWith( "#" ) )
+                         ? contentQualifier.substring( 1 ).trim().toLowerCase()
+                         : null;
+        boolean isNon2d = prodtype != null && ! "image".equals( prodtype );
+
+        /* Go through the available information and pick the best match.
+         * This is an ad hoc and messy business, since there is some
+         * freedom in how the metadata is provided, not all items may be
+         * present, and the output options are not completely orthogonal.
+         * The details may be updated if something else works better. */
+        if ( ( ctype != null &&
+               "datalink".equalsIgnoreCase( ctype.getParameters()
+                                                 .get( "content" ) ) ) ||
+             ( standardid != null &&
+               standardid.startsWith( "ivo://ivoa.net/std/datalink" ) ) ) {
+            return DATALINK;
+        }
+        if ( ( ctype != null &&
+               "votable".equalsIgnoreCase( ctype.getSubtype() ) ) ||
+             ( contenttype != null &&
+               contenttype.indexOf( "votable" ) >= 0 ) ) {
+            return TABLE;
+        }
+        if ( ctype != null && ctype.matches( "text", "csv" ) ) {
+            return TABLE;
+        }
+        if ( ctype != null && "html".equalsIgnoreCase( ctype.getSubtype() ) ) {
+            return WEB;
+        }
+        if ( extension != null &&
+             Arrays.asList( new String[] { "jpeg", "jpg", "png", "gif" } )
+                   .contains( extension ) ) {
+            return IMAGE;
+        }
+        if ( extension != null &&
+             Arrays.asList( new String[] { "html", "htm", } )
+                   .contains( extension ) ) {
+            return WEB;
+        }
+        if ( "spectrum".equals( prodtype ) || "sed".equals( prodtype ) ) {
+            return SPECTRUM;
+        }
+        if ( ctype != null && ctype.matches( "text", "xml" ) ) {
+            return TABLE;
+        }
+        if ( ctype != null && ctype.matches( "image", "fits" ) ) {
+            return isNon2d ? UNKNOWN : FITS_IMAGE;
+        }
+        if ( ctype != null && "image".equalsIgnoreCase( ctype.getType() ) ) {
+            return IMAGE;
+        }
+        if ( standardid != null &&
+             ( standardid.startsWith( "ivo://ivoa.net/std/conesearch" ) ||
+               standardid.startsWith( "ivo://ivoa.net/std/tap" ) ) ) {
+            return TABLE;
+        }
+        if ( "image".equals( prodtype ) ) {
+            return isFits ? FITS_IMAGE : IMAGE;
+        }
+        if ( contenttype != null && contenttype.indexOf( "xml" ) >= 0 ) {
+            return TABLE;
+        }
+        if ( contenttype != null && contenttype.indexOf( "fits" ) >= 0 ) {
+            return isNon2d ? UNKNOWN : FITS_IMAGE;
+        }
+        if ( extension != null &&
+             Arrays.asList( new String[] { "fits", "fit" } )
+                   .contains( extension ) ) {
+            return FITS_IMAGE;
+        }
+        if ( extension != null &&
+             Arrays.asList( new String[] { "vot", "votable", "xml", "csv", } )
+                           .contains( extension ) ) {
+            return TABLE;
+        }
+        if ( "timeseries".equals( prodtype ) ) {
+            return TABLE;
         }
         return UNKNOWN;
     }
