3db759df516fe3a7fa93cf64995a58102f399942
Mark Taylor <m.b.taylor@bristol.ac.uk>
2024-01-23 11:19:49 +0000

    topcat: CubePlotWindow allows DataGeom selection
    The CubePlot Window now allows the user to choose between
    3 Cartesian component coordinates, a single Cartesian 3-vector
    coordinate, and 3 Spherical Polar coordinates.

--- topcat/src/main/uk/ac/starlink/topcat/plot2/CubePlotWindow.java
+++ topcat/src/main/uk/ac/starlink/topcat/plot2/CubePlotWindow.java
@@ -2,13 +2,19 @@ package uk.ac.starlink.topcat.plot2;
 
 import java.awt.Component;
 import java.awt.Rectangle;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import javax.swing.ListModel;
 import uk.ac.starlink.topcat.TopcatModel;
+import uk.ac.starlink.ttools.plot2.DataGeom;
 import uk.ac.starlink.ttools.plot2.Surface;
 import uk.ac.starlink.ttools.plot2.geom.CubeAspect;
+import uk.ac.starlink.ttools.plot2.geom.CubeDataGeom;
 import uk.ac.starlink.ttools.plot2.geom.CubePlotType;
 import uk.ac.starlink.ttools.plot2.geom.CubeSurface;
 import uk.ac.starlink.ttools.plot2.geom.CubeSurfaceFactory;
+import uk.ac.starlink.ttools.plot2.geom.CubeVectorDataGeom;
+import uk.ac.starlink.ttools.plot2.geom.SphereDataGeom;
 
 /**
  * Layer plot window for 3D Cartesian plots.
@@ -20,14 +26,9 @@ public class CubePlotWindow
        extends StackPlotWindow<CubeSurfaceFactory.Profile,CubeAspect> {
     private static final CubePlotType PLOT_TYPE = CubePlotType.getInstance();
     private static final CubeRanger CUBE_RANGER = new CubeRanger();
+    private static final Map<DataGeom,CoordSpotter[]> GEOM_SPOTTERS =
+        createGeomSpotterMap();
     private static final CubePlotTypeGui PLOT_GUI = new CubePlotTypeGui();
-    private static final String[] XYZ = new String[] { "x", "y", "z" };
-    private static final CoordSpotter[] XYZ_SPOTTERS = new CoordSpotter[] {
-        CoordSpotter.createUcdSpotter( "pos.cartesian", XYZ, false ),
-        CoordSpotter.createUcdSpotter( "pos.cartesian", XYZ, true ),
-        CoordSpotter.createNamePrefixSpotter( XYZ, true ),
-        CoordSpotter.createNamePrefixSpotter( XYZ, false ),
-    };
 
     /**
      * Constructor.
@@ -53,9 +54,7 @@ public class CubePlotWindow
                   .create( new CubeAxisController( false ) );
         }
         public PositionCoordPanel createPositionCoordPanel( int npos ) {
-            return SimplePositionCoordPanel
-                  .createPanel( PLOT_TYPE.getPointDataGeoms()[ 0 ], npos,
-                                XYZ_SPOTTERS );
+            return new MultiGeomPositionCoordPanel( npos, GEOM_SPOTTERS );
         }
         public PositionCoordPanel createAreaCoordPanel() {
             throw new UnsupportedOperationException();
@@ -106,5 +105,59 @@ public class CubePlotWindow
             int npix = Math.max( bounds.width, bounds.height );
             return new int[] { npix, npix, npix };
         }
+    }
+
+    /**
+     * Sets up a map from DataGeom to a list of CoordSpotters that can
+     * guess suitable values for the coordinate sets used.
+     *
+     * <p>UCDs in the implementation are taken from v 1.5 of the
+     * <a href="https://www.ivoa.net/documents/UCD1+/"
+     *         >UCD1+ Controlled Vocabulary</a> document.
+     *
+     * @return  map from all DataGeoms suitable for the CubePlotWindow
+     *          to lists of appropriate CoordSpotters
+     */
+    private static Map<DataGeom,CoordSpotter[]> createGeomSpotterMap() {
+        Map<DataGeom,CoordSpotter[]> map = new LinkedHashMap<>();
+
+        /* Cartesian component coordinates. */
+        String[] xyz = new String[] { "x", "y", "z" };
+        map.put( CubeDataGeom.INSTANCE, new CoordSpotter[] {
+            CoordSpotter.createUcdSpotter( "pos.cartesian", xyz, false ),
+            CoordSpotter.createUcdSpotter( "pos.cartesian", xyz, true ),
+            CoordSpotter.createNamePrefixSpotter( xyz, true ),
+            CoordSpotter.createNamePrefixSpotter( xyz, false ),
+        } );
+
+        /* Cartesian 3-vector coordinates. */
+        map.put( CubeVectorDataGeom.INSTANCE, new CoordSpotter[] {
+            CoordSpotter.createVectorSpotter( 3 ),
+            CoordSpotter.createVectorSpotter( -1 ),
+        } );
+
+        /* Spherical polar coordinates. */
+        map.put( SphereDataGeom.INSTANCE, new CoordSpotter[] {
+            CoordSpotter
+           .createUcdSpotter( "pos.bodyrc",
+                              new String[] { "lon", "lat", "alt" }, true ),
+            CoordSpotter
+           .createUcdSpotter( "pos.earth",
+                              new String[] { "lon", "lat", "altitude" }, true ),
+
+            // Note these ones don't match the required coordinates,
+            // but they are similar, so filling them in may provide a useful
+            // clue to the user in absence of better guesses.
+            CoordSpotter
+           .createUcdSpotter( "pos.spherical",
+                              new String[] { "azi", "colat", "r" }, true ),
+        } );
+
+        /* Check we have set up coordinate value defaults for all the
+         * geoms in use. */
+        for ( DataGeom geom : PLOT_TYPE.getPointDataGeoms() ) {
+            assert map.containsKey( geom );
+        }
+        return map;
     }
 }
