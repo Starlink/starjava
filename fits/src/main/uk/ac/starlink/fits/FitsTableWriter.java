3242a5d978bfc2857268ce3604f920606d2f0f30
Mark Taylor <m.b.taylor@bristol.ac.uk>
2024-03-08 11:08:53 +0000

    votable: write unified FITS output handler
    There are up till now half a dozen different FITS table output handlers
    that do different things in terms of how headers are written,
    what you do with variable-length arrays, row/column orientation etc.
    This dates from a time before the IO handler config options were
    introduced.
    
    This commit introduces a single class votable.UnifiedFitsTableWriter
    that can do anything the others do, so that they can be retired.
    This inherits from fits.FitsTableWriter now reimplemented to
    provide all the required non-votable-related functionality.
    Only the UnifiedFitsTableWriter is intended to be documented.
    
    As well as allowing the possibility of tidying things up,
    it also allows more flexibility so that for instance you can choose
    whether your PHDU-less output file does or does not have
    variable-length arrays on the heap, either programmatically or
    using a textual output handler specification, which up till now
    has not been possible.
    
    Tests are included to check bytewise equivalence between the old
    and new behaviour.
    
    HealpixFitsTableWriter is a bit of a special case, so that can stay.
    
    I think this can be done without any breaking user-visible changes.

--- fits/src/main/uk/ac/starlink/fits/FitsTableWriter.java
+++ fits/src/main/uk/ac/starlink/fits/FitsTableWriter.java
@@ -1,7 +1,11 @@
 package uk.ac.starlink.fits;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import uk.ac.starlink.table.StarTable;
+import uk.ac.starlink.table.StoragePolicy;
+import uk.ac.starlink.table.TableSequence;
+import uk.ac.starlink.util.IOConsumer;
 
 /**
  * Handles writing of a StarTable in FITS binary format.
@@ -42,6 +46,11 @@ import uk.ac.starlink.table.StarTable;
  */
 public class FitsTableWriter extends AbstractFitsTableWriter {
 
+    private boolean isColfits_ = false;
+    private PrimaryType primaryType_ = PrimaryType.BASIC;
+    private VarArrayMode varArray_ = VarArrayMode.FALSE;
+    private StoragePolicy storage_ = StoragePolicy.getDefaultPolicy();
+
     /**
      * Default constructor.
      */
@@ -74,18 +83,255 @@ public class FitsTableWriter extends AbstractFitsTableWriter {
     public boolean looksLikeFile( String location ) {
         int dotPos = location.lastIndexOf( '.' );
         if ( dotPos > 0 ) {
-            String exten = location.substring( dotPos + 1 ).toLowerCase();
-            if ( exten.equals( "fit" ) ||
-                 exten.equals( "fits" ) ||
-                 exten.equals( "fts" ) ) {
-                return true;
-            }
+            String ext = location.substring( dotPos + 1 ).toLowerCase();
+            return isColfits_
+                 ? "colfits".equalsIgnoreCase( ext )
+                 : ( "fit".equalsIgnoreCase( ext ) ||
+                     "fits".equalsIgnoreCase( ext ) );
         }
-        return false;
+        else {
+            return false;
+        }
     }
 
-    protected FitsTableSerializer createSerializer( StarTable table )
+    /**
+     * Sets the type of Primary HDU that will precede the extension HDU(s)
+     * corresponding to the table(s) being written.
+     *
+     * @param   primaryType  type of primary HDU
+     */
+    public void setPrimaryType( PrimaryType primaryType ) {
+        primaryType_ = primaryType;
+    }
+
+    /**
+     * Returns the type of Primary HDU that will precede the extension HDU(s)
+     * corresponding to the table(s) being written.
+     *
+     * @return  type of primary HDU
+     */
+    public PrimaryType getPrimaryType() {
+        return primaryType_;
+    }
+
+    /**
+     * Sets whether table data should be written in column-oriented form.
+     * If true, the output will be a single-row table of array-valued columns,
+     * in which each cell is an array with <code>nrow</code> elements.
+     *
+     * @param  colfits  true for column-oriented, false for row-oriented
+     */
+    public void setColfits( boolean colfits ) {
+        isColfits_ = colfits;
+    }
+
+    /**
+     * Returns whether table data will be written in column-oriented form.
+     *
+     * @return  true for column-oriented, false for row-oriented
+     */
+    public boolean isColfits() {
+        return isColfits_;
+    }
+
+    /**
+     * Configures how variable-length array values will be stored.
+     *
+     * @param  varArray  variable-length array storage mode
+     */
+    public void setVarArray( VarArrayMode varArray ) {
+        varArray_ = varArray;
+    }
+
+    /**
+     * Indicates how variable-length array valued columns will be stored.
+     *
+     * @return  variable-length array storage mode
+     */
+    public VarArrayMode getVarArray() {
+        return varArray_;
+    }
+
+    /**
+     * Sets the storage policy to use where required.
+     * This is only relevant for column-oriented output,
+     * for which data has to be cached before rewriting it.
+     *
+     * @param  storage  storage policy to use when writing colfits
+     */
+    public void setStoragePolicy( StoragePolicy storage ) {
+        storage_ = storage;
+    }
+
+    /**
+     * Returns the storage policy that will be used if required.
+     *
+     * @return  storage policy used when writing colfits
+     */
+    public StoragePolicy getStoragePolicy() {
+        return storage_;
+    }
+
+    @Override
+    public boolean getAllowSignedByte() {
+        return super.getAllowSignedByte() && primaryType_.allowSignedByte();
+    }
+
+    public FitsTableSerializer createSerializer( StarTable table )
             throws IOException {
-        return new StandardFitsTableSerializer( getConfig(), table );
+        FitsTableSerializerConfig config = getConfig();
+        if ( isColfits_ ) {
+            // I could add vararray config for ColFits
+            return new ColFitsTableSerializer( config, table );
+        }
+        else {
+            if ( varArray_.isVarArray_ ) {
+                Boolean longIndexing = varArray_.longIndexing_;
+                VariableFitsTableSerializer fitser =
+                    new VariableFitsTableSerializer( config, table, storage_ );
+                if ( longIndexing != null ) {
+                    fitser.set64BitMode( longIndexing.booleanValue() );
+                }
+                return fitser;
+            }
+            else {
+                return new StandardFitsTableSerializer( config, table );
+            }
+        }
+    }
+
+    @Override
+    public void writeStarTables( TableSequence tableSeq, OutputStream out )
+            throws IOException {
+        primaryType_.writeTables( this, tableSeq, out );
+    }
+
+    /**
+     * Creates a PrimaryType corresponding to a PHDU with content
+     * that does not depend on the table contents of the extension HDUs.
+     *
+     * @param  name  type name
+     * @param  primaryWriter  writes primary HDU to output stream
+     */
+    private static PrimaryType
+            createStaticPrimaryType( String name,
+                                     IOConsumer<OutputStream> primaryWriter ) {
+        return new PrimaryType( name ) {
+            public boolean allowSignedByte() {
+                return true;
+            }
+            public void writeTables( FitsTableWriter fitsWriter,
+                                     TableSequence tseq, OutputStream out )
+                    throws IOException {
+                primaryWriter.accept( out );
+                for ( StarTable table; ( table = tseq.nextTable() ) != null; ) {
+                    fitsWriter
+                   .writeTableHDU( table, fitsWriter.createSerializer( table ),
+                                   out );
+                }
+            }
+        };
+    }
+
+    /**
+     * Enumeration for variable-length array value storage options.
+     * Usually just {@link #TRUE} and {@link #FALSE} will be used,
+     * {@link #P} and {@link #Q} are rather special interest.
+     */
+    public enum VarArrayMode {
+
+        /**
+         * All arrays are stored as fixed-length in the body of the table.
+         */
+        FALSE( false, null ),
+
+        /**
+         * Variable-length arrays are stored on the heap,
+         * using a suitable pointer size for indirection.
+         */
+        TRUE( true, null ),
+
+        /**
+         * Variable-length arrays are stored on the heap,
+         * using a 32-bit pointer size ('P' array descriptor) for indirection.
+         */
+        P( true, Boolean.FALSE ),
+
+        /**
+         * Variable-length arrays are stored on the heap,
+         * using a 64-bit pointer size ('Q' array descriptor) for indirection.
+         */
+        Q( true, Boolean.TRUE );
+
+        private final boolean isVarArray_;
+        private final Boolean longIndexing_;
+
+        /**
+         * Constructor.
+         *
+         * @param  isVarArray  true for variable-length array on heap,
+         *                     false for fixed-length array in table body
+         * @param  longIndexing  TRUE for long, FALSE for short;
+         *                       if null, type is determined from the data
+         */
+        VarArrayMode( boolean isVarArray, Boolean longIndexing ) {
+            isVarArray_ = isVarArray;
+            longIndexing_ = longIndexing;
+        }
+    }
+
+    /**
+     * Characterises the type of Primary HDU that will precede table
+     * extensions written by this writer.
+     */
+    public static abstract class PrimaryType {
+
+        private final String name_;
+
+        /**
+         * Basic Primary HDU, containing minimal headers and no data part.
+         */
+        public static final PrimaryType BASIC =
+            createStaticPrimaryType( "basic",
+                                     out -> FitsUtil.writeEmptyPrimary( out ) );
+
+        /**
+         * No Primary HDU is written.  The resulting output will therefore
+         * not be a legal FITS file, but it can be appended to an existing
+         * FITS file with a valid Primary HDU and perhaps other extension HDUs.
+         */
+        public static final PrimaryType NONE =
+            createStaticPrimaryType( "none", out -> {} );
+
+        /**
+         * Constructor.
+         */
+        protected PrimaryType( String name ) {
+            name_ = name;
+        }
+
+        /**
+         * Indicates whether signed byte values will be permitted
+         * in files with this PHDU type.
+         *
+         * @return  whether signed bytes are allowed
+         */
+        public abstract boolean allowSignedByte();
+
+        /**
+         * Writes a sequence of tables to an output stream.
+         *
+         * @param  writer  fits writer instance
+         * @param  tseq   sequence of tables
+         * @param  out   destination stream
+         */
+        public abstract void writeTables( FitsTableWriter writer,
+                                          TableSequence tseq, OutputStream out )
+                throws IOException;
+
+        @Override
+        public String toString() {
+            return name_;
+        }
     }
 }
