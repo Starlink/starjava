76d89c0d4b44cdc4240ff9a157201404cb6ae127
Mark Taylor <m.b.taylor@bristol.ac.uk>
2024-08-01 15:45:31 +0100

    pds: suppress this-escape warnings
    At JDK 21, javac -Xlint generates many this-escape warnings.
    These are generally for harmless or desired behaviour.
    Suppress these warnings with suitable annotations.

--- pds/src/main/uk/ac/starlink/pds4/BasePds4StarTable.java
+++ pds/src/main/uk/ac/starlink/pds4/BasePds4StarTable.java
@@ -41,6 +41,7 @@ public class BasePds4StarTable extends Pds4StarTable {
      * @param  table  table object on which this table is based
      * @param  contextUrl   parent URL for the PDS4 label
      */
+    @SuppressWarnings("this-escape")
     public BasePds4StarTable( BaseTable table, URL contextUrl )
             throws IOException {
         super( table, contextUrl );
