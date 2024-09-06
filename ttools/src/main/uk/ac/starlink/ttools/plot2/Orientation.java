836d1a1971fae631aef319e86e824c23b7c48800
Mark Taylor <m.b.taylor@bristol.ac.uk>
2024-09-06 17:30:33 +0100

    ttools: add Orientation options for angled labels

--- ttools/src/main/uk/ac/starlink/ttools/plot2/Orientation.java
+++ ttools/src/main/uk/ac/starlink/ttools/plot2/Orientation.java
@@ -9,6 +9,7 @@ import java.awt.geom.AffineTransform;
  * @author   Mark Taylor
  * @since    13 Feb 2013
  */
+@Equality
 public abstract class Orientation {
 
     /** Orientation suitable for X axis labelling. */
@@ -67,6 +68,10 @@ public abstract class Orientation {
      * can be painted.  The origin of the bounds should be the baseline
      * at the start of the line, its height should reflect the maximum
      * font height, and the width should be the actual width.
+     *
+     * @param  bounds  rectangle enclosing caption text
+     * @param  pad    number of pixels gap between caption and axis
+     * @return  transform applied to graphics context for writing caption text
      */
     public abstract AffineTransform captionTransform( Rectangle bounds,
                                                       int pad );
@@ -77,4 +82,81 @@ public abstract class Orientation {
      * @return   true for axis below text, false for axis above text
      */
     public abstract boolean isDown();
+
+    /**
+     * Returns an orientation suitable for X axis labelling, in which
+     * labels are rotated by a given angle.
+     *
+     * @param  thetaDeg  rotation angle clockwise in degrees,
+     *                   usually between 0 and 90
+     * @param  isAnti   true for top-edge X axis, false for bottom-edge
+     * @return  new orientation
+     */
+    public static Orientation createAngledX( double thetaDeg, boolean isAnti ) {
+        return new AngledXOrientation( -Math.toRadians( thetaDeg ), isAnti );
+    }
+
+    /**
+     * Orientation suitable for X axis labelling in which labels are
+     * rotated by a given angle.
+     */
+    private static class AngledXOrientation extends Orientation {
+
+        private final double thetaRad_;
+        private final boolean isAnti_;
+        private final double cosTheta_;
+        private final AffineTransform rotateTransform_;
+
+        /**
+         * Constructor.
+         *
+         * @param  thetaRad  rotation angle anticlockwise in radians
+         * @param  isAnti   true for top-edge X axis, false for bottom-edge
+         */
+        AngledXOrientation( double thetaRad, boolean isAnti ) {
+            thetaRad_ = thetaRad;
+            isAnti_ = isAnti;
+            cosTheta_ = Math.cos( thetaRad_ );
+            rotateTransform_ = AffineTransform.getRotateInstance( thetaRad_ );
+        }
+
+        public AffineTransform captionTransform( Rectangle bounds, int pad ) {
+            if ( isAnti_ ) {
+                AffineTransform trans = new AffineTransform();
+                trans.translate( -0.5 * bounds.y * cosTheta_, -pad );
+                trans.rotate( thetaRad_ );
+                return trans;
+            }
+            else {
+                AffineTransform trans = new AffineTransform( rotateTransform_ );
+                trans.translate( -bounds.width, -bounds.y / 2 );
+                trans.rotate( -thetaRad_ );
+                trans.translate( 0, pad - 0.5 * cosTheta_ * bounds.y );
+                trans.rotate( thetaRad_ );
+                return trans;
+            }
+        }
+
+        public boolean isDown() {
+            return ! isAnti_;
+        }
+
+        @Override
+        public int hashCode() {
+            return Float.floatToIntBits( (float) thetaRad_ )
+                 * ( isAnti_ ? -1 : +1 );
+        }
+
+        @Override
+        public boolean equals( Object o ) {
+            if ( o instanceof AngledXOrientation ) {
+                AngledXOrientation other = (AngledXOrientation) o;
+                return this.thetaRad_ == other.thetaRad_
+                    && this.isAnti_ == other.isAnti_;
+            }
+            else {
+                return false;
+            }
+        }
+    }
 }
