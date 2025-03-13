87a063112ab18a3175cadea69c85a00fe34e8874
Mark Taylor <m.b.taylor@bristol.ac.uk>
2025-03-13 10:49:22 +0000

    ttools: rewrite BinMapper using Scale
    The createMapper factory method that produces a linear or log mapper
    is still in place for now, but there is now a public constructor that
    lets you use any scale.  The amount of code is reduced.

--- ttools/src/main/uk/ac/starlink/ttools/plot2/layer/BinMapper.java
+++ ttools/src/main/uk/ac/starlink/ttools/plot2/layer/BinMapper.java
@@ -1,6 +1,7 @@
 package uk.ac.starlink.ttools.plot2.layer;
 
 import uk.ac.starlink.ttools.plot2.Equality;
+import uk.ac.starlink.ttools.plot2.Scale;
 
 /**
  * Maps axis values to bin indices.
@@ -9,17 +10,58 @@ import uk.ac.starlink.ttools.plot2.Equality;
  * @since    16 Jan 2017
  */
 @Equality
-public abstract class BinMapper {
+public class BinMapper {
+
+    private final Scale scale_;
+    private final double swidth_;
+    private final double sfloor_;
+    private final double swidth1_;
+
+    /**
+     * Constructor.
+     *
+     * <p>The <code>dpoint</code> parameter is used internally to determine
+     * the zero point of the bins.  In principle this should make no
+     * difference to behaviour, but in case that the data is situated
+     * a very long way from 1,  setting it close to
+     * the actual data point locations may avoid rounding errors.
+     *
+     * @param  scale  axis scaling
+     * @param  scaleWidth  extent of bins in scale units
+     * @param   binPhase   determines sub-bin boundary shifts along axis,
+     *                     normally in range 0..1
+     * @param   dpoint   representative data space point on axis
+     *                   which bins are situated
+     */
+    public BinMapper( Scale scale, double scaleWidth, double binPhase,
+                      double dpoint ) {
+        scale_ = scale;
+        swidth_ = scaleWidth;
+        swidth1_ = 1.0 / swidth_;
+        if ( binPhase > 1 ) {
+            binPhase = binPhase % 1;
+        }
+        if ( binPhase < 0 ) {
+            binPhase += 1;
+        }
+        double spoint = scale.dataToScale( dpoint );
+        double f0 = Math.floor( spoint * swidth1_ );
+        sfloor_ = ( f0 + binPhase ) * swidth_;
+        assert (float) Math.abs( sfloor_ - spoint ) <= (float) swidth_;
+    }
 
     /**
      * Returns the bin index for a given value.
-     * In case of an invalid value (NaN, or non-positive for log mapper),
+     * In case of an invalid value (NaN, or non-positive for log scale),
      * behaviour is undefined (quite likely zero will be returned).
      *
-     * @param   value  valid axis value
+     * @param   dvalue  valid axis value in data space
      * @return  bin index
      */
-    public abstract int getBinIndex( double value );
+    public int getBinIndex( double dvalue ) {
+        double svalue = scale_.dataToScale( dvalue );
+        return (int) Math.floor( ( svalue - sfloor_ ) * swidth1_ );
+    }
 
     /**
      * Returns the bin limits for a given bin index.
@@ -27,7 +69,33 @@ public abstract class BinMapper {
      * @param   index  bin index
      * @return   (lower,upper) bin limits
      */
-    public abstract double[] getBinLimits( int index );
+    public double[] getBinLimits( int index ) {
+        double slo = sfloor_ + index * swidth_;
+        return new double[] { scale_.scaleToData( slo ),
+                              scale_.scaleToData( slo + swidth_ ) };
+    }
+
+    @Override
+    public int hashCode() {
+        int code = 55289;
+        code = 23 * code + scale_.hashCode();
+        code = 23 * code + Float.floatToIntBits( (float) swidth_ );
+        code = 23 * code + Float.floatToIntBits( (float) sfloor_ );
+        return code;
+    }
+
+    @Override
+    public boolean equals( Object o ) {
+        if ( o instanceof BinMapper ) {
+            BinMapper other = (BinMapper) o;
+            return this.scale_.equals( other.scale_ )
+                && this.swidth_ == other.swidth_
+                && this.sfloor_ == other.sfloor_;
+        }
+        else {
+            return false;
+        }
+    }
 
     /**
      * Returns a BinMapper instance.
@@ -57,132 +125,18 @@ public abstract class BinMapper {
      */
     public static BinMapper createMapper( boolean log, double binWidth,
                                           double binPhase, double point ) {
-        binPhase = binPhase % 1;
-        if ( binPhase < 0 ) {
-            binPhase += 1;
-        }
-        return log ? new LogBinMapper( binWidth, binPhase, point )
-                   : new LinearBinMapper( binWidth, binPhase, point );
+        Scale scale = log ? Scale.LOG : Scale.LINEAR;
+        double scaleWidth = log ? log( binWidth ) : binWidth;
+        return new BinMapper( scale, scaleWidth, binPhase, point );
     }
 
     /**
-     * Logarithm function, used for transforming values on logarithmic X axis.
+     * Logarithm function corresponding to logarithmic mapper.
      *
      * @param  val  value
      * @return  log to base 10 of <code>val</code>
      */
     public static double log( double val ) {
         return Math.log10( val );
-    }
-
-    /**
-     * BinMapper implementation for linear axis scaling.
-     */
-    private static class LinearBinMapper extends BinMapper {
-        private final double width_;
-        private final double width1_;
-        private final double floor_;
-
-        /**
-         * Constructor.
-         *
-         * @param  width  additive bin width
-         * @param  phase  bin phase in range 0..1
-         * @param  point    representative point
-         */
-        LinearBinMapper( double width, double phase, double point ) {
-            width_ = width;
-            width1_ = 1.0 / width;
-            double f0 = Math.floor( point / width );
-            floor_ = ( f0 + phase ) * width;
-            assert (float) Math.abs( floor_ - point ) <= (float) width;
-        }
-
-        public int getBinIndex( double value ) {
-            return (int) Math.floor( ( value - floor_ ) * width1_ );
-        }
-
-        public double[] getBinLimits( int index ) {
-            double lo = floor_ + index * width_;
-            return new double[] { lo, lo + width_ };
-        }
-
-        @Override
-        public int hashCode() {
-            int code = 55289;
-            code = 23 * code + Float.floatToIntBits( (float) width_ );
-            code = 23 * code + Float.floatToIntBits( (float) floor_ );
-            return code;
-        }
-
-        @Override
-        public boolean equals( Object o ) {
-            if ( o instanceof LinearBinMapper ) {
-                LinearBinMapper other = (LinearBinMapper) o;
-                return this.width_ == other.width_
-                    && this.floor_ == other.floor_;
-            }
-            else {
-                return false;
-            }
-        }
-    }
-
-    /**
-     * BinMapper implementation for logarithmic axis scaling.
-     */
-    private static class LogBinMapper extends BinMapper {
-        private final double width_;
-        private final double floor_;
-        private final double logWidth1_;
-        private final double logFloor_;
-
-        /**
-         * Constructor.
-         *
-         * @param  width  multiplicative bin width
-         * @param  phase  bin phase in range 0..1
-         * @param  point    representative point
-         */
-        LogBinMapper( double width, double phase, double point ) {
-            width_ = width;
-            if ( point <= 0 ) {
-                point = 1;
-            }
-            double f0 = Math.floor( log( point ) / log( width ) );
-            floor_ = Math.pow( width, f0 + phase );
-            logFloor_ = log( floor_ );
-            assert (float) Math.abs(logFloor_ - log( point )) <= (float) width;
-            logWidth1_ = 1. / log( width );
-        }
-
-        public int getBinIndex( double value ) {
-            return (int) Math.floor( (log( value ) - logFloor_) * logWidth1_ );
-        }
-
-        public double[] getBinLimits( int index ) {
-            double lo = floor_ * Math.pow( width_, index );
-            return new double[] { lo, lo * width_ };
-        }
-
-        @Override
-        public int hashCode() {
-            int code = 44178;
-            code = 23 * Float.floatToIntBits( (float) width_ );
-            code = 23 * Float.floatToIntBits( (float) floor_ );
-            return code;
-        }
-
-        @Override
-        public boolean equals( Object o ) {
-            if ( o instanceof LogBinMapper ) {
-                LogBinMapper other = (LogBinMapper) o;
-                return this.width_ == other.width_
-                    && this.floor_ == other.floor_;
-            }
-            else {
-                return false;
-            }
-        }
     }
 }
