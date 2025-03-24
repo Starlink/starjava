4bbfe153d8a9ed97ecb784aaa696be5f5f94d382
Mark Taylor <m.b.taylor@bristol.ac.uk>
2025-03-24 16:20:41 +0000

    ttools: fix potential NPE in Tick.toString()

--- ttools/src/main/uk/ac/starlink/ttools/plot2/Tick.java
+++ ttools/src/main/uk/ac/starlink/ttools/plot2/Tick.java
@@ -74,6 +74,8 @@ public class Tick {
 
     @Override
     public String toString() {
-        return value_ + "->\"" + label_.toText() + "\"";
+        return label_ == null
+             ? Double.toString( value_ )
+             : value_ + "->\"" + label_.toText() + "\"";
     }
 }
