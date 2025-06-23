d7dfa239c7f12c38a5918df236210d861f1e3d56
Mark Taylor <m.b.taylor@bristol.ac.uk>
2025-06-23 15:47:41 +0100

    vo: modify AUTO TapMetaPolicy
    Previously, the default TapMetaPolicy used multi-step TAP_SCHEMA
    queries for large TAP services (ones with more than 5,000 columns)
    and single-step VOSI tables queries for small ones.
    
    Following this commit, the default is to use VOSI queries by
    preference, requesting 2-step (tables first, columns on demand)
    for large services and 1-step for small ones.  The small/large
    threshold is also increased to 20,000 columns.  There is also
    provision for falling back to TAP_SCHEMA if the (nominally optional)
    VOSI /tables endpoint is not present.
    
    The old behaviour was just a guess about what would be a good idea.
    I did some experiments and the new behaviour seems to be a bit faster
    for today's services.  The fallback is also more robust.
    The only thing that seems noticeably slower at time of writing is
    single-table queries for VizieR, now using individual-table VOSI
    tables queries; equivalent TAP_SCHEMA queries are a bit faster.
    The initial query for all VizieR tables (column-less) is faster
    using the new VOSI interface than TAP_SCHEMA queries though.
    
    One problem with the new policy is that if the service does not
    honour the VOSI tables request detail=min flag, then even for
    large services we are pulling down the whole metadata document,
    which may be quite large.  Actually most large services do not honour
    this flag, though VizieR does.  At time of writing the largest such
    column-less VOSI tables document is about 20 Mb.  If more of the
    larger services implemented HTTP-level compression this would be
    mitigated.  The largest ones take less than 10 seconds on my machine,
    but it has a fast network connection.
    
    The other driver for this change is that the ESAC TAP services
    (including GACS) at time of writing do not report private tables
    alongside public ones (when authenticated) in TAP_SCHEMA queries,
    but they do in VOSI tables output.
    This is a reported bug, but may not be fixed soon.
    I don't generally like to pander to particular services, but
    (a) the new client behaviour is no less respectable/reasonable
    than the old behaviour and (b) I get funded specifically to make
    topcat work with Gaia services.

--- vo/src/main/uk/ac/starlink/vo/TapMetaPolicy.java
+++ vo/src/main/uk/ac/starlink/vo/TapMetaPolicy.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.function.Supplier;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import uk.ac.starlink.util.ContentCoding;
+import uk.ac.starlink.util.IOFunction;
 
 /**
  * Defines the policy for acquiring TAP metadata from a remote service.
@@ -53,14 +55,10 @@ public abstract class TapMetaPolicy {
     private static final TapMetaPolicy[] KNOWN_VALUES = {
         AUTO = new TapMetaPolicy( "Auto",
                                   "Chooses a suitable place to get table "
-                                + "metadata. "
-                                + "Some services may have custom protocols. "
-                                + "Otherwise, use the VOSI tables endpoint "
-                                + "when there are a moderate number of tables, "
-                                + "or TAP_SCHEMA queries if there are many" ) {
+                                + "metadata depending on the service" ) {
             public TapMetaReader createMetaReader( TapService service,
                                                    ContentCoding coding ) {
-                return createAutoMetaReader( service, coding, 5000 );
+                return createAutoMetaReader( service, coding, 20_000 );
             }
         },
         TAPSCHEMA_C = createTapSchemaPolicy( "TAP_SCHEMA-C", false, true ),
@@ -279,13 +277,38 @@ public abstract class TapMetaPolicy {
                                   TapSchemaInterrogator.COLUMN_QUERIER
                                                        .getTableName() );
 
-        /* If there are more columns than the threshold, read the tables
-         * now and defer column reads until later. */
-        if ( ncol >= 0 && ncol > maxrow ) {
-            logger_.info( "Many columns in TAP service ("
-                        + ncol + " > " + maxrow + "); "
-                        + "use TAP_SCHEMA queries for "
-                        + service.getIdentity() );
+        /* Make decisions about how to populate the metadata based on this.
+         * For a large service, just get the table list first and get
+         * individual column lists as and when they are required;
+         * for a small service, grab all the metadata now. */
+        boolean manyCols = ncol >= 0 && ncol > maxrow;
+        String prefMsg =
+            manyCols ? ( "Many columns in TAP service"
+                       + " (" + ncol + " > " + maxrow + ");"
+                       + " prefer reading tables up front"
+                       + " and columns as required" )
+                     : ( "Not many columns in TAP service"
+                       + " (" + ncol + " <= " + maxrow + "); "
+                       + "prefer reading table and column metadata up front" );
+        logger_.info( prefMsg );
+
+        /* Attempt to read metadata using the VOSI tables endpoint.
+         * This tends to be faster, and in some cases is more reliable,
+         * than TAP_SCHEMA queries.  Note however that if the service
+         * does not honour the DetailMode flag, which many services do not,
+         * we are pulling down the whole metadata document here,
+         * which may be quite large (at time of writing up to about 20Mb). */
+        Vosi11TapMetaReader.DetailMode detailMode =
+            manyCols ? Vosi11TapMetaReader.DetailMode.MIN
+                     : Vosi11TapMetaReader.DetailMode.MAX;
+        TapMetaReader preferredReader =
+            new Vosi11TapMetaReader( service.getTablesEndpoint(), fixer,
+                                     coding, detailMode );
+
+        /* However, the VOSI tables endpoint is optional (though present
+         * on most/all respectable TAP services?), so prepare to fall back
+         * to querying the mandatory TAP_SCHEMA metadata if necessary. */
+        Supplier<TapMetaReader> fallbackReaderSupplier = () -> {
             String linkTableName =
                 TapSchemaInterrogator.LINK_QUERIER.getTableName();
             long nlink = readRowCount( service, linkTableName );
@@ -306,28 +329,15 @@ public abstract class TapMetaPolicy {
                 logger_.info( msg );
             }
             int maxrec = (int) Math.min( Integer.MAX_VALUE,
-                                     Math.max( ncol + 1, nlink + 1 ) );
+                                         Math.max( ncol + 1, nlink + 1 ) );
             boolean popSchema = true;
-            boolean popTable = false;
+            boolean popTable = ! manyCols;
             return new TapSchemaTapMetaReader( service, maxrec, coding,
                                                popSchema, popTable, fixer,
                                                preloadFkeys );
-        }
-        else {
-            logger_.info( "Not excessive column count for TAP service ("
-                        + ncol + " <= " + maxrow + ")" );
-        }
-
-        /* If there are fewer columns than the threshold, or the column
-         * count failed, use the VOSI tables endpoint instead.
-         * The VOSI 1.1 scalable request format is defined in a way
-         * that's backwardly compatible, so VOSI 1.1 should work for
-         * VOSI 1.0 services too. */
-        logger_.info( "Use VOSI-1.1 tables endpoint for "
-                    + service.getIdentity() );
-        return new Vosi11TapMetaReader( service.getTablesEndpoint(),
-                                        fixer, coding,
-                                        Vosi11TapMetaReader.DetailMode.MAX );
+        };
+        return new FallbackTapMetaReader( preferredReader,
+                                          fallbackReaderSupplier );
     }
 
     /**
@@ -355,5 +365,95 @@ public abstract class TapMetaPolicy {
             return -1;
         }
         return nrow.longValue();
+    }
+
+    /**
+     * TapMetaReader implementation that uses a preferred method if it can,
+     * but if that fails on the first attempt to use it, falls back
+     * to a secondary one.
+     */
+    private static class FallbackTapMetaReader implements TapMetaReader {
+        private TapMetaReader reader_;
+        private Boolean isWorking_;
+        final Supplier<TapMetaReader> fallbackReaderSupplier_;
+
+        /**
+         * Constructor.
+         *
+         * @param  preferredReader   preferred reader instance
+         * @param  fallbackReaderSupplier  supplier for backup instance
+         */
+        FallbackTapMetaReader( TapMetaReader preferredReader,
+                               Supplier<TapMetaReader> fallbackReaderSupplier ){
+            reader_ = preferredReader;
+            fallbackReaderSupplier_ = fallbackReaderSupplier;
+        }
+
+        public String getMeans() {
+            return reader_.getMeans();
+        }
+        public String getSource() {
+            return reader_.getSource();
+        }
+        public SchemaMeta[] readSchemas() throws IOException {
+            return fallbackRead( rdr -> rdr.readSchemas() );
+        }
+        public TableMeta[] readTables( SchemaMeta schema ) throws IOException {
+            return fallbackRead( rdr -> rdr.readTables( schema ) );
+        }
+        public ColumnMeta[] readColumns( TableMeta table ) throws IOException {
+            return fallbackRead( rdr -> rdr.readColumns( table ) );
+        }
+        public ForeignMeta[] readForeignKeys( TableMeta table )
+                throws IOException {
+            return fallbackRead( rdr -> rdr.readForeignKeys( table ) );
+        }
+
+        /**
+         * Does the work of reading and if necessary falling back to the
+         * backup implementation.
+         */
+        private <T> T[] fallbackRead( IOFunction<TapMetaReader,T[]> readFunc )
+                throws IOException {
+            TapMetaReader rdr;
+            String msg;
+
+            /* On the first occasion only when a read is attempted,
+             * prepare to fall back.  If it's already worked once,
+             * the issue is probably something wrong that falling back
+             * may not fix.  This has to be syncronized to guaranteed that
+             * two first calls don't overlap, but in expected usage
+             * scenarios concurrent calls wouldn't occur. */
+            synchronized ( this ) {
+                if ( isWorking_ == null ) {
+                    try {
+                        T[] result = readFunc.apply( reader_ );
+                        isWorking_ = Boolean.TRUE;
+                        msg = null;
+                        return result;
+                    }
+                    catch ( IOException e ) {
+                        String means1 = reader_.getMeans();
+                        reader_ = fallbackReaderSupplier_.get();
+                        String means2 = reader_.getMeans();
+                        msg = "TAP metadata read failure from " + means1 
+                            + "; fallback to " + means2;
+                        isWorking_ = Boolean.FALSE;
+                    }
+                }
+                else {
+                    msg = null;
+                }
+                rdr = reader_;
+            }
+            if ( msg != null ) {
+                logger_.warning( msg );
+            }
+
+            /* If we get here, either the first call failed and we fell back,
+             * or it's not the first call.  Either way, run it and succeed
+             * or fail as usual. */
+            return readFunc.apply( rdr );
+        }
     }
 }
