227787098f0b774e54c2c70fee59b8509b6abcf6
Mark Taylor <m.b.taylor@bristol.ac.uk>
2026-02-04 14:25:01 +0000

    table: CustomLabelUI source file cosmetics
    Apply house style to contributed CustomLabelUI file.
    Mostly whitespace jiggling.

--- table/src/main/uk/ac/starlink/table/gui/CustomLabelUI.java
+++ table/src/main/uk/ac/starlink/table/gui/CustomLabelUI.java
@@ -14,17 +14,16 @@ import javax.swing.plaf.basic.BasicLabelUI;
  * This is used to change the location of the ellipsis when a string is
  * rendered in a cell that cannot fit. By default, the ellipsis is always at
  * the end of the string, but this class instead places the ellipsis in the
- * middle of the string. See
- *
- *     https://docs.oracle.com/javase/8/docs/api/javax/swing/plaf/basic/BasicLabelUI.html
+ * middle of the string.
  *
  * @author   Fergus Baker
  * @since    03 Feb 2026
+ * @see      javax.swing.plaf.basic.BasicLabelUI
  */
 public class CustomLabelUI extends BasicLabelUI {
     private static final String ELLIPSES = "...";
     private static final int ELLIPSES_LEN = ELLIPSES.length();
-    private String displayedText;
+    private String displayedText_;
 
     /**
      * Implicit constructor, called by Swing with a particular component in
@@ -40,17 +39,16 @@ public class CustomLabelUI extends BasicLabelUI {
      * @return   String
      */
     public String getText() {
-        return displayedText;
+        return displayedText_;
     }
 
-    private int findTruncationSplit(int maxWidth, FontMetrics fontMetrics,
-            String text) {
+    private int findTruncationSplit( int maxWidth, FontMetrics fontMetrics,
+                                     String text ) {
         int textLength = text.length();
-        for (int i = 1; i <= textLength; i++) {
-            int subTextWidth = fontMetrics.stringWidth(
-                    text.substring( textLength - i )
-            );
-            if (subTextWidth > maxWidth) {
+        for ( int i = 1; i <= textLength; i++ ) {
+            int subTextWidth =
+                fontMetrics.stringWidth( text.substring( textLength - i ) );
+            if ( subTextWidth > maxWidth ) {
                 return textLength - i + 1;
             }
         }
@@ -58,46 +56,42 @@ public class CustomLabelUI extends BasicLabelUI {
     }
 
     @Override
-    protected String layoutCL(JLabel label, FontMetrics fontMetrics,
-                              String text, Icon icon, Rectangle viewR,
-                              Rectangle iconR, Rectangle textR) {
+    protected String layoutCL( JLabel label, FontMetrics fontMetrics,
+                               String text, Icon icon, Rectangle viewR,
+                               Rectangle iconR, Rectangle textR ) {
+
         /* Draw the text with the usual layout for a compound label. */
-        displayedText = super.layoutCL(label, fontMetrics, text, icon, viewR,
-                                       iconR, textR);
+        displayedText_ = super.layoutCL( label, fontMetrics, text, icon, viewR,
+                                         iconR, textR );
 
         /* Sometimes the font metrics is null if the text is being displayed
          * before the UI is ready. In such a case, there is nothing that this
          * class needs to do. */
-        if (fontMetrics == null) {
-            return displayedText;
+        if ( fontMetrics == null ) {
+            return displayedText_;
         }
 
-        int displayedLength = displayedText.length();
 
         /* Check if a truncation occurred, and if the text is long enough. If
-         * the text is too short, there is no point in truncating differently.
-         * */;
-        if (!displayedText.equals(text) && displayedLength > 2 * ELLIPSES_LEN) {
-            int splitLength = (displayedLength + ELLIPSES_LEN) / 2 - ELLIPSES_LEN;
-            String beforeEllipses = displayedText.substring(0, splitLength);
+         * the text is too short, there is no point in truncating differently.*/
+        int displayedLength = displayedText_.length();
+        if ( !displayedText_.equals( text ) &&
+             displayedLength > 2 * ELLIPSES_LEN ) {
+            int splitLength =
+                ( displayedLength + ELLIPSES_LEN ) / 2 - ELLIPSES_LEN;
+            String beforeEllipses = displayedText_.substring( 0, splitLength );
+
             /* Determine the maximum width (in pixels) that the remaining text
              * should fit into. */
-            int displayedTextWidth = fontMetrics.stringWidth(displayedText);
-            int maxWidth = (
-                    displayedTextWidth
-                    - fontMetrics.stringWidth(ELLIPSES)
-                    - fontMetrics.stringWidth(beforeEllipses)
-            );
-
-            int splitIndex = findTruncationSplit(
-                    maxWidth, fontMetrics, text
-            );
-
-            displayedText = (
-                    beforeEllipses + ELLIPSES + text.substring(splitIndex)
-            );
+            int displayedTextWidth = fontMetrics.stringWidth( displayedText_ );
+            int maxWidth = displayedTextWidth
+                         - fontMetrics.stringWidth( ELLIPSES )
+                         - fontMetrics.stringWidth( beforeEllipses );
+            int splitIndex = findTruncationSplit( maxWidth, fontMetrics, text );
+            displayedText_ =
+                beforeEllipses + ELLIPSES + text.substring( splitIndex );
         }
 
-        return displayedText;
+        return displayedText_;
     }
 }
