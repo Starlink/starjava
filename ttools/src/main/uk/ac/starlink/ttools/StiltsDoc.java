c0c75f09268deb5dae4fa7bca91b400315058bad
Mark Taylor <m.b.taylor@bristol.ac.uk>
2026-02-12 09:37:48 +0000

    ttools: build separate sun256.jar file
    The runtime-available user document classes (SUN/256 HTML files)
    are now packaged in a separate jar file sun256.jar rather than
    in the ttools.jar file.  This does not affect operation, but it's
    done for the convenience of third-party deployers who want the
    library classes but not a bundled copy of the (reasonably large)
    documentation files.

---
+++ ttools/src/main/uk/ac/starlink/ttools/StiltsDoc.java
@@ -0,0 +1,14 @@
+package uk.ac.starlink.ttools;
+
+/**
+ * Marker class, used to locate documentation resources.
+ * It must be kept with (for instance in the same jar file as)
+ * the SUN/256 HTML documentation files.
+ *
+ * @author   Mark Taylor
+ * @since    12 Feb 2026
+ */
+public final class StiltsDoc {
+    private StiltsDoc() {
+    }
+}
