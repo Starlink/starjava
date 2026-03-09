ce8f2877e68436a1f563b03097c47c6d5a8a6824
Mark Taylor <m.b.taylor@bristol.ac.uk>
2026-03-09 15:10:10 +0000

    ttools: add STMOC/TMOC tests for MocShape
    Add unit tests for the new STMOC and TMOC functionality in
    MocShapeMode.  These are really regression tests, but the
    (hopefully) correct comparison values were taken from output of
    MOCpy 0.19.1, generated using the following code:
    
    from mocpy import MOC, TimeMOC, STMOC
    import pandas
    import astropy.units as u
    from astropy.time import Time
    
    def calc_mocs(stcat, order=1, torder=20):
       ra = stcat["ra"].values*u.deg
       dec = stcat["dec"].values*u.deg
       initimes = stcat["initime"].astype(str).tolist()
       endtimes = stcat["endtime"].astype(str).tolist()
       t0 = Time(initimes, format="isot", scale="utc")
       t1 = Time(endtimes, format="isot", scale="utc")
       stmoc = STMOC.from_time_ranges_positions(
          spatial_depth=order, lon=ra, lat=dec,
          time_depth=torder, times_start=t0, times_end=t1,
       )
       tmoc = stmoc.query_by_space(MOC.new_empty(0).complement())
       smoc = stmoc.query_by_time(TimeMOC.new_empty(0).complement())
       print("\norder: %d, torder: %d\n----------" % (order, torder))
       print("TMOC:\n%s \n\nSMOC:\n%s \n\nSTMOC:\n%s" % (tmoc, smoc, stmoc ))
    
    gtc = pandas.read_csv("gtc1.csv")
    gtc = gtc.sort_values("initime")  # bug in MOCpy 0.19.1 requires this
    calc_mocs(gtc)
    calc_mocs(gtc, order=0, torder=15)
    
    Raul Gutierrez (SVO) was helpful in supplying the basis of the above
    code, the test data, and help identifying issues with MOCpy.

--- ttools/src/testcases/uk/ac/starlink/ttools/moc/MocTest.java
+++ ttools/src/testcases/uk/ac/starlink/ttools/moc/MocTest.java
@@ -7,7 +7,11 @@ import java.util.logging.Level;
 import junit.framework.TestCase;
 import uk.ac.starlink.table.StarTable;
 import uk.ac.starlink.table.StarTableFactory;
+import uk.ac.starlink.table.TimeMapper;
 import uk.ac.starlink.task.TaskException;
+import uk.ac.starlink.ttools.AreaDomain;
+import uk.ac.starlink.ttools.mode.MocShapeMode;
+import uk.ac.starlink.ttools.mode.TimeIntervalType;
 import uk.ac.starlink.ttools.task.MapEnvironment;
 import uk.ac.starlink.ttools.task.MocShape;
 import uk.ac.starlink.util.LogUtils;
@@ -58,6 +62,74 @@ public class MocTest extends TestCase {
             new MocShape().createExecutable( env3 ).execute();
             assertEquals( "0/0-4 8-11", env3.getOutputText().trim() );
         }
+    }
+
+    public void testStmoc() throws Exception {
+
+        // This is a regression test, and not a particularly good one,
+        // the data does not attempt to probe any interesting cases,
+        // except by accident?
+        // The result it's testing against is output from MOCpy (0.19.1);
+        // that's known to be buggy at the version I used in some
+        // circumstances (sensitive to the order of input values,
+        // see https://github.com/cds-astro/mocpy/issues/206),
+        // but probably it's doing the right thing for this data;
+        // anyhow it agrees with mocshape, though note that some of
+        // the code being compared originates in the same place
+        // (CDS, not independent implementation).
+        MapEnvironment stenv = new MapEnvironment();
+        stenv.setValue( "moctype", MocShapeMode.MocType.STMOC );
+        stenv.setValue( "in",
+                        MocTest.class.getResource( "gtc1.csv" ).toString() );
+        stenv.setValue( "order", Integer.valueOf( 1 ) );
+        stenv.setValue( "shape", AreaDomain.POINT_MAPPER );
+        stenv.setValue( "coords", "array(ra,dec)" );
+        stenv.setValue( "torder", Integer.valueOf( 20 ) );
+        stenv.setValue( "tshape", TimeIntervalType.RANGE );
+        stenv.setValue( "t0", "initime" );
+        stenv.setValue( "t1", "endtime" );
+        new MocShape().createExecutable( stenv ).execute();
+        String sttxt = stenv.getOutputText().trim().replaceAll( "\\s+", " " );
+        assertEquals( "t20/96468 s1/7 t20/96509 s1/27 t20/96546 s1/11 "
+                    + "t20/96575 s1/17 22 t20/96584 s1/7 t20/96587 s1/12 "
+                    + "t20/96588 s1/0 t20/96593 s1/2 t20/96674 s1/28",
+                      sttxt );
+
+        MapEnvironment stenv1 = new MapEnvironment( stenv );
+        stenv1.setValue( "order", Integer.valueOf( 0 ) );
+        stenv1.setValue( "torder", Integer.valueOf( 15 ) );
+        new MocShape().createExecutable( stenv1 ).execute();
+        String sttxt1 = stenv1.getOutputText().trim().replaceAll( "\\s+", " " );
+        assertEquals( "t15/3014 s0/1 t15/3015 s0/6 t15/3017 s0/2 4-5 "
+                    + "t15/3018 s0/0-1 3 t15/3021 s0/7",
+                      sttxt1 );
+
+        MapEnvironment tenv = new MapEnvironment( stenv );
+        tenv.setValue( "moctype", MocShapeMode.MocType.TMOC );
+        new MocShape().createExecutable( tenv ).execute();
+        String ttxt = tenv.getOutputText().trim().replaceAll( "\\s+", " " );
+        assertEquals( "20/96468 96509 96546 96575 96584 96587-96588 "
+                    + "96593 96674",
+                      ttxt );
+
+        MapEnvironment tenv1 = new MapEnvironment( tenv );
+        tenv1.setValue( "torder", Integer.valueOf( 15 ) );
+        tenv1.setValue( "ttype", TimeMapper.ISO_8601 );
+        new MocShape().createExecutable( tenv1 ).execute();
+        String ttxt1 = tenv1.getOutputText().trim().replaceAll( "\\s+", " " );
+        assertEquals( "14/1507 15/3017-3018 3021", ttxt1 );
+
+        MapEnvironment senv = new MapEnvironment( stenv );
+        senv.setValue( "moctype", MocShapeMode.MocType.SMOC );
+        new MocShape().createExecutable( senv ).execute();
+        String stxt = senv.getOutputText().trim().replaceAll( "\\s+", " " );
+        assertEquals( "1/0 2 7 11-12 17 22 27-28", stxt );
+
+        MapEnvironment senv1 = new MapEnvironment( senv );
+        senv1.setValue( "order", Integer.valueOf( 0 ) );
+        new MocShape().createExecutable( senv1 ).execute();
+        String stxt1 = senv1.getOutputText().trim().replaceAll( "\\s+", " " );
+        assertEquals( "0/0-7", stxt1 );
     }
 
     private MocImpl[] getMocImpls() {
